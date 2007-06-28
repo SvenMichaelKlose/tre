c560042e5ae7b02f6cd5c05c38a8e43c59b6f132
Sven Michael Klose <pixel@hugbox.org>
2007-06-28 14:16:12 +0000

    <empty commit message>

--- make-crunsh.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-cd interpreter
-cat *.c >_tmp.c
-gcc -DLISP_VERBOSE_LOAD -fwhole-program -O3 -fomit-frame-pointer -ffast-math -lc -lm $1 $2 $3 $4 $5 $6 $7 $8 $9 -pipe -o ../lisp _tmp.c
-rm _tmp.c
-cd ..
-strip lisp
