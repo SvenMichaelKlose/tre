c990c0ad58cb0a790dcf658c79fd5b83169a0649
Sven Michael Klose <pixel@hugbox.org>
2007-09-10 14:25:23 +0000

    Cleanup.

--- environment/stage3/hash.lisp
+++ environment/stage3/hash.lisp
@@ -16,7 +16,7 @@
 (defun make-hash-table (&key test size rehash-size rehash-threshold)
   "Create a new hash table."
   (make-%hash-table
-    :test (or test eq) :size 157
+    :test (or test #'eq) :size 157
     :rehash-size rehash-size :rehash-threshold rehash-threshold
     :hash (make-array 157)))
 
@@ -26,8 +26,8 @@
 
 (defun %make-hash-index-string (h str)
   "Make hash index from string."
-  (let ((k 0)
-	(l (length str)))
+  (with (k 0
+	     l (length str))
     (do ((i 0 (1+ i)))
         ((or (= i l) (> i 7)) (mod k (%hash-table-size h)))
       (setf k (+ k (elt str i))))))
@@ -41,8 +41,8 @@
 
 ; Get bucket list and its index.
 (defmacro %with-hash-bucket (bucket idx h key &rest body)
-  `(let* ((,idx (%make-hash-index ,h ,key))
-	  (,bucket (aref (%hash-table-hash ,h) ,idx)))
+  `(with (,idx (%make-hash-index ,h ,key)
+	      ,bucket (aref (%hash-table-hash ,h) ,idx))
     ,@body))
 
 (defun gethash (key h &optional default)
@@ -51,11 +51,11 @@
     (assoc key b :test (%hash-table-test h))))
 
 (defun (setf gethash) (new-value key h &optional default)
-  (let ((t (%hash-table-test h)))
+  (with (tst (%hash-table-test h))
     (%with-hash-bucket b i h key
-      (if (assoc key b :test t)
+      (if (assoc key b :test tst)
         ; Modify existing value.
-        (setf (assoc key b :test t) new-value)
+        (setf (assoc key b :test tst) new-value)
         ; Add new value/key pair.
         (setf (aref (%hash-table-hash h) i) (acons key new-value b)))))
   new-value)
