f652ae536d2cc08d4e5e325efbcb501d0081515f
Sven Michael Klose <pixel@hugbox.org>
2007-11-15 19:59:33 +0000

    TREE-WALK: Returns a copy of the traversed tree.

--- lib/tree-walk.lisp
+++ lib/tree-walk.lisp
@@ -3,14 +3,15 @@
 ;;;; Copyright (c) 2005-2007 Sven Klose <pixel@copei.de>
 
 ;; Replace elements in tree.
-(defun tree-walk (tree &key ascending dont-ascend-if)
-  (when (consp tree)
-    (do ((i tree (cdr i)))
-         ((atom i))
-      (when ascending
-	(funcall ascending i))
-          (when (and (not (and dont-ascend-if
-                               (funcall dont-ascend-if i)))
-		     (consp i))
-            (tree-walk (car i) :ascending ascending
-			       :dont-ascend-if dont-ascend-if)))))
+(defun tree-walk (i &key (ascending nil) (dont-ascend-if nil))
+  (if (atom i)
+	i
+	(with (a (if (and dont-ascend-if (funcall dont-ascend-if (car i)))
+				 (car i)
+	  			 (tree-walk
+					 (if ascending
+					 	 (funcall ascending (car i))
+					 	 (car i))
+					 :ascending ascending :dont-ascend-if dont-ascend-if)))
+	  (cons a
+	  		(tree-walk (cdr i) :ascending ascending :dont-ascend-if dont-ascend-if)))))
