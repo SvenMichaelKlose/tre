eeccf026964319b0229804fcfef5f3138d20e58c
Sven Michael Klose <pixel@hugbox.org>
2008-01-05 15:11:41 +0000

    Rewritten. Copies and NCONCs, uses multiple return values.

--- compiler/expression-expand.lisp
+++ compiler/expression-expand.lisp
@@ -1,72 +1,56 @@
-;;;;; nix operating system project
-;;;;; lisp compiler
-;;;;; Copyright (c) 2006-2007 Sven Klose <pixel@copei.de>
-;;;;;
-;;;;; Expression expansion.
-;;;;;
-;;;;; Expand nested expressions into a sequence of instructions assigning
-;;;;; unique variables. VM-SCOPEs are merged in this process.
+;;;;; nix operating system project ;;;;; lisp compiler
+;;;;; Copyright (c) 2006-2008 Sven Klose <pixel@copei.de>
+;;;;; 
+;;;;; Breaks up nested expressions into lists of assignments.
 
 (defvar *expexsym-counter* 0)
 
 ;; Returns newly created, unique symbol.
-(defun expexsym ()
+(defun expex-sym ()
   (setf *expexsym-counter* (+ 1 *expexsym-counter*))
   (make-symbol (string-concat "~E" (string *expexsym-counter*))))
 
-(defun expex-vmscope (q expr)
-  "Merge in VMSCOPE."
-  (with (e (expex-expand-body (cdr expr)))
-	; If the scope contains more than one expression, place return
-	; value in new variable and replace scope by the variable.
-    (aif (butlast e)
-      (with (s (expexsym))
-        (enqueue-many q (nconc ! `((,s ,(car (last e))))))
-        s)
-      (car e))))
+(defun expex-vmscope (x)
+  (with (e (expex-body (cdr x))
+    	 s (expex-sym))
+    (cons (aif (butlast e)
+			   (nconc ! `((,s ,(car (last e)))))
+               `((,s ,(car e))))
+		  s)))
 
-(defun expex-make-assignment (expr)
-  "Makes a single statement assigment."
-  (with (s (expexsym))
-    (enqueue q `(,s ,expr))
-    s))
+(defun expex-assignment (x)
+  (if (atom x)
+	  (cons nil x)
+  	  (if (vm-scope? x)
+	      (expex-vmscope x)
+  	      (with (s (expex-sym)
+				 (pre main) (expex-toplevel x))
+    	    (cons (nconc pre `((,s ,@main)))
+		  	      s)))))
 
-(defun expex-expand (expr q)
-  "Expand argument."
-  (if (atom expr)
-    expr
-    (if (vm-scope? expr)
-        (expex-vmscope q expr) ; Make VMSCOPEs disappear.
-        (with-cons a d expr
-          (if (in? a '%funref 'vm-go 'vm-go-nil '%stack 'quote)
-              expr
-              (expex-make-assignment (cons a (expex-expand-args d q))))))))
+(defun expex-args (x)
+  (with ((pre main) (assoc-splice (mapcar #'expex-assignment (cdr x))))
+    (values (apply #'nconc pre)
+			main)))
 
-(defun expex-expand-args (args q)
-  "Move arguments to new assignments."
-  (mapcar #'((a)
-			   (expex-expand a q))
-          args))
+(defun expex-able? (x)
+  (not (or (atom x)
+           (in? (car x) '%funref 'vm-go 'vm-go-nil '%stack 'quote))))
 
-(defun force-setq (x)
-  (if (vm-go? x)
-	  x
-	  (list (expexsym) x)))
+(defun expex-expr (x)
+  (with ((pre newargs) (expex-args x))
+    (values pre (list (if (eq (car x) '%setq)
+						  newargs
+						  (cons (car x) newargs))))))
 
-(defun expex-expand-toplevel (expr q)
-  "Expand an expression or return atom as is."
-  (if (consp expr)
-      (enqueue q (cons (car expr) (expex-expand-args (cdr expr) q)))
-   	  (enqueue q expr)))
+(defun expex-toplevel (x)
+  (if (not (expex-able? x))
+	  (values nil (list x))
+  	  (if (vm-scope? x)
+	      (values nil (expex-body (cdr x)))
+	      (expex-expr x))))
 
-(defun expex-expand-body (body)
-  "Expand list of expressions."
-  (when body
-    (if (vm-scope? body)
-      (expex-expand-body (cdr body))
-      (with-queue q
-        (dolist (e body)
-          (if (vm-scope? e)
-            (enqueue-many q (expex-expand-body (cdr e)))
-            (expex-expand-toplevel e q)))
-        (queue-list q)))))
+(defun expex-body (x)
+  (when x
+     (with ((pre main) (expex-toplevel (car x)))
+       (nconc pre main (expex-body (cdr x))))))
