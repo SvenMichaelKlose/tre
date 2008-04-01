29ce1cffd568c5a6718819a5febfd8bcb8d81d51
Sven Michael Klose <pixel@hugbox.org>
2008-04-01 19:59:00 +0000

    WITH: Can now assign local functions.

--- environment/stage2/with.lisp
+++ environment/stage2/with.lisp
@@ -1,6 +1,6 @@
 ;;;; nix operating system project
 ;;;; list processor environment
-;;;; Copyright (C) 2005-2007 Sven Klose <pixel@copei.de>
+;;;; Copyright (C) 2005-2008 Sven Klose <pixel@copei.de>
 
 (defmacro with (alst &rest body)
   (labels ((sub ()
@@ -10,5 +10,10 @@
     (let ((plc (car alst))
           (val (cadr alst)))
       (if (consp plc)
-          `(multiple-value-bind ,plc ,val ,@(sub))
-          `(let ((,plc ,val)) ,@(sub))))))
+          `(multiple-value-bind ,plc ,val
+			 ,@(sub))
+		  (if (and (consp val) (eq (car val) 'FUNCTION))
+			  `(labels ((,plc ,(second val)))
+				 ,@(sub))
+          	  `(let ((,plc ,val))
+				 ,@(sub)))))))
