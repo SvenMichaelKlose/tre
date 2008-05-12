246c56b70900b7b94b66358016b3b7da5d1fd1c5
Sven Michael Klose <pixel@hugbox.org>
2008-05-12 10:55:46 +0000

    MEMBER: Move to first stage.

--- environment/stage1/member.lisp
+++ environment/stage1/member.lisp
@@ -1,109 +1,22 @@
 ;;;; nix operating system project
 ;;;; list processor environment
-;;;; Copyright (C) 2005-2006 Sven Klose <pixel@copei.de>
-;;;;
-;;;; Searching sequences
+;;;; Copyright (C) 2005-2006,2008 Sven Klose <pixel@copei.de>
 
-(defmacro xchg (a b)
-  "Swaps values of the arguments."
-  (with-gensym g
-    `(setf ,g ,a
-	   ,a ,b
-	   ,b ,g)))
+(defun %member-r (elm lst)
+  (if lst
+    (or (equal elm (car lst))
+        (%member-r elm (cdr lst)))))
 
-(defmacro with-find-parameters (&rest body)
-  `(let ((e (or end (1- (length seq))))
-	 (s (or start 0))
-	 (tst (or test
-		  (if test-not
-		    #'((x y)
-		        (not (funcall test-not x y))))
-		  #'eql)))
-    ; Make sure the start and end indices are sane.
-    (if (or (and (> s e) (not from-end))
-            (and (< s e) from-end))
-      (xchg s e))
-    (do ((i s (if from-end (1- i) (1+ i))))
-        ((if from-end
-	  (< i e) (> i e)))
-      (when seq
-        ,@body))))
- 
-(defun find (val seq &key start end from-end test test-not)
-  "Return element in sequence."
-    (with-find-parameters
-      (let ((el (elt seq i)))
-        (when (funcall tst val el)
-          (return el)))))
+(defun member (elm &rest lsts)
+  "Test if object is a member of any of the pure lists."
+  (or (%member-r elm (car lsts))
+      (if (cdr lsts)
+          (apply #'member elm (cdr lsts)))))
 
-(define-test "FIND finds elements"
-  ((find 's '(l i s p)))
-  's)
-
-(define-test "FIND accepts :FROM-END"
-  ((find 's '(l i s p) :from-end t))
-  's)
-
-(define-test "FIND accepts :END"
-  ((find 's '(l i s p) :end 1))
-  nil)
-
-(define-test "FIND accepts :START"
-  ((find 'l '(l i s p) :start 1))
-  nil)
-
-(define-test "FIND accepts :START, :END, :FROM-END"
-  ((find 'l '(l i s p) :start 1 :end 2 :from-end 1))
-  nil)
-
-(defun find-if (pred seq &key start end from-end)
-  "Return first element in sequence that matches the predicate function."
-  (let ((e (or end (1- (length seq))))
-	(s (or start 0)))
-    ; Make sure the start and end indices are sane.
-    (if (or (and (> s e) (not from-end))
-            (and (< s e) from-end))
-      (xchg s e))
-    (do ((i s (if from-end (1- i) (1+ i))))
-        ((if from-end
-	   (< i e) (> i e)))
-      (let ((el (elt seq i)))
-        (when (funcall pred el)
-          (return el))))))
-
-(define-test "FIND-IF finds elements"
-  ((find-if #'numberp '(l i 5 p)))
-  5)
-
-(defun position (val seq &key start end from-end test test-not)
-  (with-find-parameters
-    (when (funcall tst val (elt seq i))
-      (return i))))
-
-(define-test "POSITION works"
-  ((position 's '(l i s p)))
-  2)
-
-(defun some (pred &rest seqs)
-  "OR predicate over list elements."
-  (dolist (seq seqs)
-    (dotimes (i (length seq) nil)
-      (when (funcall pred (elt seq i))
-        (return-from some t)))))
-
-(define-test "SOME works"
-  ((and (some #'numberp '(a b 3)))
-        (not (some #'numberp '(a b c))))
+(define-test "MEMBER finds elements"
+  ((member 's '(i) '(l i k e) '(l i s p)))
   t)
 
-(defun every (pred &rest seqs)
-  "AND predicate over list elements."
-  (dolist (seq seqs)
-    (dotimes (i (length seq) t)
-      (unless (funcall pred (elt seq i))
-        (return-from every nil)))))
-
-(define-test "EVERY works"
-  ((and (every #'numberp '(1 2 3)))
-        (not (every #'numberp '(1 2 a))))
-  t)
+(define-test "MEMBER detects foureign elements"
+  ((member 'A '(l i s p)))
+  nil)
