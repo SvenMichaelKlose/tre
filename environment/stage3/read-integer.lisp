2d0093e03b610fa657241d6c2919a0cb4d041e1f
Sven Michael Klose <pixel@hugbox.org>
2008-05-13 16:55:17 +0000

    READ-LINE: Double definition removed.

--- environment/stage3/read-integer.lisp
+++ environment/stage3/read-integer.lisp
@@ -1,15 +1,6 @@
 ;;;; TRE environment
 ;;;; Copyright (c) 2008 Sven Klose <pixel@copei.de>
 
-(defun read-line (&optional (str *standard-input*))
-  "Read line from string."
-  (with-default-stream str
-    (with-queue q
-       (do ((c (read-char str) (read-char str)))
-           ((or (= c 10) (= c 13) (end-of-file str))
-            (return-from read-line (list-string (queue-list q))))
-         (enqueue q c)))))
-
 (defun read-integer (&optional (str *standard-input*))
   (with (rec	#'(()
 				    (if (digit-char-p (peek-char str))
