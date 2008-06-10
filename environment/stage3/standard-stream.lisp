bed2c2ae4893d87e603ade0a6031c7947ca6e59f
Sven Michael Klose <pixel@hugbox.org>
2008-06-10 10:23:06 +0000

    END-OF-FILE: Now works with *STANDARD-INPUT*.

--- environment/stage3/standard-stream.lisp
+++ environment/stage3/standard-stream.lisp
@@ -7,7 +7,7 @@
 (defun make-standard-stream ()
   (make-stream :fun-in #'((str) (%read-char nil))
 	       :fun-out #'((c str) (%princ c nil))
-	       :fun-eof #'((str) nil)))
+	       :fun-eof #'((str) (%feof nil))))
 
 (defvar *standard-output* (make-standard-stream))
 (defvar *standard-input* (make-standard-stream))
