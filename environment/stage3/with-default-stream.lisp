905c1ebb102eb2f671ad7bdf0f27b10af8b336f5
Sven Michael Klose <pixel@hugbox.org>
2008-07-09 04:53:54 +0000

    Fix gensym assignments.

--- environment/stage3/with-default-stream.lisp
+++ environment/stage3/with-default-stream.lisp
@@ -1,16 +1,18 @@
 ;;;; nix operating system project
 ;;;; list processor environment
-;;;; Copyright (C) 2005-2006 Sven Klose <pixel@copei.de>
+;;;; Copyright (C) 2005-2006,2008 Sven Klose <pixel@copei.de>
 
-(defmacro with-default-stream (str &rest body)
-  "Set 'str' to *standard-output* if 'str' is T or create string-stream
-   if 'str' is NIL, evaluate 'body' and return the stream-string if 'str'
-   is NIL."
-  (let ((g (gensym)))
-    `(let ((,g str))
-      (cond
-        ((eq ,str t) (setq ,str *standard-output*))
-        ((eq ,str nil) (setq ,str (make-string-stream))))
-      ,@body
-      (unless ,g
-        (get-stream-string ,str)))))
+(defmacro with-default-stream (nstr str &rest body)
+"Set 'str' to *standard-output* if 'str' is T or create string-stream
+if 'str' is NIL, evaluate 'body' and return the stream-string if 'str'
+is NIL."
+  (with-gensym (g ng)
+    `(with (,g ,str
+			,nstr nil)
+	   (setq ,nstr (cond
+        		     ((eq ,g t)		*standard-output*)
+        		     ((eq ,g nil)	(make-string-stream))
+				     (t				,g)))
+       ,@body
+       (unless ,g
+         (get-stream-string ,nstr)))))
