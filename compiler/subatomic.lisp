2a075a1e57fd9dd28f74dcee714cb4514899ffbf
Sven Michael Klose <pixel@hugbox.org>
2008-07-09 05:18:27 +0000

    Own file for subatomic predicates.

---
+++ compiler/subatomic.lisp
@@ -0,0 +1,18 @@
+;;;; nix operating system project
+;;;; lisp compiler
+;;;; Copyright (c) 2006-2007 Sven Klose <pixel@copei.de>
+
+(defun vm-scope? (e)
+  (and (consp e) (eq 'VM-SCOPE (car e))))
+
+(defun vm-go? (e)
+  (and (consp e) (eq 'VM-GO (car e))))
+
+(defun vm-go-nil? (e)
+  (and (consp e) (eq 'VM-GO-NIL (car e))))
+
+(defun vm-jump? (e)
+  (and (consp e) (in? (car e) 'VM-GO 'VM-GO-NIL)))
+
+(defun vm-scope-body (x)
+  (cdr x))
