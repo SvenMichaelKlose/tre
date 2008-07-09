10decdec4224b53f6ca51d102803ca909c344d48
Sven Michael Klose <pixel@hugbox.org>
2008-07-09 05:00:10 +0000

    *STANDARD-INPUT*, *STANDARD-OUTPUT*: Now constant.

--- environment/stage3/standard-stream.lisp
+++ environment/stage3/standard-stream.lisp
@@ -9,5 +9,5 @@
 	       :fun-out #'((c str) (%princ c nil))
 	       :fun-eof #'((str) (%feof nil))))
 
-(defvar *standard-output* (make-standard-stream))
-(defvar *standard-input* (make-standard-stream))
+(defconstant *standard-output* (make-standard-stream))
+(defconstant *standard-input* (make-standard-stream))
