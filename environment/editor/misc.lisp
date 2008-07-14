81bb965402f6679a0795a4aa5d6c63549de4f9a1
Sven Michael Klose <pixel@hugbox.org>
2008-07-14 18:00:03 +0000

    Added missing file.

---
+++ environment/editor/misc.lisp
@@ -0,0 +1,44 @@
+;;;;; TRE environment - editor
+;;;;; Copyright (c) 2008 Sven Klose <pixel@copei.de>
+;;;;;
+;;;;; Miscellaneous.
+
+(defmacro compose (&rest function-list)
+  "Combine functions into one. All with one argument."
+  (with (rec #'((l)
+				  `(,(car l) ,(if (cdr l)
+				   				  (rec (cdr l))
+				   				  'x))))
+    `#'((x)
+		  ,(rec function-list))))
+
+;(define-test "COMPOSE"
+;  ((compose a b))
+;  #'((x) (a (b x))))
+
+(defun $ (&rest args)
+  "Converts arguments to strings, concatenates them and makes a symbol."
+  (make-symbol (apply #'string-concat (mapcar (compose string-upcase string) args))))
+
+(define-test "$"
+  (($ "dollar-" 'test))
+  'dollar-test)
+
+(defmacro repeat (n &rest body)
+  "Execute body n times. See also DOTIMES."
+  `(dotimes (,(gensym) ,n)
+	 ,@body))
+
+(defmacro define-template-macro (name head &optional tail)
+   "Define simple head/tail template."
+  `(defmacro ,name (n args &rest b)
+ 	 (with (description (and b (consp b) (stringp (first b)) (first b))
+			body (if description (cdr b) b))
+       `(defun ,,n ,,args
+		  ,,@(list description)
+          ,@head
+          ,,@body
+          ,@tail))))
+
+(defmacro define-tail-macro (name tail)
+  `(define-template-macro ,name nil ,tail))
