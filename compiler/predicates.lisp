14a8eadc6d04ba67e4c988c364d3708960c4d485
Sven Michael Klose <pixel@hugbox.org>
2008-07-14 17:58:18 +0000

    %SETQ, %SETQRET: New predicates.

--- compiler/predicates.lisp
+++ compiler/predicates.lisp
@@ -1,13 +1,24 @@
 ;;;; nix operating system project
 ;;;; lisp compiler
-;;;; Copyright (c) 2006-2007 Sven Klose <pixel@copei.de>
+;;;; Copyright (c) 2006-2008 Sven Klose <pixel@copei.de>
+
+(defun %setq? (x)
+  (and (consp x)
+	   (eq '%SETQ (car x))))
+
+(defun %setqret? (x)
+  (and (consp x)
+	   (eq '%SETQ (first x))
+	   (eq '~%RET (second x))))
 
 (defun quote? (x)
-  (and (consp x) (eq 'QUOTE (car x))))
+  (and (consp x)
+	   (eq 'QUOTE (car x))))
 
 (defun backquote? (x)
-  (and (consp x) (eq 'BACKQUOTE (car x))))
+  (and (consp x)
+	   (eq 'BACKQUOTE (car x))))
 
 (defun identity? (x)
   (and (consp x)
-       (eq (car x) 'identity)))
+       (eq 'IDENTITY (car x))))
