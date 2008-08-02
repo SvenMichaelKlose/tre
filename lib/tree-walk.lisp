f3e878f5d5d59fa207df3ec87ef8b11666d54bbd
Sven Michael Klose <pixel@hugbox.org>
2008-08-02 17:26:57 +0000

    DONT-ASCEND-AFTER-IF: New predicate blocks ascension after a node has been processed.

--- lib/tree-walk.lisp
+++ lib/tree-walk.lisp
@@ -3,18 +3,23 @@
 ;;;; Copyright (c) 2005-2007 Sven Klose <pixel@copei.de>
 
 ;; Replace elements in tree.
-(defun tree-walk (i &key (ascending nil) (dont-ascend-if nil))
+(defun tree-walk (i &key (ascending nil) (dont-ascend-if nil) (dont-ascend-after-if nil))
   (if (atom i)
-	i
+	(funcall ascending i)
 	(with (y (car i)
 		   a (if (and dont-ascend-if
 				      (funcall dont-ascend-if y))
 				 y
-	  			 (tree-walk (if ascending
-					 	 		(funcall ascending y)
-					 	 		y)
-					 		:ascending ascending
-					 		:dont-ascend-if dont-ascend-if)))
+				 (if (and dont-ascend-after-if
+						  (funcall dont-ascend-after-if y))
+					 (funcall ascending y)
+	  			     (tree-walk (if ascending
+					 	 		    (funcall ascending y)
+					 	 		    y)
+					 		    :ascending ascending
+					 		    :dont-ascend-if dont-ascend-if
+					 		    :dont-ascend-after-if dont-ascend-after-if))))
 	  (cons a
 	  		(tree-walk (cdr i) :ascending ascending
-							   :dont-ascend-if dont-ascend-if)))))
+							   :dont-ascend-if dont-ascend-if
+							   :dont-ascend-after-if dont-ascend-after-if)))))
