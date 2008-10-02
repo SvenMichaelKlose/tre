726d823cd009a6a37a1dc74d8b5c12805b804a5a
Sven Michael Klose <pixel@hugbox.org>
2008-10-02 08:35:53 +0000

    File ordering changed.

--- environment/stage2/main.lisp
+++ environment/stage2/main.lisp
@@ -1,14 +1,14 @@
 ;;;; nix operating system project
 ;;;; list processor environment
-;;;; Copyright (C) 2005-2007 Sven Klose <pixel@copei.de>
+;;;; Copyright (C) 2005-2008 Sven Klose <pixel@copei.de>
 ;;;;
 ;;;; Second environment stage
 
 (env-load "stage2/with-gensym.lisp")
+(env-load "stage2/list-manipulation.lisp")
 (env-load "stage2/search-sequence.lisp")
 (env-load "stage2/define-mapped-fun.lisp")
 (env-load "stage2/queue-utils.lisp")
-(env-load "stage2/list-manipulation.lisp")
 (env-load "stage2/assoc.lisp")
 (env-load "stage2/pairlis.lisp")
 (env-load "stage2/char.lisp")
