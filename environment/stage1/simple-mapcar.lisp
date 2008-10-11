1afb4f3c62364d4674cd8acb77ad9d30400af15b
Sven Michael Klose <pixel@hugbox.org>
2008-10-11 17:28:22 +0000

    %SIMPLE-MAP: New function.

--- environment/stage1/simple-mapcar.lisp
+++ environment/stage1/simple-mapcar.lisp
@@ -1,14 +1,23 @@
 ;;;; nix operating system project
 ;;;; list processor environment
-;;;; Copyright (C) 2005 Sven Klose <pixel@copei.de>
+;;;; Copyright (C) 2005,2008 Sven Klose <pixel@copei.de>
 ;;;;
 ;;;; Simple map functions.
+
+;; Apply function to elements of list.
+(%defun %simple-map (func lst)
+  (cond
+    (lst
+        (cond
+          ((consp lst)
+            (apply func (list (car lst)))
+            (%simple-map func (cdr lst)))))))
 
 ;; Apply function to elements of list and return new list with results.
 (%defun %simple-mapcar (func lst)
   (cond
     (lst
       (cond
-        ((listp lst)
+        ((consp lst)
           (cons (apply func (list (car lst)))
                 (%simple-mapcar func (cdr lst))))))))
