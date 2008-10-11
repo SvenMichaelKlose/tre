74792c2b0b3eb88ab55f454e802548eb97e6a5ae
Sven Michael Klose <pixel@hugbox.org>
2008-10-11 17:25:00 +0000

    DO: Ignore empty binding list.

--- environment/stage1/loops.lisp
+++ environment/stage1/loops.lisp
@@ -4,29 +4,25 @@
 ;;;;
 ;;;; Loop macros
 
-(defmacro dotimes ((iter times &rest result) &rest body)
-  (let ((tag (gensym))
-	(m (gensym)))
-    `(block nil
-      (let ((,iter 0)
-            (,m ,times))
-        (tagbody
-          ,tag
-          (if (not (< ,iter ,m))
-            (return (progn ,@result)))
-          ,@body
-          (setq ,iter (+ ,iter 1))
-	  (go ,tag))))))
-
-
 (defmacro do (binds (test &rest result) &rest body)
   (let ((tag (gensym)))
     `(block nil
-      (let (,@(mapcar #'((b) `(,(first b) ,(second b))) binds))
-        (tagbody
-          ,tag
-          (if ,test
-            (return (progn ,@result)))
-          ,@body
-          ,@(mapcar #'((b) (and (third b) `(setq ,(first b) ,(third b)))) binds)
-          (go ,tag))))))
+       (let ,(mapcar #'((b)
+						  `(,(first b) ,(second b)))
+					 binds)
+         (tagbody
+           ,tag
+           (if ,test
+               (return (progn
+						 ,@result)))
+           ,@body
+           ,@(mapcar #'((b)
+						  (and (third b)
+							   `(setq ,(first b) ,(third b))))
+					 binds)
+           (go ,tag))))))
+
+(defmacro dotimes ((iter times &rest result) &rest body)
+  `(do ((,iter 0 (+ 1 ,iter)))
+	   ((not (< ,iter ,times)) ,@result)
+	 ,@body))
