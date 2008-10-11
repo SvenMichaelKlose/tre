700139fb39a983a2cd485e2be519e16f7458172f
Sven Michael Klose <pixel@hugbox.org>
2008-10-11 17:25:38 +0000

    WHILE: Reimplemented with DO.

--- environment/stage1/loops.lisp
+++ environment/stage1/loops.lisp
@@ -1,6 +1,6 @@
 ;;;; nix operating system project
 ;;;; list processor environment
-;;;; Copyright (C) 2005-2007 Sven Klose <pixel@copei.de>
+;;;; Copyright (C) 2005-2008 Sven Klose <pixel@copei.de>
 ;;;;
 ;;;; Loop macros
 
--- environment/stage2/while.lisp
+++ environment/stage2/while.lisp
@@ -1,14 +1,9 @@
 ;;;; nix operating system project
 ;;;; list processor environment
-;;;; Copyright (C) 2005-2006 Sven Klose <pixel@copei.de>
+;;;; Copyright (C) 2005-2006,2008 Sven Klose <pixel@copei.de>
 
 (defmacro while (test result &rest body)
   "Loops over body unless test evaluates to NIL and returns result."
-  (let ((tag (gensym)))
-    `(block nil
-       (tagbody
-         ,tag
-         (when ,test
-           ,@body
-           (go ,tag))
-         (return-from nil ,result)))))
+  `(do ()
+	   ((not ,test) ,result)
+	 ,@body))
