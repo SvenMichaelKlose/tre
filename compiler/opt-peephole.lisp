c15dd7779496419e26329e937b6d0fda15dbd8e9
Sven Michael Klose <pixel@hugbox.org>
2008-10-13 20:11:00 +0000

    Accumulate %VAR declaration in head of function bodies.

--- compiler/opt-peephole.lisp
+++ compiler/opt-peephole.lisp
@@ -22,108 +22,130 @@
   (with
 	  (removed-tags nil
 
-	  ; Remove IDENTITY expressions to unify code.
-	  remove-identity
-	    #'((x)
-		     (opt-peephole-fun #'remove-identity
-			   ((and (%setq? a)
-				     (consp (third a))
-					 (identity? (third a)))
-			 	     ; Remove IDENTITY from %SETQ value.
-					 (cons `(%setq ,(second a) ,(second (third a)))
-						   (remove-identity d)))))
+	   accumulate-vars
+		 #'((x)
+			  (with (acc nil
+					 rec #'((x)
+			                  (when x
+				                (with-cons a d x
+				                  (if (%var? a)
+					                  (progn
+										(when (find-tree d (second a))
+										  (setf acc (push a acc)))
+						                (rec d))
+				  	                  (if (and (%setq? a)
+					       	                   (is-lambda? (third a)))
+						                  (cons `(%setq ,(second a)
+												        ,(copy-recurse-into-lambda (third a) #'accumulate-vars))
+												(rec d))
+						                  (cons a
+								                (rec d))))))))
+				(with (ret (rec x))
+				  (append acc ret))))
 
-		 find-next-tag
-		   #'((x)
-			    (when x
-				  (if (atom (car x))
-					  x
-					  (find-next-tag (cdr x)))))
+	   ; Remove IDENTITY expressions to unify code.
+	   remove-identity
+	     #'((x)
+		      (opt-peephole-fun #'remove-identity
+			    ((and (%setq? a)
+				      (consp (third a))
+					  (identity? (third a)))
+			 	   ; Remove IDENTITY from %SETQ value.
+				   (cons `(%setq ,(second a) ,(second (third a)))
+						 (remove-identity d)))))
 
-		 ; Remove unreached code or code that does nothing.
-		 remove-void
-		   #'((x)
-			    (opt-peephole-fun #'remove-void
-				  ((and (%setq? a)
-						(eq (second a) (third a)))
-				  		; Remove void assigment.
-						(remove-void d))
+	   find-next-tag
+		 #'((x)
+			  (when x
+				(if (atom (car x))
+					x
+					(find-next-tag (cdr x)))))
 
-				  ((and (%setq? a)
-						(consp d) (%setq? (car d))
-						(eq (second a) (third (car d)))
-						(eq (second (car d)) (third a)))
-				    	; Remove second of (setf x y y x).
-						(cons a (remove-void (cdr d))))
+	   ; Remove unreached code or code that does nothing.
+	   remove-void
+		 #'((x)
+			  (opt-peephole-fun #'remove-void
+				((and (%setq? a)
+					  (eq (second a) (third a)))
+				   ; Remove void assigment.
+				   (remove-void d))
 
-				  ((and (consp a)
-						(eq 'vm-go (first a))
-						d (atom (car d))
-						(eq (second a) (car d)))
-						; Remove jump to following tag.
-						(remove-void d))
+				((and (%setq? a)
+				      (consp d) (%setq? (car d))
+					  (eq (second a) (third (car d)))
+					  (eq (second (car d)) (third a)))
+				   ; Remove second of (setf x y y x).
+				   (cons a (remove-void (cdr d))))
 
-				  ((and (consp a)
-						(eq 'vm-go (first a)))
-						; Remove code after label until next tag.
-					    (cons a (remove-void (find-next-tag d))))
+				((and (consp a)
+					  (eq 'vm-go (first a))
+					  d (atom (car d))
+					  (eq (second a) (car d)))
+				   ; Remove jump to following tag.
+				   (remove-void d))
 
-				  ((and (%setq? a) (%setq? (car d))
-						(expex-sym? (second a))
-						(eq (second a) (third (car d))))
-						; Shorten (%setq expexsym sth) (%setq sth expexsym).
-						(cons `(%setq ,(second (car d)) ,(third a))
-							  (remove-void (cdr d))))))
+				((and (consp a)
+					  (eq 'vm-go (first a)))
+				   ; Remove code after label until next tag.
+				   (cons a (remove-void (find-next-tag d))))
 
-		 will-be-set-again?
-		   #'((x v)
-				(if x
-			        (or (and (%setq? (car x))
-					         (eq v (second (car x))))
-		                (unless (or (vm-jump? (car x)) ; We don't know what happens after a jump.
-				                 (find-tree (car x) v)) ; Variable will be used.
+				((and (%setq? a) (%setq? (car d))
+				      (expex-sym? (second a))
+					  (eq (second a) (third (car d))))
+				   ; Shorten (%setq expexsym sth) (%setq sth expexsym).
+				   (cons `(%setq ,(second (car d)) ,(third a))
+						 (remove-void (cdr d))))))
+
+	   will-be-set-again?
+		 #'((x v)
+			  (if x
+			      (or (and (%setq? (car x))
+					       (eq v (second (car x))))
+		              (unless (or (vm-jump? (car x)) ; We don't know what happens after a jump.
+				                  (find-tree (car x) v)) ; Variable will be used.
 				        (will-be-set-again? (cdr x) v)))
-				    (and (atom v) ; End of block, EXPEX-sym not used.
-					     (expex-sym? v))))
+				  (and (atom v) ; End of block, EXPEX-sym not used.
+					   (expex-sym? v))))
 
-		 ; Remove code without side-effects whose result won't be used.
-		 remove-code
-		   #'((x)
-				(opt-peephole-fun #'remove-code
-				  ((and (%setq? a)
-					    (atom (second a))
-					    (or (atom (third a))
-						    (%slot-value? (third a))
-						    (%stack? (third a)))
-						(will-be-set-again? d (second a)))
-				  		; Don't set variable that will be modified anyway.
-					  	(remove-code d))))
+	   ; Remove code without side-effects whose result won't be used.
+	   remove-code
+	     #'((x)
+			  (opt-peephole-fun #'remove-code
+				((and (%setq? a)
+					  (atom (second a))
+					  (or (atom (third a))
+						  (%slot-value? (third a))
+						  (%stack? (third a)))
+					  (will-be-set-again? d (second a)))
+				  	  ; Don't set variable that will be modified anyway.
+					  (remove-code d))))
 
-		 reduce-tags
-		   #'((x)
-				(opt-peephole-fun #'reduce-tags
-    		      ((and a (car d)
-						(atom a)
-			 		    (atom (car d)))
-				    	; Remove first of two subsequent tags.
-						(awhen (find-if #'((y)
-										     (eq a (cdr y)))
-									    removed-tags)
-							   (rplacd ! (second x)))
-				        (acons! a (second x) removed-tags)
-					    (reduce-tags d))))
+	   reduce-tags
+		 #'((x)
+			  (opt-peephole-fun #'reduce-tags
+    		    ((and a (car d)
+					  (atom a)
+			 		  (atom (car d)))
+				   ; Remove first of two subsequent tags.
+				   (awhen (find-if #'((y)
+					 				    (eq a (cdr y)))
+								   removed-tags)
+					 (rplacd ! (second x)))
+				   (acons! a (second x) removed-tags)
+				   (reduce-tags d))))
 
-		 rec
-		   #'((x)
-			   (maptree #'((x)
-			                 (aif (assoc x removed-tags)
-				                  (cdr !)
-					              x))
-						(funcall
-						  (compose #'reduce-tags
-								   #'remove-code
-								   #'remove-void
-)
-						  x))))
+	   rec
+		 #'((x)
+			  (maptree #'((x)
+			                (aif (assoc x removed-tags)
+				                 (cdr !)
+					             x))
+					   (funcall
+						 (compose #'reduce-tags
+								  #'remove-code
+								  #'remove-void)
+						 x))))
 
-	(repeat-while-changes #'rec (remove-identity x))))
+	(accumulate-vars
+	  (repeat-while-changes #'rec (accumulate-vars
+									(remove-identity x))))))
