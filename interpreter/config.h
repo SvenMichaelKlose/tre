5bba918d203e0d7d599e5818f2f5aab18c5156b8
Sven Michael Klose <pixel@hugbox.org>
2008-10-16 05:49:13 +0000

    Corrected index width.

--- interpreter/config.h
+++ interpreter/config.h
@@ -88,7 +88,7 @@ typedef int bool;
 #define TRE_IMAGE_HEADER  "#!/usr/local/bin/tre -i\n" TRE_INFO
 #endif
 
-#define TREPTR_INDEX_WIDTH	27
+#define TREPTR_INDEX_WIDTH	28
 
 #ifdef LITTLE_ENDIAN
 #define TRE_ENDIANESS_STRING	"LITTLE"
