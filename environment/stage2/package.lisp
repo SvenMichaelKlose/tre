fb9a407ea038414df28819ee3ddc477ddca9beee
Sven Michael Klose <pixel@hugbox.org>
2008-11-09 17:31:20 +0000

    Experimental package code.

---
+++ environment/stage2/package.lisp
@@ -0,0 +1,21 @@
+;;;;; TRE tree processor
+;;;;; Copyright (c) 2008 Sven Klose <pixel@copei.de>
+;;;;;
+;;;;; Package section
+;;;;;
+;;;;; XXX experimental
+
+(defun make-package (name)
+  (unless (package? name)
+    (error "package ~A is already defined"))
+  (%set-atom-fun name (%make-package)))
+
+(defmacro defpackage (name &rest exports)
+  (with (pkg (make-package name))
+	(map (fn make-symbol _ pkg t) exports)))
+
+(defmacro use-package (name &rest-imports)
+  (with (pkg (symbol-function name))
+    (map (fn (with (n (symbol-name _))
+			   (make-symbol n nil (make-symbol _ pkg))))
+	     imports)))
