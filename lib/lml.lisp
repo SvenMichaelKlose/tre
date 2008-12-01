8ff5c9b18671c6a1d903c9d86d4f0578b35678fd
Sven Michael Klose <pixel@hugbox.org>
2008-12-01 05:50:01 +0000

    LML-GET-CHILDREN, LML-GET-ATTRIBUTES: Moved here.

--- lib/lml.lisp
+++ lib/lml.lisp
@@ -1,5 +1,5 @@
 ;;;;; TRE environment
-;;;;; Copyright (C) 2006-2007 Sven Klose <pixel@copei.de>
+;;;;; Copyright (C) 2006-2008 Sven Klose <pixel@copei.de>
 ;;;;;
 ;;;;; LML function library
 
@@ -7,6 +7,19 @@
   (or (stringp expr) (consp expr)))
 
 ;;;; LML utilities
+
+(defun lml-get-children (x)
+  (when (consp x)
+    (if (consp x.)
+        x
+        (lml-get-children .x))))
+
+(defun lml-get-attribute (x name)
+  (when x
+    (unless (consp x.)
+      (if (eq name x.)
+          (second x)
+          (lml-get-attribute .x name)))))
 
 (defun lml-child? (expr)
   (string-or-cons? expr))
@@ -20,8 +33,8 @@
 (defun collect-attrs (expr attrs)
   (when expr
     (if (not (lml-child? expr))
-	(concat-attrs expr attrs)
-	expr)))
+	    (concat-attrs expr attrs)
+	    expr)))
 
 (defun lml-attrs-and-childs (expr)
   (with-queue attrs
@@ -31,8 +44,8 @@
 (defmacro with-lml-expr ((name attrs childs) expr &body body)
   (with-gensym g
     `(with (,g ,expr
-	    ,name (first ,g))
-      (multiple-value-bind (,attrs ,childs) (lml-attrs-and-childs ,g)
+	        ,name (first ,g))
+       (multiple-value-bind (,attrs ,childs) (lml-attrs-and-childs ,g)
 	,@body))))
 
 ;;;; LML to XML conversion
@@ -44,22 +57,22 @@
   (print-tag out name :opening attrs)
   (dolist (c childs)
     (if (stringp c)
-	(write-string c out)
-	(lml-to-xml out c)))
+	    (write-string c out)
+	    (lml-to-xml out c)))
   (print-tag out name :closing))
 
 (defun print-list (out lst)
-  (mapcar #'((x) (lml-to-xml out x)) lst))
+  (mapcar (fn lml-to-xml out x) lst))
 
 (defun lml-to-xml (out expr)
   "Convert LML expression or list to well-formed XML."
   (when (and expr (consp expr))
     (if (consp (first expr))
-	(print-list out expr)
-	(with-lml-expr (name attrs childs) expr
-	  (if childs
-	      (print-block out name attrs childs)
-	      (print-inline out name attrs))))))
+	    (print-list out expr)
+	    (with-lml-expr (name attrs childs) expr
+	      (if childs
+	          (print-block out name attrs childs)
+	          (print-inline out name attrs))))))
 
 ;;;; Template evaluation
 
@@ -70,10 +83,10 @@
   "Evaluuate LML template element with its attributes assigned to variables
    of the same names. Variable !childs will contain the unevaluated list
    of child elements."
-  (when (consp tpl)
-    (if (consp (car tpl))
-	(mapcar #'eval-lml-template (car tpl))
-	(with-lml-expr (name attrs childs) tpl
-	  (eval `(let (,@(append (map-attrs-to-vars attrs)
-				 `(!childs (eval-lml-template ,childs))))
-		  ,tpl))))))
+  (when tpl
+    (if (consp tpl.)
+	    (mapcar #'eval-lml-template tpl.)
+	    (with-lml-expr (name attrs childs) tpl
+	      (eval `(let (,@(append (map-attrs-to-vars attrs)
+				                 `(!childs (eval-lml-template ,childs))))
+		           ,tpl))))))
