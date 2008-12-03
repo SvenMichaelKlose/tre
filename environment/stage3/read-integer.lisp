3dd2a58d086063a4f0ee430efde7547f1b27ab52
Sven Michael Klose <pixel@hugbox.org>
2008-12-03 11:34:35 +0000

    READ-INTEGER: Test on end of stream.

--- environment/stage3/read-integer.lisp
+++ environment/stage3/read-integer.lisp
@@ -2,9 +2,13 @@
 ;;;; Copyright (c) 2008 Sven Klose <pixel@copei.de>
 
 (defun read-integer (&optional (str *standard-input*))
-  (with (rec	#'(()
-				    (if (digit-char-p (peek-char str))
-						(+ (- #\0 (read-char str)) (rec))
-						   0)))
-	  (when (digit-char-p (peek-char str))
-		(rec))))
+  "Read positive integer from stream."
+  (with (rec #'((v)
+			      (if (awhen (peek-char str)
+						(digit-char-p !))
+					  (rec (+ (- (read-char str) #\0)
+							  (* v 10)))
+					  v)))
+	(when (awhen (peek-char str)
+		    (digit-char-p !))
+	  (integer (rec 0)))))
