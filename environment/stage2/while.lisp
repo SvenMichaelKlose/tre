55e021ee08662127e4fefc27b87f585fba30663b
Sven Michael Klose <pixel@hugbox.org>
2008-12-22 20:43:29 +0000

    AWHILE: New macro.

--- environment/stage2/while.lisp
+++ environment/stage2/while.lisp
@@ -1,9 +1,15 @@
-;;;; nix operating system project
-;;;; list processor environment
-;;;; Copyright (C) 2005-2006,2008 Sven Klose <pixel@copei.de>
+;;;;; TRE environment
+;;;;; Copyright (C) 2005-2006,2008 Sven Klose <pixel@copei.de>
 
 (defmacro while (test result &rest body)
   "Loops over body unless test evaluates to NIL and returns result."
   `(do ()
 	   ((not ,test) ,result)
+	 ,@body))
+
+(defmacro awhile (test result &rest body)
+  "Loops over body unless test evaluates to NIL and returns result.
+   The test value is stored in variable !."
+  `(do ((! nil))
+	   ((not (setq ! ,test)) ,result)
 	 ,@body))
