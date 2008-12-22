49270973fcf8940736e0949793561fb47b02f6ba
Sven Michael Klose <pixel@hugbox.org>
2008-12-22 20:40:20 +0000

    1+, 1-: New functions.

--- environment/stage1/incdec.lisp
+++ environment/stage1/incdec.lisp
@@ -1,11 +1,16 @@
-;;;; nix operating system project
-;;;; list processor environment
-;;;; Copyright (C) 2005, 2008 Sven Klose <pixel@copei.de>
-;;;;
-;;;; Increment/decrement functions
+;;;;; TRE environment
+;;;;; Copyright (C) 2005, 2008 Sven Klose <pixel@copei.de>
+;;;;;
+;;;;; Increment/decrement functions
 
 (defmacro incf (place &optional (n 1))
   `(setf ,place (+ ,place ,n)))
 
 (defmacro decf (place &optional (n 1))
   `(setf ,place (- ,place ,n)))
+
+(defmacro 1+! (place &optional (n 1))
+  `(incf ,place ,n))
+
+(defmacro 1-! (place &optional (n 1))
+  `(decf ,place ,n))
