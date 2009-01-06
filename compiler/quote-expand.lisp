e405e3b0f05d61eb22cd5b3e4f047ec4d5789264
Sven Michael Klose <pixel@hugbox.org>
2009-01-06 16:55:52 +0000

    Fixed.

--- compiler/quote-expand.lisp
+++ compiler/quote-expand.lisp
@@ -1,81 +1,76 @@
 ;;;;; TRE compiler
-;;;;; Copyright (c) 2006-2008 Sven Klose <pixel@copei.de>
+;;;;; Copyright (c) 2006-2009 Sven Klose <pixel@copei.de>
 ;;;;;
 ;;;;; Convert BACKQUOTE-expressions into run-time consing code.
 
-;; BACKQUOTE-expand argument list with decremented sublevel.
-(defun quasiquote-subexpand (typ e sublevel)
-      `(cons (cons ',typ
-                   ,(quote-expand-0 (cdar e) (1- sublevel)))
-             ,(quote-expand-1 .e sublevel)))
-
 ;; Expand QUASIQUOTE.
-(defun quote-expand-quasiquote (e sublevel)
-    (if (= sublevel 0)
-        ; No sublevel. Insert evaluated QUASIQUOTE value.
-        `(cons ,(cadar e)
-               ,(quote-expand-1 .e sublevel))
-        (quasiquote-subexpand 'QUASIQUOTE e sublevel)))
+(defun backquote-cons-quasiquote (x)
+  (if (quasiquote? (cadar x))
+      `(cons ,(backquote-cons (cadar x))
+             ,(backquote-cons-1 .x))
+      `(cons ,(copy-tree (cadar x))
+             ,(backquote-cons-1 .x))))
 
 ;; Expand QUASIQUOTE-SPLICE.
-(defun quote-expand-quasiquote-splice (e sublevel)
-    (if (= sublevel 0)
-        ; No sublevel. Splice evaluated QUASIQUOTE-SPLICE expression.
-        `(append ,(cadar e)
-                 ,(quote-expand-1 .e sublevel))
-         (quasiquote-subexpand 'QUASIQUOTE-SPLICE e sublevel)))
+(defun backquote-cons-quasiquote-splice (x)
+  (if (quasiquote? (cadar x))
+      `(cons ,(copy-tree (cadar x))
+             ,(backquote-cons-1 .x))
+      `(%nconc ,(copy-tree (cadar x))
+       		   ,(backquote-cons-1 .x))))
+
+(defun backquote-cons-atom (x)
+  (when x
+	(if (string= "" (symbol-name x))
+		x
+		`(%quote ,x))))
 
 ;; Expand BACKQUOTE arguments.
-(defun quote-expand-1 (e sublevel)
-    (if
-	  (endp e)
-	    nil
+(defun backquote-cons-1 (x)
+  (if
+    ; Return atom as is.
+    (atom x)
+	  (backquote-cons-atom x)
 
-      ; Return atom as is.
-      (atom e)
-        `(%quote ,e)
+    ; Return element if it's not a cons.
+    (atom x.)
+      `(cons ,(backquote-cons-atom x.)
+             ,(backquote-cons-1 .x))
 
-      ; Return element if it's not a cons.
-      (atom e.)
-        `(cons (%quote ,e.)
-               ,(quote-expand-1 .e sublevel))
+    ; Do QUASIQUOTE expansion.
+    (eq (caar x) 'QUASIQUOTE)
+      (backquote-cons-quasiquote x)
 
-      ; Do QUASIQUOTE expansion.
-      (eq (caar e) 'QUASIQUOTE)
-        (quote-expand-quasiquote e sublevel)
+    ; Do QUASIQUOTE-SPLICE expansion.
+    (eq (caar x) 'QUASIQUOTE-SPLICE)
+      (backquote-cons-quasiquote-splice x)
 
-      ; Do QUASIQUOTE-SPLICE expansion.
-      (eq (caar e) 'QUASIQUOTE-SPLICE)
-        (quote-expand-quasiquote-splice e sublevel)
+    ; Expand sublist and rest.
+    `(cons ,(backquote-cons x.)
+           ,(backquote-cons-1 .x))))
 
-      ; Expand sublist and rest.
-      `(cons ,(quote-expand-0 e. sublevel)
-             ,(quote-expand-1 .e sublevel))))
+;; Expand BACKQUOTE, check for nested BACKQUOTE first.
+(defun backquote-cons (x)
+  (if
+    ; Return atom as is.
+    (atom x)
+	  (backquote-cons-atom x)
 
-;; Expand BACKQUOTE, check for nested BACKQUOTE (and increment sublevel) first.
-(defun quote-expand-0 (e sublevel)
-    (if
-      ; Return atom as is.
-      (atom e)
-        `(%quote e)
+    ; Enter new backquote level.
+    (eq x. 'BACKQUOTE)
+	  `(cons 'BACKQUOTE
+             ,(backquote-cons .x))
 
-      ; Enter new backquote level.
-      (eq e. 'BACKQUOTE)
-        (quote-expand-0 .e (1+ sublevel))
-
-      ; No new backquote level, continue normally.
-      (quote-expand-1 e sublevel)))
-
-;; Initialise expansion with sublevel of 0.
-(defun quote-expand (e)
-  (quote-expand-0 e 0))
+    ; No new backquote level, continue normally.
+    (backquote-cons-1 x)))
 
 (defun simple-quote-expand (x)
   (when x
 	(if (atom x)
-		(if (not (or (stringp x) (numberp x)))
-			`(%quote ,x)
-			x)
+		(if (or (stringp x)
+				(numberp x))
+			x
+			`(%quote ,x))
 		`(cons ,(simple-quote-expand x.)
 		       ,(simple-quote-expand .x)))))
 
@@ -83,10 +78,11 @@
   (tree-walk l
 	:ascending
 	  #'((x)
-		   (if (quote? x)
+		   (if
+			 (quote? x)
 			   (if (eq '%stack (cadr x))
 				   '(%quote %stack)
 			   	   (simple-quote-expand (cadr x)))
-		   	   (if (backquote? x)
-			  	   (quote-expand .x)
-			       x)))))
+		   	 (backquote? x)
+			   (backquote-cons (second x))
+			 x))))
