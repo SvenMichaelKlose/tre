9c8b6333b14469912360b7e6451a9abb493a96be
Sven Michael Klose <pixel@hugbox.org>
2009-01-19 11:00:57 +0000

    Don't feed code into string stream. Print it directly. Saves 2/3rds of time!

--- transpiler/javascript/toplevel.lisp
+++ transpiler/javascript/toplevel.lisp
@@ -1,29 +1,35 @@
 ;;;;; Transpiler: TRE to JavaScript
-;;;;; Copyright (c) 2008 Sven Klose <pixel@copei.de>
+;;;;; Copyright (c) 2008-2009 Sven Klose <pixel@copei.de>
 ;;;;;
 ;;;;; Toplevel
 
-(defun js-transpile-0 (files)
-  (with-string-stream f
-    (format f "/*~%")
-    (format f " * Copyright (c) 2005-2008 Sven Klose <pixel@copei.de>~%")
-    (format f " *~%")
-    (format f " * Softwarearchitekturbuero Sven Klose~%")
-    (format f " * Westermuehlstrasse 31~%")
-    (format f " * D-80469 Muenchen~%")
-    (format f " * Tel.: ++49 / 89 / 57 08 22 38~%")
-    (format f " *~%")
-    (format f " * caroshi ECMAScript obfuscator~%")
-    (format f " */~%")
-    (princ (transpiler-transpile *js-transpiler*
-		     (append (transpiler-sighten *js-transpiler* *js-base*)
-				     (transpiler-sighten-files *js-transpiler* files)))
+(defun js-transpile-0 (f files)
+  (format f "/*~%")
+  (format f " * Copyright (c) 2005-2008 Sven Klose <pixel@copei.de>~%")
+  (format f " *~%")
+  (format f " * Softwarearchitekturbuero Sven Klose~%")
+  (format f " * Westermuehlstrasse 31~%")
+  (format f " * D-80469 Muenchen~%")
+  (format f " * Tel.: ++49 / 89 / 57 08 22 38~%")
+  (format f " *~%")
+  (format f " * caroshi ECMAScript obfuscator~%")
+  (format f " */~%")
+  (with (tr *js-transpiler*
+		 base (transpiler-sighten tr *js-base*)
+    	 base2 (transpiler-sighten tr *js-base2*)
+	 	 user (transpiler-sighten-files tr files)
+		 deps (transpiler-transpile-wanted-functions tr))
+    (princ (transpiler-concat-string-tree
+ 		   (transpiler-transpile tr base)
+		   deps
+ 		   (transpiler-transpile tr base2)
+ 		   (transpiler-transpile tr user))
 	       f)))
 
-(defun js-transpile (files &key (obfuscate? nil))
+(defun js-transpile (out files &key (obfuscate? nil))
   (transpiler-reset *js-transpiler*)
   (transpiler-switch-obfuscator *js-transpiler* obfuscate?)
-  (js-transpile-0 files))
+  (js-transpile-0 out files))
 
 ;; XXX defunct
 (defun js-machine (outfile)
