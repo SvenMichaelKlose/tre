663608ac728944529c57b27525f4d4ebf1e5a66a
Sven Michael Klose <pixel@hugbox.org>
2009-01-19 11:11:49 +0000

    Added tests for APPLY.

--- environment/stage1/define-test.lisp
+++ environment/stage1/define-test.lisp
@@ -39,6 +39,18 @@
                       *tests*))
   (do-test (car *tests*)))
 
+(define-test "APPLY one argument"
+  ((apply #'list '(1 2 3)))
+  '(1 2 3))
+
+(define-test "APPLY many arguments"
+  ((apply #'list 1 '(2 3)))
+  '(1 2 3))
+
+(define-test "APPLY many arguments"
+  ((apply #'list 1 2 '(3)))
+  '(1 2 3))
+
 (define-test "EQ-SYMBOL"
   ((eq 'x 'x))
   t)
