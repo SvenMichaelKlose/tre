3d155b9e838eea8abb026691cb1ccd11ab35c3d5
Sven Michael Klose <pixel@hugbox.org>
2009-02-23 23:34:20 +0000

    Be verbose when *SHOW-DEFINITIONS* is set.

--- transpiler/expand.lisp
+++ transpiler/expand.lisp
@@ -45,11 +45,15 @@
 
 ;;;; LAMBDA EXPANSION
 
+; XXX remove this
+(defvar *is-imported-funinfo* nil)
+
 (defun transpiler-lambda-expand-one (tr x)
   (with (forms (argument-expand-names
 			       'transpiler-lambda-expand
 			       (lambda-args x.))
-         imported	(transpiler-current-funinfo tr)
+         imported	(unless *is-imported-funinfo* ; XXX
+					  (transpiler-current-funinfo tr))
          fi			(or imported
 						(make-funinfo :env forms
 							  		  :args forms)))
@@ -61,18 +65,26 @@
 				   fi
                    (lambda-body x.)
                    (transpiler-lambda-export? tr)))
+		  ; XXX not so good....
+		  ; Avoid prepared FUNINFO from lambda-expansion.
+          (when imported
+			(setf *is-imported-funinfo* t))
           (dolist (e (funinfo-closures fi))
             (transpiler-add-exported-closure tr e. .e)
             (transpiler-add-wanted-function tr e.)))))
 
-(defun transpiler-lambda-expand (tr x)
+(defun transpiler-lambda-expand-0 (tr x)
   "Expand top-level LAMBDA expressions."
   (if (atom x)
 	  x
 	  (cons (if (lambda? x.)
 				(transpiler-lambda-expand-one tr x)
-				(transpiler-lambda-expand tr x.))
-		    (transpiler-lambda-expand tr .x))))
+				(transpiler-lambda-expand-0 tr x.))
+		    (transpiler-lambda-expand-0 tr .x))))
+
+(defun transpiler-lambda-expand (tr x)
+  (setf *is-imported-funinfo* nil)
+  (transpiler-lambda-expand-0 tr x))
 
 (defun transpiler-expression-expand (tr x)
   (expression-expand (transpiler-expex tr) x))
@@ -153,7 +165,6 @@
     ; Make (SLOT-VALUE this ...) expressions for class members.
     (fn thisify (transpiler-thisify-classes tr) _)
 
-#'print
 	; Inline local functions and export constant LAMBDA expressions.
     (fn transpiler-lambda-expand tr _)
 
