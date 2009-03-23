56818ce80965c7ce16695838b5835aa8a5233055
Sven Michael Klose <pixel@hugbox.org>
2009-03-23 11:34:21 +0000

    WITH-CONS: Skip on NIL.

--- environment/stage4/with-cons.lisp
+++ environment/stage4/with-cons.lisp
@@ -1,7 +1,8 @@
 ;;;; TRE environment
-;;;; Copyright (C) 2005-2008 Sven Klose <pixel@copei.de>
+;;;; Copyright (C) 2005-2009 Sven Klose <pixel@copei.de>
 
 (defmacro with-cons (a d c &rest body)
-  `(let* ((,a (car ,c))
-          (,d (cdr ,c)))
-     ,@body))
+  `(when ,c
+	 (with (,a (car ,c)
+            ,d (cdr ,c))
+       ,@body)))
