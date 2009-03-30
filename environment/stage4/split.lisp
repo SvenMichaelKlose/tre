beddfee809b3018d4acb606ec2724cc83ed37a98
Sven Michael Klose <pixel@hugbox.org>
2009-03-30 20:15:21 +0000

    Clean-up.

--- environment/stage4/split.lisp
+++ environment/stage4/split.lisp
@@ -1,14 +1,20 @@
 ;;;; TRE environment
-;;;; Copyright (c) 2008 Sven Klose <pixel@copei.de>
+;;;; Copyright (c) 2008-2009 Sven Klose <pixel@copei.de>
 
 (defun split (obj seq &key (test #'eql))
   "Split sequence where element is equal to 'obj' and excluding them."
-  (when seq
-    (with (pos (position obj seq :test test))
-	  (if pos
-		  (cons (subseq seq 0 pos)
-			    (split obj (subseq seq (1+ pos)) :test test))
-		  (list seq)))))
+  (and seq
+       (let pos (position obj seq :test test)
+	     (if pos
+		     (cons (subseq seq 0 pos)
+			       (split obj
+						  (subseq seq (1+ pos))
+					      :test test))
+		     (list seq)))))
 
-; XXX tests missing
-;(print (split #\  "fnord bla"))
+(define-test "SPLIT works on string"
+  ((let x (split #\/ "foo/bar")
+	 (and (string= "foo" (first x))
+	 	  (string= "bar" (second x))
+		  (not (cddr x)))))
+  t)
