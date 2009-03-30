b83d11efd424461344f795dbf62a967ebb7ffa56
Sven Michael Klose <pixel@hugbox.org>
2009-03-30 20:11:49 +0000

    %SUBSEQ-STRING: New function overrides default one.

--- transpiler/baselib/string.lisp
+++ transpiler/baselib/string.lisp
@@ -3,17 +3,22 @@
 
 (js-type-predicate stringp string)
 
+;; XXX must be optional.
 (defun string-concat (&rest x)
   (apply #'+ x))
 
+;; XXX ECMAScript only.
 (defun %elt-string (seq idx)
-  (code-char (seq.char-code-at idx)))
+  (when (%%%< idx seq.length)
+    (code-char (seq.char-code-at idx))))
 
+;; XXX ECMAScript only.
 (defun %setf-elt-string (val seq idx)
   (assert (characterp val)
     (error "can only write CHARACTER to string"))
   (setf (aref seq idx) (*string.from-char-code (char-code val))))
 
+;; XXX ECMAScript only.
 (defun string (x)
   (if
 	(stringp x)
@@ -33,15 +38,24 @@
     (let* ((n (length lst))
            (s (make-string 0)))
       (do ((i 0 (1+ i))
-           (l lst (cdr l)))
+           (l lst .l))
           ((>= i n) s)
-        (setf s (+ s (string (car l))))))))
+        (setf s (+ s (string l.)))))))
 
+;; XXX must be optional.
 (defun string= (x y)
   (%%%= x y))
 
+;; XXX ECMAScript only.
 (defun string-upcase (x)
   (x.to-upper-case))
 
+;; XXX ECMAScript only.
 (defun string-downcase (x)
   (x.to-lower-case))
+
+;; XXX ECMAScript only.
+(defun %subseq-string (seq start end)
+  (and seq
+	   (%%%< start seq.length)
+	   (seq.substr start end)))
