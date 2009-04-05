c23c5f43978a0cd3eb85002259bfd574c2d9ae85
Sven Michael Klose <pixel@hugbox.org>
2009-04-05 23:59:49 +0000

    Have argument first.

--- environment/stage4/mapped-macro.lisp
+++ environment/stage4/mapped-macro.lisp
@@ -1,12 +1,12 @@
 ;;;;; TRE environment
 ;;;;; Copyright (c) 2009 Sven Klose <pixel@copei.de>
 
-(defmacro mapcar-macro (param arg &rest body)
+(defmacro mapcar-macro (arg param &rest body)
   `(progn
      ,,@(mapcar #'((,arg) ,@body)
                 ,param)))
 
-(defmacro mapcan-macro (param arg &rest body)
+(defmacro mapcan-macro (arg param &rest body)
   `(progn
      ,,@(mapcan #'((,arg) ,@body)
                 ,param)))
