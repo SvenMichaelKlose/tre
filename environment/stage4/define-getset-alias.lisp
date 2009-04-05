27b7bd75f7ca4eef91c87eb9bb9bc5b8398415e6
Sven Michael Klose <pixel@hugbox.org>
2009-04-05 23:59:16 +0000

    DEFINE-GET-ALIAS, DEFINE-SET-ALIAS: New macros.

--- environment/stage4/define-getset-alias.lisp
+++ environment/stage4/define-getset-alias.lisp
@@ -1,12 +1,20 @@
 ;;;;; Caroshi ECMAScript library
 ;;;;; Copyright (c) 2009 Sven Klose <pixel@copei.de>
 
+(defun get-definer (class)
+  (if class
+	  `(defmethod ,class)
+	  '(defun)))
+
+(defmacro define-get-alias (alias real &key (class nil))
+  `(,@(get-definer class) ,($ 'get- alias) ()
+      ,real))
+
+(defmacro define-set-alias (alias real &key (class nil))
+  `(,@(get-definer class) ,($ 'set- alias) (x)
+      (setf ,real x)))
+
 (defmacro define-getset-alias (alias real &key (class nil))
-  (let definer (if class
-				   `(defmethod ,class)
-				   '(defun))
-    `(progn
-       (,@definer ,($ 'get- alias) ()
-         ,real)
-       (,@definer ,($ 'set- alias) (x)
-         (setf ,real x)))))
+  `(progn
+	 (define-get-alias ,alias ,real :class ,class)
+	 (define-set-alias ,alias ,real :class ,class)))
