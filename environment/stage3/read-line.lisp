3b56b15fcbac648550eb9be7fd1dd14b88b61132
Sven Michael Klose <pixel@hugbox.org>
2009-04-24 01:37:43 +0000

    READ-ALL-LINES: New function returns an array of strings.

--- environment/stage3/read-line.lisp
+++ environment/stage3/read-line.lisp
@@ -6,6 +6,16 @@
   (with-default-stream nstr str
     (with-queue q
        (do ((c (read-char nstr) (read-char nstr)))
-           ((or (= c 10) (= c 13) (end-of-file nstr))
+           ((or (= c 10)
+				(= c 13)
+				(end-of-file nstr))
+			(or (end-of-file nstr)
+				(enqueue q c))
             (return-from read-line (list-string (queue-list q))))
          (enqueue q c)))))
+
+(defun read-all-lines (&optional (str *standard-input*))
+  (with-default-stream nstr str
+    (unless (end-of-file nstr)
+	  (cons (read-line nstr)
+			(read-all-lines nstr)))))
