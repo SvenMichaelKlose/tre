b56cc624ba5eb0b0cca35ab16de99c2a3efe5d74
Sven Michael Klose <pixel@hugbox.org>
2009-04-30 23:14:12 +0000

    Use dot-notation.

--- environment/stage4/find-tree.lisp
+++ environment/stage4/find-tree.lisp
@@ -1,8 +1,8 @@
 ;;;; TRE environment
-;;;; Copyright (C) 2005-2008 Sven Klose <pixel@copei.de>
+;;;; Copyright (C) 2005-2009 Sven Klose <pixel@copei.de>
 
 (defun find-tree (x v)
   (or (equal x v)
       (when (consp x)
-        (or (find-tree (car x) v)   
-            (find-tree (cdr x) v)))))
+        (or (find-tree x. v)   
+            (find-tree .x v)))))
