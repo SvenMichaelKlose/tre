7ad28f51f3cb8bc9fbc50997078f6d2fbdc406af
Sven Michael Klose <pixel@hugbox.org>
2009-06-28 17:36:19 +0000

    ASSOC-REPLACE, ASSOC-REPLACE-MANY: Test argument added.

--- environment/stage4/assoc-replace.lisp
+++ environment/stage4/assoc-replace.lisp
@@ -1,11 +1,11 @@
 ;;;;; TRE environment
 ;;;;; Copyright (c) 2009 Sven Klose <pixel@copei.de>
 
-(defun assoc-replace (x alst)
-  (or (assoc-value x alst)
+(defun assoc-replace (x alst &key (test #'eql))
+  (or (assoc-value x alst :test test)
 	   x))
 
-(defun assoc-replace-many (x alst)
+(defun assoc-replace-many (x alst &key (test #'eql))
   (when x
-	 (cons (assoc-replace x. alst)
-		   (assoc-replace-many .x alst))))
+	 (cons (assoc-replace x. alst :test test)
+		   (assoc-replace-many .x alst :test test))))
