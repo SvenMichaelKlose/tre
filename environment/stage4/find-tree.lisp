2a4a628dfc013f8df24655af576a672b2389695b
Sven Michael Klose <pixel@hugbox.org>
2009-07-04 15:59:18 +0000

    FIND-TREE: Use EQ.

--- environment/stage4/find-tree.lisp
+++ environment/stage4/find-tree.lisp
@@ -2,7 +2,7 @@
 ;;;; Copyright (C) 2005-2009 Sven Klose <pixel@copei.de>
 
 (defun find-tree (x v)
-  (or (equal x v)
+  (or (eq x v)
       (when (consp x)
         (or (find-tree x. v)   
             (find-tree .x v)))))
