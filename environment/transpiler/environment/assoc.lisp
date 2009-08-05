027460071c30e696dddd88196819c75036a978ce
Sven Michael Klose <pixel@hugbox.org>
2009-08-05 21:21:04 +0000

    Less checks when not asserting.

--- environment/transpiler/environment/assoc.lisp
+++ environment/transpiler/environment/assoc.lisp
@@ -3,12 +3,14 @@
 
 (defun assoc (key lst &key (test nil))
   "Search value for key in associative list."
-  (when lst
-	(unless (consp lst)
-	  (%error "list expected"))
-    (dolist (i lst)
-      (if (consp i)
-		  (if (funcall (or test #'eql) key (car i))
-	  	  	  (return i))
-		  (and (print i)
-			   (%error "not a pair"))))))
+  (when-debug
+	(when lst
+	  (unless (consp lst)
+	    (%error "list expected"))))
+  (dolist (i lst)
+    (when-debug
+	  (when (consp i)
+		(print i)
+		(%error "not a pair")))
+    (when (funcall (or test #'eql) key i.)
+  	  (return i))))
--- environment/transpiler/javascript/environment/member.lisp
+++ environment/transpiler/javascript/environment/member.lisp
@@ -2,16 +2,15 @@
 ;;;;; Copyright (C) 2005-2006,2008 Sven Klose <pixel@copei.de>
 
 (defun %member-r (elm lst)
-  (if lst
-    (or (equal elm lst.)
-        (%member-r elm .lst))))
+  (dolist (i lst)
+    (when (equal elm i)
+	  (return t))))
 
 (defun member (elm &rest lsts)
   "Test if object is a member of any of the pure lists."
-  (let x .lsts
-    (or (%member-r elm x)
-        (when x
-          (apply #'member elm x)))))
+  (dolist (i lsts)
+    (when (%member-r elm i)
+	  (return t))))
 
 ;(define-test "MEMBER finds elements"
 ;  ((member 's '(i) '(l i k e) '(l i s p)))
