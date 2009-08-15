b178cd736e834b1edfdd4ea12c8bfc2a13b52228
Sven Michael Klose <pixel@hugbox.org>
2009-08-15 12:01:15 +0000

    Clean-up.

--- makefiles/make-compiled-0.lisp
+++ makefiles/make-compiled-0.lisp
@@ -1,37 +1,4 @@
 (defun fnord ()
-,@(reverse (symbols-function-exprs *functions-after-math*))
-#'c-transpile
-#'js-transpile
-;  ,@*universe-functions*
-;  #'reverse #'append #'tree-list #'find #'assoc #'href #'%macroexpand #'position #'mapcan
-;  #'argument-expand #'lambda-expand #'expression--expand #'place-expand
-;  #'opt-peephole
-;  #'opt-inline
-;  #'transpiler-quote-keywords
-;  #'thisify
-;  #'rename-function-arguments
-;  #'quasiquote-expand
-;  #'backquote-expand
-;  #'simple-quote-expand
-;  #'dot-expand
-;  #'concat-stringtree
-;  #'transpiler-to-string
-;  #'transpiler-finalize-sexprs
-;  #'transpiler-encapsulate-strings
-;  #'transpiler-obfuscate
-;  #'read
-;  #'late-print
-)
-;  #'transpiler-add-wanted-variable
-;  #'transpiler-add-wanted-function
-;  #'transpiler-import-exported-closures
-;  #'transpiler-import-wanted-functions
-;  #'transpiler-import-wanted-variables
-;  #'transpiler-import-from-environment
-;  #'transpiler-import-from-expex
-;  #'transpiler-make-expex
-;  #'transpiler-make-named-functions
-;  #'transpiler-update-funinfo
-;  #'transpiler-expression-expand
-;  #'transpiler-argument-definitions
-;  #'special-form-expand
+  ,@(reverse (symbols-function-exprs *functions-after-math*))
+  #'c-transpile
+  #'js-transpile)
