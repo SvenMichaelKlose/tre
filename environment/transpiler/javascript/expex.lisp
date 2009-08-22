5879bd7bc5bca9ad609fa8edf570999f1ae60521
Sven Michael Klose <pixel@hugbox.org>
2009-08-22 09:34:29 +0000

    Include set global variables.

--- environment/transpiler/javascript/expex.lisp
+++ environment/transpiler/javascript/expex.lisp
@@ -2,7 +2,7 @@
 ;;;;; Copyright (c) 2009 Sven Klose <pixel@copei.de>
 
 (defun js-setter-filter (tr x)
-  ;(transpiler-add-wanted-variable tr (second x))
+  (transpiler-add-wanted-variable tr (second x))
   x)
 
 (defun js-expand-literals (x)
