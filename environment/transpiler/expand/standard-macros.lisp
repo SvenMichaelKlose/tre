5eea071705a4197be3bf083c736bbe4d22b92f68
Sven Michael Klose <pixel@hugbox.org>
2009-08-25 20:04:16 +0000

    Import functions that have the same name as a transpiler macro.

--- environment/transpiler/expand/standard-macros.lisp
+++ environment/transpiler/expand/standard-macros.lisp
@@ -8,5 +8,5 @@
 		 name x.)
 	(when (expander-has-macro? (transpiler-macro-expander tre) name)
 	  (error "Macro ~A already defined in code-generator." name))
-	(transpiler-add-unwanted-function tre name)
+	;(transpiler-add-unwanted-function tre name)
     `(define-expander-macro ,(transpiler-std-macro-expander tre) ,@x)))
