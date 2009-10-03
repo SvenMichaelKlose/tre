8f6b6b4724da582cb41333e16b629c8d052d00a4
Sven Michael Klose <pixel@hugbox.org>
2009-10-03 14:58:46 +0000

    Allow predicate argument.

--- environment/stage4/remove.lisp
+++ environment/stage4/remove.lisp
@@ -1,5 +1,5 @@
 ;;;;; TRE environment
 ;;;;; Copyright (c) 2009 Sven Klose <pixel@copei.de>
 
-(defmacro remove! (x lst)
-  `(setf ,lst (remove ,x ,lst)))
+(defmacro remove! (x lst &rest args)
+  `(setf ,lst (remove ,x ,lst ,@args)))
