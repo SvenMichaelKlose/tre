de756ddad32577f18fb1a04acb5a1fd000be285a
Sven Michael Klose <pixel@hugbox.org>
2009-10-03 14:57:13 +0000

    Workaround to print into pop-up from Firefox-extension.

--- environment/transpiler/javascript/environment/debug-print.lisp
+++ environment/transpiler/javascript/environment/debug-print.lisp
@@ -1,21 +1,25 @@
 ;;;;; Transpiler: TRE to JavaScript
 ;;;;; Copyright (c) 2008-2009 Sven Klose <pixel@copei.de>
 
+(defun js-print-write (x doc)
+  (setf doc.body.inner-h-t-m-l
+  	    (+ doc.body.inner-h-t-m-l x)))
+
 (defun js-print-cons-r (x doc)
   (when x
     (js-print x. doc)
     (if (consp .x)
 	    (js-print-cons-r .x doc)
 	    (when .x
-		  (doc.write " . ")
+		  (js-print-write " . " doc)
 		  (js-print-atom .x doc)))))
 
 (dont-obfuscate write)
 
 (defun js-print-cons (x doc)
-  (doc.write "(")
+  (js-print-write "(" doc)
   (js-print-cons-r x doc)
-  (doc.write ")"))
+  (js-print-write ")" doc))
 
 (defun js-print-symbol (x)
   (+ (if (keywordp x)
@@ -30,21 +34,24 @@
 	"}<br/>"))
 
 (defun js-print-atom (x doc)
-  (doc.write
-    (+ (if
-	     (symbolp x)
-	       (js-print-symbol x)
-	     (characterp x)
-		   (+ "#\\\\" (*string.from-char-code (char-code x)))
-	     (arrayp x)
-	       "{array}"
-	     (stringp x)
-	       (+ "\"" x "\"")
-		 (when x
-	       (if (objectp x)
-			   (js-print-object x)
-		   	   (string x))))
-	   " ")))
+  (js-print-write
+      (+ (if
+		   (not x)
+		   	 "NIL"
+	       (symbolp x)
+	         (js-print-symbol x)
+	       (characterp x)
+		     (+ "#\\\\" (*string.from-char-code (char-code x)))
+	       (arrayp x)
+	         "{array}"
+	       (stringp x)
+	         (+ "\"" x "\"")
+		   (when x
+	         (if (objectp x)
+			     (js-print-object x)
+		   	     (string x))))
+	     " ")
+	doc))
 
 (dont-inline js-print)
 
