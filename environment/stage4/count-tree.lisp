2ea1d2a6c01b9d4d2dd58acaac209861811de0fb
Sven Michael Klose <pixel@hugbox.org>
2009-10-22 20:12:26 +0000

    COUNT-TREE: New function counts objects in tree.

---
+++ environment/stage4/count-tree.lisp
@@ -0,0 +1,15 @@
+;;;;; TRE environment
+;;;;; Copyright (c) 2009 Sven Klose <pixel@copei.de>
+
+(defun count-tree-0 (v x init max test)
+  (if
+	(funcall test v x)  (1+ max)
+	(atom x)  max
+	(let sum (count-tree-0 v x. init max test)
+	  (if (and max
+			   (<= max sum))
+		  sum
+	      (count-tree-0 v .x sum max test)))))
+
+(defun count-tree (v x &key (max nil) (test #'eql))
+  (count-tree-0 v x 0 max test))
