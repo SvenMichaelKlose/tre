1168225c9019546ba0169fd46c29cd423f2bd17f
Sven Michael Klose <pixel@hugbox.org>
2009-10-22 20:10:37 +0000

    FIND-TREE: Now takes predicate.

--- environment/stage4/find-tree.lisp
+++ environment/stage4/find-tree.lisp
@@ -1,8 +1,8 @@
 ;;;; TRE environment
 ;;;; Copyright (C) 2005-2009 Sven Klose <pixel@copei.de>
 
-(defun find-tree (x v)
-  (or (eq x v)
+(defun find-tree (x v &key (test #'eql))
+  (or (funcall test x v)
       (when (consp x)
-        (or (find-tree x. v)   
-            (find-tree .x v)))))
+        (or (find-tree x. v :test test)   
+            (find-tree .x v :test test)))))
