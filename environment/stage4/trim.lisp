a9c29119dcb4359a6e8c64ca132421528d936d6a
Sven Michael Klose <pixel@hugbox.org>
2009-10-22 20:11:22 +0000

    TRIM-HEAD, TRIM-TAIL: New functions.

--- environment/stage4/trim.lisp
+++ environment/stage4/trim.lisp
@@ -1,15 +1,26 @@
 ;;;; TRE environment
 ;;;; Copyright (c) 2008 Sven Klose <pixel@copei.de>
 
+(defun trim-tail (obj seq &key (test #'eql))
+  (let len (length seq)
+    (if (funcall test obj (elt seq (integer-1- len)))
+	    (trim-tail obj
+				   (subseq seq 0 (integer-1- len))
+				   :test test)
+		seq)))
+
+(defun trim-head (obj seq &key (test #'eql))
+  (if (funcall test obj (elt seq 0))
+	  (trim-head obj
+				 (subseq seq 1)
+				 :test test)
+	  seq))
+
 (defun trim (obj seq &key (test #'eql))
   "Trim start and end of sequence where element is equal to 'obj'."
-  (when seq
-    (if (funcall test obj (elt seq 0))
-		(trim obj (subseq seq 1) :test test)
-		(with (len (length seq))
-          (if (funcall test obj (elt seq (integer-1- len)))
-		      (trim obj (subseq seq 0 (integer-1- len)) :test test)
-			  seq)))))
+  (and seq
+	   (if (integer< 0 (length seq))
+  		   (trim-tail obj (trim-head obj seq :test test) :test test))
+		   seq))
 
 ; XXX tests missing
-;(print (trim " " "  malcolm in the middle  " :test #'string=))
