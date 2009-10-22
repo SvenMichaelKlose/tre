1a0f01ba090034fd86d8c5b7b653a55b6c0b0c26
Sven Michael Klose <pixel@hugbox.org>
2009-10-22 20:13:16 +0000

    ESCAPE-STRING: Can now handle many quote types.

--- environment/stage4/escape-string.lisp
+++ environment/stage4/escape-string.lisp
@@ -1,22 +1,21 @@
 ;;;; TRE environment
 ;;;; Copyright (c) 2009 Sven Klose <pixel@copei.de>
 
-(defun escape-charlist (x &key (quote-char #\"))
+(defun escape-charlist (x quote-char)
   (when x
     (if
 	  (= quote-char x.)
         (cons #\\
               (cons x.
-                    (escape-charlist .x :quote-char quote-char)))
+                    (escape-charlist .x quote-char)))
 	  (= #\\ x.)
         (cons #\\
 			  (if (and .x (digit-char-p .x.))
-                  (escape-charlist .x :quote-char quote-char)
+                  (escape-charlist .x quote-char)
                   (cons #\\
-                        (escape-charlist .x :quote-char quote-char))))
+                        (escape-charlist .x quote-char))))
       (cons x.
-            (escape-charlist .x :quote-char quote-char)))))
+            (escape-charlist .x quote-char)))))
 
-(defun escape-string (x &key (quote-char #\"))
-  (list-string (escape-charlist (string-list x)
-								:quote-char quote-char)))
+(defun escape-string (x quote-char)
+  (list-string (escape-charlist (string-list x) quote-char)))
