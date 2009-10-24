8c1a7cffd063c8736b5712231fea6f91a8155192
Sven Michael Klose <pixel@hugbox.org>
2009-10-24 17:03:18 +0000

    Directory moved.

--- environment/transpiler/codegen/escaped-string.lisp
+++
@@ -1,9 +0,0 @@
-;;;;; TRE transpiler
-;;;;; Copyright (c) 2008-2009 Sven Klose <pixel@copei.de>
-;;;;;
-;;;;; Code-generation top-level
-
-(defun transpiler-make-escaped-string (x quote-char)
-  (string-concat (string quote-char)
-				 (make-c-newlines (escape-string x quote-char))
-				 (string quote-char)))
