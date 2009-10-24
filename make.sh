d05d14fba793710a116ebc75f0cd2cec25734032
Sven Michael Klose <pixel@hugbox.org>
2009-10-24 02:25:12 +0000

    recompile: New target.

--- make.sh
+++ make.sh
@@ -190,6 +190,12 @@ bootunclean)
 	./make.sh crunshraw
 	;;
 
+recompile)
+	echo "(quit)" | tre -n
+	tre makefiles/make-compiled.lisp
+	./make.sh crunshraw
+	;;
+
 install)
 	install_it_without_reload
 	;;
