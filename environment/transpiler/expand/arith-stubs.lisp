def2453ba6c4ba906c7d32c742cd00991a762ac2
Sven Michael Klose <pixel@hugbox.org>
2009-10-24 17:03:43 +0000

    Directory moved.

--- environment/transpiler/expand/arith-stubs.lisp
+++
@@ -1,7 +0,0 @@
-;;;;; TRE transpiler
-;;;;; Copyright (c) 2008-2009 Sven Klose <pixel@copei.de>
-
-(mapcan-macro _
-    '(+ - = < > <= >=)
-  `((defun ,($ '%%% _) (&rest x)
-	  (apply (function ,_) x))))
--- environment/transpiler/expand/expex-funinfo.lisp
+++
@@ -1,6 +0,0 @@
-;;;;; TRE transpiler
-;;;;; Copyright (c) 2009 Sven Klose <pixel@copei.de>
-
-(defun expex-funinfo-defined-variable? (x)
-  (or (funinfo-env-pos *expex-funinfo* x)
-      (expex-global-variable? x)))
