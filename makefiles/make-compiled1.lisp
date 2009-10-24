c49a28dd34dd19766fa31f60c6a6338a620baaa4
Sven Michael Klose <pixel@hugbox.org>
2009-10-24 14:32:52 +0000

    Show names of inlined functions.

--- makefiles/make-compiled1.lisp
+++ makefiles/make-compiled1.lisp
@@ -2,6 +2,7 @@
 ;;;;; Copyright (c) 2008-2009 Sven Klose <pixel@copei.de>
 
 (setf *show-definitions* t)
+(setf *show-inlines?* t)
 (setf *opt-inline?* t)
 (setf *opt-inline-max-levels* 3)
 (setf *opt-inline-min-size* 0)
