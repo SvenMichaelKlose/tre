6471fc06a9c7640e01a14d6cc919839ab43bcce0
Sven Michael Klose <pixel@hugbox.org>
2009-11-05 06:53:38 +0000

    ASSOC-HASH: Default predicate is EQL.

--- environment/stage4/assoc-hash.lisp
+++ environment/stage4/assoc-hash.lisp
@@ -1,7 +1,7 @@
 ;;;; TRE environment
 ;;;; Copyright (c) 2009 Sven Klose <pixel@copei.de>
 
-(defun assoc-hash (x &key (test nil))
+(defun assoc-hash (x &key (test #'eql))
   (let h (make-hash-table :test test)
 	(dolist (i x h)
 	  (setf (href h i.) .i))))
