5d39fdba805e41d3b9c957101549afdbc3ee17f3
Sven Michael Klose <pixel@hugbox.org>
2009-11-05 00:29:54 +0000

    Fixed printing of objects.

--- environment/transpiler/targets/javascript/environment/debug-print.lisp
+++ environment/transpiler/targets/javascript/environment/debug-print.lisp
@@ -21,37 +21,37 @@
   (js-print-cons-r x doc)
   (js-print-write ")" doc))
 
-(defun js-print-symbol (x)
-  (+ (if (keywordp x)
-		 ":"
-		 "")
-	 (symbol-name x)))
+(defun js-print-symbol (x doc)
+  (when (keywordp x)
+	(js-print-write ":" doc))
+  (js-print-write (+ (symbol-name x) " ") doc))
 
-(defun js-print-object (x)
-  (+ "{"
-     (apply #'+ (maphash #'((k v) (+ k " => " v "<br/>"))
-				         x))
-	"}<br/>"))
+(defun js-print-character (x doc)
+  (js-print-write (+ "#\\\\" (*string.from-char-code (char-code x)))
+				  x))
+
+(defun js-print-string (x doc)
+  (js-print-write (+ "\"" x "\"") doc))
+
+(defun js-print-object (x doc)
+  (js-print-write "{" doc)
+  (maphash #'((k v)
+			    (js-print k doc)
+				(js-print-write " => " doc)
+				(js-print v doc)
+				(js-print-write "<br/>" doc))
+	       x)
+  (js-print-write "}<br/>" doc))
 
 (defun js-print-atom (x doc)
-  (js-print-write
-      (+ (if
-		   (not x)
-		   	 "NIL"
-	       (symbolp x)
-	         (js-print-symbol x)
-	       (characterp x)
-		     (+ "#\\\\" (*string.from-char-code (char-code x)))
-	       (arrayp x)
-	         "{array}"
-	       (stringp x)
-	         (+ "\"" x "\"")
-		   (when x
-	         (if (objectp x)
-			     (js-print-object x)
-		   	     (string x))))
-	     " ")
-	doc))
+  (if
+    (not x)		   (js-print-write "NIL" doc)
+    (symbolp x)	   (js-print-symbol x doc)
+    (characterp x) (js-print-character x doc)
+    (stringp x)	   (js-print-string x doc)
+    (objectp x)	   (js-print-object x doc)
+	(js-print-write (+ "[unknown type: " (string x) "]")
+					doc)))
 
 (dont-inline js-print)
 
