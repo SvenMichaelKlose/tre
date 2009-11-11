7f39195718a4c367af0b095e7ca11dcbd0151c9c
Sven Michael Klose <pixel@hugbox.org>
2009-11-11 14:30:27 +0000

    SORT: Have keyword argument TEST.

--- environment/stage4/sort.lisp
+++ environment/stage4/sort.lisp
@@ -1,17 +1,17 @@
 ;;;;; TRE environment
 ;;;;; Copyright (c) 2009 Sven Klose <pisel@copei.de>
 
-(defun sort-divide (x left right)
+(defun sort-divide (x left right test)
   (with (i left
 		 j (1- right)
 		 pivot (elt x right))
     (while (< i j)
 		   nil
-	  (while (and (<= (elt x i) pivot)
+	  (while (and (funcall test (elt x i) pivot)
 				  (< i right))
 			 nil
 	    (1+! i))
-	  (while (and (>= (elt x j) pivot)
+	  (while (and (funcall test pivot (elt x j))
 				  (> j left))
 			 nil
 		(1-! j))
@@ -19,16 +19,16 @@
 		(xchg (elt x i) (elt x j))
 		(1+! i)
 		(1-! j)))
-	(when (< pivot (elt x i))
+	(when (funcall test pivot (elt x i))
 	  (xchg (elt x i) (elt x right)))
 	i))
 
-(defun sort-0 (x left right)
+(defun sort-0 (x left right test)
   (when (< left right)
-	(let divisor (sort-divide x left right)
-	  (sort-0 x left (1- divisor))
-	  (sort-0 x (1+ divisor) right))))
+	(let divisor (sort-divide x left right test)
+	  (sort-0 x left (1- divisor) test)
+	  (sort-0 x (1+ divisor) right test))))
 
-(defun sort (x)
-  (sort-0 x 0 (1- (length x)))
+(defun sort (x &key (test #'<=))
+  (sort-0 x 0 (1- (length x)) test)
   x)
