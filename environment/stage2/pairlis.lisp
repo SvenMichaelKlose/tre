a1e07818cd668b3806b701d145b1e0b5cb9690a0
Sven Michael Klose <pixel@hugbox.org>
2010-01-27 21:58:42 +0000

    Clean formatting.

--- environment/stage2/pairlis.lisp
+++ environment/stage2/pairlis.lisp
@@ -1,11 +1,10 @@
-;;;; nix operating system project
-;;;; list processor environment
-;;;; Copyright (C) 2006 Sven Klose <pixel@copei.de>
+;;;;; TRE environment
+;;;;; Copyright (C) 2006 Sven Klose <pixel@copei.de>
 
 (defun pairlis (keys vals)
   "Merge lists into associative list."
   (if keys   
-    (acons (car keys) (car vals)
-           (pairlis (cdr keys) (cdr vals)))
-    (if vals
-      (%error "lists must have the same length"))))
+      (acons (car keys) (car vals)
+             (pairlis (cdr keys) (cdr vals)))
+      (if vals
+        (%error "lists must have the same length"))))
