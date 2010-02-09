6532029db5b727836eb0aec9e42c11e0a40d610f
Sven Michael Klose <pixel@hugbox.org>
2010-02-09 20:16:46 +0000

    Rollback.

--- environment/transpiler/back-end/place-assign.lisp
+++ environment/transpiler/back-end/place-assign.lisp
@@ -1,15 +1,31 @@
 ;;;;; TRE compiler
-;;;;; Copyright (c) 2009-2010 Sven Klose <pixel@copei.de>
+;;;;; Copyright (c) 2009 Sven Klose <pixel@copei.de>
 
-(metacode-walker place-assign (x)
-	:if-atom		x
-	:if-stack		`(%stack ,(funinfo-env-pos (get-lambda-funinfo-by-sym .x.) ..x.))
-	:if-vec			`(%vec ,(place-assign .x.)
-			   			   ,(or (funinfo-lexical-pos (get-lambda-funinfo-by-sym ..x.)
-										 			 ...x.)
-								(error "can't find index in lexicals")))
-	:if-lambda		`#'(,@(lambda-head x)
-		     				,@(place-assign (lambda-body x)))
+(defun place-assign (x)
+  (if
+	(or (atom x)
+		(%quote? x)
+		(%transpiler-native? x))
+	  x
 
-    :if-slot-value	`(%slot-value ,(place-assign .x.)
-								  ,..x.))
+	(and (%stack? x)
+		 ..x)
+		`(%stack ,(funinfo-env-pos (get-lambda-funinfo-by-sym .x.) ..x.))
+
+	(and (%vec? x)
+		 ...x)
+		`(%vec ,(place-assign .x.)
+			   ,(or (funinfo-lexical-pos (get-lambda-funinfo-by-sym ..x.)
+										 ...x.)
+					(error "can't find index in lexicals")))
+
+	(lambda? x) ; XXX Add variables to ignore in subfunctions.
+      `#'(,@(lambda-head x)
+		     ,@(place-assign (lambda-body x)))
+
+    (%slot-value? x)
+      `(%slot-value ,(place-assign .x.)
+					,..x.)
+
+    (cons (place-assign x.)
+		  (place-assign .x))))
