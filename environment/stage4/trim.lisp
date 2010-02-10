71cc49b49b81c893e17e5eee6a2d538bbb360c6b
Sven Michael Klose <pixel@hugbox.org>
2010-02-10 02:48:46 +0000

    Fixed for Javascript target.

--- environment/stage4/trim.lisp
+++ environment/stage4/trim.lisp
@@ -1,20 +1,22 @@
 ;;;; TRE environment
-;;;; Copyright (c) 2008 Sven Klose <pixel@copei.de>
+;;;; Copyright (c) 2008-2010 Sven Klose <pixel@copei.de>
 
 (defun trim-tail (obj seq &key (test #'eql))
   (let len (length seq)
-    (if (funcall test obj (elt seq (integer-1- len)))
-	    (trim-tail obj
-				   (subseq seq 0 (integer-1- len))
-				   :test test)
-		seq)))
+	(when (< 0 len)
+      (if (funcall test obj (elt seq (integer-1- len)))
+	      (trim-tail obj
+				     (subseq seq 0 (integer-1- len))
+				     :test test)
+		  seq))))
 
 (defun trim-head (obj seq &key (test #'eql))
-  (if (funcall test obj (elt seq 0))
-	  (trim-head obj
-				 (subseq seq 1)
-				 :test test)
-	  seq))
+  (when (< 0 (length seq))
+    (if (funcall test obj (elt seq 0))
+	    (trim-head obj
+				   (subseq seq 1)
+				   :test test)
+	    seq)))
 
 (defun trim (obj seq &key (test #'eql))
   "Trim start and end of sequence where element is equal to 'obj'."
@@ -23,4 +25,14 @@
   		   (trim-tail obj (trim-head obj seq :test test) :test test))
 		   seq))
 
-; XXX tests missing
+(define-test "TRIM-HEAD works"
+  ((trim-head #\  "  "))
+  nil)
+
+(define-test "TRIM-TAIL works"
+  ((trim-tail #\  "  "))
+  nil)
+
+(define-test "TRIM works"
+  ((trim #\  "  "))
+  nil)
