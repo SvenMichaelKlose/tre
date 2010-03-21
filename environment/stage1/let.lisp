fe30f61bc113dfa65375f181329c52c8d3531d87
Sven Michael Klose <pixel@hugbox.org>
2010-03-21 23:24:18 +0000

    More verbose error message.

--- environment/stage1/let.lisp
+++ environment/stage1/let.lisp
@@ -1,13 +1,15 @@
 ;;;;; TRE environment
 ;;;;; Copyright (c) 2005-2006,2008,2010 Sven Klose <pixel@copei.de>
-;;;;;
-;;;;; LET macro
 
 ;; Create new local variable.
 (defmacro let (place expr &rest body)
   (if
 	(not body)
-	  (%error "body expected")
+	  (progn
+		(print place)
+		(print expr)
+		(print body)
+	    (%error "body expected"))
     (consp place)
 	  (%error "place is not an atom")
       `(#'((,place)
