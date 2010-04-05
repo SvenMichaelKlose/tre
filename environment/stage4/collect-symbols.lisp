3b0da0a99a9d430f86129b8ef51fdf3776759e34
Sven Michael Klose <pixel@hugbox.org>
2010-04-05 22:28:16 +0000

    Keep symbol order.

--- environment/stage4/collect-symbols.lisp
+++ environment/stage4/collect-symbols.lisp
@@ -1,15 +1,15 @@
 ;;;;; TRE environment
-;;;;; Copyright (c) 2009 Sven Klose <pixel@copei.de>
+;;;;; Copyright (c) 2009-2010 Sven Klose <pixel@copei.de>
 
 (defun collect-symbols (x)
-  (with (ret nil
+  (with (ret (make-queue)
   		 rec #'((x)
 				  (when x
     				(if (and (atom x)
 							 (not (string= "" (symbol-name x))))
-        			  (push! x ret)
-        			(when (consp x)
-          			  (rec x.)
-          			  (rec .x))))))
+        			    (enqueue ret x)
+        			    (when (consp x)
+          			      (rec x.)
+          			      (rec .x))))))
 	(rec x)
-	ret))
+	(queue-list ret)))
