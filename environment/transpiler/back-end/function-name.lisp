35df15a88e98b768283437d84b50a2ac7b979f8e
Sven Michael Klose <pixel@hugbox.org>
2010-04-05 22:23:59 +0000

    Passthrough %TRANSPILER-NATIVE forms as function names.

--- environment/transpiler/back-end/function-name.lisp
+++ environment/transpiler/back-end/function-name.lisp
@@ -1,5 +1,7 @@
 ;;;;; TRE transpiler
-;;;;; Copyright (c) 2009 Sven Klose <pixel@copei.de>
+;;;;; Copyright (c) 2009-2010 Sven Klose <pixel@copei.de>
 
 (defun compiled-function-name (x)
-    ($ 'compiled_ x))
+  (if (%transpiler-native? x)
+	  x
+      ($ 'compiled_ x)))
