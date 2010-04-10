9eecf02bb34456f9c2c253763c110361d9e1dacb
Sven Michael Klose <pixel@hugbox.org>
2010-04-10 15:38:48 +0000

    Clean-up.

--- environment/stage4/pad.lisp
+++ environment/stage4/pad.lisp
@@ -1,10 +1,9 @@
 ;;;; TRE environment
-;;;; Copyright (c) 2008-2009 Sven Klose <pixel@copei.de>
+;;;; Copyright (c) 2008-2010 Sven Klose <pixel@copei.de>
 
 (defun pad (seq p)
   (when seq
-    (if (< 1 (length seq))
-        (cons seq.
-			  (cons p
-					(pad .seq p)))
-        (list seq.))))
+    (cons seq.
+    	  (when (< 1 (length seq))
+			(cons p
+				  (pad .seq p))))))
