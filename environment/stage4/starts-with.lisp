7023cb4524382387f27b69b53df5d20fc15874a2
Sven Michael Klose <pixel@hugbox.org>
2010-05-27 14:07:47 +0000

    STARTS-WITH: New keyword argument IGNORE-CASE?.

--- environment/stage4/starts-with.lisp
+++ environment/stage4/starts-with.lisp
@@ -1,6 +1,12 @@
 ;;;;; TRE environment
-;;;;; Copyright (c) 2009 Sven Klose <pixel@copei.de>
+;;;;; Copyright (c) 2009-2010 Sven Klose <pixel@copei.de>
 
-(defun starts-with? (x head)
+(defun starts-with? (x head &key (ignore-case? nil))
   (let s (force-string x)
-    (string= head (subseq s 0 (length head)))))
+    (string= (if ignore-case?
+			     (string-downcase head)
+				 head)
+			 (let sub (subseq s 0 (length head))
+			   (if ignore-case?
+				   (string-downcase sub)
+				   sub)))))
