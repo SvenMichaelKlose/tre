d098567fce2afaa297fe958f5e69bd3b29daf621
Sven Michael Klose <pixel@hugbox.org>
2010-05-29 03:25:55 +0000

    Prefers functions over macros.

--- environment/stage1/queue.lisp
+++ environment/stage1/queue.lisp
@@ -1,40 +1,31 @@
-;;;; nix operating system project
-;;;; list processor environment
-;;;; Copyright (C) 2005-2006, 2008 Sven Klose <pixel@copei.de>
-;;;;
-;;;; Queue functions
-;;;;
-;;;; Queues are reversed stacks where the first cons' CAR points to the last
-;;;; and its CDR to the first element.
+;;;; TRE environment
+;;;; Copyright (C) 2005-2006, 2008, 2010 Sven Klose <pixel@copei.de>
 
-(defmacro make-queue ()
-  '(cons nil nil))
+(defun make-queue ()
+  (cons nil nil))
 
-(defmacro enqueue (queue obj)
-  "Append element to end of queue."
-  (let* ((q (gensym))
-	     (o (gensym)))
-  `(let* ((,q ,queue)
-	      (,o ,obj))
-     (if (car ,q)
-       (setf (car ,q) (setf (cdar ,q) (list ,o)))
-       (setf (car ,q) (setf (cdr ,q) (list ,o))))
-     ,o)))
-
-(defmacro queue-pop (queue)
-  "Pop first element off a queue and return it."
-  (let q (gensym)
-    `(let ,q ,queue
-       (prog1
-	     (second ,q)
-	     (setf (cdr ,q) (cddr ,q))))))
-
-(defmacro queue-list (queue)
-  `(cdr ,queue))
+(defun enqueue (queue obj)
+  (if (car queue)
+      (setf (car queue) (setf (cdar queue) (list obj)))
+      (setf (car queue) (setf (cdr queue) (list obj))))
+  obj)
 
 (defun enqueue-list (queue x)
   (setf (cdr queue) (nconc (cdr queue) x)
 		(car queue) (last x)))
+
+(defun queue-pop (queue)
+  (let q queue
+    (let v (cdr q)
+	  (if v
+	      (setf (cdr q) (cddr q))
+		  v.))))
+
+(defun queue-list (queue)
+  (cdr queue))
+
+(defun queue-front (queue)
+  (car (cdr queue)))
 
 (define-test "ENQUEUE and QUEUE-LIST work"
   ((let q (make-queue)
