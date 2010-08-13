60caf73552558ba53b9def06722319b976d9b3d4
Sven Michael Klose <pixel@hugbox.org>
2010-08-13 16:27:45 +0000

    DOTIMES-STEP: New macro.

--- environment/stage1/loops.lisp
+++ environment/stage1/loops.lisp
@@ -1,8 +1,5 @@
-;;;; nix operating system project
-;;;; list processor environment
-;;;; Copyright (C) 2005-2008 Sven Klose <pixel@copei.de>
-;;;;
-;;;; Loop macros
+;;;; TRE environment
+;;;; Copyright (C) 2005-2008,2010 Sven Klose <pixel@copei.de>
 
 (defmacro do (binds (test &rest result) &rest body)
   (let tag (gensym)
@@ -24,5 +21,10 @@
 
 (defmacro dotimes ((iter times &rest result) &rest body)
   `(do ((,iter 0 (integer+ 1 ,iter)))
+	   ((not (< ,iter ,times)) ,@result)
+	 ,@body))
+
+(defmacro dotimes-step ((iter times step &rest result) &rest body)
+  `(do ((,iter 0 (integer+ ,step ,iter)))
 	   ((not (< ,iter ,times)) ,@result)
 	 ,@body))
