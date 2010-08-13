2520832955604a7b02e09dd1c3d1188c7b2ab261
Sven Michael Klose <pixel@hugbox.org>
2010-08-13 16:31:52 +0000

    Be faster.

--- environment/stage4/sort.lisp
+++ environment/stage4/sort.lisp
@@ -1,34 +1,35 @@
 ;;;;; TRE environment
-;;;;; Copyright (c) 2009 Sven Klose <pisel@copei.de>
+;;;;; Copyright (c) 2009-2010 Sven Klose <pisel@copei.de>
 
 (defun sort-divide (x left right test)
   (with (i left
-		 j (1- right)
+		 j (integer-1- right)
 		 pivot (elt x right))
-    (while (< i j)
+    (while (integer< i j)
 		   nil
 	  (while (and (funcall test (elt x i) pivot)
-				  (< i right))
+				  (integer< i right))
 			 nil
-	    (1+! i))
+	    (integer1+! i))
 	  (while (and (funcall test pivot (elt x j))
-				  (> j left))
+				  (integer> j left))
 			 nil
-		(1-! j))
-	  (when (< i j)
+		(integer1-! j))
+	  (when (integer< i j)
 		(xchg (elt x i) (elt x j))
-		(1+! i)
-		(1-! j)))
+		(integer1+! i)
+		(integer1-! j)))
 	(when (funcall test pivot (elt x i))
 	  (xchg (elt x i) (elt x right)))
 	i))
 
 (defun sort-0 (x left right test)
-  (when (< left right)
+  (when (integer< left right)
 	(let divisor (sort-divide x left right test)
-	  (sort-0 x left (1- divisor) test)
-	  (sort-0 x (1+ divisor) right test))))
+	  (sort-0 x left (integer-1- divisor) test)
+	  (sort-0 x (integer-1+ divisor) right test))))
 
 (defun sort (x &key (test #'<=))
-  (sort-0 x 0 (1- (length x)) test)
-  x)
+  (when x
+    (sort-0 x 0 (integer-1- (length x)) test)
+    x))
