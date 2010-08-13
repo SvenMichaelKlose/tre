83efc1bd2b79b253fc67cee2bbbc3ff546d7f24c
Sven Michael Klose <pixel@hugbox.org>
2010-08-13 16:29:01 +0000

    ASSOC-VALUE: Now settable.

--- environment/stage2/assoc-value.lisp
+++ environment/stage2/assoc-value.lisp
@@ -1,5 +1,8 @@
 ;;;; TRE environment
-;;;; Copyright (c) 2008 Sven Klose <pixel@copei.de>
+;;;; Copyright (c) 2008,2010 Sven Klose <pixel@copei.de>
 
 (defun assoc-value (&rest args)
   (cdr (apply #'assoc args)))
+
+(defun (setf assoc-value) (val &rest args)
+  (rplacd (apply #'assoc args) val))
