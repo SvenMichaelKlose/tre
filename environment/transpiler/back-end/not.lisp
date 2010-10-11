243615c2aabcf2d6a43cef0d50eda5c5bd0ab897
Sven Michael Klose <pixel@hugbox.org>
2010-10-11 10:54:15 +0000

    Removed leftovers from last crash merge.

--- environment/transpiler/back-end/not.lisp
+++
@@ -1,5 +0,0 @@
-;;;;; TRE compiler
-;;;;; Copyright (c) 2010 Sven Klose <pixel@copei.de>
-
-(defun transpiler-not (x)
-  (eq x (transpiler-obfuscate-symbol *current-transpiler* nil)))
--- environment/transpiler/front-end/expex-funinfo.lisp
+++
@@ -1,6 +0,0 @@
-;;;;; TRE transpiler
-;;;;; Copyright (c) 2009 Sven Klose <pixel@copei.de>
-
-(defun expex-funinfo-defined-variable? (x)
-  (or (funinfo-in-env? *expex-funinfo* x)
-      (expex-global-variable? x)))
