fa175926e372c58e358759a9f5287ce7ad94e561
Sven Michael Klose <pixel@hugbox.org>
2010-10-16 19:38:53 +0000

    Fixed misplaced bracket.

--- environment/stage4/trim.lisp
+++ environment/stage4/trim.lisp
@@ -3,7 +3,7 @@
 
 (defun trim-tail (obj seq &key (test #'eql))
   (let len (length seq)
-	(when (< 0 len)
+	(when (integer< 0 len)
       (if (funcall test obj (elt seq (integer-1- len)))
 	      (trim-tail obj
 				     (subseq seq 0 (integer-1- len))
@@ -11,7 +11,7 @@
 		  seq))))
 
 (defun trim-head (obj seq &key (test #'eql))
-  (when (< 0 (length seq))
+  (when (integer< 0 (length seq))
     (if (funcall test obj (elt seq 0))
 	    (trim-head obj
 				   (subseq seq 1)
@@ -22,8 +22,8 @@
   "Trim start and end of sequence where element is equal to 'obj'."
   (and seq
 	   (if (integer< 0 (length seq))
-  		   (trim-tail obj (trim-head obj seq :test test) :test test))
-		   seq))
+  		   (trim-tail obj (trim-head obj seq :test test) :test test)
+		   seq)))
 
 (define-test "TRIM-HEAD works"
   ((trim-head #\  "  "))
