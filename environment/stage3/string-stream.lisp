aeb3d710d3017674c93c5d508a5facf5e1d05aba
Sven Michael Klose <pixel@hugbox.org>
2010-11-01 23:10:31 +0000

    Fixed end-of-file predicate.

--- environment/stage3/string-stream.lisp
+++ environment/stage3/string-stream.lisp
@@ -1,26 +1,20 @@
 ;;;; TRE environment
-;;;; Copyright (C) 2005-2006, 2008 Sven Klose <pixel@copei.de>
-;;;;
-;;;; String stream
+;;;; Copyright (C) 2005-2006,2008,2010 Sven Klose <pixel@copei.de>
 
 (defun make-string-stream ()
-  "Makes stream which accumulates all strings written to it.
-   See also GET-STREAM-STRING."
   (make-stream
-    :user-detail  (make-queue)
-    :fun-in       #'((str)
-                       (queue-pop (stream-user-detail str)))
-    :fun-out      #'((x str)
-					   (if (stringp x)
-						   (enqueue-list (stream-user-detail str)
-										 (string-list x))
-                       	   (enqueue (stream-user-detail str) x)))
-	:fun-eof	  #'((str)
-					   (eq (stream-user-detail str) nil))))
+      :user-detail  (make-queue)
+      :fun-in       #'((str)
+                         (queue-pop (stream-user-detail str)))
+      :fun-out      #'((x str)
+					     (if (stringp x)
+						     (enqueue-list (stream-user-detail str)
+										   (string-list x))
+                       	     (enqueue (stream-user-detail str) x)))
+	  :fun-eof	  #'((str)
+					     (not (queue-list (stream-user-detail str))))))
 
 (defun get-stream-string (str)
-  "Returns string accumulated by string-stream. The stream is
-   emptied. See also MAKE-STRING-STREAM."
   (prog1
 	(queue-string (stream-user-detail str))
 	(setf (stream-user-detail str) (make-queue))))
