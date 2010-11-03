a57a4738fb2f91ff064ed553281c86c960201d91
Sven Michael Klose <pixel@hugbox.org>
2010-11-03 21:35:13 +0000

    GROUP: Now iterative.

--- environment/stage2/group.lisp
+++ environment/stage2/group.lisp
@@ -2,5 +2,8 @@
 ;;;;; Copyright (c) 2007 Sven Klose <pixel@copei.de>
 
 (defun group (l size)
-  (when l
-    (cons (subseq l 0 size) (group (subseq l size) size))))
+  (let result (make-queue)
+    (while l nil
+      (enqueue result (subseq l 0 size))
+      (setf l (subseq l size)))
+    (queue-list result)))
