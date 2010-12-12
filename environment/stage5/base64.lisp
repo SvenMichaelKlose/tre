8a7146f017d48a569c760ea03944923b9064b789
Sven Michael Klose <pixel@hugbox.org>
2010-12-12 14:34:01 +0000

    Added base64 encoder/decoder.

---
+++ environment/stage5/base64.lisp
@@ -0,0 +1,70 @@
+;;;;; TRE environment
+;;;;; Copyright (c) 2009-2010 Sven Klose <pixel@copei.de>
+
+(defvar *base64-key*
+  		"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=")
+
+(defun base64-encode-char (x)
+  (string (elt *base64-key* x)))
+
+(defun base64-encode (x)
+  (let out ""
+	(let len (length x)
+	  (while x
+		     out
+	    (with (c1 x.
+			   c2 (when .x .x.)
+			   c3 (when ..x ..x.)
+	  		   e1 (>> c1 2)
+			   e2 (bit-or (<< (bit-and c1 3)
+							  4)
+						  (>> (or c2 0) 4))
+			   e3 (if c2
+				    (bit-or (<< (bit-and c2 15)
+							    2)
+						    (>> (or c3 0) 6))
+					64)
+			   e4 (if c3
+					(bit-and c3 63)
+					64))
+          (setf x ...x)
+		  (+! out (base64-encode-char e1)
+				  (base64-encode-char e2)
+				  (base64-encode-char e3)
+				  (base64-encode-char e4)))))))
+ 
+(defun base64-decode-char (x)
+  (position x *base64-key* :test #'=))
+
+(defun base64-compress (x)
+  (when x
+    (if (or (alphanumericp x.)
+            (in=? x. #\+ #\/ #\=))
+        (cons x. (base64-compress .x))
+        (base64-compress .x))))
+
+(defun base64-decode (x)
+  (let out ""
+	(setf x (base64-compress x))
+	(let len (length x)
+	  (while x out
+	    (with (e1 (base64-decode-char x.)
+	  		   e2 (base64-decode-char .x.)
+	  		   e3 (base64-decode-char ..x.)
+	  		   e4 (base64-decode-char ...x.)
+			   c1 (bit-or (<< e1 2)
+						  (>> e2 4))
+			   c2 (bit-or (<< (bit-and e2 15)
+							  4)
+						  (>> e3 2))
+			   c3 (bit-or (<< (bit-and e3 3)
+							  6)
+						  e4))
+			  (setf x ....x)
+			  (+! out (string (code-char c1))
+					  (if (not (= 64 e3))
+					      (string (code-char c2))
+						  "")
+				      (if (not (= 64 e4))
+					      (string (code-char c3))
+						  "")))))))
--- environment/stage5/main.lisp
+++ environment/stage5/main.lisp
@@ -1,8 +1,7 @@
 ;;;; TRE environment
 ;;;; Copyright (c) 2010 Sven Klose <pixel@copei.de>
-;;;;
-;;;; Fifth stage
 
+(env-load "stage5/base64.lisp")
 (env-load "stage5/comma-separated-list.lisp")
 (env-load "stage5/string-has-whitespace-p.lisp")
 (env-load "stage5/stringlist-skip-spaces.lisp")
