03ae94870328c790b7f92de89feb1fad1c9069d4
Sven Michael Klose <pixel@hugbox.org>
2010-12-12 14:40:54 +0000

    Handle CR/LF correctly.

--- environment/stage3/read-line.lisp
+++ environment/stage3/read-line.lisp
@@ -1,18 +1,28 @@
 ;;;; TRE environment
-;;;; Copyright (c) 2007 Sven Klose <pixel@copei.de>
+;;;; Copyright (c) 2007,2010 Sven Klose <pixel@copei.de>
 
 (defun read-line (&optional (str *standard-input*))
   "Read line from string."
   (with-default-stream nstr str
     (with-queue q
-       (do ((c (read-char nstr) (read-char nstr)))
-           ((or (= c 10)
-				(= c 13)
-				(end-of-file nstr))
-			(or (end-of-file nstr)
-				(enqueue q c))
-            (return-from read-line (list-string (queue-list q))))
-         (enqueue q c)))))
+      (while (and (not (end-of-file nstr))
+                  (not (let c (peek-char nstr)
+                         (or (= c 10)
+				             (= c 13)))))
+              (progn
+                (let lst (peek-char nstr)
+                  (when (and (not (end-of-file nstr))
+                             (or (= lst 10)
+				                 (= lst 13)))
+                    (enqueue q (read-char nstr))
+                    (when (let c (peek-char nstr)
+                            (and (not (end-of-file nstr))
+                                 (or (= c 10)
+				                     (= c 13))
+                                 (not (= c lst))))
+                      (enqueue q (read-char nstr)))))
+                (return-from read-line (list-string (queue-list q))))
+           (enqueue q (read-char nstr))))))
 
 (defun read-all-lines (&optional (str *standard-input*))
   (with-default-stream nstr str
