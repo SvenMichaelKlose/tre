d8137f3c01fe790239855b38580f97b9d57e3fab
Sven Michael Klose <pixel@hugbox.org>
2011-01-02 06:01:08 +0000

    STRING-CONCAT fixed.

--- environment/transpiler/targets/php/environment/string.lisp
+++ environment/transpiler/targets/php/environment/string.lisp
@@ -1,31 +1,29 @@
-;;;;; Transpiler: TRE to JavaScript
-;;;;; Copyright (c) 2008-2009 Sven Klose <pixel@copei.de>
+;;;;; Transpiler: TRE to PHP
+;;;;; Copyright (c) 2008-2011 Sven Klose <pixel@copei.de>
 
 (dont-obfuscate is_string)
 
 (defun stringp (x)
   (is_string x))
 
-;; XXX must be optional.
 (defun string-concat (&rest x)
-  (apply #'+ x))
+  (let result ""
+    (dolist (i x result)
+      (setf result (%%%string+ result i)))))
 
 (dont-obfuscate ord strlen substr)
 
-;; XXX ECMAScript only.
 (defun %elt-string (seq idx)
   (when (%%%< idx (strlen seq))
     (code-char (ord (substr seq idx 1)))))
 
 (dont-obfuscate from-char-code)
 
-;; XXX ECMAScript only.
 (defun %setf-elt-string (val seq idx)
   (error "cannot modify strings"))
 
 (dont-obfuscate strval)
 
-;; XXX ECMAScript only.
 (defun string (x)
   (if
 	(stringp x)
@@ -38,25 +36,21 @@
 	  ,*nil-symbol-name*
    	(strval x)))
 
-;; XXX must be optional.
 (defun string= (x y)
   (%%%= x y))
 
 (dont-obfuscate strtoupper)
 
-;; XXX ECMAScript only.
 (defun string-upcase (x)
   (strtoupper x))
 
 (dont-obfuscate strtolower)
 
-;; XXX ECMAScript only.
 (defun string-downcase (x)
   (strtolower x))
 
 (dont-obfuscate substr)
 
-;; XXX ECMAScript only.
 (defun %subseq-string (seq start end)
   (if (= start end)
 	  ""
