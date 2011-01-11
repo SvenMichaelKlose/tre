96593404a4f94bb04bd63fa9ce726868072e2d4b
Sven Michael Klose <pixel@hugbox.org>
2011-01-11 16:13:20 +0000

    Print errors to standard stream.

--- environment/transpiler/targets/php/environment/error.lisp
+++ environment/transpiler/targets/php/environment/error.lisp
@@ -1,14 +1,14 @@
 ;;;;; Transpiler: TRE to PHP
-;;;;; Copyright (c) 2008-2009 Sven Klose <pixel@copei.de>
+;;;;; Copyright (c) 2008-2009,2011 Sven Klose <pixel@copei.de>
 
 (dont-inline %error)
 
 (defun %error (msg)
-  (alert msg)
+  (princ msg)
   (invoke-native-debugger))
 
 (dont-inline error)
 
 (defun error (fmt &rest args)
-  (alert (+ "Error :" (apply #'format nil fmt args)))
+  (princ (+ "Error :" (apply #'format nil fmt args)))
   (invoke-native-debugger))
