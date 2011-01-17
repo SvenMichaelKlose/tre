d6b68618bfef3f10bd34c3f57a92fa9937b6e788
Sven Michael Klose <pixel@hugbox.org>
2011-01-17 03:14:06 +0000

    Don't have end-of-line in version number.

--- environment/version.lisp
+++ environment/version.lisp
@@ -1,11 +1,11 @@
-;;;;; Caroshi ECMAScript library
-;;;;; Copyright (c) 2009 Sven Klose <pixel@copei.de>
+;;;;; TRE environment
+;;;;; Copyright (c) 2009,2011 Sven Klose <pixel@copei.de>
 
-,(with-open-file in (open "_current-version" :direction 'input)
-   (let l (string-list (read-line in))
-	 `(defvar *tre-revision*
-	    ,(list-string
-	         (aif (position #\: l)
-		          (subseq l (1+ !) (or (position #\M l :test #'=)
-								       999))
-		          l)))))
+(defvar *tre-revision*
+    ,(with-open-file in (open "_current-version" :direction 'input)
+       (let l (string-list (read-line in))
+         (list-string (subseq l
+                              (aif (position #\: l)
+                                   (1+ !)
+                                   0)
+                              (1- (length l)))))))
