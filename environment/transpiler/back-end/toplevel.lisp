42cc9fcdf03a4adddc5ead3af4f4627c41f464fd
Sven Michael Klose <pixel@hugbox.org>
2011-01-27 07:57:51 +0000

    Cleanup.

--- environment/transpiler/back-end/toplevel.lisp
+++ environment/transpiler/back-end/toplevel.lisp
@@ -1,4 +1,4 @@
-;;;;; TRE transpile1
+;;;;; TRE transpiler
 ;;;;; Copyright (c) 2008-2011 Sven Klose <pixel@copei.de>
 
 ;; After this pass:
@@ -6,27 +6,25 @@
 ;; - Strings are encapsulated.
 ;; - Expressions are expanded via code generating macros.
 ;; - Everything is converted to strings and concatenated.
-(defun transpiler-emit-code-compose (tr)
-  (transpiler-pass
-      print-o (fn (princ #\o)
-			      (force-output)
-			      _)
-	  concat-stringtree #'concat-stringtree
-	  to-string (fn transpiler-to-string tr _)
-	  obfuscate (fn transpiler-obfuscate tr _)
-	  codegen-expander (fn expander-expand (transpiler-macro-expander tr) _)
-	  finalize-sexprs #'transpiler-finalize-sexprs
-	  encapsulate-strings #'transpiler-encapsulate-strings
-	  function-names (fn translate-function-names nil _)))
+(transpiler-pass transpiler-emit-code-compose (tr)
+    print-o (fn (princ #\o)
+	      (force-output)
+	      _)
+    concat-stringtree #'concat-stringtree
+    to-string (fn transpiler-to-string tr _)
+    obfuscate (fn transpiler-obfuscate tr _)
+    codegen-expander (fn expander-expand (transpiler-macro-expander tr) _)
+    finalize-sexprs #'transpiler-finalize-sexprs
+    encapsulate-strings #'transpiler-encapsulate-strings
+    function-names (fn translate-function-names nil _))
 
 (defun transpiler-emit-code (tr x)
   (funcall (transpiler-emit-code-compose tr) x))
 
-(defun transpiler-make-places-compose ()
-  (transpiler-pass
-	  place-assign #'place-assign
-      place-expand #'place-expand
-      make-function-prologues #'make-function-prologues))
+(transpiler-pass transpiler-make-places-compose ()
+    place-assign #'place-assign
+    place-expand #'place-expand
+    make-function-prologues #'make-function-prologues)
 
 ;; After this pass:
 ;; - Function prologues are generated.
