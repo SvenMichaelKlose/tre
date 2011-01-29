4215f47c766a58914c3bedd388cc01756afe8afc
Sven Michael Klose <pixel@hugbox.org>
2011-01-29 10:23:04 +0000

    DEFINE-FORMAT: New macro to define escape sequences.

--- environment/stage3/format.lisp
+++ environment/stage3/format.lisp
@@ -3,25 +3,36 @@
 ;;;;
 ;;;; FORMAT function
 
+(defvar *format-handlers* nil)
+
+(defmacro define-format (chr (str l i txt args) &rest body)
+  (with-gensym g
+    `(progn
+       (defun ,g (,str ,l ,i ,txt ,args)
+         ,@body)
+       (acons! ,chr #',g *format-handlers*))))
+
+(define-format #\% (str l i txt args)
+  (terpri str)
+  (values (integer-1+ i) args))
+
+(define-format #\A (str l i txt args)
+  (? args
+     (? (or (cons? args.)
+			(variablep args.))
+		(late-print args. str) ; XXX
+        (princ args. str))
+	 (error "argument specified in format is missing"))
+  (values (integer-1+ i) .args))
+
 (defun %format-directive (str l i txt args)
-   (let el (char-upcase (elt txt i))
-     (?
-       (character= el #\%)
-		 (progn
-		   (terpri str)
-           (%format str l (integer-1+ i) txt args))
-       (character= el #\A)
-		 (progn
-		   (? args
-		      (? (or (cons? args.)
-					  (variablep args.))
-				 (late-print args. str) ; XXX
-                 (princ args. str))
-			  (error "argument specified in format is missing"))
-           (%format str l (integer-1+ i) txt .args))
-       (progn
-		 (princ #\~ str)
-         (%format str l i txt args)))))
+  (let el (char-upcase (elt txt i))
+    (aif (assoc-value el *format-handlers* :test #'character=)
+         (with ((new-index next-args) (funcall ! str l (integer-1+ i) txt args))
+           (%format str l new-index txt next-args))
+         (progn
+		   (princ #\~ str)
+           (%format str l (integer-1+ i) txt args)))))
 
 (defun %format (str l i txt args)
   (when (integer< i l)
