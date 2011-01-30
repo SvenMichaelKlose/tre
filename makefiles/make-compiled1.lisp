c8bf4c26f557f589dcb132ac4c0e509a5030f56a
Sven Michael Klose <pixel@hugbox.org>
2011-01-30 17:49:59 +0000

    Don't inline the interpreted pass.

--- makefiles/make-compiled1.lisp
+++ makefiles/make-compiled1.lisp
@@ -1,13 +1,8 @@
 ;;;;; TRE C transpiler
-;;;;; Copyright (c) 2008-2010 Sven Klose <pixel@copei.de>
+;;;;; Copyright (c) 2008-2011 Sven Klose <pixel@copei.de>
 
 (setf *show-definitions* t)
-(setf *opt-inline?* t)
-(setf *opt-inline-max-levels* 3)
-(setf *opt-inline-min-size* 0)
-(setf *opt-inline-max-size* 16)
-(setf *opt-inline-max-repetitions* 0)
-(setf *opt-inline-max-small-repetitions* 0)
+(setf *opt-inline?* nil)
 
 (let code (c-transpile '("makefiles/make-compiled-1.lisp"))
   (with-open-file out (open "interpreter/_compiled-env.c" :direction 'output)
