3d5c4386edb181d4b108d10b7cf7632e0897231c
Sven Michael Klose <pixel@hugbox.org>
2011-02-10 21:19:40 +0000

    MEMBER: Return list starting with found element.

--- environment/transpiler/environment/member.lisp
+++ environment/transpiler/environment/member.lisp
@@ -1,10 +1,12 @@
 ;;;;; TRE environment
-;;;;; Copyright (C) 2005-2006,2008 Sven Klose <pixel@copei.de>
+;;;;; Copyright (C) 2005-2006,2008,2011 Sven Klose <pixel@copei.de>
 
 (defun %member-r (elm lst)
-  (dolist (i lst)
-    (when (equal elm i)
-	  (return i))))
+  (while lst
+         nil
+    (when (equal elm lst.)
+	  (return lst))
+    (setf lst .lst)))
 
 (defun member (elm &rest lsts)
   (dolist (i lsts)
