d9edd437876cd2ba921ba67f8553d70eacdc2f7f
Sven Michael Klose <pixel@hugbox.org>
2011-03-17 23:02:59 +0000

    EMPTY-STRING*: Now with variable arguments.

--- environment/stage4/empty-string.lisp
+++ environment/stage4/empty-string.lisp
@@ -1,12 +1,18 @@
 ;;;; TRE environment
-;;;; Copyright (c) 2008-2009 Sven Klose <pixel@copei.de>
+;;;; Copyright (c) 2008-2009.2011 Sven Klose <pixel@copei.de>
 
-(defun empty-string? (x)
-  "Tells if string is empty or contains just spaces."
+(defun %empty-string? (x)
+  (string= "" (or (trim #\  x) "")))
+
+(defun empty-string-0? (x)
+  (? x
+     (and (%empty-string? x.)
+          (empty-string-0? .x))
+	 t))
+
+(defun empty-string? (&rest x)
   (when x
-   	(aif (trim #\  x)
-   	     (string= "" !)
-		 t)))
+   	(empty-string-0? x)))
 
 ; XXX Needs TRIM first.
 (define-test "EMPTY-STRING? works"
@@ -14,6 +20,7 @@
 		(empty-string? "")))
   t)
 
-(defun empty-string-or-nil? (x)
-  (or (not x)
-	  (empty-string? x)))
+(defun empty-string-or-nil? (&rest x)
+  (every (fn or (not _)
+                (%empty-string? _))
+         x))
