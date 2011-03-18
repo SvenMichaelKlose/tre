edad2b822918c59a2fddb58fd0add3cf83ff9050
Sven Michael Klose <pixel@hugbox.org>
2011-03-18 05:11:11 +0000

    EQUAL: Use = insteal of %%%=.

--- environment/transpiler/environment/equality.lisp
+++ environment/transpiler/environment/equality.lisp
@@ -1,5 +1,5 @@
 ;;;;; TRE transpiler environment
-;;;;; Copyright (c) 2008-2009 Sven Klose <pixel@copei.de>
+;;;;; Copyright (c) 2008-2009,2011 Sven Klose <pixel@copei.de>
 
 (defun eq (x y)
   (%%%eq x y))
@@ -9,5 +9,5 @@
 	(setq x nil))
   (unless y
 	(setq y nil))
-  (or (eq x y)
-	  (%%%= x y)))
+  (or (%%%eq x y)
+	  (= x y)))
