e24218c027202de4d14b64e9fe9a9379eb3e5dab
Sven Michael Klose <pixel@hugbox.org>
2011-03-23 23:46:01 +0000

    Adapt to new environment PRINT.

--- environment/transpiler/environment/print.lisp
+++ environment/transpiler/environment/print.lisp
@@ -1,11 +1,11 @@
 ;;;;; TRE transpiler environment
-;;;;; Copyright (c) 2009 Sven Klose <pixel@copei.de>
+;;;;; Copyright (c) 2009,2011 Sven Klose <pixel@copei.de>
 
 (dont-inline print)
 
 (defun print (x &optional (str *standard-output*))
   (with-default-stream s str
-    (%late-print x s)))
+    (%late-print x s (make-hash-table :test #'eq))))
 
 (dont-inline force-output)
 
