20e10b89026f807d66733b3d3077bdefc774f596
Sven Michael Klose <pixel@hugbox.org>
2011-04-16 21:38:22 +0000

    Compare symbols by content.

--- environment/transpiler/targets/php/environment/eq.lisp
+++ environment/transpiler/targets/php/environment/eq.lisp
@@ -1,9 +1,10 @@
 ;;;;; Caroshi
 ;;;;; Copyright (c) 2011 Sven Klose <pixel@copei.de>
 
+(dont-obfuscate is_a)
+
 (defun eq (x y)
-  (? (symbol? x)
-     (and (symbol? y)
-          (string= (symbol-name x) (symbol-name y))
-          (%%%= (keyword? x) (keyword? y)))
-     (%%%eq x y)))
+  (or (%%%eq x y)
+      (and (is_a x "__symbol") (is_a y "__symbol")
+           (%%%= x.n y.n)
+           (%%%= (keyword? x) (keyword? y)))))
