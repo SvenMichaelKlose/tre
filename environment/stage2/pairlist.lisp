e6e3c0599c6f747d030ec5898fe9dba7510e8de6
Sven Michael Klose <pixel@hugbox.org>
2011-05-29 15:09:21 +0000

    PAIRLIST rewritten.

--- environment/stage2/pairlist.lisp
+++ environment/stage2/pairlist.lisp
@@ -1,9 +1,5 @@
 ;;;;; TRE environment
-;;;;; Copyright (c) 2006,2010 Sven Klose <pixel@copei.de>
+;;;;; Copyright (c) 2006,2010-2011 Sven Klose <pixel@copei.de>
 
 (defun pairlist (keys vals)
-  (if keys   
-      (acons (car keys) (car vals)
-             (pairlist (cdr keys) (cdr vals)))
-      (if vals
-        (%error "lists must have the same length"))))
+  (mapcar #'cons keys vals))
