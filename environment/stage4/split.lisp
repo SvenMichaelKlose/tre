e2ea5100c59a060e1dffc0dd9db103d29057322a
Sven Michael Klose <pixel@hugbox.org>
2011-06-20 11:37:16 +0000

    Faster version of SPLIT for JavaScript.

--- environment/stage4/split.lisp
+++ environment/stage4/split.lisp
@@ -1,7 +1,7 @@
 ;;;; TRE environment
-;;;; Copyright (c) 2008-2009 Sven Klose <pixel@copei.de>
+;;;; Copyright (c) 2008-2009,2011 Sven Klose <pixel@copei.de>
 
-(defun split (obj seq &key (test #'eql))
+(defun generic-split (obj seq &key (test #'eql))
   "Split sequence where element is equal to 'obj' and excluding them."
   (and seq
        (let pos (position obj seq :test test)
@@ -11,6 +11,9 @@
 						  (subseq seq (integer-1+ pos))
 					      :test test))
 		     (list seq)))))
+
+(defun split (obj seq &key (test #'eql))
+  (generic-split obj seq :test test))
 
 (define-test "SPLIT works on string"
   ((let x (split #\/ "foo/bar")
--- environment/transpiler/targets/javascript/environment/sequence.lisp
+++ environment/transpiler/targets/javascript/environment/sequence.lisp
@@ -4,8 +4,17 @@
 (dont-obfuscate length)
 
 (defun length (x)
-  (if x
-      (if (cons? x)
-	      (%list-length x)
-	      x.length)
-      0))
+  (? x
+     (? (cons? x)
+	     (%list-length x)
+	     x.length)
+     0))
+
+(defun split (obj seq &key (test #'eql))
+  (? (and (eq #'eql test)
+          (string? seq))
+     (?
+       (character? obj) (array-list (seq.split (char-string obj)))
+       (string? obj) (array-list (seq.split obj))
+       (generic-split obj seq))
+     (generic-split obj seq)))
