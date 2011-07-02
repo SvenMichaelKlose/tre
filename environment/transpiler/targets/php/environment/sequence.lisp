157b122b54b6660cc4af6e8a473d58f70342812c
Sven Michael Klose <pixel@hugbox.org>
2011-07-02 11:56:03 +0000

    Native version of SPLIT.

--- environment/transpiler/targets/php/environment/sequence.lisp
+++ environment/transpiler/targets/php/environment/sequence.lisp
@@ -9,3 +9,12 @@
     (cons? x) (%list-length x)
     (string? x) (strlen x)
     (sizeof x)))
+
+(defun split (obj seq &key (test #'eql))
+  (? (and (eq #'eql test)
+          (string? seq))
+     (array-list (explode (? (character? obj)
+                             (char-string obj)
+                             obj)
+                          seq))
+     (generic-split obj seq)))
