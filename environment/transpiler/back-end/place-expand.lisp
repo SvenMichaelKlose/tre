c07db09867d9ae0f3e737bc17ffac31c630050c1
Sven Michael Klose <pixel@hugbox.org>
2011-07-27 15:59:57 +0000

    Cleanup.

--- environment/transpiler/back-end/place-expand.lisp
+++ environment/transpiler/back-end/place-expand.lisp
@@ -82,36 +82,25 @@
 	   (,@(lambda-head fun-expr)
   	        ,@(place-expand-0 fi (lambda-body fun-expr))))))
 
+(defun place-expand-setter (fi x)
+  (let p (place-expand-0 fi (%setq-place x))
+    `(%set-vec ,.p. ,..p. ,...p. ,(place-expand-0 fi (%setq-value x)))))
+
 (define-tree-filter place-expand-0 (fi x)
-  (not fi)
-	(error "place-expand-0: no funinfo")
-  (atom x)
-    (place-expand-atom fi x)
+  (not fi) (error "place-expand-0: no funinfo")
+  (atom x) (place-expand-atom fi x)
   (or (%quote? x)
 	  (%transpiler-native? x)
-	  (%var? x))
-	x
-  (named-lambda? x)
-    (place-expand-fun fi .x. ..x.)
-  (lambda? x) ; XXX Add variables to ignore in subfunctions.
-    (place-expand-fun fi nil x)
-
+	  (%var? x)) x
+  (named-lambda? x) (place-expand-fun fi .x. ..x.)
+  (lambda? x) (place-expand-fun fi nil x) ; XXX Add variables to ignore in subfunctions.
   (and (%setq? x)
-       (%vec? (place-expand-0 fi (%setq-place x))))
-    (let p (place-expand-0 fi (%setq-place x))
-      `(%set-vec ,.p. ,..p. ,...p. ,(place-expand-0 fi (%setq-value x))))
-
+       (%vec? (place-expand-0 fi (%setq-place x)))) (place-expand-setter fi x)
   (and (%set-atom-fun? x)
-       (%vec? (place-expand-0 fi (%setq-place x))))
-    (let p (place-expand-0 fi (%setq-place x))
-      `(%set-vec ,.p. ,..p. ,...p. ,(place-expand-0 fi (%setq-value x))))
-
-  (%%funref? x)
-    `(%%funref ,.x. ,(place-expand-0 fi ..x.))
-  (%setq-atom-value? x)
-    `(%setq-atom-value ,.x. ,(place-expand-0 fi ..x.))
-  (%slot-value? x)
-    `(%slot-value ,(place-expand-0 fi .x.) ,..x.))
+       (%vec? (place-expand-0 fi (%setq-place x)))) (place-expand-setter fi x)
+  (%%funref? x) `(%%funref ,.x. ,(place-expand-0 fi ..x.))
+  (%setq-atom-value? x) `(%setq-atom-value ,.x. ,(place-expand-0 fi ..x.))
+  (%slot-value? x) `(%slot-value ,(place-expand-0 fi .x.) ,..x.))
 
 (defun place-expand (x)
   (place-expand-0 (transpiler-global-funinfo *current-transpiler*) x))
