460c4d32a725db83b40bcff115432a746a2f83e1
Sven Michael Klose <pixel@hugbox.org>
2011-11-01 15:20:37 +0000

    Don't quote hash table keywords.

--- environment/transpiler/middle-end/quote-keywords.lisp
+++ environment/transpiler/middle-end/quote-keywords.lisp
@@ -1,10 +1,9 @@
-;;;;; TRE transpiler
-;;;;; Copyright (c) 2008-2011 Sven Klose <pixel@copei.de>
+;;;;; tré - Copyright (c) 2008-2011 Sven Klose <pixel@copei.de>
 
 (define-tree-filter transpiler-quote-keywords (x)
   (or (%quote? x)
    	  (and (cons? x)
-     	   (eq 'make-hash-table x.)))
+     	   (in? x. 'make-hash-table '%%%make-hash-table)))
 	x
   (keyword? x) 
     `(%quote ,x))
