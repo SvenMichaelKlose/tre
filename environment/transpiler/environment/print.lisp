abfafef8cb9af7debaca7cbb2c1b99a263658428
Sven Michael Klose <pixel@hugbox.org>
2011-11-01 21:04:23 +0000

    PRINT: Use LATE-PRINT properly.

--- environment/transpiler/environment/print.lisp
+++ environment/transpiler/environment/print.lisp
@@ -1,11 +1,9 @@
-;;;;; TRE transpiler environment
-;;;;; Copyright (c) 2009,2011 Sven Klose <pixel@copei.de>
+;;;;; tré - Copyright (c) 2009,2011 Sven Klose <pixel@copei.de>
 
 (dont-inline print)
 
 (defun print (x &optional (str *standard-output*))
-  (with-default-stream s str
-    (%late-print x s (make-hash-table :test #'eq))))
+  (late-print x str))
 
 (dont-inline force-output)
 
