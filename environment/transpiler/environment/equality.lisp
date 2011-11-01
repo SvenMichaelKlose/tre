23df2c9cf93fd1cb93548c1899cc7cec097a7470
Sven Michael Klose <pixel@hugbox.org>
2011-11-01 17:39:04 +0000

    EQL: Return NIL if numbers don't have the same type.

--- environment/transpiler/environment/equality.lisp
+++ environment/transpiler/environment/equality.lisp
@@ -1,5 +1,4 @@
-;;;;; TRE transpiler environment
-;;;;; Copyright (c) 2008-2009,2011 Sven Klose <pixel@copei.de>
+;;;;; tré - Copyright (c) 2008-2009,2011 Sven Klose <pixel@copei.de>
 
 (defun eql (x y)
   (unless x			; Convert falsity to 'null'.
@@ -7,4 +6,10 @@
   (unless y
 	(setq y nil))
   (or (%%%eq x y)
-	  (= x y)))
+      (? (or (number? x) (number? y))
+         (?
+           (and (integer? x) (integer? y))
+	         (integer= x y)
+           (and (character? x) (character? y))
+	         (character= x y))
+	    (= x y))))
