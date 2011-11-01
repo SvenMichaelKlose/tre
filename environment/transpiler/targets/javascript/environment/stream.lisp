5def1f9c5006ac5343159fe7a7441d495d06faf1
Sven Michael Klose <pixel@hugbox.org>
2011-11-01 12:19:42 +0000

    Have dummy MAKE-STANDARD-STREAM.

--- environment/transpiler/targets/javascript/environment/stream.lisp
+++ environment/transpiler/targets/javascript/environment/stream.lisp
@@ -1,4 +1,5 @@
-;;;;; Transpiler: TRE to JavaScript
-;;;;; Copyright (c) 2008-2009 Sven Klose <pixel@copei.de>
+;;;;; tré - Copyright (c) 2008-2009,2011 Sven Klose <pixel@copei.de>
 
 (defun %force-output (&optional strm))
+
+(defun make-standard-stream ())
