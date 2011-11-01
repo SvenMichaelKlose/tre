f95d29fd9218d14d1f02f9cfd30cd4ed66da5eef
Sven Michael Klose <pixel@hugbox.org>
2011-11-01 17:38:24 +0000

    EQL: Return NIL if numbers don't have the same type.

--- environment/transpiler/targets/javascript/environment/number.lisp
+++ environment/transpiler/targets/javascript/environment/number.lisp
@@ -1,5 +1,4 @@
-;;;;; Transpiler: TRE to JavaScript
-;;;;; Copyright (c) 2008-2011 Sven Klose <pixel@copei.de>
+;;;;; tré - Copyright (c) 2008-2011 Sven Klose <pixel@copei.de>
 
 (js-type-predicate %number? "number")
 
@@ -16,3 +15,9 @@
 (defun number-integer (x)
   (declare type number x)
   (*math.floor x))
+
+(dont-obfuscate is-na-n)
+
+(defun integer? (x)
+  (and (not (is-na-n x))
+       (%%%= (parse-int x 10) (parse-float x 10))))
