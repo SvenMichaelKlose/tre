46f06119229f706050575a1ad6ac7cf174d058bc
Sven Michael Klose <pixel@hugbox.org>
2011-12-21 14:40:46 +0000

    Fix naming.

--- environment/stage4/less-or-equal-list.lisp
+++ environment/stage4/less-or-equal-list.lisp
@@ -1,27 +1,21 @@
-;;;;; TRE environment
-;;;;; Copyright (c) 2009 Sven Klose <pixel@copei.de>
+;;;;; tré - Copyright (c) 2009,2011 Sven Klose <pixel@copei.de>
 
-(defun <=-list (a b)
-  (if
-	(and (not a)
-		 (not b))
-	  t
-	(and (not a) b)
-	  t
-	(and a (not b))
-	  nil
-	(= a. b.)
-	  (<=-list .a .b)
+(defun list<= (a b)
+  (?
+	(or (not a b)
+	    (and (not a) b))  t
+	(and a (not b))       nil
+	(= a. b.)             (list<= .a .b)
 	(< a. b.)))
 
 (define-test "<=-LIST: first list is less"
-  ((<=-list '(1 2) '(2 2)))
+  ((list<= '(1 2) '(2 2)))
   t)
 
 (define-test "<=-LIST: equal"
-  ((<=-list '(2 2) '(2 2)))
+  ((list<= '(2 2) '(2 2)))
   t)
 
 (define-test "<=-LIST: first list is greater"
-  ((<=-list '(3 2) '(2 2)))
+  ((list<= '(3 2) '(2 2)))
   nil)
--- environment/stage4/less-or-equal-string.lisp
+++ environment/stage4/less-or-equal-string.lisp
@@ -1,6 +1,4 @@
-;;;;; TRE environment
-;;;;; Copyright (c) 2009 Sven Klose <pixel@copei.de>
+;;;;; tré - Copyright (c) 2009,2011 Sven Klose <pixel@copei.de>
 
-(defun <=-string (a b)
-  (<=-list (string-list a)
-		   (string-list b)))
+(defun string<= (a b)
+  (list<= (string-list a) (string-list b)))
