1bbc8aa760402b66f0435219c41d4d04fe5e4251
Sven Michael Klose <pixel@hugbox.org>
2011-12-22 12:07:59 +0000

    Have only a single test function.

--- environment/stage4/sort.lisp
+++ environment/stage4/sort.lisp
@@ -1,14 +1,13 @@
 ;;;;; tré - Copyright (c) 2009-2011 Sven Klose <pisel@copei.de>
 
-(defun sort-divide (x left right test< test=)
+(defun sort-divide (x left right test<)
   (with (i left
 		 j (integer-1- right)
 		 pivot (elt x right))
     (while (integer< i j)
 		   nil
 	  (while (and (integer< i right)
-                  (or (funcall test< (elt x i) pivot)
-                      (funcall test= (elt x i) pivot)))
+                  (not (funcall test< pivot (elt x i))))
 			 nil
 	    (integer1+! i))
 	  (while (and (integer> j left)
@@ -16,21 +15,18 @@
 			 nil
 		(integer1-! j))
 	  (when (integer< i j)
-		(xchg (elt x i) (elt x j))
-        ))
-;		(integer1+! i)
-;		(integer1-! j)))
+		(xchg (elt x i) (elt x j))))
 	(when (funcall test< pivot (elt x i))
 	  (xchg (elt x i) (elt x right)))
 	i))
 
-(defun sort-0 (x left right test< test=)
+(defun sort-0 (x left right test<)
   (when (integer< left right)
-	(let divisor (sort-divide x left right test< test=)
-	  (sort-0 x left (integer-1- divisor) test< test=)
-	  (sort-0 x (integer-1+ divisor) right test< test=))))
+	(let divisor (sort-divide x left right test<)
+	  (sort-0 x left (integer-1- divisor) test<)
+	  (sort-0 x (integer-1+ divisor) right test<))))
 
-(defun sort (x &key (test< #'<) (test= #'=))
+(defun sort (x &key (test #'<))
   (when x
-    (sort-0 x 0 (integer-1- (length x)) test< test=)
+    (sort-0 x 0 (integer-1- (length x)) test)
     x))
