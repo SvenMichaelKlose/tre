cd9624a3147b958c70bf3fedd3c5b809ff51f9c9
Sven Michael Klose <pixel@hugbox.org>
2011-12-22 12:01:36 +0000

    Fix comparisons.

--- environment/stage4/sort.lisp
+++ environment/stage4/sort.lisp
@@ -1,35 +1,36 @@
-;;;;; TRE environment
-;;;;; Copyright (c) 2009-2010 Sven Klose <pisel@copei.de>
+;;;;; tré - Copyright (c) 2009-2011 Sven Klose <pisel@copei.de>
 
-(defun sort-divide (x left right test)
+(defun sort-divide (x left right test< test=)
   (with (i left
 		 j (integer-1- right)
 		 pivot (elt x right))
     (while (integer< i j)
 		   nil
-	  (while (and (funcall test (elt x i) pivot)
-				  (integer< i right))
+	  (while (and (integer< i right)
+                  (or (funcall test< (elt x i) pivot)
+                      (funcall test= (elt x i) pivot)))
 			 nil
 	    (integer1+! i))
-	  (while (and (funcall test pivot (elt x j))
-				  (integer> j left))
+	  (while (and (integer> j left)
+                  (funcall test< pivot (elt x j)))
 			 nil
 		(integer1-! j))
 	  (when (integer< i j)
 		(xchg (elt x i) (elt x j))
-		(integer1+! i)
-		(integer1-! j)))
-	(when (funcall test pivot (elt x i))
+        ))
+;		(integer1+! i)
+;		(integer1-! j)))
+	(when (funcall test< pivot (elt x i))
 	  (xchg (elt x i) (elt x right)))
 	i))
 
-(defun sort-0 (x left right test)
+(defun sort-0 (x left right test< test=)
   (when (integer< left right)
-	(let divisor (sort-divide x left right test)
-	  (sort-0 x left (integer-1- divisor) test)
-	  (sort-0 x (integer-1+ divisor) right test))))
+	(let divisor (sort-divide x left right test< test=)
+	  (sort-0 x left (integer-1- divisor) test< test=)
+	  (sort-0 x (integer-1+ divisor) right test< test=))))
 
-(defun sort (x &key (test #'<=))
+(defun sort (x &key (test< #'<) (test= #'=))
   (when x
-    (sort-0 x 0 (integer-1- (length x)) test)
+    (sort-0 x 0 (integer-1- (length x)) test< test=)
     x))
