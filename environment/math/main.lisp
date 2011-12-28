6b254f97d08d0f581c6494ef51c86b32269017da
Sven Michael Klose <pixel@hugbox.org>
2011-12-28 15:54:23 +0000

    ...

--- environment/math/main.lisp
+++ environment/math/main.lisp
@@ -1,4 +1,4 @@
-;;;; TRE environment
-;;;; Copyright (c) 2008 Sven Klose <pixel@copei.de>
+;;;; tré - Copyright (c) 2008,2011 Sven Klose <pixel@copei.de>
 
+(env-load "math/min.lisp")
 (env-load "math/saturate.lisp")
