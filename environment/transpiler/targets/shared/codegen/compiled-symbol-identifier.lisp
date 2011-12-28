b4f88f0cb211e98ebf058470e7f7c1af86a263b3
Sven Michael Klose <pixel@hugbox.org>
2011-12-28 15:50:35 +0000

    Rollback to work around,

--- environment/transpiler/targets/shared/codegen/compiled-symbol-identifier.lisp
+++ environment/transpiler/targets/shared/codegen/compiled-symbol-identifier.lisp
@@ -1,6 +1,7 @@
 ;;;;; tré - Copyright (c) 2011 Sven Klose <pixel@copei.de>
 
 (defun compiled-symbol-identifier (x)
-  ($ 'compiled-symbol- x (aif (symbol-package x)
-                              ($ '-k- (symbol-name !))
-                              "")))
+  (gensym))
+;  ($ 'compiled-symbol- x (aif (symbol-package x)
+;                              ($ '-k- (symbol-name !))
+;                              "")))
