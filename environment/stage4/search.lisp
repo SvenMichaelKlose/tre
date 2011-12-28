5e2736bcca6ed0879cf5e6151a2ae569a4c7513f
Sven Michael Klose <pixel@hugbox.org>
2011-12-28 15:43:30 +0000

    Treat special case when sequence is empty.

--- environment/stage4/search.lisp
+++ environment/stage4/search.lisp
@@ -2,6 +2,7 @@
 
 (defun search (needle haystack &key (test #'eql))
   (and haystack
+       (not (zero? (length haystack)))
        (or (and (funcall test needle (subseq haystack 0 (length needle)))
                 haystack)
            (search needle (subseq haystack 1) :test test))))
