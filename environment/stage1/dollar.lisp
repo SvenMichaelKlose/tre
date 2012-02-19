78005ba9cf9e88d96f0f97e4f7d889bbe9aebc6f
Sven Michael Klose <pixel@hugbox.org>
2012-02-19 16:35:25 +0000

    User faster FILTER.

--- environment/stage1/dollar.lisp
+++ environment/stage1/dollar.lisp
@@ -1,10 +1,9 @@
-;;;;; TRE environment
-;;;;; Copyright (c) 2008,2011 Sven Klose <pixel@copei.de>
+;;;;; tré - Copyright (c) 2008,2011-2012 Sven Michael Klose <pixel@copei.de>
 
 (functional $)
 
 (defun $ (&rest args)
-  (make-symbol (apply #'string-concat (mapcar #'string args))))
+  (make-symbol (apply #'string-concat (filter #'string args))))
 
 (define-test "$"
   (($ "DOLLAR-" 'test))
