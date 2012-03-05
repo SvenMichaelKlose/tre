48123b7644874ab830bb2438e4cb2afe76d54876
Sven Michael Klose <pixel@hugbox.org>
2012-03-05 11:58:56 +0000

    Cleanup.

--- doc/compile-time-options.lisp
+++ doc/compile-time-options.lisp
@@ -11,7 +11,7 @@
         TRE_BOOT_IMAGE (if exists).
 
     TRE_BOOT_IMAGE_HEADER
-        String to prefix images with. Used to for hash bang and informative
+        String to prefix images with. Used for hash bang and informative
         messages.
 
     TRE_DIAGNOSTICS
@@ -47,18 +47,3 @@
 
     TRE_VERBOSE_READ      
 	Print READ expressions in read-eval loop.
-
-### TERMINOLOGY ############################################################
-
-    expression, S-expression ("symbolic expression")
-	A textual representation of an object.
-
-    non-atomic S-expression ("dotted-pair")
-	A binary tree of conses whose leaf nodes are atoms.
-
-    pure list
-	An S-expression where every element x
-
-	    (AND (NOT (CONSP (CAR x)))
-	         (LISTP (CDR x)))
-	    => T
