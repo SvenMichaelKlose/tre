355b47a3eb1bbdcb557ae2a26210f5afcf4888ca
Sven Michael Klose <pixel@hugbox.org>
2012-03-08 19:18:36 +0000

    Strictest test on 'true' for T.

--- environment/transpiler/targets/javascript/environment/late-symbol.lisp
+++ environment/transpiler/targets/javascript/environment/late-symbol.lisp
@@ -12,28 +12,28 @@
 
 (defun symbol-name (x)
   (?
-    (%%%= t x) ,*t-symbol-name*
+    (%%%eq t x) ,*t-symbol-name*
     x x.n
     ,*nil-symbol-name*))
 
 (defun symbol-value (x)
   (?
-    (%%%= t x) t
+    (%%%eq t x) t
     x x.v))
 
 (defun symbol-function (x)
   (?
-    (%%%= t x) nil
+    (%%%eq t x) nil
     x x.f))
 
 (defun symbol-package (x)
   (?
-    (%%%= t x) nil
+    (%%%eq t x) nil
     x x.p))
 
 (defun symbol? (x)
   (or (not x)
-      (%%%= t x)
+      (%%%eq t x)
       (and (object? x)
 	       x.__class
            (%%%= x.__class ,(transpiler-obfuscated-symbol-string *current-transpiler* 'symbol)))))
