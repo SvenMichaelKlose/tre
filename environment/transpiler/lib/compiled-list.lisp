0ea89c17da1a8c228e593fdd7b6db53edf920153
Sven Michael Klose <pixel@hugbox.org>
2012-03-30 08:52:48 +0000

    Also make quoted symbols if so desired.

--- environment/transpiler/lib/compiled-list.lisp
+++ environment/transpiler/lib/compiled-list.lisp
@@ -1,14 +1,18 @@
-;;;; TRE compiler
-;;;; Copyright (c) 2005-2009,2011 Sven Klose <pixel@copei.de>
+;;;;; tré - Copyright (c) 2005-2009,2011-2012 Sven Michael Klose <pixel@copei.de>
 
-(defun compiled-list (x)
-  (? (cons? x)
-     `(cons ,x.
-            ,(compiled-list .x))
-	 x))
+(defun %compiled-atom (x quoted?)
+  (? (and quoted? x (symbol? x))
+     (list 'quote x)
+     x))
 
-(defun compiled-tree (x)
+(defun compiled-list (x &key (quoted? nil))
   (? (cons? x)
-     `(cons ,(compiled-tree x.)
-            ,(compiled-tree .x))
-	 x))
+     `(cons ,(%compiled-atom x. quoted?)
+            ,(compiled-list .x :quoted? quoted?))
+	 (%compiled-atom x quoted?)))
+
+(defun compiled-tree (x &key (quoted? nil))
+  (? (cons? x)
+     `(cons ,(compiled-tree x. :quoted? quoted?)
+            ,(compiled-tree .x :quoted? quoted?))
+	 (%compiled-atom x quoted?)))
