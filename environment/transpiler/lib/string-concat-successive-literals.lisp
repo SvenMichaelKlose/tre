8d330268a01abcaa7c08430fcd3c6f4146bac8ee
Sven Michael Klose <pixel@hugbox.org>
2012-04-08 17:00:11 +0000

    Cleanup.

--- environment/transpiler/lib/string-concat-successive-literals.lisp
+++ environment/transpiler/lib/string-concat-successive-literals.lisp
@@ -1,17 +1,16 @@
-;;;;; Transpiler: TRE to JavaScript
-;;;;; Copyright (c) 2010-2011 Sven Klose <pixel@copei.de>
+;;;;; tré - Copyright (c) 2010-2012 Sven Michael Klose <pixel@copei.de>
 
 (defun string-concat-successive-literals-0 (x y)
-  (if
+  (?
 	(not x)
 	  (awhen y
 	    (list !))
 	(not x.)
 	  (string-concat-successive-literals-0 .x nil)
 	(string? x.)
-   	  (if y
-		  (string-concat-successive-literals-0 .x (string-concat y x.))
-   	  	  (string-concat-successive-literals-0 .x x.))
+   	  (? y
+	     (string-concat-successive-literals-0 .x (string-concat y x.))
+   	  	 (string-concat-successive-literals-0 .x x.))
 	y
 	  (cons y (cons x. (string-concat-successive-literals-0 .x nil)))
 	(cons x. (string-concat-successive-literals-0 .x nil))))
