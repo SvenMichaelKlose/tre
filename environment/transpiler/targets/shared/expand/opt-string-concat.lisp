28c11c0d861aefab00333e0094f68c0db9971ee1
Sven Michael Klose <pixel@hugbox.org>
2012-04-08 17:00:58 +0000

    Add missing file.

---
+++ environment/transpiler/targets/shared/expand/opt-string-concat.lisp
@@ -0,0 +1,11 @@
+;;;;; tré - Copyright (c) 2088-2012 Sven Michael Klose
+
+(defun opt-string-concat (x op)
+  (?
+    (not .x) x.
+    (every #'string? x) (apply #'string-concat x)
+    (some #'string? x) (alet (string-concat-successive-literals x)
+                         (? .!
+                           `(string-concat ,@!)
+                           !.))
+    `(,op ,@x)))
