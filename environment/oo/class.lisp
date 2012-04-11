3593c88e38399b8207bb61359a0b299cdb33e544
Sven Michael Klose <pixel@hugbox.org>
2012-04-11 17:53:34 +0000

    New getters and setters.

--- environment/oo/class.lisp
+++ environment/oo/class.lisp
@@ -1,7 +1,18 @@
-;;;;; TRE environment
-;;;;; Copyright (c) 2008 Sven Klose <pixel@copei.de>
+;;;;; tré - Copyright (c) 2008,2012 Sven Michael Klose <pixel@copei.de>
 
 (defstruct class
   (members nil)
   (methods nil)
   (parent nil))
+
+(defun class-add-method (cls name code)
+  (acons! name code (class-methods cls)))
+
+(defun class-change-method (cls name code)
+  (setf (assoc-value name (class-methods cls)) code))
+
+(defun class-method (cls name)
+  (assoc-value name cls))
+
+(defun class-add-member (cls name)
+  (push (list name t) (class-members cls)))
