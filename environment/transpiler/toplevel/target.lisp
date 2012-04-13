b142e6f0f3d90568b906ba979069cd802f4dd7ce
Sven Michael Klose <pixel@hugbox.org>
2012-04-13 14:36:17 +0000

    When recompiling, import newly missing functions.

--- environment/transpiler/toplevel/target.lisp
+++ environment/transpiler/toplevel/target.lisp
@@ -10,9 +10,8 @@
      (string= x y)))
 
 (defun compile-file? (file processed-files files-to-update)
-  (or (eq 'imported-deps file)
-      (member file files-to-update :test #'eq-string=)
-      (not (assoc-value file processed-files :test #'eq-string=))))
+  (or (member file files-to-update :test #'eq-string=)
+      (not (assoc file processed-files :test #'eq-string=))))
 
 (defun target-transpile-2 (tr files files-to-update)
   (let compiled-code (make-queue)
@@ -28,23 +27,18 @@
 	(dolist (i files (queue-list sightened-code))
       (let code (? (compile-file? i. (transpiler-sightened-files tr) files-to-update)
                    (? (symbol? i.)
-                      (transpiler-sighten tr (? (function? .i)
-                                                (funcall .i)
-                                                .i))
+                      (transpiler-sighten tr (? (function? .i) (funcall .i) .i))
 		  			  (transpiler-sighten-file tr i.))
                    (assoc-value i. (transpiler-sightened-files tr) :test #'eq-string=))
         (assoc-adjoin code i. (transpiler-sightened-files tr) :test #'eq-string=)
 	    (enqueue sightened-code (cons i. code))))))
 
-(defun target-transpile-add-deps (dep-gen after-deps)
+(defun target-sighten-deps (tr dep-gen)
   (when dep-gen
     (when *have-compiler?*
       (setf *save-compiled-source?* t)
       (clr *save-args-only?*))
-    (alet (assoc-value 'imported-deps after-deps :test #'eq-string=)
-      (setf after-deps (aremove 'imported-deps after-deps :test #'eq-string=))
-      (acons! 'imported-deps (append ! (funcall dep-gen)) after-deps)))
-  after-deps)
+    (funcall dep-gen)))
 
 (defun target-transpile (tr &key (files-before-deps nil)
                                  (files-after-deps nil)
@@ -59,14 +53,19 @@
       (clr (transpiler-emitted-decls tr)))
     (transpiler-switch-obfuscator tr obfuscate?)
     (with (before-deps (target-transpile-1 tr files-before-deps files-to-update)
-		   after-deps  (target-transpile-add-deps dep-gen (target-transpile-1 tr files-after-deps files-to-update))
+		   after-deps  (target-transpile-1 tr files-after-deps files-to-update)
+		   deps        (target-sighten-deps tr dep-gen)
 	       compiled-before (target-transpile-2 tr before-deps files-to-update)
+	       compiled-deps   (awhen deps (transpiler-make-code tr !))
 		   compiled-after  (target-transpile-2 tr after-deps files-to-update))
+      (awhen compiled-deps
+        (setf (transpiler-imported-deps tr) (string-concat (transpiler-imported-deps tr) !)))
 	  (prog1
 	    (concat-stringtree (awhen decl-gen
 	                         (funcall !))
 	                       compiled-before
                            (reverse (transpiler-raw-decls tr))
+                           (transpiler-imported-deps tr)
 	                       compiled-after)
         (when (and print-obfuscations? (transpiler-obfuscate? tr))
           (transpiler-print-obfuscations tr))))))
