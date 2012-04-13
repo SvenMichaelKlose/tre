328b77e21bd72f6271383f5013276d939db87a08
Sven Michael Klose <pixel@hugbox.org>
2012-04-13 02:08:55 +0000

    Long awaited cleanup...

--- environment/transpiler/toplevel/target.lisp
+++ environment/transpiler/toplevel/target.lisp
@@ -10,7 +10,8 @@
      (string= x y)))
 
 (defun compile-file? (file processed-files files-to-update)
-  (or (member file files-to-update :test #'eq-string=)
+  (or (eq 'imported-deps file)
+      (member file files-to-update :test #'eq-string=)
       (not (assoc-value file processed-files :test #'eq-string=))))
 
 (defun target-transpile-2 (tr files files-to-update)
@@ -35,65 +36,37 @@
         (assoc-adjoin code i. (transpiler-sightened-files tr) :test #'eq-string=)
 	    (enqueue sightened-code (cons i. code))))))
 
-(defun target-transpile-generic (tr &key (front-before nil)
-							  	   		 (front-after nil)
-							  	   		 (back-before nil)
-							  	   		 (back-after nil)
-								   		 (dep-gen nil)
-							 	   		 (decl-gen nil)
-                                         (print-obfuscations? nil))
-  (with (before-deps (funcall front-before)
-		 after-deps  (funcall front-after))
-	(awhen dep-gen
-      (when *have-compiler?*
-        (setf *save-compiled-source?* t)
-        (clr *save-args-only?*))
-      (let former-deps (assoc-value 'imported-deps after-deps :test #'eq-string=)
-        (setf after-deps (aremove 'imported-deps after-deps :test #'eq-string=))
-        (acons! 'imported-deps (append former-deps (funcall !)) after-deps)))
-	(with (compiled-before (funcall back-before before-deps)
-		   compiled-after  (funcall back-after after-deps))
-	  (prog1
-	    (concat-stringtree (awhen decl-gen
-		                     (funcall !))
-			               compiled-before
-                           (reverse (transpiler-raw-decls tr))
-			               compiled-after)
-        (when (and print-obfuscations? (transpiler-obfuscate? tr))
-          (transpiler-print-obfuscations tr))))))
+(defun target-transpile-add-deps (dep-gen after-deps)
+  (when dep-gen
+    (when *have-compiler?*
+      (setf *save-compiled-source?* t)
+      (clr *save-args-only?*))
+    (alet (assoc-value 'imported-deps after-deps :test #'eq-string=)
+      (setf after-deps (aremove 'imported-deps after-deps :test #'eq-string=))
+      (acons! 'imported-deps (append ! (funcall dep-gen)) after-deps)))
+  after-deps)
 
-(defun target-transpile-0 (tr &key (files-after-deps nil)
-							  	   (files-before-deps nil)
-								   (dep-gen nil)
-							 	   (decl-gen nil)
-                                   (files-to-update nil)
-                                   (print-obfuscations? nil))
-  (target-transpile-generic tr
-	  :front-before #'(() (target-transpile-1 tr files-before-deps files-to-update))
-	  :front-after #'(() (target-transpile-1 tr files-after-deps files-to-update))
-	  :back-after #'((processed) (target-transpile-2 tr processed files-to-update))
-	  :back-before #'((processed) (target-transpile-2 tr processed files-to-update))
-	  :dep-gen dep-gen
-	  :decl-gen decl-gen
-      :print-obfuscations? print-obfuscations?))
-
-(defvar *updater* nil)
-
-(defun target-transpile (tr &key (files-after-deps nil)
-								 (files-before-deps nil)
-								 (files-to-update nil)
-								 (dep-gen nil)
-								 (decl-gen nil)
+(defun target-transpile (tr &key (files-before-deps nil)
+                                 (files-after-deps nil)
+                                 (files-to-update nil)
+                                 (dep-gen nil)
+                                 (decl-gen nil)
                                  (obfuscate? nil)
                                  (print-obfuscations? nil))
-  (with-temporary *recompiling?* (? files-to-update t)
+  (with-temporaries (*recompiling?* (? files-to-update t)
+                     *current-transpiler* tr)
     (when files-to-update
       (clr (transpiler-emitted-decls tr)))
     (transpiler-switch-obfuscator tr obfuscate?)
-    (with-temporary *current-transpiler* tr
-	  (target-transpile-0 tr :files-after-deps files-after-deps
-					  	     :files-before-deps files-before-deps
-                             :files-to-update files-to-update
-					         :dep-gen dep-gen
-						     :decl-gen decl-gen
-                             :print-obfuscations? print-obfuscations?))))
+    (with (before-deps (target-transpile-1 tr files-before-deps files-to-update)
+		   after-deps  (target-transpile-add-deps dep-gen (target-transpile-1 tr files-after-deps files-to-update))
+	       compiled-before (target-transpile-2 tr before-deps files-to-update)
+		   compiled-after  (target-transpile-2 tr after-deps files-to-update))
+	  (prog1
+	    (concat-stringtree (awhen decl-gen
+	                         (funcall !))
+	                       compiled-before
+                           (reverse (transpiler-raw-decls tr))
+	                       compiled-after)
+        (when (and print-obfuscations? (transpiler-obfuscate? tr))
+          (transpiler-print-obfuscations tr))))))
