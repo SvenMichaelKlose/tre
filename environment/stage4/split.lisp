5e13cf5d846d2901565ee85e03b9fa42ed87b0b1
Sven Michael Klose <pixel@hugbox.org>
2012-04-20 13:31:37 +0000

    Cleanup.

--- environment/stage4/split.lisp
+++ environment/stage4/split.lisp
@@ -1,24 +1,22 @@
 ;;;;; tré - Copyright (c) 2008-2009,2011-2012 Sven Michael Klose <pixel@copei.de>
 
-(defun split-if (test seq)
+(defun split-if (predicate seq)
+  "Split sequence at elements for which the predicate is true. Those elements are excluded. Returns a list of subsequences."
   (and seq
-       (!? (position-if test seq)
+       (!? (position-if predicate seq)
            (cons (subseq seq 0 !)
-                 (split-if test (subseq seq (integer-1+ !))))
+                 (split-if predicate (subseq seq (integer-1+ !))))
            (list seq))))
 
 (defun generic-split (obj seq &key (test #'eql))
-  "Split sequence where element is equal to 'obj' and excluding them."
   (and seq
-       (let pos (position obj seq :test test)
-	     (? pos
-		    (cons (subseq seq 0 pos)
-		          (split obj
-					     (subseq seq (integer-1+ pos))
-				         :test test))
-		     (list seq)))))
+       (!? (position obj seq :test test)
+		   (cons (subseq seq 0 !)
+		         (generic-split obj (subseq seq (integer-1+ !)) :test test))
+		    (list seq))))
 
 (defun split (obj seq &key (test #'eql))
+  "Split sequence at elements equal to OBJ. Those elements are excluded. Returns a list of subsequences."
   (generic-split obj seq :test test))
 
 (define-test "SPLIT works on string"
