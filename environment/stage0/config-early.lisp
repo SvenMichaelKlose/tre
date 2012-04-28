20339aad9bdf75dc1fc09cf6fb781583c68ccde0
Sven Michael Klose <pixel@hugbox.org>
2012-04-28 21:52:02 +0000

    Show definitions by default.

--- environment/stage0/config-early.lisp
+++ environment/stage0/config-early.lisp
@@ -1,7 +1,6 @@
-;;;; TRE environment
-;;;; Copyright (c) 2005-2010 Sven Klose <pixel@copei.de>
+;;;; tré - Copyright (c) 2005-2010,2012 Sven Michael Klose <pixel@copei.de>
 
 (setq *variables* (cons (cons '*show-definitions* nil)
                         *variables*))
 
-(setq *show-definitions* nil)
+(setq *show-definitions* t)
