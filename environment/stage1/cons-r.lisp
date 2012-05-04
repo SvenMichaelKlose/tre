3083562560ca8f93156a91c6663684536e68d5fb
Sven Michael Klose <pixel@hugbox.org>
2012-05-04 15:15:26 +0000

    CONS-R: Can have additional arguments.

--- environment/stage1/cons-r.lisp
+++ environment/stage1/cons-r.lisp
@@ -1,8 +1,8 @@
-;;;;; TRE - Copyright (c) 2011 Sven Klose <pixel@copei.de>
+;;;;; tré - Copyright (c) 2011-2012 Sven Michael Klose <pixel@copei.de>
 
-(defmacro cons-r (fun-name x)
+(defmacro cons-r (fun-name x &rest args)
   (let g (gensym)
     `(#'((,g)
-          (cons (,fun-name (car ,g))
-                (,fun-name (cdr ,g))))
+          (cons (,fun-name (car ,g) ,@args)
+                (,fun-name (cdr ,g) ,@args)))
         ,x)))
