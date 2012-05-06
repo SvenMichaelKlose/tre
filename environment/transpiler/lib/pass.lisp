ce83fa2bb309fd1afdc5364daa6192fb50e20349
Sven Michael Klose <pixel@hugbox.org>
2012-05-06 13:50:25 +0000

    Don't compose functions at run-time.

--- environment/transpiler/lib/pass.lisp
+++ environment/transpiler/lib/pass.lisp
@@ -1,4 +1,4 @@
-;;;;; tré - Copyright (c) 2010-2012 Sven Klose <pixel@copei.de>
+;;;;; tré - Copyright (c) 2010-2012 Sven Michael Klose <pixel@copei.de>
 
 (defvar *transpiler-debug-dump* nil)
 (defvar *current-pass* nil)
@@ -7,18 +7,17 @@
   (let cache-var ($ '*pass- name '*)
     `(progn
        (defvar ,cache-var nil)
-       (defun ,name ,args
-         #'((init)
-             (setf ,cache-var init)
-             (dolist (i (list ,@(mapcan (fn `((? *transpiler-debug-dump*
-                                                 #'((x)
-                                                     (format t ,(string-concat "; **** before " (symbol-name _.) "~%"))
-                                                     (with-temporary *current-pass* ,(list 'quote _.)
-                                                       (prog1
-                                                         (print (funcall ,._. x))
-                                                         (format t ,(string-concat "; **** after " (symbol-name _.) "~%"))
-                                                         (force-output))))
-                                                 ,._.)))
-                                        (reverse (group x 2))))
-                      ,cache-var)
-             (setf ,cache-var (funcall i ,cache-var))))))))
+       (defun ,name (,@args ,init)
+         (setf ,cache-var init)
+         (dolist (i (list ,@(mapcan (fn `((? *transpiler-debug-dump*
+                                             #'((x)
+                                                 (format t ,(string-concat "; **** before " (symbol-name _.) "~%"))
+                                                 (with-temporary *current-pass* ,(list 'quote _.)
+                                                   (prog1
+                                                     (print (funcall ,._. x))
+                                                     (format t ,(string-concat "; **** after " (symbol-name _.) "~%"))
+                                                     (force-output))))
+                                             ,._.)))
+                                    (reverse (group x 2))))
+                   ,cache-var)
+           (setf ,cache-var (funcall i ,cache-var)))))))
