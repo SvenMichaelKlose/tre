a6becbb03a2508cd40e0542c177b590c7971f7a5
Sven Michael Klose <pixel@hugbox.org>
2012-05-24 19:54:34 +0000

    Remove unused file.

--- environment/transpiler/targets/javascript/main.lisp
+++ environment/transpiler/targets/javascript/main.lisp
@@ -1,11 +1,10 @@
-;;;; tré - Copyright (c) 2005-2010,2012 Sven Michael Klose <pixel@copei.de>
+;;;; tré – Copyright (c) 2005-2010,2012 Sven Michael Klose <pixel@copei.de>
 
 (env-load "transpiler/targets/javascript/config.lisp")
 (env-load "transpiler/targets/javascript/expand.lisp")
 (env-load "transpiler/targets/javascript/pre-std-macros.lisp")
 (env-load "transpiler/targets/javascript/class.lisp")
 (env-load "transpiler/targets/javascript/codegen.lisp")
-;(env-load "transpiler/targets/javascript/codegen-inline.lisp")
 (env-load "transpiler/targets/javascript/codegen-inline-cons.lisp")
 (env-load "transpiler/targets/javascript/env-load.lisp")
 (env-load "transpiler/targets/javascript/toplevel.lisp")
