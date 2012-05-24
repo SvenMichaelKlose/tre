60f7001497a108cecba4345f7db64029fe491fab
Sven Michael Klose <pixel@hugbox.org>
2012-05-24 12:35:45 +0000

    No transpiler debug dumps.

--- environment/transpiler/targets/javascript/environment/eval.lisp
+++ environment/transpiler/targets/javascript/environment/eval.lisp
@@ -14,7 +14,7 @@
     (dolist (i *defined-functions*)
       (let-when f (symbol-function i)
         (transpiler-add-defined-function tr i)
-        (transpiler-add-function-args tr i (car f.__source)) ; XXX dot notation!
+        (transpiler-add-function-args tr i (car f.__source))
         (transpiler-add-function-body tr i (cdr f.__source))))
     (setf *js-eval-transpiler* tr)))
 
@@ -24,16 +24,16 @@
        (transpiler-raw-decls tr))
   (concat-stringtree (js-transpile-pre tr)
    	                 (target-transpile tr :files-after-deps (list (cons 'eval (list expression)))
-	 	                                  :dep-gen #'(())
 		                                  :decl-gen (js-make-decl-gen tr))
    	                 (js-transpile-post)))
 
 (defun eval (x)
   (with-temporary *js-transpiler* (or *js-eval-transpiler* (make-js-eval-transpiler))
     (let tr *js-transpiler*
-      (%%%eval (+ (js-eval-transpile tr x)
-                  (transpiler-obfuscated-symbol-string tr '*native-eval-return-value*)
-                  " = "
-                  (transpiler-obfuscated-symbol-string tr '~%ret)
-                  ";"))
+      (%%%eval (with-temporary *transpiler-debug-dump* nil
+                 (+ (js-eval-transpile tr x)
+                    (transpiler-obfuscated-symbol-string tr '*native-eval-return-value*)
+                    " = "
+                    (transpiler-obfuscated-symbol-string tr '~%ret)
+                    ";")))
       *native-eval-return-value*)))
