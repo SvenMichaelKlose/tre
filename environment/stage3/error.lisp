1f3ccb5adb5d12f42d763810da355261dfb7234f
Sven Michael Klose <pixel@hugbox.org>
2012-05-27 17:40:16 +0000

    Cleanup,

--- environment/stage3/error.lisp
+++ environment/stage3/error.lisp
@@ -1,11 +1,10 @@
-;;;; TRE environment
-;;;; Copyright (c) 2006-2008,2011 Sven Klose <pixel@copei.de>
+;;;;; tré – Copyright (c) 2006–2008,2011–2012 Sven Michael Klose <pixel@copei.de>
 
 (defun error (&rest args)
   (%error (apply #'format nil args)))
 
 (defun warn (&rest args)
-  (apply #'format t (string-concat "; WARNING: " (car args)) (cdr args)))
+  (apply #'format t (string-concat "; WARNING: " args.) .args))
 
 (defun hint (&rest args)
-  (apply #'format t (string-concat "; HINT: " (car args)) (cdr args)))
+  (apply #'format t (string-concat "; HINT: " args.) .args))
