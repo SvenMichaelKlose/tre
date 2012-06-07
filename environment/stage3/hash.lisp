3804deebc5fd8a50763cec52c3cc618a09f37ed6
Sven Michael Klose <pixel@hugbox.org>
2012-06-07 21:46:41 +0000

    Tests added.

--- environment/stage3/hash.lisp
+++ environment/stage3/hash.lisp
@@ -71,3 +71,47 @@
                           :size (%hash-table-size h))
     (dolist (i (hashkeys h) n)
       (setf (href n i) (href h i)))))
+
+(define-test "HREF symbol key"
+  ((let h (make-hash-table :test #'eq)
+     (setf (href h 'a) 'x)
+     (eq (href h 'a) 'x)))
+  t)
+
+(define-test "HREF number key"
+  ((let h (make-hash-table :test #'eql)
+     (setf (href h 1) 'x)
+     (eq (href h 1) 'x)))
+  t)
+
+(define-test "HREF string key"
+  ((let h (make-hash-table :test #'string=)
+     (setf (href h "str") 'x)
+     (eq (href h "str") 'x)))
+  t)
+
+(define-test "HREF empty string key"
+  ((let h (make-hash-table :test #'string=)
+     (setf (href h "") 'x)
+     (eq (href h "") 'x)))
+  t)
+
+(define-test "HREF character key"
+  ((let h (make-hash-table :test #'eql)
+     (setf (href h #\a) 'x)
+     (eq (href h #\a) 'x)))
+  t)
+
+(define-test "HREF hashtable key"
+  ((with (h (make-hash-table :test #'eq)
+          k (make-hash-table))
+     (setf (href h k) 'x)
+     (eq (href h k) 'x)))
+  t)
+
+(define-test "HREF array key"
+  ((with (h (make-hash-table :test #'eq)
+          k (make-array 10))
+     (setf (href h k) 'x)
+     (eq (href h k) 'x)))
+  t)
