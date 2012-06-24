f6bc6c406dff9814d418ab480e5c3ea5e9bf0127
Sven Michael Klose <pixel@hugbox.org>
2012-06-24 12:49:06 +0000

    DECIMAL-DIGIT?, NONDECIMAL-DIGIT?: New functions.

--- environment/stage2/char.lisp
+++ environment/stage2/char.lisp
@@ -1,4 +1,4 @@
-;;;;; tré - Copyright (c) 2005-2006,2008-2012 Sven Michael Klose <pixel@copei.de>
+;;;;; tré – Copyright (c) 2005–2006,2008-2012 Sven Michael Klose <pixel@copei.de>
 
 (functional char-upcase char-downcase lower-case-p upper-case-p alpha-char-p digit-char-p alphanumericp
             char-code code-char)
@@ -30,16 +30,29 @@
   (or (lower-case-p c)
       (upper-case-p c)))
 
+(defun decimal-digit? (x)
+  (range-p x #\0 #\9))
+
+(defun %nondecimal-digit? (x start base)
+  (range-p x start (character+ start (character- base 10))))
+
+(defun nondecimal-digit? (x &key (base 10))
+  (and (< 10 base)
+       (or (%nondecimal-digit? x #\a base)
+           (%nondecimal-digit? x #\A base))))
+
 (defun digit-char-p (c &key (base 10))
-  (labels ((digit-p ()
-             (range-p c #\0 #\9))
-           (digit-alpha-p (start)
-             (range-p c start (character+ start (character- base 10)))))
-	(and (character? c)
-   	     (or (digit-p)
-      	     (and (< 10 base)
-        	      (or (digit-alpha-p #\a)
-				      (digit-alpha-p #\A)))))))
+  (and (character? c)
+       (or (decimal-digit? c)
+           (nondecimal-digit? c :base base))))
+
+(define-test "DIGIT-CHAR-P #\0"
+  ((digit-char-p #\0))
+  t)
+
+(define-test "DIGIT-CHAR-P #\a"
+  ((digit-char-p #\a))
+  nil)
 
 (def-rest-predicate alphanumericp c ()
   (or (alpha-char-p c)
