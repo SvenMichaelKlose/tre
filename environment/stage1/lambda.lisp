416107c0650076965f3de1bf8eebad8d4bd672b2
Sven Michael Klose <pixel@hugbox.org>
2012-07-09 10:23:07 +0000

    Cleanup.

--- environment/stage1/lambda.lisp
+++ environment/stage1/lambda.lisp
@@ -1,4 +1,24 @@
-;;;; tré – Copyright (c) 2006–2012 Sven Michael Klose <pixel@copei.de>
+;;;;; tré – Copyright (c) 2006–2012 Sven Michael Klose <pixel@copei.de>
+
+;;;; A LAMBDA-expression is an anonymous function.  In many other dialects
+;;:: the LAMBDA symbol is required to describe anonymous functions:
+;;;;
+;;;; #'(lambda (x) x)
+;;;;
+;;;; tré will ignore the LAMBDA symbol, so this is also valid:
+;;;;
+;;;; #'((x) x)
+;;;;
+;;;; This is NOT a LAMBDA-expression:
+;;;;
+;;;; #'foo
+
+;;;; Please don't hate me but the compiler stores the ID of function
+;;;; information as the first argument.
+;;;; If an argument list points to a FUNINFO structure, it starts
+;;;; with the symbol %FUNINFO followed by the ID.
+;;;;
+;;;; #'((%funinfo ID x) x)
 
 (defun past-lambda-1 (x)
   (? (eq (car x) 'lambda)
@@ -19,8 +39,6 @@
         (cadr x)) ; (FUNCTION lambda-expression)
 	 x))
 
-;; The compiler stores function information before the arguments of
-;; a LAMBDA-expression.
 (defun past-lambda-before-funinfo (x)
   (past-lambda-1 (past-function x)))
 
@@ -29,7 +47,7 @@
      (cddr x)
      x))
 
-;;;; Tré accepts the LAMBDA notation for anonymous functions also
+;;;; tré accepts the LAMBDA notation for anonymous functions also
 ;;;; without the LAMBDA symbol. PAST-LAMBDA gets you past the
 ;;;; LAMBDA symbol, if it's there.
 (defun past-lambda (x)
