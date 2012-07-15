ae644db4d41a26e94aab19f35d8c5d9f00179521
Sven Michael Klose <pixel@hugbox.org>
2012-07-15 16:46:35 +0000

    Tracer switched off – it crashes.

--- environment/stage4/tree-list.lisp
+++ environment/stage4/tree-list.lisp
@@ -1,13 +1,14 @@
 ;;;;; tré – Copyright (c) 2008,2011–2012 Sven Michael Klose <pixel@copei.de>
 
 (defun tree-list-0 (q x)
-  (?
-    (atom x) (enqueue q x)
-    (dolist (i x)
-      (tree-list-0 q i))))
+  (? (atom x)
+     (enqueue q x)
+     (progn
+       (tree-list-0 q x.)
+       (& .x (tree-list-0 q .x)))))
 
 (defun tree-list (x)
-  (when x
-    (with-queue q
-      (tree-list-0 q x)
-      (queue-list q))))
+  (& x
+     (with-queue q
+       (tree-list-0 q x)
+       (queue-list q))))
