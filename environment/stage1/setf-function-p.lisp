eddfeacbf086a9cdaf6edc9973f92ec6dd04677c
Sven Michael Klose <pixel@hugbox.org>
2012-07-16 19:38:25 +0000

    Use SYMBOL-FUNCTION instead of EVAL.

--- environment/stage1/setf-function-p.lisp
+++ environment/stage1/setf-function-p.lisp
@@ -1,4 +1,4 @@
 ;;;;; tré – Copyright (C) 2005-2009,2011–2012 Sven Michael Klose <pixel@copei.de>
 
 (defun %=-function? (x)
-  (function? (eval `(function ,x))))
+  (function? (symbol-function x)))
