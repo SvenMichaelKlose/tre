4007a9b83dd27f48c83442711ff0d248df7db696
Sven Michael Klose <pixel@hugbox.org>
2012-07-26 18:03:09 +0000

    Keep order of first argument. Take :TEST.

--- environment/stage4/intersect.lisp
+++ environment/stage4/intersect.lisp
@@ -1,7 +1,7 @@
-;;;;; tré - Copyright (c) 2009,2012 Sven Michael Klose <pixel@copei.de>
+;;;;; tré – Copyright (c) 2009,2012 Sven Michael Klose <pixel@copei.de>
 
-(defun intersect (a b)
-  (when b
-    (? (member b. a)
-       (cons b. (intersect a .b))
-       (intersect a .b))))
+(defun intersect (a b &key (test #'eql))
+  (& a b
+     (? (member a. b :test test)
+        (cons a. (intersect .a b))
+        (intersect .a b))))
