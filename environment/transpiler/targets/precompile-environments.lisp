1c890ffbafc5dc702b10a0e586966649e2aec85b
Sven Michael Klose <pixel@hugbox.org>
2012-07-29 14:38:46 +0000

    PRECOMPILE-ENVIRONMENTS: New function to compile and cache back-end environments.

---
+++ environment/transpiler/targets/precompile-environments.lisp
@@ -0,0 +1,7 @@
+;;;;; tré – Copyright (c) 2012 Sven Michael Klose <pixel@copei.de>
+
+(defun precompile-environments ()
+  (format t "; Initializing JavaScript/ECMAScript target...~%" *boot-image*)
+  (compile nil :target 'js :transpiler *js-transpiler*)
+  (format t "; Initializing PHP target...~%" *boot-image*)
+  (compile nil :target 'php :transpiler *php-transpiler*))
