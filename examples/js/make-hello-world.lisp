79201e5c5404edbe2819c699a4ff0ad713157150
Sven Michael Klose <pixel@hugbox.org>
2012-07-29 15:04:19 +0000

    Some example.

---
+++ examples/js/make-hello-world.lisp
@@ -0,0 +1,5 @@
+(with-open-file out (open "examples/js/hello-world.js" :direction 'output)
+  (make-project "Hello World"
+                :files '("examples/js/hello-world.lisp")
+                :target 'js
+                :emitter (fn princ _ out)))
