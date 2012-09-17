179eeb2c0b49bd1f50665365b3a4a7bc936acb0a
Sven Michael Klose <pixel@hugbox.org>
2012-09-17 18:29:43 +0000

    MAKE-KEYWORD: Also take string instead of a symbol.

--- environment/stage4/make-keyword.lisp
+++ environment/stage4/make-keyword.lisp
@@ -1,5 +1,7 @@
-;;;;; TRE
-;;;;; Copyright (c) 2011 Sven Klose <pixel@copei.de>
+;;;;; tré – Copyright (c) 2011–2012 Sven Michael Klose <pixel@copei.de>
 
 (defun make-keyword (x)
-  (make-symbol (symbol-name x) *keyword-package*))
+  (make-symbol (? (symbol? x)
+                  (symbol-name x)
+                  x)
+               *keyword-package*))
