ac111cc81d3d4a4ac0537dadb8c78f51cb585767
Sven Michael Klose <pixel@hugbox.org>
2012-09-17 18:30:34 +0000

    Load files in category BC instead of C.

--- environment/transpiler/targets/bytecode/main.lisp
+++ environment/transpiler/targets/bytecode/main.lisp
@@ -1,8 +1,8 @@
 ;;;;; tré – Copyright (c) 2005–2012 Sven Michael Klose <pixel@copei.de>
 
-(env-load "transpiler/targets/bytecode/config.lisp" 'c)
-(env-load "transpiler/targets/bytecode/expand.lisp" 'c)
-(env-load "transpiler/targets/bytecode/builtin.lisp" 'c)
-(env-load "transpiler/targets/bytecode/codegen.lisp" 'c)
-(env-load "transpiler/targets/bytecode/expr-to-code.lisp" 'c)
-(env-load "transpiler/targets/bytecode/toplevel.lisp" 'c)
+(env-load "transpiler/targets/bytecode/config.lisp" 'bc)
+(env-load "transpiler/targets/bytecode/expand.lisp" 'bc)
+(env-load "transpiler/targets/bytecode/builtin.lisp" 'bc)
+(env-load "transpiler/targets/bytecode/codegen.lisp" 'bc)
+(env-load "transpiler/targets/bytecode/expr-to-code.lisp" 'bc)
+(env-load "transpiler/targets/bytecode/toplevel.lisp" 'bc)
