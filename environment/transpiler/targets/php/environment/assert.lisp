a6ba2b033a0807f6caaa43098e2435ccb569d4a3
Sven Michael Klose <pixel@hugbox.org>
2012-09-18 10:47:24 +0000

    Assert argument of CAR and CDR correctly.

---
+++ environment/transpiler/targets/php/environment/assert.lisp
@@ -0,0 +1,17 @@
+;;;;; tré – Copyright (c) 2012 Sven Michael Klose <pixel@copei.de>
+
+(defmacro assert-slot (place slot)
+  (& *transpiler-assert*
+     (with-gensym p
+       `(let ,p ,place
+          (| (is_object ,p)
+             (error "tried to access slot on non-object ~A" ,p))
+          (| (method_exists ,p ,(string-downcase (symbol-name slot)))
+             (error "tried to access an undefined slot on object ~A" ,p))))))
+
+(defmacro assert-slot-if-not-nil (place slot)
+  `(& *transpiler-assert*
+     (with-gensym p
+       `(let ,p ,place
+          (unless ,p
+            (assert-slot ,p ,slot))))))
--- environment/transpiler/targets/php/environment/late-cons.lisp
+++ environment/transpiler/targets/php/environment/late-cons.lisp
@@ -1,13 +1,20 @@
-;;;;; tré - Copyright (c) 2008-2012 Sven Michael Klose <pixel@copei.de>
+;;;;; tré – Copyright (c) 2008–2012 Sven Michael Klose <pixel@copei.de>
 
-(defun car (x) (when x (x.a)))
-(defun cdr (x) (when x (x.d)))
+(defun car (x)
+  (assert-slot-if-not-nil x a)
+  (& x (x.a)))
+
+(defun cdr (x)
+  (assert-slot-if-not-nil x d)
+  (& x (x.d)))
 
 (defun rplaca (x val)
+  (assert-slot x sa)
   (x.sa val)
   x)
 
 (defun rplacd (x val)
+  (assert-slot x sd)
   (x.sd val)
   x)
 
