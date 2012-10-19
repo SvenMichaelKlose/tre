162f4eb2953b97cb1fd11503689c5152ad32e60d
Sven Michael Klose <pixel@hugbox.org>
2012-10-19 15:39:17 +0000

    Assign structure to a variable of the same name.

--- environment/stage3/with-struct.lisp
+++ environment/stage3/with-struct.lisp
@@ -1,8 +1,8 @@
 ;;;;; tré - Copyright (C) 2006,2008,2012 Sven Michael Klose <pixel@copei.de>
 
 (defmacro with-struct (typ strct &body body)
-  `(let* (,@(mapcar #'((d)
-		                 (let n (%struct-field-name d)
-	                       `(,n (,(%struct-getter-symbol typ n) ,strct))))
+  `(let* ((,typ ,strct)
+          ,@(mapcar [let n (%struct-field-name _)
+	                  `(,n (,(%struct-getter-symbol typ n) ,strct))]
 	                (assoc-value typ *struct-defs*)))
      ,@body))
