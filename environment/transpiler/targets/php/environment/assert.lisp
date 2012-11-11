66a44f99df8425b37de48580d0d5dc5d7990dae1
Sven Michael Klose <pixel@hugbox.org>
2012-11-11 06:33:35 +0000

    Fix assertions for accessing CONSes.

--- environment/transpiler/targets/php/environment/assert.lisp
+++ environment/transpiler/targets/php/environment/assert.lisp
@@ -1,17 +1,10 @@
 ;;;;; tré – Copyright (c) 2012 Sven Michael Klose <pixel@copei.de>
 
-(defmacro assert-slot (place slot)
+(defmacro assert-method (place slot)
   (& *transpiler-assert*
      (with-gensym p
        `(let ,p ,place
           (| (is_object ,p)
-             (error "tried to access slot on non-object ~A" ,p))
+             (error "tried to access method on non-object ~A" ,p))
           (| (method_exists ,p ,(string-downcase (symbol-name slot)))
-             (error "tried to access an undefined slot on object ~A" ,p))))))
-
-(defmacro assert-slot-if-not-nil (place slot)
-  `(& *transpiler-assert*
-      (with-gensym p
-        `(let ,p ,place
-           (unless ,p
-             (assert-slot ,p ,slot))))))
+             (error "tried to access an undefined method on object ~A" ,p))))))
--- environment/transpiler/targets/php/environment/late-cons.lisp
+++ environment/transpiler/targets/php/environment/late-cons.lisp
@@ -1,20 +1,22 @@
 ;;;;; tré – Copyright (c) 2008–2012 Sven Michael Klose <pixel@copei.de>
 
 (defun car (x)
-  (assert-slot-if-not-nil x a)
-  (& x (x.a)))
+  (& x
+     (assert-method x a)
+     (x.a)))
 
 (defun cdr (x)
-  (assert-slot-if-not-nil x d)
-  (& x (x.d)))
+  (& x
+     (assert-method x d)
+     (x.d)))
 
 (defun rplaca (x val)
-  (assert-slot x sa)
+  (assert-method x sa)
   (x.sa val)
   x)
 
 (defun rplacd (x val)
-  (assert-slot x sd)
+  (assert-method x sd)
   (x.sd val)
   x)
 
