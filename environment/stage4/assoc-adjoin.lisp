348ee3163f533e94b89f1ec68558b575c3f32128
Sven Michael Klose <pixel@hugbox.org>
2012-11-11 06:35:05 +0000

    ASSOC-ADJOIN: New argument :TO-END?.

--- environment/stage4/assoc-adjoin.lisp
+++ environment/stage4/assoc-adjoin.lisp
@@ -1,9 +1,11 @@
 ;;;;; tré – Copyright (c) 2008–2012 Sven Michael Klose <pixel@copei.de>
 
-(defmacro assoc-adjoin (value key place &key (test #'eql))
+(defmacro assoc-adjoin (value key place &key (test #'eql) (to-end? nil))
   (with-gensym (gkey gvalue)
     `(with (,gkey ,key
             ,gvalue ,value)
-       (? (assoc-value ,gkey ,place :test ,test)
+       (? (assoc ,gkey ,place :test ,test)
           (= (assoc-value ,gkey ,place :test ,test) ,gvalue)
-          (acons! ,gkey ,gvalue ,place)))))
+          ,(? to-end?
+              `(append! ,place (list (cons ,gkey ,gvalue)))
+              `(acons! ,gkey ,gvalue ,place))))))
