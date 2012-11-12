ea95b1c9e1ac2d1e7f91207b01da04667b4f26b4
Sven Michael Klose <pixel@hugbox.org>
2012-11-12 10:13:20 +0000

    Works with assertions switched off.

--- environment/transpiler/targets/php/environment/late-cons.lisp
+++ environment/transpiler/targets/php/environment/late-cons.lisp
@@ -1,14 +1,14 @@
 ;;;;; tré – Copyright (c) 2008–2012 Sven Michael Klose <pixel@copei.de>
 
 (defun car (x)
-  (& x
-     (assert-method x a)
-     (x.a)))
+  (when x
+    (assert-method x a)
+    (x.a)))
 
 (defun cdr (x)
-  (& x
-     (assert-method x d)
-     (x.d)))
+  (when x
+    (assert-method x d)
+    (x.d)))
 
 (defun rplaca (x val)
   (assert-method x sa)
