fdeb36e941cdee95c43ae92625b85edf5b2f0d61
Sven Michael Klose <pixel@hugbox.org>
2012-11-15 10:01:26 +0000

    LOGPRINT: New function.

--- environment/transpiler/targets/php/environment/log.lisp
+++ environment/transpiler/targets/php/environment/log.lisp
@@ -1,5 +1,10 @@
-;;;;; tré - Copyright (c) 2012 Sven Michael Klose <pixel@copei.de>
+;;;;; tré – Copyright (c) 2012 Sven Michael Klose <pixel@copei.de>
 
 (defun %%%log (txt)
   (%setq nil (echo txt))
   txt)
+
+(defun logprint (x)
+  (log (with-string-stream s
+         (print x s)))
+  x)
