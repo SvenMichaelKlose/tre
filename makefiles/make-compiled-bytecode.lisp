0b166267864b7a644c829426ff6ad7f4b14efc0a
Sven Michael Klose <pixel@hugbox.org>
2012-11-19 15:51:15 +0000

    Inline.

--- makefiles/make-compiled-bytecode.lisp
+++ makefiles/make-compiled-bytecode.lisp
@@ -1,11 +1,8 @@
 ;;;;; tré – Copyright (c) 2008–2012 Sven Michael Klose <pixel@copei.de>
 
 (= *show-definitions?* t)
-(= *opt-inline?* nil)
 
 (let code (compile-files '("makefiles/make-compiled-0.lisp") :target 'bytecode)
   (with-open-file out (open "bytecode-image" :direction 'output)
     (dolist (i code)
       (late-print i out))))
-
-(fnord)
