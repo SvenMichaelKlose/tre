0398d906d1193d3fa24ad0e8cd1bd35dcf372a5c
Sven Michael Klose <pixel@hugbox.org>
2012-11-27 14:29:07 +0000

    INCF is gone.

--- environment/transpiler/lib/tag.lisp
+++ environment/transpiler/lib/tag.lisp
@@ -1,10 +1,9 @@
-;;;;; TRE compiler
-;;;;; Copyright (c) 2006-2010 Sven Klose <pixel@copei.de>
+;;;;; tré – Copyright (c) 2006–2010,2012 Sven Michael Klose <pixel@copei.de>
 
 (defvar *compiler-tag-counter* 1)
 
 (defun make-compiler-tag ()
-  (incf *compiler-tag-counter*))
+  (1+! *compiler-tag-counter*))
 
 (defmacro with-compiler-tag (l &rest body)
   `(let ,l (make-compiler-tag)
