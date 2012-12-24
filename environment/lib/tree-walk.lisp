bebd99faf607b9013e8d125fae2ba5cd014010b5
Sven Michael Klose <pixel@hugbox.org>
2012-12-24 11:38:37 +0000

    Track expressions' file locations.

--- environment/lib/tree-walk.lisp
+++ environment/lib/tree-walk.lisp
@@ -4,16 +4,19 @@
 (defun tree-walk (i &key (ascending nil) (dont-ascend-if nil) (dont-ascend-after-if nil))
   (? (atom i)
 	 (funcall ascending i)
-	 (with (y i.
-		    a (| (& dont-ascend-if (funcall dont-ascend-if y) y)
-				 (? (& dont-ascend-after-if (funcall dont-ascend-after-if y))
-					(funcall ascending y)
-	  			    (tree-walk (? ascending
-					 	 		  (funcall ascending y)
-					 	 		  y)
-					 		   :ascending ascending
-					 		   :dont-ascend-if dont-ascend-if
-					 		   :dont-ascend-after-if dont-ascend-after-if))))
-	  (cons a (tree-walk .i :ascending ascending
-						    :dont-ascend-if dont-ascend-if
-						    :dont-ascend-after-if dont-ascend-after-if)))))
+     (progn
+       (awhen (cpr i)
+         (= *default-listprop* !))
+	   (with (y i.
+		      a (| (& dont-ascend-if (funcall dont-ascend-if y) y)
+				   (? (& dont-ascend-after-if (funcall dont-ascend-after-if y))
+					  (funcall ascending y)
+	  			      (tree-walk (? ascending
+					 	 		    (funcall ascending y)
+					 	 		    y)
+					 		     :ascending ascending
+					 		     :dont-ascend-if dont-ascend-if
+					 		     :dont-ascend-after-if dont-ascend-after-if))))
+	    (cons a (tree-walk .i :ascending ascending
+						      :dont-ascend-if dont-ascend-if
+						      :dont-ascend-after-if dont-ascend-after-if))))))
