373b2a19d0a2b74ca9d2cd35d1354a43443cafdd
Sven Michael Klose <pixel@hugbox.org>
2012-12-31 19:38:02 +0000

    Cleanup.

--- environment/transpiler/front-end/backquote-expand.lisp
+++ environment/transpiler/front-end/backquote-expand.lisp
@@ -17,15 +17,14 @@
           (copy-tree (cadr x.)))))
 
 (defun backquote-cons-atom (x)
-  (when x
-	(? (| (number? x)
-          (string? x)
-          (array? x)
-          (hash-table? x)
-          (eq t x))
-          ;(string== "" (symbol-name x)) ; XXX?
-	   x
-	   `(%quote ,x))))
+  (? (| (not x)
+        (number? x)
+        (string? x)
+        (array? x)
+        (hash-table? x)
+        (eq t x))
+     x
+     `(%quote ,x)))
 
 (defun backquote-cons-2 (x)
   (?
@@ -35,36 +34,33 @@
 
 (defun backquote-cons-1 (x)
   (?
-    (atom x) (backquote-cons-atom x)
-    (atom x.) `(cons ,(backquote-cons-atom x.)
-                     ,(backquote-cons-2 .x))
-    (quasiquote? x.) (backquote-cons-quasiquote x)
+    (atom x)                (backquote-cons-atom x)
+    (atom x.)               `(cons ,(backquote-cons-atom x.)
+                                   ,(backquote-cons-2 .x))
+    (quasiquote? x.)        (backquote-cons-quasiquote x)
     (quasiquote-splice? x.) (backquote-cons-quasiquote-splice x)
     `(cons ,(backquote-cons x.)
            ,(backquote-cons-2 .x))))
 
 (defun backquote-cons (x)
   (?
-    (atom x) (backquote-cons-atom x)
+    (atom x)       (backquote-cons-atom x)
     (backquote? x) `(cons 'BACKQUOTE ,(backquote-cons .x))
     (backquote-cons-1 x)))
 
 (defun simple-quote-expand (x)
-  (when x
-	(? (atom x)
-	   (? (| (in? x nil t)
-             (string? x)
-		  (number? x))
-		  x
-		  `(%quote ,x))
-	   `(cons ,(simple-quote-expand x.)
-		      ,(simple-quote-expand .x)))))
+  (? (atom x)
+     (? (| (in? x nil t)
+           (string? x)
+           (number? x))
+        x
+        `(%quote ,x))
+     `(cons ,(simple-quote-expand x.)
+            ,(simple-quote-expand .x))))
 
 (defun backquote-expand (l)
   (tree-walk l
-	:ascending
-	  #'((x)
-		   (?
-			 (quote? x) (simple-quote-expand .x.)
-		   	 (backquote? x) (backquote-cons .x.)
-			 x))))
+	  :ascending [?
+                   (quote? _)     (simple-quote-expand ._.)
+                   (backquote? _) (backquote-cons ._.)
+                   _]))
