776fd6e20fefe2ff080ffbe60203f2668cdd834d
Sven Michael Klose <pixel@hugbox.org>
2012-12-31 19:36:41 +0000

    Added CPR and RPLACP.

--- environment/transpiler/targets/javascript/environment/cons.lisp
+++ environment/transpiler/targets/javascript/environment/cons.lisp
@@ -2,10 +2,13 @@
 
 (dont-inline %cons)
 
+(defvar *default-listprop* nil)
+
 (define-native-js-fun %cons (a d)
   (= this.__class ,(transpiler-obfuscated-symbol-string *current-transpiler* 'cons)
      this._ a
-     this.__ d)
+     this.__ d
+     this._p *default-listprop*)
   this)
 
 (define-native-js-fun cons (x y)
--- environment/transpiler/targets/javascript/environment/late-cons.lisp
+++ environment/transpiler/targets/javascript/environment/late-cons.lisp
@@ -2,9 +2,11 @@
 
 (defun car (x) (& x x._))
 (defun cdr (x) (& x x.__))
+(defun cpr (x) (& x x._p))
 
 (defvar *rplaca-breakpoints* nil)
 (defvar *rplacd-breakpoints* nil)
+(defvar *rplacp-breakpoints* nil)
 
 (defun rplaca (x val)
   (declare type cons x)
@@ -20,6 +22,14 @@
     (& (member x *rplacd-breakpoints* :test #'eq)
        (invoke-debugger)))
   (= x.__ val)
+  x)
+
+(defun rplacp (x val)
+  (declare type cons x)
+  (when-debug
+    (& (member x *rplacp-breakpoints* :test #'eq)
+       (invoke-debugger)))
+  (= x._p val)
   x)
 
 (defun cons? (x)
