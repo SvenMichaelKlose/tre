8885496e98226b8618bc9df0d05f8ff10a497c3f
Sven Michael Klose <pixel@hugbox.org>
2013-01-03 14:51:54 +0000

    PHP back end test move here from Caroshi.

---
+++ makefiles/test-php.lisp
@@ -0,0 +1,14 @@
+;;;;; tré – Copyright (c) 2008–2010,2012–2013 Sven Michael Klose <pixel@copei.de>
+
+(defvar *have-environment-tests* t)
+(= *show-definitions* t)
+
+(unix-sh-mkdir "compiled")
+(alet "compiled/test.php"
+  (with-open-file out (open ! :direction 'output)
+    (format t "; Compiling to file '~A'...~%" !)
+    (princ
+      (compile-files `("makefiles/test-toplevel.lisp")
+	                 :obfuscate? nil
+                     :target 'php)
+      out)))
