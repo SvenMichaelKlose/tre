860dd1c867a9f6f362cb4e9649fa012651a637dd
Sven Michael Klose <pixel@hugbox.org>
2013-01-14 07:00:59 +0000

    Clean up.

--- environment/stage4/count-tree.lisp
+++ environment/stage4/count-tree.lisp
@@ -1,13 +1,9 @@
-;;;;; tré – Copyright (c) 2009–2010,2012 Sven Michael Klose <pixel@copei.de>
+;;;;; tré – Copyright (c) 2009–2010,2012–2013 Sven Michael Klose <pixel@copei.de>
 
-(defun count-tree-0 (v x init max test)
+(defun count-tree (v x &key (test #'eql) (counter 0))
   (?
-	(funcall test v x)  (integer-1+ max)
-	(atom x)  max
-	(let sum (count-tree-0 v x. init max test)
-	  (? (& max (<= max sum))
-		  sum
-	      (count-tree-0 v .x sum max test)))))
-
-(defun count-tree (v x &key (max nil) (test #'eql))
-  (count-tree-0 v x 0 max test))
+	(atom x) (? (funcall test v x)
+                (integer-1+ counter)
+                counter)
+    (integer+ (count-tree v x. :test test :counter counter)
+              (count-tree v .x :test test :counter counter))))
