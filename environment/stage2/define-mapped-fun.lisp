08e6bc3b0b67e7600331686faa879466e92340fb
Sven Michael Klose <pixel@hugbox.org>
2013-01-17 23:34:44 +0000

    Shortcut added.

--- environment/stage2/define-mapped-fun.lisp
+++ environment/stage2/define-mapped-fun.lisp
@@ -1,9 +1,14 @@
-;;;;; tré - Copyright (c) 2006,2009,2012 Sven Michael Klose <pixel@copei.de>
+;;;;; tré – Copyright (c) 2006,2009,2012–2013 Sven Michael Klose <pixel@copei.de>
 
 (defmacro define-mapped-fun (mapfun name &rest fun)
   (with-gensym g
     `(defun ,name (,g)
-       (,mapfun #'(,@fun) ,g))))
+       (,mapfun ,(? (& (not (cdr fun))
+                       (cons? (car fun))
+                       (eq 'function (caar fun)))
+                    (car fun)
+                    `#'(,@fun))
+                ,g))))
 
 (defmacro define-filter (name &rest fun)
   `(define-mapped-fun filter ,name ,@fun))
@@ -11,8 +16,5 @@
 (defmacro define-mapcan-fun (name &rest fun)
   `(define-mapped-fun mapcan ,name ,@fun))
 
-(define-filter carlist (x)
-  (car x))
-
-(define-filter cdrlist (x)
-  (cdr x))
+(define-filter carlist #'car)
+(define-filter cdrlist #'cdr)
