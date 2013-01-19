909fefeb2c7cb6e3c7f81a9d70bd373172819a20
Sven Michael Klose <pixel@hugbox.org>
2013-01-19 17:57:54 +0000

    Avoid costly FUNCALLs.

--- environment/transpiler/targets/shared/expand/opt-filter.lisp
+++ environment/transpiler/targets/shared/expand/opt-filter.lisp
@@ -1,7 +1,14 @@
-;;;;; tré – Copyright (c) 2012 Sven Michael Klose <pixel@copei.de>
+;;;;; tré – Copyright (c) 2010–2013 Sven Michael Klose <pixel@copei.de>
 
-(defun shared-opt-filter (fun lst)
+(defun shared-opt-filter (fun &rest lsts)
   (with-gensym (q i)
     `(with-queue ,q
-       (dolist (,i ,lst (queue-list ,q))
-         (enqueue ,q (funcall ,fun ,i))))))
+       (dolist (,i ,(? .lsts
+                       `(append ,@lsts)
+                       lsts.)
+                (queue-list ,q))
+         (enqueue ,q ,(?
+                        (static-symbol-function? fun) `(,.fun. ,i)
+                        (function-expr? fun)          `(,fun ,i)
+                        (atom fun)                    `(funcall ,fun ,i)
+                        (error "function or variable required instead of ~A" fun)))))))
