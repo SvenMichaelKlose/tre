8aaa109687a9f5d9efdce0784d010d1403d9c8dc
Sven Michael Klose <pixel@hugbox.org>
2013-01-19 17:58:26 +0000

    Clean up.

--- environment/transpiler/targets/php/environment/apply.lisp
+++ environment/transpiler/targets/php/environment/apply.lisp
@@ -3,17 +3,17 @@
 (dont-obfuscate apply call function_exists call_user_func_array array)
 
 (defun apply (&rest lst)
-  (with (fun lst.
-         l (last .lst)
-         args (%nconc (butlast .lst) l.)
-         funref? (is_a fun "__funref")
-         fun-name (? funref? fun.n fun)
+  (with (fun           lst.
+         l             (last .lst)
+         args          (%nconc (butlast .lst) l.)
+         funref?       (is_a fun "__funref")
+         fun-name      (? funref? fun.n fun)
          expander-name (%%%string+ fun-name "_treexp"))
     (when funref?
       (= args (cons fun.g args)))
 	(?
       (function_exists expander-name) (call_user_func_array expander-name (%transpiler-native "array ($" args ")"))
-	  (function_exists fun-name) (call_user_func_array fun-name (list-phphash args))
+	  (function_exists fun-name)      (call_user_func_array fun-name (list-phphash args))
       (error (+ "Function '" fun-name "' does not exist.")))))
 
 (defmacro cps-wrap (x) x)
