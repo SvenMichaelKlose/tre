2b0718a494cad8bc36337a4a18ad8862f376e5a2
Sven Michael Klose <pixel@hugbox.org>
2013-01-23 05:25:46 +0000

    Assign bytecode functions without calling a function, because the bytecode interpreter doesn't call C functions.

--- environment/transpiler/targets/bytecode/load-bytecode-functions.lisp
+++ environment/transpiler/targets/bytecode/load-bytecode-functions.lisp
@@ -1,8 +1,6 @@
 ;;;;; tré – Copyright (c) 2012–2013 Sven Michael Klose <pixel@copei.de>
 
-(defun load-bytecode-function (x)
-  (= (symbol-function x.) (list-array `(,.x. ,(function-body (symbol-function x.)) ,@..x))))
-
 (defun load-bytecode-functions (x)
-  (dolist (i x)
-    (load-bytecode-function i)))
+  (dolist (i (filter [cons _. (list-array `(,._. ,(function-body (symbol-function _.)) ,@.._))]
+                     x))
+    (= (symbol-function i.) .i)))
