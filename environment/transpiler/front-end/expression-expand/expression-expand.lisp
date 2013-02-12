34390f0383b5097edb4071cf3505add4cafcce69
Sven Michael Klose <pixel@hugbox.org>
2013-02-12 11:45:41 +0000

    Don't nest %SETQ statements when generating one.

--- environment/transpiler/front-end/expression-expand/expression-expand.lisp
+++ environment/transpiler/front-end/expression-expand/expression-expand.lisp
@@ -46,7 +46,11 @@
 ;;;; UTILS
 
 (defun expex-make-%setq (ex plc val)
-  (expex-guest-filter-setter ex `(%setq ,plc ,(peel-identity val))))
+  (+ (? (%setq? val)
+        (expex-guest-filter-setter ex val))
+     (expex-guest-filter-setter ex `(%setq ,plc ,(? (%setq? val)
+                                                    (%setq-place val)
+                                                    (peel-identity val))))))
 
 (defun expex-funinfo-var-add ()
   (aprog1 (expex-sym)
