7c576a71a0861cffae86692b9d713e29527e622d
Sven Michael Klose <pixel@hugbox.org>
2013-02-12 19:58:48 +0000

    Ignore local variables.

--- environment/transpiler/back-end/translate-function-names.lisp
+++ environment/transpiler/back-end/translate-function-names.lisp
@@ -6,20 +6,17 @@
      x))
 
 (define-tree-filter translate-function-names (tr funinfo x) ; XXX use metacode-walker instead
-  (named-lambda? x)
-	(copy-lambda x :name (compiled-function-name tr .x.)
-				   :body (translate-function-names tr (get-lambda-funinfo x) (lambda-body x)))
-  (lambda? x)
-	(copy-lambda x :body (translate-function-names tr (get-lambda-funinfo x) (lambda-body x)))
+  (named-lambda? x)          (copy-lambda x :name (compiled-function-name tr .x.)
+				                            :body (translate-function-names tr (get-lambda-funinfo x) (lambda-body x)))
+  (lambda? x)                (copy-lambda x :body (translate-function-names tr (get-lambda-funinfo x) (lambda-body x)))
   (| (%quote? x)
      (%transpiler-native? x)
      (%%closure? x)
-     (%setq-atom-value? x))
-	x
-  (%slot-value? x)
-	`(%slot-value ,(translate-function-name tr funinfo .x.) ,..x.)
+     (%setq-atom-value? x))  x
+  (%slot-value? x)           `(%slot-value ,(translate-function-name tr funinfo .x.) ,..x.)
   (& (transpiler-raw-constructor-names? tr)
-     (%new? x))
-    x
-  (atom x)
-	(translate-function-name tr funinfo x))
+     (%new? x))              x
+  (& (atom x)
+     (| (not (funinfo-parent funinfo))
+        (not (funinfo-arg-or-var? funinfo x))))
+	                         (translate-function-name tr funinfo x))
