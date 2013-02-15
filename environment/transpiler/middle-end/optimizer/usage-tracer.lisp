ca19cff7476ad9ece10e6254ebfc08729b52c8f7
Sven Michael Klose <pixel@hugbox.org>
2013-02-15 18:45:50 +0000

    Fix for %SLOT-VALUE places.

--- environment/transpiler/middle-end/optimizer/usage-tracer.lisp
+++ environment/transpiler/middle-end/optimizer/usage-tracer.lisp
@@ -15,30 +15,29 @@
 
 (defun opt-peephole-will-be-used-again? (x v)
    (with (traversed-tags nil
-          traversed-tag?
-            [member _ traversed-tags :test #'number==]
-          traverse-tag
-            [unless (traversed-tag? _)
-              (push _ traversed-tags)
-              (traverse-statements (tag-code _))]
+          traversed-tag? [member _ traversed-tags :test #'number==]
+          traverse-tag   [unless (traversed-tag? _)
+                           (push _ traversed-tags)
+                           (traverse-statements (tag-code _))]
           traverse-statements
             [? (not _)
                (& (funinfo-parent *funinfo*)
                   (~%ret? v))
                (with-cons a d _
                  (?
-                   (%setq? a)    (with (place (%setq-place a)
-                                        value (%setq-value a))
+                   (%setq? a)    (with-%setq place value a
                                    (? (eq v place value)
                                       (traverse-statements d)
                                       (| (find-tree v value :test #'eq)
+                                         (& (%slot-value? place)
+                                            (find-tree v place :test #'eq))
                                          (unless (eq v place)
                                            (traverse-statements d)))))
                    (%%go? a)     (traverse-tag .a.)
                    (%%go-nil? a) (| (eq v .a.)
-                                       (traverse-tag ..a.)
-                                       (traverse-statements d))
-                   (number? a)      (traverse-statements d)
+                                    (traverse-tag ..a.)
+                                    (traverse-statements d))
+                   (number? a)   (traverse-statements d)
                    (progn
                      (print _)
                      (error "illegal metacode statement ~A" _))))])
