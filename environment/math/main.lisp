453036a9fde757147a4b6047fabcb2521735866d
Sven Michael Klose <pixel@hugbox.org>
2013-02-18 20:26:42 +0000

    Moved math stuff into math section.

--- environment/math/main.lisp
+++ environment/math/main.lisp
@@ -1,5 +1,9 @@
 ;;;; tré – Copyright (c) 2008,2011,2013 Sven Michael Klose <pixel@copei.de>
 
 (env-load "math/min.lisp")
-(env-load "math/saturate.lisp")
 (env-load "math/dct.lisp" 'c)
+(env-load "math/english-numbers.lisp")
+(env-load "math/half.lisp")
+(env-load "math/levenshtein.lisp" 'c)
+(env-load "math/percent.lisp")
+(env-load "math/saturate.lisp")
