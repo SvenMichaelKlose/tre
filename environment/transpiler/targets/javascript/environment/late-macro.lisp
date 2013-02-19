d6033c73085961c13275ffd2c20a3551c0072ff1
Sven Michael Klose <pixel@hugbox.org>
2013-02-19 00:58:59 +0000

    Clean up.

--- environment/transpiler/targets/javascript/environment/late-macro.lisp
+++ environment/transpiler/targets/javascript/environment/late-macro.lisp
@@ -4,21 +4,21 @@
 (set-expander-macros 'standard-macros *macros*)
 (= *standard-macro-expander* (expander-get 'standard-macros))
 
-(defvar *environment-macros* (copy-alist *macros*))
-(define-expander 'environment-macros)
-(set-expander-macros 'environment-macros *environment-macros*)
-(defvar *environment-macro-expander* (expander-get 'environment-macros))
-
-(= *macroexpand-backquote-diversion* #'%macroexpand-backquote)
-
 (defun %%macrop (x)
   (expander-has-macro? 'standard-macros x.))
 
 (defun %%macrocall (x)
   (funcall (expander-call *standard-macro-expander*) x))
 
+(defvar *environment-macros* (copy-alist *macros*))
+(define-expander 'environment-macros)
+(set-expander-macros 'environment-macros *environment-macros*)
+(defvar *environment-macro-expander* (expander-get 'environment-macros))
+
 (defun %%env-macrop (x)
   (expander-has-macro? 'environment-macros x.))
 
 (defun %%env-macrocall (x)
   (funcall (expander-call *environment-macro-expander*) x))
+
+(= *macroexpand-backquote-diversion* #'%macroexpand-backquote)
