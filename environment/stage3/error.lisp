bfd3de4a6296d4638ef624a55cdfec5223af2e69
Sven Michael Klose <pixel@hugbox.org>
2013-02-21 01:06:01 +0000

    Have line feed.

--- environment/stage3/error.lisp
+++ environment/stage3/error.lisp
@@ -1,10 +1,10 @@
-;;;;; tré – Copyright (c) 2006–2008,2011–2012 Sven Michael Klose <pixel@copei.de>
+;;;;; tré – Copyright (c) 2006–2008,2011–2013 Sven Michael Klose <pixel@copei.de>
 
 (defun error (&rest args)
-  (%error (apply #'format nil args)))
+  (%error (apply #'format nil (+ args. "~%") .args)))
 
 (defun warn (&rest args)
-  (apply #'format t (string-concat "; WARNING: " args.) .args))
+  (apply #'format t (string-concat "; WARNING: " args. "~%") .args))
 
 (defun hint (&rest args)
-  (apply #'format t (string-concat "; HINT: " args.) .args))
+  (apply #'format t (string-concat "; HINT: " args. "~%") .args))
