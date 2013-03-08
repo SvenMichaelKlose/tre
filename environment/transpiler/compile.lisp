8652c1cdeeeaa43f602c059f5ab5f41de498e2b1
Sven Michael Klose <pixel@hugbox.org>
2013-03-08 13:21:36 +0000

    Print obfuscations by default.

--- environment/transpiler/compile.lisp
+++ environment/transpiler/compile.lisp
@@ -1,6 +1,6 @@
 ;;;;; tré – Copyright (c) 2008–2013 Sven Michael Klose <pixel@copei.de>
 
-(defun compile-0 (sources &key (target nil) (transpiler nil) (obfuscate? nil) (print-obfuscations? nil) (files-to-update nil))
+(defun compile-0 (sources &key (target nil) (transpiler nil) (obfuscate? nil) (print-obfuscations? t) (files-to-update nil))
   (| target (error "target missing"))
   (funcall (case target
              'c ,(& *have-c-compiler?* '#'c-transpile)
@@ -20,14 +20,14 @@
            :print-obfuscations? print-obfuscations?
            :files-to-update files-to-update))
 
-(defun compile-files (files &key (target nil) (transpiler nil) (obfuscate? nil) (print-obfuscations? nil) (files-to-update nil))
+(defun compile-files (files &key (target nil) (transpiler nil) (obfuscate? nil) (print-obfuscations? t) (files-to-update nil))
   (compile-0 (mapcar #'list files)
              :target target :transpiler transpiler :obfuscate? obfuscate? :print-obfuscations? print-obfuscations? :files-to-update files-to-update))
 
-(defun compile-sections (sections &key (target nil) (transpiler nil) (obfuscate? nil) (print-obfuscations? nil) (files-to-update nil) (section-id 'compile))
+(defun compile-sections (sections &key (target nil) (transpiler nil) (obfuscate? nil) (print-obfuscations? t) (files-to-update nil) (section-id 'compile))
   (compile-0 sections
              :target target :transpiler transpiler :obfuscate? obfuscate? :print-obfuscations? print-obfuscations? :files-to-update files-to-update))
 
-(defun compile (expression &key (target nil) (transpiler nil) (obfuscate? nil) (print-obfuscations? nil) (files-to-update nil) (section-id 'compile))
+(defun compile (expression &key (target nil) (transpiler nil) (obfuscate? nil) (print-obfuscations? t) (files-to-update nil) (section-id 'compile))
   (compile-0 (& expression `((,section-id . (,expression))))
              :target target :transpiler transpiler :obfuscate? obfuscate? :print-obfuscations? print-obfuscations? :files-to-update files-to-update))
