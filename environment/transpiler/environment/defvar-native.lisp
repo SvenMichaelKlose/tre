b76605c57bc937273cb0c8fafc8c07fd448e0949
Sven Michael Klose <pixel@hugbox.org>
2013-03-08 13:20:19 +0000

    Don't obfuscate predefined symbols.

--- environment/transpiler/environment/defvar-native.lisp
+++ environment/transpiler/environment/defvar-native.lisp
@@ -3,4 +3,5 @@
 (defmacro defvar-native (&rest x)
   (print-definition `(defvar-native ,@x))
   (+! (transpiler-predefined-symbols *transpiler*) x)
+  (apply #'transpiler-add-obfuscation-exceptions *transpiler* x)
   nil)
