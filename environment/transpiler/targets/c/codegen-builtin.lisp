b6acf792e1f1f0007c24d5f790f57221ae9c7c2d
Sven Michael Klose <pixel@hugbox.org>
2013-03-11 04:44:43 +0000

    Oops.

---
+++ environment/transpiler/targets/c/codegen-builtin.lisp
@@ -0,0 +1,8 @@
+;;;;; tré – Copyright (c) 2008–2013 Sven Michael Klose <pixel@copei.de>
+
+,`(progn
+    ,@(macroexpand (mapcar [let n (make-symbol (c-builtin-name _))
+                             (unless (expander-has-macro? 'c-codegen _)
+                               `(define-c-macro ,_ (&rest x)
+                                  `(,n ,,(compiled-list x))))]
+                           (c-builtin-names))))
