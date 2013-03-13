9ad39461f220adbc325c29457ebd2db41f87e8fd
Sven Michael Klose <pixel@hugbox.org>
2013-03-13 07:34:44 +0000

    Oops.

--- environment/math/degree.lisp
+++ environment/math/degree.lisp
@@ -1,5 +1,5 @@
 ;;;;; tré – Copyright (c) 2008–2009,2011–2013 Sven Michael Klose <pixel@copei.de>
 
 (defun rad-deg (x) (* x *pi* 180))
-(defun degree-sin (x) (sin (deg-rand x)))
-(defun degree-cos (x) (cos (deg-rand x)))
+(defun degree-sin (x) (sin (rad-deg x)))
+(defun degree-cos (x) (cos (rad-deg x)))
