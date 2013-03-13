c10eee1aa2371dccfa2f649589afcf0a7d285e27
Sven Michael Klose <pixel@hugbox.org>
2013-03-13 07:18:00 +0000

    Fixes for C environment.

--- environment/stage5/array.lisp
+++ environment/stage5/array.lisp
@@ -1,4 +1,4 @@
-;;;;; tré – Copyright (c) 2008–2012 Sven Michael Klose <pixel@copei.de>
+;;;;; tré – Copyright (c) 2008–2013 Sven Michael Klose <pixel@copei.de>
 
 (defun array-list (x)
   (let result (make-queue)
@@ -6,34 +6,16 @@
       (enqueue result (aref x i)))))
 
 (defun array-copy (arr)
-  (let ret (make-array)
-    (doarray (x arr ret)
-      (ret.push x))))
+  (do ((ret (make-array))
+       (i 0 (1+ i)))
+      ((< i (length arr)) ret)
+    (= (aref ret i) (aref arr i))))
 
 (defun array-filter (arr pred)
-  (declare type array arr)
-  (let ret (make-array)
-    (doarray (v arr ret)
-      (when (pred v)
-	    (ret.push v)))))
-
-(defun array-merge (a b)
-  (declare type array a b)
-  (when (| a b)
-    (when (not a)
-	  (= a (make-array)))
-    (? b
-       (doarray (x b a)
-	     (a.push x))
-	   a)))
-
-(defun array-first (x)
-  (declare type array x)
-  (when (< 0 x.length)
-	(aref x 0)))
-
-(defun force-array (x)
-  (? (array? x)
-     x
-     (aprog1 (make-array)
-	   (= (aref ! 0) x))))
+  (do ((ret (make-array))
+       (i 0 (1+ i))
+       (j 0))
+      ((< i (length arr)) ret)
+    (when (funcall pred (aref arr i))
+      (= (aref ret j) (aref arr i))
+      (1+! j))))
