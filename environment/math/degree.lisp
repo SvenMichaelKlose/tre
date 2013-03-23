ac137295e91ade132771c4b301c36a100dcf3fff
Sven Michael Klose <pixel@hugbox.org>
2013-03-23 19:44:22 +0000

    Fixed conversion from degree to radian.

--- environment/math/degree.lisp
+++ environment/math/degree.lisp
@@ -1,5 +1,5 @@
 ;;;;; tré – Copyright (c) 2008–2009,2011–2013 Sven Michael Klose <pixel@copei.de>
 
-(defun rad-deg (x) (* x *pi* 180))
-(defun degree-sin (x) (sin (rad-deg x)))
-(defun degree-cos (x) (cos (rad-deg x)))
+(defun deg-rad (x) (/ (* x *pi*) 180))
+(defun degree-sin (x) (sin (deg-rad x)))
+(defun degree-cos (x) (cos (deg-rad x)))
