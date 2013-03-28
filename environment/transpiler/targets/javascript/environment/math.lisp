d865261f50199a98169978cdb2d8db64e91dfe22
Sven Michael Klose <pixel@hugbox.org>
2013-03-28 19:53:11 +0000

    We love math! More essential functions added.

--- environment/transpiler/targets/javascript/environment/math.lisp
+++ environment/transpiler/targets/javascript/environment/math.lisp
@@ -1,13 +1,23 @@
 ;;;;; tré – Copyright (c) 2013 Sven Michael Klose <pixel@copei.de>
 
-(dont-obfuscate *math *p-i abs pow sqrt sin cos atan atan2)
+(dont-obfuscate *math *p-i abs acos asin atan atan2 ceil cos exp floor log max min pow rount sin sqrt tan)
 
 (defconstant *pi* *math.*p-i)
 
 (defun abs (x)     (*math.abs x))
-(defun pow (x y)   (*math.pow x y))
-(defun sqrt (x)    (*math.sqrt x))
-(defun sin (x)     (*math.sin x))
-(defun cos (x)     (*math.cos x))
+(defun acos (x)    (*math.acos x))
+(defun asin (x)    (*math.asin x))
 (defun atan (x)    (*math.atan x))
 (defun atan2 (a b) (*math.atan2 a b))
+(defun ceil (x)    (*math.ceil x))
+(defun cos (x)     (*math.cos x))
+(defun exp (x)     (*math.exp x))
+(defun floor (x)   (*math.floor x))
+;(defun log (x)     (*math.log x))   ; TODO rename LOG in Caroshi.
+;(defun max (a b)   (*math.max a b))
+;(defun min (a b)   (*math.min a b))
+(defun pow (x y)   (*math.pow x y))
+(defun round (x)   (*math.round x))
+(defun sin (x)     (*math.sin x))
+(defun sqrt (x)    (*math.sqrt x))
+(defun tan (x)     (*math.tan x))
