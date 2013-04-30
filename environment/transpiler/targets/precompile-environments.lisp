29c2ad91383c2cf449a20a82261854f41b121536
Sven Michael Klose <pixel@hugbox.org>
2013-04-30 20:41:06 +0000

    PRECOMPILE-ENVIRONMENTS now returns NIL.

--- environment/transpiler/targets/precompile-environments.lisp
+++ environment/transpiler/targets/precompile-environments.lisp
@@ -1,7 +1,8 @@
-;;;;; tré – Copyright (c) 2012 Sven Michael Klose <pixel@copei.de>
+;;;;; tré – Copyright (c) 2012–2013 Sven Michael Klose <pixel@copei.de>
 
 (defun precompile-environments ()
   (format t "; Initializing JavaScript/ECMAScript target...~%" *boot-image*)
   (compile nil :target 'js :transpiler *js-transpiler*)
   (format t "; Initializing PHP target...~%" *boot-image*)
-  (compile nil :target 'php :transpiler *php-transpiler*))
+  (compile nil :target 'php :transpiler *php-transpiler*)
+  nil)
