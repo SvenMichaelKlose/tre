9b645cf1d2f26655cb101c710992a61e6d4b248c
Sven Michael Klose <pixel@hugbox.org>
2013-05-12 21:25:06 +0000

    DEFTYPE and basic types added.

--- environment/stage2/main.lisp
+++ environment/stage2/main.lisp
@@ -26,6 +26,7 @@
 (env-load "stage2/butlast.lisp" 'c)
 (env-load "stage2/make-default-listprop.lisp")
 (env-load "stage2/dot-expand.lisp")
+(env-load "stage2/deftype.lisp")
 (env-load "stage2/defstruct.lisp")
 (env-load "stage2/abs.lisp" 'c)
 (env-load "stage2/traverse.lisp")
