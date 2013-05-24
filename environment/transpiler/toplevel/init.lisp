1db47a6d4cac62897d3dd02a5942a5c9f8dae536
Sven Michael Klose <pixel@hugbox.org>
2013-05-24 20:54:18 +0000

    Switch on assertions with *ASSERT*.

--- environment/transpiler/toplevel/init.lisp
+++ environment/transpiler/toplevel/init.lisp
@@ -33,6 +33,7 @@
 (defun create-transpiler (&rest args)
   (aprog1 (apply #'make-transpiler args)
 	(transpiler-reset !)
+    (= (transpiler-assert? !) *assert*)
 	(transpiler-make-std-macro-expander !)
 	(transpiler-make-code-expander !)
 	(funcall (transpiler-expex-initializer !) (transpiler-make-expex !))
