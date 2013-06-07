93877554f9cf4b55286db5255ca3c255694e72e6
Sven Michael Klose <pixel@hugbox.org>
2013-06-07 20:30:17 +0000

    Cleaned up.

--- interpreter/print.h
+++ interpreter/print.h
@@ -1,5 +1,5 @@
 /*
- * tré – Copyright (c) 2005–2006,2009,2012 Sven Michael Klose <pixel@copei.de>
+ * tré – Copyright (c) 2005–2006,2009,2012–2013 Sven Michael Klose <pixel@copei.de>
  */
 
 #ifdef INTERPRETER
@@ -10,8 +10,6 @@
 extern treptr treprint_highlight;
 
 extern treptr treprint (treptr);
-
-extern treptr treprint_builtin_princ (treptr);
 
 #endif /* #ifndef TRE_PRINT_H */
 
