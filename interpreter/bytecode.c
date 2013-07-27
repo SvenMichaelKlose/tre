714fb699ffc6f1bb8acfa7c9a060d7e9e945e2fe
Sven Michael Klose <pixel@hugbox.org>
2013-07-27 19:01:31 +0000

    Removed debug code.

--- interpreter/bytecode.c
+++ interpreter/bytecode.c
@@ -2,10 +2,6 @@
  * tré – Copyright (c) 2012–2013 Sven Michael Klose <pixel@copei.de>
  */
 
-/*
-#define BYTECODE_LOG
-*/
-
 #include <string.h>
 #include <strings.h>
 #include <stdlib.h>
@@ -71,14 +67,10 @@ trecode_set_place (treptr ** p, treptr value)
     treptr * x = *p;
     treptr v = *x++;
 
-    if (TREPTR_IS_NUMBER(v)) {
-#ifdef BYTECODE_LOG
-        printf ("set stack %d\n", TRENUMBER_INT(v));
-#endif
+    if (TREPTR_IS_NUMBER(v))
         trestack_ptr[TRENUMBER_INT(v)] = value;
-    } else if (v != treptr_nil) {
+    else if (v != treptr_nil)
         TRESYMBOL_VALUE(v) = value;
-    }
 
     *p = x;
 }
@@ -99,36 +91,17 @@ trecode_get (treptr ** p)
     int     i;
 
     v = *x++;
-#ifdef BYTECODE_LOG
-printf ("GET: ");
-treprint (v);
-#endif
 
     if (v == treptr_nil) {
-#ifdef BYTECODE_LOG
-        printf ("NIL\n");
-#endif
         /* Return NIL. */
     } else if (TREPTR_IS_NUMBER(v)) {
-#ifdef BYTECODE_LOG
-        printf ("stack %d\n", TRENUMBER_INT(v));
-#endif
         v = trestack_ptr[TRENUMBER_INT(v)];
     } else if (v == treptr_quote) {
         v = *x++;
-#ifdef BYTECODE_LOG
-        printf ("quote %s\n", TRESYMBOL_NAME(v));
-#endif
     } else if (v == treptr_vec) {
-#ifdef BYTECODE_LOG
-        printf ("vec\n");
-#endif
         vec = trecode_get (&x);
         v = _TREVEC(vec, TRENUMBER_INT(*x++));
     } else if (v == treptr_closure) {
-#ifdef BYTECODE_LOG
-        printf ("closure\n");
-#endif
         fun = *x++;
         tregc_push_secondary (fun);
         lex = trecode_get (&x);
@@ -137,68 +110,37 @@ treprint (v);
         tregc_pop_secondary ();
         tregc_pop_secondary ();
     } else if (TREPTR_IS_SYMBOL(v) && TRESYMBOL_FUN(v)) {
-#ifdef BYTECODE_LOG
-        printf ("funcall ");
-#endif
         fun = TRESYMBOL_FUN(v);
         if (TREPTR_IS_BUILTIN(fun)) {
-#ifdef BYTECODE_LOG
-            printf ("builtin ");
-#endif
             if (v == treptr_cons) {
-#ifdef BYTECODE_LOG
-                printf ("cons\n");
-#endif
                 car = trecode_get (&x);
                 tregc_push_secondary (car);
                 cdr = trecode_get (&x);
                 v = CONS(car, cdr);
                 tregc_pop_secondary ();
             } else if (v == treptr_set_atom_fun) {
-#ifdef BYTECODE_LOG
-                printf ("set-atom-fun\n");
-#endif
                 trecode_set_place (&x, trecode_get (&x));
             } else {
-#ifdef BYTECODE_LOG
-                printf ("\n");
-#endif
                 num_args = TRENUMBER_INT(*x++);
                 args = trecode_list (&x, num_args);
                 tregc_push_secondary (args);
                 v = treeval_xlat_function (treeval_xlat_builtin, fun, args, FALSE);
                 tregc_pop_secondary ();
-/*
-                TRELIST_FREE_TOPLEVEL_EARLY(args);
-*/
             }
         } else if (TREFUNCTION_BYTECODE(fun) != treptr_nil) {
-#ifdef BYTECODE_LOG
-            printf ("bytecode\n");
-#endif
             tregc_push_secondary (fun);
             num_args = TRENUMBER_INT(*x++);
             old_sp = trestack_ptr;
             DOTIMES(i, num_args)
                 *--old_sp = trecode_get (&x);
             trestack_ptr = old_sp;
-#ifdef BYTECODE_LOG
-car = v;
-#endif
             v = trecode_exec (fun);
             trestack_ptr += num_args;
             tregc_pop_secondary ();
-#ifdef BYTECODE_LOG
-printf ("returned from %s\n", TRESYMBOL_NAME(car));
-#endif
         } else
             treerror_norecover (v, "function expected in bytecode");
     } else if (v != treptr_nil && v != treptr_t)
         treerror_norecover (v, "Un%QUOTEd literal in bytecode.");
-#ifdef BYTECODE_LOG
-else
-            printf ("NIL|T\n");
-#endif
     *p = x;
 
     return v;
@@ -207,9 +149,6 @@ else
 void
 trecode_set (treptr ** x)
 {
-#ifdef BYTECODE_LOG
-printf ("SET: ");
-#endif
     trecode_set_place (x, trecode_get (x));
 }
 
@@ -223,11 +162,6 @@ trecode_exec (treptr fun)
     int      num_locals;
     int      i;
     int      vec;
-
-#ifdef BYTECODE_LOG
-printf ("EXEC: ");
-treprint (fun);
-#endif
 
     fun = TREFUNCTION_BYTECODE(fun);
     if (TREPTR_IS_ARRAY(fun) == FALSE)
@@ -245,32 +179,15 @@ treprint (fun);
         if (v == treptr_jmp) {
             x++;
             dest = *x++;
-            if (dest == treptr_nil) {
-#ifdef BYTECODE_LOG
-printf ("End of function.\n");
-#endif
+            if (dest == treptr_nil)
                 break;
-            }
-#ifdef BYTECODE_LOG
-printf ("Jump to %d.\n", TRENUMBER_INT(dest));
-#endif
             x = &code[TRENUMBER_INT(dest)];
         } else if (v == treptr_cond) {
             x++;
             dest = *x++;
-#ifdef BYTECODE_LOG
-printf ("Conditional jump to %d.\n", TRENUMBER_INT(dest));
-#endif
-            if (trecode_get (&x) == treptr_nil) {
-#ifdef BYTECODE_LOG
-printf ("Jump to %d.\n", TRENUMBER_INT(dest));
-#endif
+            if (trecode_get (&x) == treptr_nil)
                 x = &code[TRENUMBER_INT(dest)];
-            }
         } else if (v == treptr_set_vec) {
-#ifdef BYTECODE_LOG
-printf ("Set vector.\n");
-#endif
             x++;
             vec = trecode_get (&x);
             tregc_push_secondary (vec);
