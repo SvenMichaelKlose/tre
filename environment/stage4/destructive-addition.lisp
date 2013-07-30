25336deae1cb3c754936d13cb837bc97fe0cd4a6
Sven Michael Klose <pixel@hugbox.org>
2013-07-30 18:56:13 +0000

    Added new macros +! and INTEGER+!.

--- environment/stage4/destructive-addition.lisp
+++
@@ -1,7 +0,0 @@
-;;;;; tré – Copyright (c) 2010,2012 Sven Michael Klose <pixel@copei.de>
-
-(defmacro +! (place &rest vals)
-  `(= ,place (+ ,place ,@vals)))
-
-(defmacro integer+! (place &rest vals)
-  `(= ,place (integer+ ,place ,@vals)))
---
+++ environment/stage4/destructive-arith.lisp
@@ -0,0 +1,13 @@
+;;;;; tré – Copyright (c) 2010,2012–2013 Sven Michael Klose <pixel@copei.de>
+
+(defmacro +! (place &rest vals)
+  `(= ,place (+ ,place ,@vals)))
+
+(defmacro -! (place &rest vals)
+  `(= ,place (+ ,place ,@vals)))
+
+(defmacro integer+! (place &rest vals)
+  `(= ,place (integer+ ,place ,@vals)))
+
+(defmacro integer-! (place &rest vals)
+  `(= ,place (integer- ,place ,@vals)))
