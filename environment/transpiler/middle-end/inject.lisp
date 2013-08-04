3bd9595dfda67fec5d0e00aada52aaadb88a8c60
Sven Michael Klose <pixel@hugbox.org>
2013-08-04 14:36:42 +0000

    Don't repeat locations.

--- environment/transpiler/middle-end/inject.lisp
+++ environment/transpiler/middle-end/inject.lisp
@@ -1,21 +1,19 @@
 ;;;;; tré – Copyright (c) 2012–2013 Sven Michael Klose <pixel@copei.de>
 
-(defvar *former-position* nil)
+(defvar *previous-position* nil)
 
 (def-head-predicate debugger-step)
 
-(defun same-location? (a b)
+(defun same-or-previous-location? (a b)
   (& a b
      (string== a. b.)
-     (== (car .a) (car .b))
-     (== (cdr .a) (cdr .b))))
-;     (| (> (cdr .a) (cdr .b))
-;        (? (== (cdr .a) (cdr .b))
-;           (>= (car .a) (car .b))))))
+     (| (> (cdr .a) (cdr .b))
+        (? (== (cdr .a) (cdr .b))
+           (>= (car .a) (car .b))))))
 
 (defun find-next-location (x old)
   (& (cons? x)
-     (| (& (not (same-location? old (cpr x)))
+     (| (& (not (same-or-previous-location? old (cpr x)))
            (let section (transpiler-current-section *transpiler*)
              (alet (cpr x)
                (& (string? section)
@@ -26,9 +24,9 @@
 
 (metacode-walker inject-debugging (x)
 	:if-cons (!? (& (%setq? x.)
-                    (find-next-location x *former-position*))
+                    (find-next-location x *previous-position*))
                  (progn
-                   (= *former-position* !)
+                   (= *previous-position* !)
                    `((%setq nil (debugger-step ,(car !) ,(car .!) ,(cdr .!)))
                      ,x.))
                  (list x.)))
