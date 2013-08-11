257678841810c4a4ba6a59496782172ed235710e
Sven Michael Klose <pixel@hugbox.org>
2013-08-11 13:38:18 +0000

    Enable backtrace by default.

--- environment/transpiler/targets/c/config.lisp
+++ environment/transpiler/targets/c/config.lisp
@@ -3,6 +3,7 @@
 (defun make-c-transpiler ()
   (aprog1 (create-transpiler
               :name 'c
+              :backtrace? t
 			  :separator (format nil ";~%")
 			  :inline-exceptions '(c-init error format replace-tree)
 			  :identifier-char?  [| (<= #\a _ #\z)
