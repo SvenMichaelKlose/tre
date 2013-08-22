97bf01717faf3b06bf5a9c294e11878fabc895b9
Sven Michael Klose <pixel@hugbox.org>
2013-08-22 16:23:59 +0000

    TRANSPILER-ALL-PASSES has been removed.

--- environment/transpiler/toplevel/toplevel.lisp
+++ environment/transpiler/toplevel/toplevel.lisp
@@ -1,4 +1,4 @@
-;;;;; tré – Copyright (c) 2008–2010,2012 Sven Michael Klose <pixel@copei.de>
+;;;;; tré – Copyright (c) 2008–2010,2012–2013 Sven Michael Klose <pixel@copei.de>
 
 (defun transpiler-make-code (tr forms)
   (transpiler-backend tr (transpiler-middleend tr forms)))
@@ -6,6 +6,3 @@
 (defun transpiler-make-toplevel-function (tr)
   `((defun accumulated-toplevel ()
       ,@(reverse (transpiler-accumulated-toplevel-expressions tr)))))
-
-(defun transpiler-all-passes (tr x)
-  (transpiler-make-code tr (transpiler-frontend tr x)))
