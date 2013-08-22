f332ad73252ba2ac8b15017fd339bf6c3801e98b
Sven Michael Klose <pixel@hugbox.org>
2013-08-22 13:38:19 +0000

    Cleaned up stack macros.

--- interpreter/list.h
+++ interpreter/list.h
@@ -7,30 +7,25 @@
 
 #include <stddef.h>
 
-#define TRELIST_PUSH(stack, expr) \
-    stack = CONS(expr, stack);
+#define TRELIST_PUSH(stack, expr)  stack = CONS(expr, stack);
+#define TRELIST_POP(stack)         stack = CDR(stack);
 
-#define TRELIST_POP(stack) \
-    { int __llpp = CDR(stack); 	\
-      trelist_free (stack);	\
-      stack = __llpp;		\
-    }
 #define TRELIST_DEFREGS() \
 	treptr car;	\
 	treptr cdr
 
 extern struct tre_sequence_type trelist_seqtype;
 
-extern treptr trelist_copy (treptr);
-extern treptr trelist_copy_tree (treptr);
-extern treptr trelist_delete (tre_size, treptr);
-extern void   trelist_append (treptr *lst, treptr lst2);
-extern long   trelist_position (treptr elt, treptr lst);
-extern long   trelist_position_name (treptr elt, treptr lst);
+extern treptr   trelist_copy (treptr);
+extern treptr   trelist_copy_tree (treptr);
+extern treptr   trelist_delete (tre_size, treptr);
+extern void     trelist_append (treptr *lst, treptr lst2);
+extern long     trelist_position (treptr elt, treptr lst);
+extern long     trelist_position_name (treptr elt, treptr lst);
 extern tre_size trelist_length (treptr);
-extern bool   trelist_equal (treptr, treptr);
-extern treptr trelist_nth (treptr, tre_size);
-extern treptr trelist_last (treptr);
-extern bool   trelist_check_type (treptr, tre_size atom_type);
+extern bool     trelist_equal (treptr, treptr);
+extern treptr   trelist_nth (treptr, tre_size);
+extern treptr   trelist_last (treptr);
+extern bool     trelist_check_type (treptr, tre_size atom_type);
 
 #endif 	/* #ifndef TRE_LIST_H */
