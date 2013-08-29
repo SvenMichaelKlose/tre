d56d79446d207a7b1cdfcb3883b839a7c108831d
Sven Michael Klose <pixel@hugbox.org>
2013-08-29 11:35:53 +0000

    Removed unused code.

--- environment/transpiler/targets/bytecode/builtin.lisp
+++
@@ -1,9 +0,0 @@
-;;;;; tré – Copyright (c) 2009–2013 Sven Michael Klose <pixel@copei.de>
-
-(defmacro make-bc-builtins ()
-  `(progn
-     ,@(mapcar ^(define-bc-macro ,_ (&rest x)
- 			      `(%bc-builtin ,(list '%quote _) ,,(length x) ,,@x))
-               (remove-if [in? _ 'cons '%quote] *builtins*))))
-
-;(make-bc-builtins)
