b8a734093e2ac40d6e95fc4218cd281128337b38
Sven Michael Klose <pixel@hugbox.org>
2013-09-02 13:33:21 +0000

    BODY-WITH-NOARGS-TAG throws an error message if NOARGS tag is already there.

--- environment/transpiler/targets/shared/noargs-tag.lisp
+++ environment/transpiler/targets/shared/noargs-tag.lisp
@@ -1,13 +1,15 @@
-;;;;; tré – Copyright (c) 2008–2010,2012 Sven Michael Klose <pixel@copei.de>
+;;;;; tré – Copyright (c) 2008–2010,2012–2013 Sven Michael Klose <pixel@copei.de>
 
 (defun list-without-noargs-tag (x)
   (remove 'no-args x))
 
-(defun body-with-noargs-tag (body)
-  `(no-args ,@body))
-
 (defun body-has-noargs-tag? (body)
   (eq 'no-args body.))
+
+(defun body-with-noargs-tag (body)
+  (& (body-has-noargs-tag? body)
+     (error "Body already has NOARGS tag."))
+  `(no-args ,@body))
 
 (defun body-without-noargs-tag (body)
   (? (body-has-noargs-tag? body)
