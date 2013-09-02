56d64c6ef86614de33dca9ca819565c689261488
Sven Michael Klose <pixel@hugbox.org>
2013-09-02 08:48:49 +0000

    Don't inline optimized versions of FUNCALL.

--- environment/transpiler/targets/javascript/expand-inline.lisp
+++ environment/transpiler/targets/javascript/expand-inline.lisp
@@ -6,14 +6,5 @@
          (eq ,x. ,@..x))
      `(eq ,@x)))
 
-(define-js-std-macro funcall (fun &rest args)
-  (with-gensym (f e a)
-    `(with (,f ,fun
-            ,e (slot-value ,f 'tre-exp))
-       (? ,e
-          (let ,a (list ,@args)
-            ((slot-value ,e 'apply) nil (%%native "[" ,a "]")))
-          (,f ,@args)))))
-
 (define-js-std-macro make-string (&optional len)
   "")
