3cc33e222fad8a78d0f18353e6ff2270f32a1167
Sven Michael Klose <pixel@hugbox.org>
2013-09-02 07:13:02 +0000

    Don't backtrace argument expanders.

--- environment/transpiler/targets/shared/expand/defun.lisp
+++ environment/transpiler/targets/shared/expand/defun.lisp
@@ -10,41 +10,44 @@
       (make-symbol (symbol-name x) !)
       x))
 
-(defun shared-defun-profiling-body (tr name body)
-  (? (& (transpiler-profile? tr)
-        (not (eq 'add-profile name)
-             (eq 'add-profile-call name)))
-     (? (transpiler-profile-num-calls? tr)
-        `((progn
-            (& (not *profile-lock*)
-               (add-profile-call ',name))
-            ,@body))
-        `((let ~%profiling-timer (& (not *profile-lock*) (%%%nanotime))
-            (prog1
-              (progn
-                ,@body)
-              (& ~%profiling-timer
-                 (add-profile ',name (integer- (%%%nanotime) ~%profiling-timer)))))))
-     body))
+(defun shared-defun-profiling-body (name body)
+  (alet *transpiler*
+    (? (& (transpiler-profile? !)
+          (not (eq 'add-profile name)
+               (eq 'add-profile-call name)))
+       (? (transpiler-profile-num-calls? !)
+          `((progn
+              (& (not *profile-lock*)
+                 (add-profile-call ',name))
+              ,@body))
+          `((let ~%profiling-timer (& (not *profile-lock*)
+                                      (%%%nanotime))
+              (prog1
+                (progn
+                  ,@body)
+                (& ~%profiling-timer
+                   (add-profile ',name (integer- (%%%nanotime) ~%profiling-timer)))))))
+       body)))
 
-(defun shared-defun-memorize-source (tr name args body)
-  (acons! name (cons args body) (transpiler-memorized-sources tr))
+(defun shared-defun-memorize-source (name args body)
+  (acons! name (cons args body) (transpiler-memorized-sources *transpiler*))
   nil)
 
-(defun shared-defun-source-memorizer (tr name args body)
-  (+ (& *have-compiler?*
-        (not (transpiler-memorize-sources? tr))
-        `((%setq *defined-functions* (cons ',name *defined-functions*))))
-     (when (transpiler-save-sources? tr)
-       (apply #'transpiler-add-obfuscation-exceptions tr (collect-symbols (list name args body)))
-       (? (transpiler-memorize-sources? tr)
-          (shared-defun-memorize-source tr name args body)
-          `((%setq (slot-value ,name '__source) ,(let source (assoc-value name *function-sources* :test #'eq)
-                                                   `'(,(| source. args) . ,(unless (transpiler-save-argument-defs-only? tr)
-                                                                             (| .source body))))))))))
+(defun shared-defun-source-memorizer (name args body)
+  (alet *transpiler*
+    (+ (& *have-compiler?*
+          (not (transpiler-memorize-sources? !))
+          `((%setq *defined-functions* (cons ',name *defined-functions*))))
+       (when (transpiler-save-sources? !)
+         (apply #'transpiler-add-obfuscation-exceptions ! (collect-symbols (list name args body)))
+         (? (transpiler-memorize-sources? !)
+            (shared-defun-memorize-source name args body)
+            `((%setq (slot-value ,name '__source) ,(let source (assoc-value name *function-sources* :test #'eq)
+                                                     `'(,(| source. args) . ,(unless (transpiler-save-argument-defs-only? !)
+                                                                               (| .source body)))))))))))
 
-(defun shared-defun-backtrace (tr name body)
-  (? (transpiler-backtrace? tr)
+(defun shared-defun-backtrace (name body)
+  (? (transpiler-backtrace? *transpiler*)
      `((push ',name *backtrace*)
        (prog1
          (progn
@@ -52,32 +55,34 @@
          (= *backtrace* .*backtrace*)))
      body))
 
-(defun shared-defun-without-expander (name args body &key (make-source-memorizer? nil))
+(defun shared-defun-without-expander (name args body &key (source-memorizer? nil) (allow-backtrace? nil))
   (= name (apply-current-package name))
   (print-definition `(defun ,name ,args))
   (| (list? args)
      (error "Argument list expected instead of ~A." args))
-  (let tr *transpiler*
-    (& (transpiler-defined-function tr name)
-       (redef-warn "Redefinition of function ~A." name))
-	(transpiler-add-defined-function tr name args body)
-	`((function ,name (,args
-                       ,@(& (body-has-noargs-tag? body)
-                            '(no-args))
-                       (block ,name
-                         ,@(shared-defun-backtrace tr name (shared-defun-profiling-body tr name body)))))
-      ,@(& make-source-memorizer?
-           (shared-defun-source-memorizer tr name args body)))))
+  (& (transpiler-defined-function *transpiler* name)
+     (redef-warn "Redefinition of function ~A." name))
+  (transpiler-add-defined-function *transpiler* name args body)
+  `((function ,name (,args
+                     ,@(& (body-has-noargs-tag? body)
+                          '(no-args))
+                     (block ,name
+                       ,@(alet (shared-defun-profiling-body name body)
+                           (? allow-backtrace?
+                              (shared-defun-backtrace name !)
+                              !)))))
+      ,@(& source-memorizer?
+           (shared-defun-source-memorizer name args body))))
 
 (defun shared-defun (name args body &key (make-expander? t))
   (let fun-name (%defun-name name)
     `(%%block
-       ,@(shared-defun-without-expander fun-name args body :make-source-memorizer? t)
+       ,@(shared-defun-without-expander fun-name args body :source-memorizer? t :allow-backtrace? t)
        ,@(when (& args make-expander?
-                  (not (simple-argument-list? args)
-                       (transpiler-assert? *transpiler*)))
+                  (| (not (simple-argument-list? args))
+                     (transpiler-assert? *transpiler*)))
            (with-gensym p
              (shared-defun-without-expander (c-expander-name fun-name) (list p)
-                                            (list (compile-argument-expansion-function-body fun-name args p nil
-                                                                                            (argument-expand-names 'compile-argument-expansion args)))
-                                            :make-source-memorizer? nil))))))
+                                            (compile-argument-expansion-function-body fun-name args p nil
+                                                                                      (argument-expand-names 'compile-argument-expansion args))
+                                            :source-memorizer? nil))))))
