6a121f6337069b0c963cd6959840ca64d5e2111f
Sven Michael Klose <pixel@hugbox.org>
2013-09-04 11:22:16 +0000

    This target doesn't support lambda export anymore.

--- environment/transpiler/targets/javascript/environment/bind.lisp
+++ environment/transpiler/targets/javascript/environment/bind.lisp
@@ -1,22 +1,18 @@
 ;;;;; tré – Copyright (c) 2008–2010,2012–2013 Sven Michael Klose <pixel@copei.de>
 
+;;;; Bind function to an object.
+;;;;
+;;;; ECMAScript doesn't know method references. Functions and their objects
+;;;; are separated.
+;;;;
+;;;; See also macro BIND in 'expand.lisp'.
+
 (dont-obfuscate arguments)
 
-;; Bind function to an object.
-;;
-;; ECMAScript doesn't know method references. Functions and their objects
-;; are separated.
-;;
-;; See also macro BIND in 'expand.lisp'.
 (defun %bind (obj fun)
   (when-debug
 	(unless (function? fun)
 	  (js-print fun logwindow.document)))
   (assert (function? fun) "BIND requires a function")
   #'(()
-	  ,(? (transpiler-lambda-export? *js-transpiler*)
-		  ; Get rid of the ghost argument.
-		  '(let a (__manual-array-copy arguments)
-		     (a.shift)
-    	     (fun.apply obj a))
-      	  '(fun.apply obj arguments))))
+      (fun.apply obj arguments)))
