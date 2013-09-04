018928ea6bef9b64e8eb01103d68a51843e5b086
Sven Michael Klose <pixel@hugbox.org>
2013-09-04 11:29:39 +0000

    Don't have a function name prefix.

--- environment/transpiler/targets/bytecode/config.lisp
+++ environment/transpiler/targets/bytecode/config.lisp
@@ -2,19 +2,21 @@
 
 (defun make-bc-transpiler ()
   (aprog1 (create-transpiler
-              :name 'bytecode
-			  :lambda-export? t
-			  :stack-locals? t
-			  :arguments-on-stack? t
-			  :literal-conversion #'identity
-	          :expex-initializer #'((ex)
-	                                  (= (expex-argument-filter ex) #'bc-expex-argument-filter
-			                             (expex-setter-filter ex) (compose [mapcan [expex-set-global-variable-value _] _]
-									                                       #'expex-compiled-funcall)))
-              :code-concatenator #'tree-list
-              :make-text? nil
+              :name                 'bytecode
+			  :lambda-export?       t
+			  :stack-locals?        t
+			  :arguments-on-stack?  t
+			  :literal-conversion   #'identity
+	          :expex-initializer
+                #'((ex)
+	                 (= (expex-argument-filter ex) #'bc-expex-argument-filter
+			            (expex-setter-filter ex) (compose [mapcan [expex-set-global-variable-value _] _]
+									                      #'expex-compiled-funcall)))
+              :code-concatenator    #'tree-list
+              :make-text?           nil
               :encapsulate-strings? nil
-              :function-prologues? nil)
+              :function-prologues?  nil
+              :function-name-prefix nil)
     (transpiler-add-plain-arg-funs ! *builtins*)))
 
 (defvar *bc-transpiler* (copy-transpiler (make-bc-transpiler)))
