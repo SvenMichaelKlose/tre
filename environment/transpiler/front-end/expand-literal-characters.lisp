b68a8a33814d15f64e84acedbde03dc28b36c67c
Sven Michael Klose <pixel@hugbox.org>
2013-09-06 12:41:03 +0000

    Removed unused code.

--- environment/transpiler/front-end/expand-literal-characters.lisp
+++ environment/transpiler/front-end/expand-literal-characters.lisp
@@ -1,6 +1,4 @@
 ;;;;; tré – Copyright (c) 2008–2013 Sven Michael Klose <pixel@copei.de>
 
 (define-tree-filter transpiler-expand-literal-characters (x)
-  (character? x) (? (eq 'c (transpiler-name *transpiler*))
-                    `(trenumber_builtin_code_char ,(compiled-list (list (char-code x))))
-                    `(code-char ,(char-code x))))
+  (character? x) `(code-char ,(char-code x)))
