48449f1c968c10bd3e825cc456eb4a86b9411728
Sven Michael Klose <pixel@hugbox.org>
2013-09-08 17:43:55 +0000

    Removed unused code.

--- environment/transpiler/targets/shared/expand/defmacro.lisp
+++
@@ -1,7 +0,0 @@
-;;;;; tré - Copyright (c) 2008-2013 Sven Michael Klose <pixel@copei.de>
-
-(defun shared-defmacro (&rest x)
-  (print-definition `(defmacro ,x. ,.x.))
-  (eval (macroexpand `(define-transpiler-std-macro *transpiler* ,@x)))
-  (when *have-compiler?*
-    `(define-std-macro ,@x)))
