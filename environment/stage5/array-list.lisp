092a477d4bb37910e1390c1a4b5d01db6d8baab0
Sven Michael Klose <pixel@hugbox.org>
2013-09-09 20:04:15 +0000

    Removed unused file.

--- environment/stage5/array-list.lisp
+++
@@ -1,7 +0,0 @@
-;;;;; TRE environment
-;;;;; Copyright (c) 2011 Sven Klose <pixel@copei.de>
-
-(defun array-list (x)
-  (let result (make-queue)
-    (dotimes (i (length x) (queue-list result))
-      (enqueue result (aref x i)))))
