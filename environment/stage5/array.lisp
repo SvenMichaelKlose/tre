c984ea3fc4408e64949ded5cb0e39efa7090958e
Sven Michael Klose <pixel@hugbox.org>
2013-09-09 20:10:07 +0000

    ARRAY-COPY has been renamed COPY-ARRAY.

--- environment/stage5/array.lisp
+++ environment/stage5/array.lisp
@@ -1,21 +1,12 @@
 ;;;;; tré – Copyright (c) 2008–2013 Sven Michael Klose <pixel@copei.de>
 
-(defun array-list (x)
-  (let result (make-queue)
-    (adotimes ((length x) (queue-list result))
-      (enqueue result (aref x !)))))
-
-(defun array-copy (arr)
+(defun copy-array (arr)
   (do ((ret (make-array))
        (i 0 (++ i)))
       ((integer== i (length arr)) ret)
     (= (aref ret i) (aref arr i))))
 
-(defun array-filter (arr pred)
-  (do ((ret (make-array))
-       (i 0 (++ i))
-       (j 0))
-      ((integer== i (length arr)) ret)
-    (when (funcall pred (aref arr i))
-      (= (aref ret j) (aref arr i))
-      (++! j))))
+(defun array-list (x)
+  (let result (make-queue)
+    (adotimes ((length x) (queue-list result))
+      (enqueue result (aref x !)))))
