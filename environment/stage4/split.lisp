e7c70cba9177ca5315011fff578f84de6a37e3b9
Sven Michael Klose <pixel@hugbox.org>
2013-09-10 21:50:36 +0000

    Added :INCLUDE? argument, to tell that splitpoints have to be included in returned sequences.

--- environment/stage4/split.lisp
+++ environment/stage4/split.lisp
@@ -1,23 +1,25 @@
 ;;;;; tré – Copyright (c) 2008–2009,2011–2013 Sven Michael Klose <pixel@copei.de>
 
-(defun split-if (predicate seq)
-  "Split sequence at elements for which the predicate is true. Those elements are excluded. Returns a list of subsequences."
+(defun split-if (predicate seq &key (include? nil))
   (& seq
      (!? (position-if predicate seq)
-         (cons (subseq seq 0 !)
-               (split-if predicate (subseq seq (integer++ !))))
+         (cons (subseq seq 0 (? include?
+                                (integer++ !)
+                                !))
+               (split-if predicate (subseq seq (integer++ !) :include? include?)))
          (list seq))))
 
-(defun generic-split (obj seq &key (test #'eql))
+(defun generic-split (obj seq &key (test #'eql) (include? nil))
   (& seq
      (!? (position obj seq :test test)
-         (cons (subseq seq 0 !)
-               (generic-split obj (subseq seq (integer++ !)) :test test))
+         (cons (subseq seq 0 (? include?
+                                (integer++ !)
+                                !))
+               (generic-split obj (subseq seq (integer++ !)) :test test :include? include?))
          (list seq))))
 
-(defun split (obj seq &key (test #'eql))
-  "Split sequence at elements equal to OBJ. Those elements are excluded. Returns a list of subsequences."
-  (generic-split obj seq :test test))
+(defun split (obj seq &key (test #'eql) (include? nil))
+  (generic-split obj seq :test test :include? include?))
 
 (define-test "SPLIT works on string"
   ((let x (split #\/ "foo/bar")
