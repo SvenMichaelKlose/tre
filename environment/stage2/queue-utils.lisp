17b7988c2cc4dba4d75526858467c7132ee53058
Sven Michael Klose <pixel@hugbox.org>
2013-09-11 13:04:51 +0000

    Macro DOLIST-QUEUE has been removed.

--- environment/stage2/queue-utils.lisp
+++ environment/stage2/queue-utils.lisp
@@ -1,10 +1,5 @@
-;;;;; tré – Copyright (c) 2005-2006,2008,2011–2012 Sven Michael Klose <pixel@copei.de>
+;;;;; tré – Copyright (c) 2005-2006,2008,2011–2013 Sven Michael Klose <pixel@copei.de>
 
 (defmacro with-queue (q &rest body)
   `(let* (,@(mapcar ^(,_ (make-queue)) (force-list q)))
      ,@body))
-
-(defmacro dolist-queue (q dlargs &rest body)
-  `(with-queue ,q
-     (dolist ,(append dlargs `((queue-list ,q)))
-       ,@body)))
