184e014f9687e3b5d8410ab91f687799f41fda8b
Sven Michael Klose <pixel@hugbox.org>
2013-09-11 18:14:17 +0000

    Fix scope error.

--- environment/stage4/tree-list.lisp
+++ environment/stage4/tree-list.lisp
@@ -1,11 +1,11 @@
 ;;;;; tré – Copyright (c) 2008,2011–2013 Sven Michael Klose <pixel@copei.de>
 
 (defun tree-list (&rest x)
-  (with (rec [? (atom x)
-                (enqueue q x)
-                (adolist x
-                  (rec q !))])
-    (& x
-       (with-queue q
-         (rec q x)
-         (queue-list q)))))
+  (with-queue q
+    (with (rec [? (atom x)
+                  (enqueue q x)
+                  (adolist x
+                    (rec q !))])
+      (& x
+           (rec q x)
+           (queue-list q)))))
