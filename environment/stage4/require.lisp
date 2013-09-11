de81db5177cb5283cd7ac6c97c265eb4c7164c2e
Sven Michael Klose <pixel@hugbox.org>
2013-09-11 17:59:00 +0000

    Removed unused file.

--- environment/stage4/require.lisp
+++
@@ -1,9 +0,0 @@
-;;;;; tré – Copyright (c) 2009,2012 Sven Michael Klose <pixel@copei.de>
-
-(defvar *loaded-required-files* nil)
-
-(define-js-std-macro require (file)
-  (unless (member file *loaded-required-files* :test #'string==)
-    (print `(require ,file))
-    `(progn
-       ,@(dot-expand (read-file-all file)))))
