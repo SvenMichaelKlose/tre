32cdc2cf2f294fe6854f0e0f279ec9462a11de3b
Sven Michael Klose <pixel@hugbox.org>
2013-09-11 12:16:57 +0000

    Removed debug code.

--- environment/stage1/math.lisp
+++ environment/stage1/math.lisp
@@ -5,16 +5,8 @@
 (early-defun ++ (x)
   (number+ x 1))
 
-(define-test "++"
-  ((++ 1))
-  2)
-
 (early-defun -- (x)
   (number- x 1))
-
-(define-test "--"
-  ((-- 2))
-  1)
 
 (early-defun integer++ (x)
   (integer+ x 1))
@@ -22,15 +14,10 @@
 (early-defun integer-- (x)
   (integer- x 1))
 
+(define-test "++"
+  ((++ 1))
+  2)
 
-(defmacro 1+ (x)
-  (%error "1+ is deprecated."))
-
-(defmacro 1- (x)
-  (%error "1- is deprecated."))
-
-(defmacro integer-1+ (x)
-  (%error "integer-1+ is deprecated."))
-
-(defmacro integer-1- (x)
-  (%error "integer-1- is deprecated."))
+(define-test "--"
+  ((-- 2))
+  1)
