3068a81e2c708ea836fc07444d968538657c67f1
Sven Michael Klose <pixel@hugbox.org>
2013-09-11 12:43:58 +0000

    Removed debug code.

--- environment/stage1/incdec.lisp
+++ environment/stage1/incdec.lisp
@@ -1,4 +1,4 @@
-;;;;; tré – Copyright (c) 2005, 2008–2010,2012–2013 Sven Michael Klose <pixel@copei.de>
+;;;;; tré – Copyright (c) 2005,2008–2010,2012–2013 Sven Michael Klose <pixel@copei.de>
 
 (defmacro ++! (place &optional (n 1))
   `(= ,place (number+ ,place ,n)))
@@ -11,16 +11,3 @@
 
 (defmacro integer--! (place &optional (n 1))
   `(= ,place (integer- ,place ,n)))
-
-
-(defmacro 1+! (place &optional (n 1))
-  (%error "1+! is deprecated."))
-
-(defmacro 1-! (place &optional (n 1))
-  (%error "1-! is deprecated."))
-
-(defmacro integer1+! (place &optional (n 1))
-  (%error "INTEGER1+! is deprecated."))
-
-(defmacro integer1-! (place &optional (n 1))
-  (%error "INTEGER1-! is deprecated."))
