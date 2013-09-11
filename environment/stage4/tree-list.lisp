2705ab3cdb597ae0bd172f9dd65472bcb8b924c3
Sven Michael Klose <pixel@hugbox.org>
2013-09-11 18:09:54 +0000

    Cleaned up.

--- environment/stage4/tree-list.lisp
+++ environment/stage4/tree-list.lisp
@@ -1,13 +1,11 @@
 ;;;;; tré – Copyright (c) 2008,2011–2013 Sven Michael Klose <pixel@copei.de>
 
-(defun tree-list-0 (q x)
-  (? (atom x)
-     (enqueue q x)
-     (dolist (i x)
-       (tree-list-0 q i))))
-
 (defun tree-list (&rest x)
-  (& x
-     (with-queue q
-       (tree-list-0 q x)
-       (queue-list q))))
+  (with (rec [? (atom x)
+                (enqueue q x)
+                (adolist x
+                  (rec q !))])
+    (& x
+       (with-queue q
+         (rec q x)
+         (queue-list q)))))
