0831ea0898cb248f9de4ae05a661b525bd3279a6
Sven Michael Klose <pixel@hugbox.org>
2013-09-11 15:42:27 +0000

    Cleaned up.

--- environment/stage4/empty-string.lisp
+++ environment/stage4/empty-string.lisp
@@ -1,18 +1,13 @@
-;;;;; tré – Copyright (c) 2008–2009.2011–2012 Sven Michael Klose <pixel@copei.de>
-
-(defun %empty-string? (x)
-  (string== "" (| (trim #\  x) "")))
+;;;;; tré – Copyright (c) 2008–2009.2011–2013 Sven Michael Klose <pixel@copei.de>
 
 (defun empty-string? (&rest x)
-  (& x (every #'%empty-string? x)))
+  (every [string== "" (| (trim #\  _) "")] x))
+
+(defun empty-string-or-nil? (&rest x)
+  (every [| (not _)
+            (%empty-string? _)]
+         x))
 
 (define-test "EMPTY-STRING? works"
   ((empty-string? "  " ""))
   t)
-
-(defun %empty-string-or-nil? (x)
-  (| (not x)
-     (%empty-string? x)))
-
-(defun empty-string-or-nil? (&rest x)
-  (& x (every #'%empty-string-or-nil? x)))
