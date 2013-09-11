a62dc01822c0cbdea5cd4be3ca7f38c313456446
Sven Michael Klose <pixel@hugbox.org>
2013-09-11 17:52:17 +0000

    New function DEFAULT-STREAM.

--- environment/stage3/default-stream.lisp
+++ environment/stage3/default-stream.lisp
@@ -1,12 +1,15 @@
 ;;;;; tré – Copyright (c) 2005–2006,2008–2009,2012–2013 Sven Michael Klose <pixel@copei.de>
 
+(defun default-stream (x)
+  (case x :test #'eq
+    nil  (make-string-stream)
+    t	 *standard-output*
+	x))
+
 (defmacro with-default-stream (nstr str &body body)
   (with-gensym (g body-result)
     `(with (,g    ,str
-	        ,nstr (case ,g :test #'eq
-        		    nil  (make-string-stream)
-        	        t	 *standard-output*
-				    ,g)
+	        ,nstr (default-stream ,g)
             ,body-result (progn ,@body))
        (? ,g
 	      ,body-result
