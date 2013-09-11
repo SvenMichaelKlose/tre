b63e9305ad0f51d8560ed916b7a015209fe8c7fc
Sven Michael Klose <pixel@hugbox.org>
2013-09-11 13:31:35 +0000

    Removed experimental and unused code.

--- environment/transpiler/middle-end/graph/make-cblock-taglist.lisp
+++
@@ -1,9 +0,0 @@
-;;;;; tré – Copyright (c) 2010–2012 Sven Michael Klose <pixel@copei.de>
-
-(defun make-cblock-taglist (cblks)
-  (let tags nil
-    (dolist (cb cblks tags)
-      (let first (car (cblock-code cb))
-        (when (number? first)
-          (acons! first cb tags)
-          (= (cblock-code cb) (cdr (cblock-code cb))))))))
--- environment/transpiler/middle-end/main.lisp
+++ environment/transpiler/middle-end/main.lisp
@@ -1,18 +1,5 @@
 ;;;;; tré – (c) 2005–2013 Sven Michael Klose <pixel@copei.de>
 
-(when nil
-    (env-load "transpiler/middle-end/graph/cblock.lisp")
-    (env-load "transpiler/middle-end/graph/traverse.lisp")
-    (env-load "transpiler/middle-end/graph/print.lisp")
-    (env-load "transpiler/middle-end/graph/make-cblock-taglist.lisp")
-    (env-load "transpiler/middle-end/graph/make-cblock-links.lisp")
-    (env-load "transpiler/middle-end/graph/metacode-to-cblocks.lisp")
-    (env-load "transpiler/middle-end/graph/cblocks-to-metacode.lisp")
-    (env-load "transpiler/middle-end/graph/dataflow.lisp")
-    (env-load "transpiler/middle-end/graph/ssa.lisp")
-    (env-load "transpiler/middle-end/graph/opt-remove-doubles.lisp")
-    (env-load "transpiler/middle-end/graph/toplevel.lisp"))
-
 (env-load "transpiler/middle-end/update-funinfos.lisp")
 (env-load "transpiler/middle-end/opt-places.lisp")
 (env-load "transpiler/middle-end/optimizer/main.lisp")
