981bd49dd51e8f7d24905903c7f116ff17f16587
Sven Michael Klose <pixel@hugbox.org>
2013-09-11 18:14:40 +0000

    Removed unused code.

--- environment/stage5/backslash-to-slash.lisp
+++
@@ -1,9 +0,0 @@
-;;;;; tré – Copyright (c) 2011–2012 Sven Michael Klose <pixel@copei.de>
-
-(defun backslash-to-slash (x)
-  (? (== #\\ x)
-     #\/
-     x))
-
-(defun backslashes-to-slashes (x)
-  (list-string (mapcar #'backslash-to-slash (string-list x))))
