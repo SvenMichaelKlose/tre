a533a4ca3f5ee3c3deae1c45f0f6ed2c795b7cc1
Sven Michael Klose <pixel@hugbox.org>
2013-09-14 14:08:39 +0000

    Allow more than two arguments to EQL.

--- environment/transpiler/environment/equality.lisp
+++ environment/transpiler/environment/equality.lisp
@@ -1,6 +1,6 @@
 ;;;;; tré – Copyright (c) 2008–2009,2011–2013 Sven Michael Klose <pixel@copei.de>
 
-(defun eql (x y)
+(defun %eql (x y)
   (| x (setq x nil))
   (| y (setq y nil))
   (| (%%%eq x y)
@@ -9,3 +9,8 @@
           (& (integer? x) (integer? y)) (integer== x y)
           (& (character? x) (character? y)) (character== x y))
         (== x y))))
+
+(defun eql (&rest x)
+  (adolist (.x t)
+    (| (%eql x. !)
+       (return))))
