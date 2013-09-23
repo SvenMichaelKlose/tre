4c35512a7fddad6bfac832adfca31351497099a3
Sven Michael Klose <pixel@hugbox.org>
2013-09-23 04:16:12 +0000

    A little more constant propagation over conditional jumps.

--- environment/transpiler/middle-end/optimizer/invert-jumps.lisp
+++ environment/transpiler/middle-end/optimizer/invert-jumps.lisp
@@ -1,9 +1,9 @@
 ;;;;; tré – Copyright (c) 2008–2013 Sven Michael Klose <pixel@copei.de>
 
 (defun inverted-%%go (x)
-  (?
-    (eq x '%%go-nil)     '%%go-not-nil
-    (eq x '%%go-not-nil) '%%go-nil
+  (case x :test #'eq
+    '%%go-nil      '%%go-not-nil
+    '%%go-not-nil  '%%go-nil
     (error "Jump expected instead of ~A." x)))
 
 (defun jumps-to-tag (x)
@@ -11,45 +11,42 @@
                (== x (%%go-tag _))]
             *body*))
 
+(defun constant-jump? (x constant)
+  (| (? constant
+        (%%go-not-nil? x)
+        (%%go-nil? x))
+     (%%go? x)))
+
+(defun target-tag (x constant)
+  (?
+    (not x)                      nil
+    (number? x.)                 (target-tag .x constant)
+    (constant-jump? x. constant) (| (target-tag (member (%%go-tag x.) *body*) constant)
+                                    (%%go-tag x.))))
+
+(defun t|nil? (x)
+  (| (not x)
+     (t? x)))
+
 (def-opt-peephole-fun opt-peephole-invert-jumps
-#|
   (& (%setq? a)
      (~%ret? (%setq-place a))
-     (| (not (%setq-value a))
-        (t? (%setq-value a)))
-     (%%go-cond? d.)
-     (awhen (member (%%go-tag d.) *body*)
-       (& (? (%setq-value a)
-             (%%go-not-nil? .!.)
-             (%%go-nil? .!.))
-          (not (opt-peephole-will-be-used-again? (member (%%go-tag .!.) *body*) '~%ret)))))
-    `((%%go ,(%%go-tag (cadr (member (%%go-tag d.) *body*))))
-      ,@(opt-peephole-invert-jumps .d))
-  (& (%%go-nil?             a)
-     (equal                 d. '(%setq ~%ret nil))
-     (%%go?                .d.)
-     (number?             ..d.)
-     (== (%%go-tag a)     ..d.)
-     (equal              ...d. '(%setq ~%ret t))
-     (number?           ....d.)
-     (== (%%go-tag .d.) ....d.)
-     (%%go-cond?       .....d.)
-     (with (tag1   ..d.
-            tag2 ....d.)
-       (& (== 1 (jumps-to-tag tag1))
-       (& (== 1 (jumps-to-tag tag2))))))
-    `((,(inverted-%%go (car .....d.)) ,(%%go-tag .....d.) ~%ret)
-      ,@(opt-peephole-invert-jumps ......d))
-|#
-  (& (%%go-nil?   a)
-     (%%go?      d.)
-     (number?   .d.)
+     (t|nil? (%setq-value a))
+     (!? (target-tag d (%setq-value a))
+         (not (opt-peephole-will-be-used-again? (member ! *body*) '~%ret))))
+    `((%%go ,(target-tag d (%setq-value a)))
+      ,@(opt-peephole-invert-jumps d))
+  (& (%setq? a)
+     (~%ret? (%setq-place a))
+     (t|nil? (%setq-value a))
+     (? (%setq-value a)
+        (%%go-nil? d.)
+        (%%go-not-nil? d.)))
+     `(,a
+       ,@(opt-peephole-invert-jumps .d))
+  (& (%%go-cond? a)
+     (%%go? d.)
+     (number? .d.)
      (== (%%go-tag a) .d.))
-    `((%%go-not-nil ,(%%go-tag d.) ~%ret)
-      ,@(opt-peephole-invert-jumps .d))
-  (& (%%go-not-nil?  a)
-     (%%go?         d.)
-     (number?   .d.)
-     (== (%%go-tag a) .d.))
-    `((%%go-nil ,(%%go-tag d.) ~%ret)
+    `((,(inverted-%%go a.) ,(%%go-tag d.) ~%ret)
       ,@(opt-peephole-invert-jumps .d)))
