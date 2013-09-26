294ae157c806f1417b1f3538ad4822a1d235a33c
Sven Michael Klose <pixel@hugbox.org>
2013-09-26 22:22:34 +0000

    Greatly reduce the number of gensyms.

--- environment/transpiler/front-end/rename-arguments.lisp
+++ environment/transpiler/front-end/rename-arguments.lisp
@@ -1,18 +1,31 @@
 ;;;;; tré – Copyright (c) 2005–2013 Sven Michael Klose <pixel@copei.de>
 
+(defvar *argument-sym-counter* 0)
+
+(defun argument-sym ()
+  (alet ($ '~A (++! *argument-sym-counter*))
+    (? (& (eq ! (symbol-value !))
+          (not (symbol-function !)))
+       !
+       (argument-sym))))
+
 (defun rename-argument (replacements x)
   (| (assoc-value x replacements :test #'eq) x))
 
 (defun rename-arguments-r (replacements x)
   (? (get-lambda-funinfo x)
      x
-     (alet (+ (list-aliases (expanded-lambda-args x)) replacements)
-	   (copy-lambda x :args (rename-arguments ! (lambda-args x))
-                      :body (rename-arguments ! (lambda-body x))))))
+     (alet (+ (list-aliases (expanded-lambda-args x) :gensym-generator #'argument-sym) replacements)
+	   (copy-lambda x :args (rename-arguments-0 ! (lambda-args x))
+                      :body (rename-arguments-0 ! (lambda-body x))))))
 
-(define-tree-filter rename-arguments (replacements x)
+(define-tree-filter rename-arguments-0 (replacements x)
   (atom x)         (rename-argument replacements x)
   (%quote? x)      x
   (any-lambda? x)  (rename-arguments-r replacements x)
-  (%slot-value? x) `(%slot-value ,(rename-arguments replacements .x.)
+  (%slot-value? x) `(%slot-value ,(rename-arguments-0 replacements .x.)
 				                 ,..x.))
+
+(defun rename-arguments (x)
+  (= *argument-sym-counter* 0)
+  (rename-arguments-0 nil x))
