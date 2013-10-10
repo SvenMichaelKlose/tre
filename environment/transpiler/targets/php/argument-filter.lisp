56e92152508143f130bc0766d23b00ca64365718
Sven Michael Klose <pixel@hugbox.org>
2013-10-10 12:53:26 +0000

    Oops.

---
+++ environment/transpiler/targets/php/argument-filter.lisp
@@ -0,0 +1,13 @@
+;;;;; tré – Copyright (c) 2009–2013 Sven Michael Klose <pixel@copei.de>
+
+(defun php-expex-add-global (x)
+  (funinfo-var-add (transpiler-global-funinfo *transpiler*) x)
+  (adjoin! x (funinfo-globals *funinfo*))
+  x)
+
+(defun php-argument-filter (x)
+  (?
+    (character? x)                          (php-expex-add-global (php-compiled-char x))
+    (%quote? x)                             (php-expex-add-global (php-compiled-symbol .x.))
+    (keyword? x)                            (php-expex-add-global (php-compiled-symbol x))
+    x))
