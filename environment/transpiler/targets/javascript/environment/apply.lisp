0fa416a9ad90047ce6f17fa821ab851784c07562
Sven Michael Klose <pixel@hugbox.org>
2013-10-10 07:55:21 +0000

    No need for an extra FUNCALL. The one in the general environment does nicely.

--- environment/transpiler/targets/javascript/environment/apply.lisp
+++ environment/transpiler/targets/javascript/environment/apply.lisp
@@ -3,9 +3,9 @@
 (dont-obfuscate apply call)
 
 (defun apply (&rest lst)
-  (with (fun lst.
-         l (last .lst)
-         args (%nconc (butlast .lst) l.))
+  (with (fun   lst.
+         l     (last .lst)
+         args  (%nconc (butlast .lst) l.))
     (when-debug
       (| (function? fun)
 	     (error "First argument ~A is not a function." fun))
@@ -13,7 +13,4 @@
 	     (error "Last argument is not a list.")))
     (!? fun.tre-exp
         (!.apply nil (%%native "[" args "]"))
-     (fun.apply nil (list-array args)))))
-
-(defun funcall (fun &rest args)
-  (apply fun args))
+        (fun.apply nil (list-array args)))))
--- environment/transpiler/targets/php/environment/apply.lisp
+++ environment/transpiler/targets/php/environment/apply.lisp
@@ -1,20 +1,19 @@
 ;;;;; tré – Copyright (c) 2008–2013 Sven Michael Klose <pixel@copei.de>
 
-(dont-obfuscate apply call function_exists call_user_func_array array)
+(dont-obfuscate function_exists call_user_func_array )
 
 (defun apply (&rest lst)
-  (with (fun           lst.
-         l             (last .lst)
-         args          (%nconc (butlast .lst) l.)
-         closure?      (is_a fun "__closure")
-         fun-name      (? closure? fun.n fun)
-         expander-name (%%%string+ fun-name "_treexp"))
-    (when closure?
-      (= args (cons fun.g args)))
+  (with (fun            lst.
+         l              (last .lst)
+         args           (%nconc (butlast .lst) l.)
+         closure?       (is_a fun "__closure")
+         fun-name       (? closure?
+                           fun.n
+                           fun)
+         expander-name  (%%%string+ fun-name "_treexp"))
+    (& closure?
+       (= args (. fun.g args)))
 	(?
-      (function_exists expander-name) (call_user_func_array expander-name (%%native "array ($" args ")"))
-	  (function_exists fun-name)      (call_user_func_array fun-name (list-phphash args))
+      (function_exists expander-name)  (call_user_func_array expander-name (%%native "array ($" args ")"))
+	  (function_exists fun-name)       (call_user_func_array fun-name (list-phphash args))
       (error (+ "Function '" fun-name "' does not exist.")))))
-
-(defun funcall (fun &rest args)
-  (apply fun args))
