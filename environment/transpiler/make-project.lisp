734a014932441fd952fcab823b8373d317b6240a
Sven Michael Klose <pixel@hugbox.org>
2013-10-11 09:25:43 +0000

    List of sections is now a regular argument.

--- environment/transpiler/make-project.lisp
+++ environment/transpiler/make-project.lisp
@@ -1,31 +1,30 @@
 ;;;;; tré – Copyright (c) 2008–2013 Sven Michael Klose <pixel@copei.de>
 
-(defun make-project (project-name &key transpiler
-                                       (files                 nil)
-                                       (modified-file-getter  nil)
-                                       (files-to-update       nil)
-                                       (recompiler-path       nil)
-                                       (emitter               nil)
-                                       (obfuscate?            nil))
+(defun make-project (project-name sections
+                     &key transpiler
+                          (modified-file-getter  nil)
+                          (sections-to-update    nil)
+                          (recompiler-path       nil)
+                          (emitter               nil)
+                          (obfuscate?            nil))
   (format t "; Making project '~A'...~%" project-name)
   (| emitter
      (error "Argument EMITTER is required, which takes the compiled code and puts it somewhere."))
-  (= (transpiler-sections-to-update transpiler) files-to-update)
+  (= (transpiler-sections-to-update transpiler) sections-to-update)
   (= (transpiler-obfuscate? transpiler) obfuscate?)
-  (let code (compile-sections files :transpiler transpiler)
+  (let code (compile-sections sections :transpiler transpiler)
     (funcall emitter code)
     (awhen recompiler-path
       (| modified-file-getter
-         (error "The recompiler requires argument MODIFIED-FILE-GETTER: a function without arguments which returns a list of modified files."))
+         (error "The recompiler requires argument MODIFIED-FILE-GETTER: a function without arguments which returns a list of modified sections."))
       (format t "; Making recompiler '~A'...~F" recompiler-path)
       (= *allow-redefinitions?* t)
       (sys-image-create recompiler-path
                         #'(()
-                             (make-project project-name
+                             (make-project project-name sections
                                            :transpiler            transpiler
-                                           :files                 files
                                            :modified-file-getter  modified-file-getter
-                                           :files-to-update       (funcall modified-file-getter)
+                                           :sections-to-update    (funcall modified-file-getter)
                                            :recompiler-path       recompiler-path
                                            :emitter               emitter
                                            :obfuscate?            obfuscate?)
