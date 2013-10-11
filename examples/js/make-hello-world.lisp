411d2bc8d30aaef84babe874e7ebe16aa278f820
Sven Michael Klose <pixel@hugbox.org>
2013-10-11 09:38:23 +0000

    Use MAKE-PROJECT.

--- examples/js/make-hello-world.lisp
+++ examples/js/make-hello-world.lisp
@@ -1,5 +1,5 @@
 (with-open-file out (open "examples/js/hello-world.js" :direction 'output)
   (make-project "Hello World"
-                :files '("examples/js/hello-world.lisp")
-                :transpiler *js-transpiler*
-                :emitter [princ _ out]))
+                '("examples/js/hello-world.lisp")
+                :transpiler  *js-transpiler*
+                :emitter     [princ _ out]))
