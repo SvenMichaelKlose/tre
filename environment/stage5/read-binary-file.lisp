c3ec4a4503dd32aae0facdde6cc22893c45f86af
Sven Michael Klose <pixel@hugbox.org>
2013-10-18 11:09:31 +0000

    Cleaned up.

--- environment/stage5/read-binary-file.lisp
+++ environment/stage5/read-binary-file.lisp
@@ -2,7 +2,7 @@
 
 (defun read-binary-file (name)
   (let data (make-queue)
-    (with-open-file in (open name :direction 'input)
+    (with-input-file in name
       (while (not (end-of-file? in))
              (queue-list data)
         (enqueue data (integer (read-char in)))))))
