3842bfed30c1bc5200b6c978a1c7081ba3c1a784
Sven Michael Klose <pixel@copei.de>
2013-11-13 17:42:38 +0100

    Fixed and cleaned up.

--- environment/transpiler/tests.lisp
+++ environment/transpiler/tests.lisp
@@ -1,18 +1,18 @@
 ;;;;; tré – Copyright (c) 2008–2009,2011–2013 Sven Michael Klose <pixel@copei.de>
 
 (defun make-environment-tests ()
-  (with (names nil
-		 num 0
-  		 funs (mapcar [(= num (++ num))
-				       (let n ($ 'test- num)
-					     (= names (push n names))
-				         `(defun ,n ()
-				            (%%%log (+ "Test " (string ,num) ": " ,_. "</br>"))
-				            (unless (equal ,.._. ,._.)
-				              (%%%log (+ "Test '" ,_. "' failed</br>"))
-						      (print ,._.)
-						      (%%%log "</br>"))))]
-		    	*tests*))
+  (with (names  nil
+		 num    0
+  		 funs   (mapcar [(++! num)
+				         (let n ($ 'test- num)
+					       (push n names)
+				           `(defun ,n ()
+				              (print (+ "Test " ,num ", " ,_.))
+				              (unless (equal ,.._. ,._.)
+				                (print " FAILED")
+						        (print ',._.))
+                              (print "</br>")))]
+                        *tests*))
 	`(,@funs
-	    (defun environment-tests ()
-		  ,@(mapcar #'list (reverse names))))))
+      (defun environment-tests ()
+	    ,@(mapcar #'list (reverse names))))))
