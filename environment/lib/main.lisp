339cc40a28fc388c6dacd72ee092b88eebed48c5
Sven Michael Klose <pixel@copei.de>
2013-11-21 20:17:01 +0100

    LML2XML is platform–dependend.

--- environment/lib/main.lisp
+++ environment/lib/main.lisp
@@ -1,9 +1,9 @@
 ;;;;; tré – Copyright (c) 2009,2012–2013 Sven Michael Klose <pixel@copei.de>
 
-(env-load "lib/lml2xml.lisp")
+(env-load "lib/lml2xml.lisp" 'c)
+(env-load "lib/xml2lml.lisp" 'c)
 (env-load "lib/lml.lisp")
 (env-load "lib/mime.lisp")
 (env-load "lib/tree-walk.lisp")
 (env-load "lib/xml-entities.lisp")
-(env-load "lib/xml2lml.lisp")
 ;(env-load "lib/bytecode-interpreter.lisp")
