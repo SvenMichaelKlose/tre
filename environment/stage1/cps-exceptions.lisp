422c4d17fe722ddc44bc5e13d2a82de0d877fa79
Sven Michael Klose <pixel@copei.de>
2013-12-05 20:49:01 +0100

    Fake DECLARE-CPS-EXCEPTION in C environment.

---
+++ environment/stage1/cps-exceptions.lisp
@@ -0,0 +1,4 @@
+;;;;; tré – Copyright (c) 2013 Sven Michael Klose <pixel@copei.de>
+
+(declare-cps-exception %nconc nth last list copy-list copy-tree)
+(declare-cps-exception caar cadr cdar cddr cadar cddar caadar caddr caadr cdddr cdadar caaddr caddar cdddar cadadr cadaddr cadadar cddadar)
