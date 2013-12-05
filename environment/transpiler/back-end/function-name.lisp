10732c43c29df09cff51eec12a66f6839cca87f0
Sven Michael Klose <pixel@copei.de>
2013-12-05 18:31:19 +0100

    Static expanders for non-CPS functions.

--- environment/transpiler/back-end/function-name.lisp
+++ environment/transpiler/back-end/function-name.lisp
@@ -1,7 +1,13 @@
 ;;;;; tré – Copyright (c) 2009–2013 Sven Michael Klose <pixel@copei.de>
 
+(defvar *compiled-function-names* (make-hash-table :test #'eq))
+
 (defun compiled-function-name (name)
-  (make-symbol (+ (transpiler-function-name-prefix *transpiler*) (symbol-name name)) (symbol-package name)))
+  (aprog1 (make-symbol (+ (transpiler-function-name-prefix *transpiler*) (symbol-name name)) (symbol-package name))
+    (= (href *compiled-function-names* !) name)))
+
+(defun real-function-name (x)
+  (href *compiled-function-names* x))
 
 (defun compiled-function-name-string (name)
   (obfuscated-identifier (compiled-function-name name)))
