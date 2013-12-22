872a6d6d3216251a1fd1c7e144c15e8be040c506
Sven Michael Klose <pixel@copei.de>
2013-12-22 21:16:32 +0100

    Remove symbol %%NATIVE.

--- environment/transpiler/targets/bytecode/codegen.lisp
+++ environment/transpiler/targets/bytecode/codegen.lisp
@@ -46,5 +46,6 @@
 (define-bc-macro %set-vec (vec index x)
   `(%bc-set-vec ,vec ,index ,(bc-make-value x)))
 
-(define-bc-macro %global  (x) x)
-(define-bc-macro identity (x) x)
+(define-bc-macro %global  (x)       `(symbol-value 1 x))
+(define-bc-macro identity (x)       x)
+(define-bc-macro %%native (&rest x) x)
