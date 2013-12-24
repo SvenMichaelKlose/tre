755a3434c6f902587588354461f35e35f515eb6d
Sven Michael Klose <pixel@copei.de>
2013-12-24 17:04:04 +0100

    %GLOBAL added.

--- environment/transpiler/targets/shared/functionals.lisp
+++ environment/transpiler/targets/shared/functionals.lisp
@@ -1,3 +1,3 @@
 ;;;;; tré – Copyright (c) 2013 Sven Michael Klose <pixel@copei.de>
 
-(functional %quote %%closure cons %%%eq %immediate-aref)
+(functional %quote %%closure cons %%%eq %immediate-aref %global)
