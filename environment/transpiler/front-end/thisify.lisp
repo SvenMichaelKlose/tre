54baacd8ea3ccf400593aacd2987d529fd7d3289
Sven Michael Klose <pixel@copei.de>
2014-01-02 11:04:47 +0100

    Convert THIS to ~%THIS.

--- environment/transpiler/front-end/thisify.lisp
+++ environment/transpiler/front-end/thisify.lisp
@@ -1,26 +1,28 @@
 ;;;;; tré – Copyright (c) 2008–2013 Sven Michael Klose <pixel@copei.de>
-;;;;;
-;;;;; Wrap local method calls into SLOT-VALUEs.
 
 (defun thisify-collect-methods-and-members (clsdesc)
-  (append (class-methods clsdesc)
-		  (class-members clsdesc)
-		  (awhen (class-parent clsdesc)
-			(thisify-collect-methods-and-members !))))
+  (+ (class-methods clsdesc)
+     (class-members clsdesc)
+     (!? (class-parent clsdesc)
+         (thisify-collect-methods-and-members !))))
 
 (defun thisify-symbol (classdef x exclusions)
-  (!? (& classdef
-         (not (| (number? x) (string? x)))
-         (not (member x exclusions :test #'eq))
-         (assoc x classdef))
-      `(%slot-value ~%this ,x)
-      x))
+  (? (eq 'this x)
+     '~%this
+     (!? (& classdef
+            (not (| (number? x)
+                    (string? x))
+                 (member x exclusions :test #'eq))
+            (assoc x classdef))
+         `(%slot-value ~%this ,x)
+         x)))
 
 (defun thisify-list-0 (classdef x exclusions)
   (?
-	(atom x)    (thisify-symbol classdef x exclusions)
-	(%quote? x) x
-	(lambda? x) (copy-lambda x :body (thisify-list-0 classdef (lambda-body x) (append exclusions (lambda-args x))))
+	(atom x)     (thisify-symbol classdef x exclusions)
+	(%quote? x)  x
+	(lambda? x)  (copy-lambda x :body (thisify-list-0 classdef (lambda-body x) (+ exclusions
+                                                                                  (lambda-args x))))
     (listprop-cons x
                    (? (%slot-value? x.)
 			          `(%slot-value ,(thisify-list-0 classdef (cadr x.) exclusions)
@@ -35,7 +37,7 @@
 
 (defun thisify (classes x)
   (?
-	 (atom x)       x
-	 (%thisify? x.) (append (thisify-list classes (cddr x.) (cadr x.))
-			                (thisify classes .x))
+	 (atom x)        x
+	 (%thisify? x.)  (+ (thisify-list classes (cddr x.) (cadr x.))
+			            (thisify classes .x))
      (listprop-cons x (thisify classes x.) (thisify classes .x))))
