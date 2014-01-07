efa062d97aa0c45b001992d84da5589eb4ddb3ca
Sven Michael Klose <pixel@copei.de>
2014-01-07 18:17:59 +0100

    Track I/O locations by default.

--- environment/stage3/stream.lisp
+++ environment/stage3/stream.lisp
@@ -1,28 +1,28 @@
-;;;; tré – Copyright (c) 2005–2006,2010,2012–2013 Sven Michael Klose <pixel@copei.de>
+;;;; tré – Copyright (c) 2005–2006,2010,2012–2014 Sven Michael Klose <pixel@copei.de>
 
 (defvar *default-stream-tabsize* 8)
 
 (defstruct stream-location
-  (track?   nil)
+  (track?   t)
   (id       nil)
   (line     1)
   (column   1)
   (tabsize  *default-stream-tabsize*))
 
 (defstruct stream
-  (handle nil)
+  (handle           nil)
 
   fun-in
   fun-out
   fun-eof
 
-  (last-char	nil)
-  (peeked-char	nil)
+  (last-char	    nil)
+  (peeked-char	    nil)
 
-  (input-location         (make-stream-location))
-  (output-location        (make-stream-location :track? nil))
+  (input-location   (make-stream-location))
+  (output-location  (make-stream-location :track? nil))
 
-  (user-detail nil))
+  (user-detail      nil))
 
 (defun next-tabulator-column (column size)
   (++ (* size (++ (integer (/ (-- column) size))))))
