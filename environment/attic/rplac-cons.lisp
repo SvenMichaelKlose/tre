db0f9ff897415d43878d87988242e5d9b2b26593
Sven Michael Klose <pixel@copei.de>
2014-01-12 12:38:07 +0100

    Won't use this anymore.

--- environment/attic/rplac-cons.lisp
+++
@@ -1,8 +0,0 @@
-;;;; nix operating system project
-;;;; list processor environment
-;;;; Copyright (C) 2005-2006 Sven Klose <pixel@copei.de>
-
-(defun rplac-cons (dest what)
-  "Replace CAR and CDR of a cons."
-  (= (car dest) (car what)
-	 (cdr dest) (cdr what)))
