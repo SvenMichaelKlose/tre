9018309155217c1a14f821d73e907adca7dc19c0
Sven Michael Klose <pixel@copei.de>
2014-01-20 13:27:19 +0100

    Stylesheet support added.

--- environment/platforms/shared/html/script.lisp
+++ environment/platforms/shared/html/script.lisp
@@ -1,10 +1,12 @@
-;;;;; tré – Copyright (c) 2008–2013 Sven Michael Klose <pixel@copei.de>
+;;;;; tré – Copyright (c) 2008–2014 Sven Michael Klose <pixel@copei.de>
 
 (defun print-html-script (out script
                           &key (title nil)
                                (no-cache? nil)
                                (strict? t)
-                               (copyright-title nil) (copyright-href nil))
+                               (copyright-title nil) (copyright-href nil)
+                               (external-stylesheet nil)
+                               (internal-stylesheet nil))
   (with-default-stream o out
     (format o (+ (doctype-html-5) "~%"))
     (princ (lml2xml `(html
@@ -20,6 +22,11 @@
                                             `(:title ,(escape-string !)))
                                       ,@(!? copyright-href
                                             `(:href  ,(escape-string !))))))
+                         ,@(!? external-stylesheet
+                               (mapcan [`(link :rel "stylesheet" :type "text/css" :href ,_)]
+                                       (ensure-list !)))
+                         ,@(!? internal-stylesheet
+                               `((style ,!)))
                          (script :type "text/javascript"
                            "<!--"
                            ,@(& strict?
@@ -32,6 +39,10 @@
                          &key (title nil)
                               (no-cache? nil)
                               (strict? t)
-                              (copyright-title nil) (copyright-href nil))
+                              (copyright-title nil) (copyright-href nil)
+                              (external-stylesheet nil)
+                              (internal-stylesheet nil))
   (with-output-file o pathname
-    (print-html-script o script ,@(keyword-copiers 'title 'no-cache? 'strict? 'copyright-title 'copyright-href))))
+    (print-html-script o script ,@(keyword-copiers 'title 'no-cache? 'strict?
+                                                   'copyright-title 'copyright-href
+                                                   'external-stylesheet 'internal-stylesheet))))
