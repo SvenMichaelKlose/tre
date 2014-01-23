5264c722e1a314e93f78437f56646ca9c78544e9
Sven Michael Klose <pixel@copei.de>
2014-01-23 11:38:57 +0100

    Cleaned up.

--- environment/debugger/backtrace.lisp
+++ environment/debugger/backtrace.lisp
@@ -1,20 +1,20 @@
-;;;;; tré – Copyright (c) 2013 Sven Michael Klose <pixel@copei.de>
+;;;;; tré – Copyright (c) 2013–2014 Sven Michael Klose <pixel@copei.de>
 
 (defvar *backtrace* nil)
 
-(defun backtrace ()
-  (with (print-repetition
-             #'((x repetitions)
-                  (format t "~A (~A times) " (symbol-name x.) (++ repetitions))
-                  (rec .x nil 0))
-         rec #'((x former repetitions)
-                  (?
-                    x
-                      (?
-                        (eq x. former)      (rec .x former (++ repetitions))
-                        (zero? repetitions) (format t "~A " (symbol-name x.))
-                        (print-repetition (list former) repetitions))
-                    (not (zero? repetitions))
-                      (print-repetition (list former) repetitions))))
-    (format t "Backtrace: ")
-    (rec (copy-list *backtrace*) nil 0)))
+(defun backtrace (&optional (str))
+  (with-default-stream s str)
+    (with (print-repetition
+               #'((former next repetitions)
+                    (format s "~A (~A times) " former repetitions)
+                    (f next nil 0))
+           f #'((x former repetitions)
+                  (? (not x)
+                     (| (zero? repetitions)
+                        (print-repetition x repetitions))))
+                     (?
+                       (eq x. former)       (f .x former (++ repetitions))
+                       (zero? repetitions)  (format s "~A " x.)
+                       (print-repetition x repetitions))
+      (format s "Backtrace: ")
+      (f (copy-list *backtrace*) nil 0))))
