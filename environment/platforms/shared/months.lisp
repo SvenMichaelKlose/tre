eea2cf3e94be2fe94ec9784a3de24652fae9b434
Sven Michael Klose <pixel@copei.de>
2014-02-24 12:00:35 +0100

    Added English names of the months.

---
+++ environment/platforms/shared/months.lisp
@@ -0,0 +1,3 @@
+;;;;; tré – Copyright (c) 2014 Sven Michael Klose <pixel@copei.de>
+
+(defvar *months* '("January" "February" "March" "April" "May" "June" "July" "August" "September" "October" "November" "December"))
