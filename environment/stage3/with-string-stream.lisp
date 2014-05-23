51a7742b2036e91de0026d542a2d25fc59bdabb0
Sven Michael Klose <pixel@copei.de>
2014-05-23 16:16:49 +0200

    Cleaned up.

--- environment/stage3/with-string-stream.lisp
+++ environment/stage3/with-string-stream.lisp
@@ -1,13 +1,13 @@
 ;;;;; tré – Copyright (C) 2005–2008,2012,2014 Sven Michael Klose <pixel@copei.de>
 
-(defmacro with-string-stream (x &body body)
-  `(let ,x (make-string-stream)
+(defmacro with-string-stream (str &body body)
+  `(let ,str (make-string-stream)
 	 (progn
 	   ,@body)
-	 (get-stream-string ,x)))
+	 (get-stream-string ,str)))
 
-(defmacro with-stream-string (x str &body body)
-  `(let ,x (make-string-stream)
-     (princ ,str ,x)
+(defmacro with-stream-string (str x &body body)
+  `(let ,str (make-string-stream)
+     (princ ,x ,str)
 	 (progn
 	   ,@body)))
