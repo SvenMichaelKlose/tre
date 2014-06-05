1cf4c686593244090176f91c31bf7b57cf075b56
Sven Michael Klose <pixel@copei.de>
2014-06-05 12:28:33 +0200

    Print to right stream.

--- environment/platforms/shared/html/script.lisp
+++ environment/platforms/shared/html/script.lisp
@@ -9,31 +9,31 @@
                                (internal-stylesheet nil))
   (with-default-stream o out
     (format o (+ (doctype-html-5) "~%"))
-    (princ (lml2xml `(html
-                       (head
-                         (title ,title)
-                         (meta :http-equiv "Content-Type" :content "text/html; charset=utf-8")
-                         ,@(& no-cache
-                              `((meta :http-equiv "pragma" :content "no-cache")))
-                         ,@(& (| copyright-title
-                                 copyright-href)
-                              `((link :rel "copyright"
-                                      ,@(!? copyright-title
-                                            `(:title ,(escape-string !)))
-                                      ,@(!? copyright-href
-                                            `(:href  ,(escape-string !))))))
-                         ,@(!? external-stylesheet
-                               (mapcan [`(link :rel "stylesheet" :type "text/css" :href ,_)]
-                                       (ensure-list !)))
-                         ,@(!? internal-stylesheet
-                               `((style ,!)))
-                         (script :type "text/javascript"
-                           "<!--"
-                           ,@(& strict?
-                                `("\"use strict\";"))
-                           ,script
-                           "//-->"))))
-           o)))
+    (lml2xml `(html
+                  (head
+                    (title ,title)
+                    (meta :http-equiv "Content-Type" :content "text/html; charset=utf-8")
+                    ,@(& no-cache
+                         `((meta :http-equiv "pragma" :content "no-cache")))
+                    ,@(& (| copyright-title
+                            copyright-href)
+                         `((link :rel "copyright"
+                                 ,@(!? copyright-title
+                                       `(:title ,(escape-string !)))
+                                 ,@(!? copyright-href
+                                       `(:href  ,(escape-string !))))))
+                    ,@(!? external-stylesheet
+                          (mapcan [`(link :rel "stylesheet" :type "text/css" :href ,_)]
+                                  (ensure-list !)))
+                    ,@(!? internal-stylesheet
+                          `((style ,!)))
+                    (script :type "text/javascript"
+                        "<!--"
+                        ,@(& strict?
+                             `("\"use strict\";"))
+                        ,script
+                        "//-->")))
+             o)))
 
 (defun make-html-script (pathname script
                          &key (title nil)
