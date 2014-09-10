f2d99dbba3795478384f8479fda58616ce9b10ed
Sven Michael Klose <pixel@copei.de>
2014-09-10 17:53:49 +0100

    Don't confuse READ with block comments. Temporary workaround.

--- environment/transpiler/middle-end/optimizer/funinfos.lisp
+++ environment/transpiler/middle-end/optimizer/funinfos.lisp
@@ -53,12 +53,12 @@
 
 (defun correct-funinfo ()
   (alet *funinfo*
-#|
-    (when (transpiler-lambda-export? *transpiler*)
+;#| XXX confuses READ
+;    (when (transpiler-lambda-export? *transpiler*)
 ;      (remove-unused-scope-arg !)
-      (remove-scoped-vars !)
-      (replace-scope-arg !))
-|#
+;      (remove-scoped-vars !)
+;      (replace-scope-arg !))
+;|#
     (funinfo-vars-set ! (intersect (funinfo-vars !) (funinfo-used-vars !) :test #'eq))
     (when (transpiler-stack-locals? *transpiler*)
       (remove-argument-stackplaces !))))
