a17ab6a2e8303166b95d461df28b1af250cde764
Sven Michael Klose <pixel@copei.de>
2014-09-23 14:24:19 +0100

    Use abbreviated CONS.

--- environment/transpiler/front-end/backquote-expand.lisp
+++ environment/transpiler/front-end/backquote-expand.lisp
@@ -1,11 +1,11 @@
-;;;;; tré – Copyright (c) 2006–2013 Sven Michael Klose <pixel@copei.de>
+;;;;; tré – Copyright (c) 2006–2014 Sven Michael Klose <pixel@copei.de>
 
 (defun backquote-quasiquote (x)
   (? (any-quasiquote? (cadr x.))
-     `(cons ,(backquote-1 (cadr x.))
-            ,(backquote-1 .x))
-     `(cons ,(cadr x.)
-            ,(backquote-1 .x))))
+     `(. ,(backquote-1 (cadr x.))
+         ,(backquote-1 .x))
+     `(. ,(cadr x.)
+         ,(backquote-1 .x))))
 
 (defun backquote-quasiquote-splice (x)
   (? (any-quasiquote? (cadr x.))
@@ -14,7 +14,8 @@
          `(%nconc (copy-list ,(let tr *transpiler*
                                 (? (transpiler-assert? tr)
                                    (frontend-macroexpansions `(aprog1 ,(cadr x.)
-                                                                (| (list? !) (error ",@ expects a list instead of ~A." !))))
+                                                                (| (list? !)
+                                                                   (error ",@ expects a list instead of ~A." !))))
                                    (cadr x.))))
                    ,(backquote-1 .x))
          (cadr x.))))
@@ -33,18 +34,18 @@
 (defun backquote-1 (x)
   (?
     (atom x)                (quote-literal x)
-    (atom x.)               `(cons ,(quote-literal x.)
-                                   ,(backquote-2 .x))
+    (atom x.)               `(. ,(quote-literal x.)
+                                ,(backquote-2 .x))
     (quasiquote? x.)        (backquote-quasiquote x)
     (quasiquote-splice? x.) (backquote-quasiquote-splice x)
-    `(cons ,(backquote-1 x.)
-           ,(backquote-2 .x))))
+    `(. ,(backquote-1 x.)
+        ,(backquote-2 .x))))
 
 (defun simple-quote-expand (x)
   (? (atom x)
      (quote-literal x)
-     `(cons ,(simple-quote-expand x.)
-            ,(simple-quote-expand .x))))
+     `(. ,(simple-quote-expand x.)
+         ,(simple-quote-expand .x))))
 
 (defun backquote-expand (l)
   (tree-walk l
