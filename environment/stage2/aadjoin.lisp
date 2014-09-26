b6bac1ae347caa42cd31baadc3680ced4fbb87c2
Sven Michael Klose <pixel@copei.de>
2014-09-26 14:55:48 +0100

    AADJOIN: Updates existing keys.

--- environment/stage2/aadjoin.lisp
+++ environment/stage2/aadjoin.lisp
@@ -1,11 +1,12 @@
 ;;;;; tré – Copyright (c) 2014 Sven Michael Klose <pixel@copei.de>
 
 (defun aadjoin (key value lst &key (test #'eql) (to-end? nil))
-  (!? (assoc-value key lst :test test)
-      (return !))
-  (? to-end?
-     (+ lst (list (. key value)))
-     (. (. key value) lst)))
+  (? (assoc key lst :test test)
+     (aprog1 (assoc key (copy-alist lst) :test test)
+       (= .! value))
+     (? to-end?
+        (+ lst (list (. key value)))
+        (. (. key value) lst))))
 
 (defmacro aadjoin! (key value lst &key (test #'eql) (to-end? nil))
   `(= ,lst (aadjoin ,key ,value ,lst :test ,test :to-end? ,to-end?)))
