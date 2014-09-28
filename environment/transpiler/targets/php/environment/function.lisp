bf0dd4aa123012362e91b9f1b7d84fe6ee9409fe
Sven Michael Klose <pixel@copei.de>
2014-09-28 19:08:47 +0200

    Oops. Missing commit.

---
+++ environment/transpiler/targets/php/environment/function.lisp
@@ -0,0 +1,13 @@
+;;;;; tré – Copyright (c) 2011–2014 Sven Michael Klose <pixel@copei.de>
+
+(dont-obfuscate isset function_exists)
+
+(defun function? (x)
+  (?
+    (is_a x "__closure") (function_exists x.n)
+    (string? x)          (function_exists x)))
+
+(defun builtin? (x))
+
+(defun function-bytecode (x))
+(defun function-source (x))
