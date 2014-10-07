9a7c0127080b43946ca227341cc42635d4d6e2dc
Sven Michael Klose <pixel@hugbox.org>
2014-10-07 16:29:50 +0200

    Switched assertions on by default.

--- environment/stage0/config-defaults.lisp
+++ environment/stage0/config-defaults.lisp
@@ -1,11 +1,12 @@
-;;;; tré – Copyright (c) 2005–2013 Sven Michael Klose <pixel@copei.de>
+;;;; tré – Copyright (c) 2005–2014 Sven Michael Klose <pixel@copei.de>
 
-(defvar *assert* nil)
-(defvar *tre-has-math* t)
-(defvar *tre-has-alien* t)
-(defvar *tre-has-class* t)
-(defvar *tre-has-editor* nil)
-(defvar *print-circularities?* nil)
-(defvar *have-compiler?* nil)
-(defvar *have-c-compiler?* t)
+(defvar *assert*          t)
+(defvar *tre-has-math*    t)
+(defvar *tre-has-alien*   t)
+(defvar *tre-has-class*   t)
+(defvar *tre-has-editor*  nil)
+
+(defvar *print-circularities?*      nil)
+(defvar *have-compiler?*            nil)
+(defvar *have-c-compiler?*          t)
 (defvar *show-transpiler-progress?* t)
