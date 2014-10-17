a5ecd9a34f81b93b5cb1e7960014177062690663
Sven Michael Klose <pixel@copei.de>
2014-10-17 08:15:57 +0200

    No js-debugger or bytecode-image in releasetests.

--- make.sh
+++ make.sh
@@ -281,8 +281,8 @@ all)
     echo "Making all..."
 	./make.sh boot $ARGS || exit 1
 	./make.sh tests || exit 1
-	./make.sh bytecode-image || exit 1
-	./make.sh jsdebugger || exit 1
+#	./make.sh bytecode-image || exit 1
+#   ./make.sh jsdebugger || exit 1
     $TRE makefiles/webconsole.lisp || exit 1
     ;;
 
