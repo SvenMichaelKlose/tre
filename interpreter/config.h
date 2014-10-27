25c09fa31d0edf98c74bb084994b13bdc8f8bd4a
Sven Michael Klose <pixel@copei.de>
2014-10-27 20:28:48 +0100

    Double pool sizes to run makefiles/webconsole.lisp.

--- interpreter/config.h
+++ interpreter/config.h
@@ -40,7 +40,7 @@ typedef int bool;
 #define TREDEBUG_MAX_BREAKPOINTS	16
 
 #ifndef NUM_LISTNODES
-#define NUM_LISTNODES	            (32 * MEGA)
+#define NUM_LISTNODES	            (64 * MEGA)
 #endif
 
 #ifndef NUM_ATOMS
