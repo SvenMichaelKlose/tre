4d32334c2c1ec4edabc1212c1e695cbc9f760473
Sven Michael Klose <pixel@copei.de>
2014-10-27 10:43:03 +0100

    Test INTEGER> and INTEGER<.

--- environment/stage1/test-arith.lisp
+++ environment/stage1/test-arith.lisp
@@ -1,4 +1,4 @@
-;;;;; tré – Copyright (c) 2005,2008-2009,2012 Sven Michael Klose <pixel@copei.de>
+;;;;; tré – Copyright (c) 2005,2008-2009,2012,2014 Sven Michael Klose <pixel@copei.de>
 
 (define-test "CHARACTER- literal"
   ((== 1 (character- #\b #\a)))
@@ -26,6 +26,22 @@
 
 (define-test "INTEGER== to be NIL"
   ((integer== 0 1))
+  nil)
+
+(define-test "INTEGER> to be T"
+  ((integer> 1 0))
+  t)
+
+(define-test "INTEGER> to be NIL"
+  ((integer> 0 1))
+  nil)
+
+(define-test "INTEGER< to be T"
+  ((integer< 0 1))
+  t)
+
+(define-test "INTEGER< to be NIL"
+  ((integer< 1 0))
   nil)
 
 (define-test "== empty string"
