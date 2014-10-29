b57eff529c090a2506940ebf4c743fc7881947dd
Sven Michael Klose <pixel@copei.de>
2014-10-29 10:40:14 +0100

    Minor cleanup.

--- environment/transpiler/middle-end/correct-functions.lisp
+++ environment/transpiler/middle-end/correct-functions.lisp
@@ -1,19 +1,19 @@
-;;;;; tré – Copyright (c) 2008,2012–2013 Sven Michael Klose <pixel@copei.de>
+;;;;; tré – Copyright (c) 2008,2012–2014 Sven Michael Klose <pixel@copei.de>
 
 (defun correct-functions (x)
-  (?
-    (not x) x
-    (named-lambda? x.)
-        (alet x.
-          (. (copy-lambda ! :body (correct-functions (lambda-body !)))
-             (correct-functions .x)))
-    (& (%=? x.)
-       (named-lambda? (%=-value x.)))
-        (alet (%=-value x.)
-          `(,(copy-lambda ! :body (correct-functions (lambda-body !)))
-            ,@(& (not (transpiler-lambda-export? *transpiler*))
-                 `((%= ,(%=-place x.) ,(lambda-name !))))
-            ,@(correct-functions .x)))
-    (%%block? x.)
-      (cons-r correct-functions x)
-    (. x. (correct-functions .x))))
+  (alet x.
+    (?
+      (not x) x
+      (named-lambda? !)
+            (. (copy-lambda ! :body (correct-functions (lambda-body !)))
+               (correct-functions .x))
+      (& (%=? !)
+         (named-lambda? (%=-value !)))
+          (alet (%=-value !)
+            `(,(copy-lambda ! :body (correct-functions (lambda-body !)))
+              ,@(& (not (transpiler-lambda-export? *transpiler*))
+                   `((%= ,(%=-place x.) ,(lambda-name !))))
+              ,@(correct-functions .x)))
+      (%%block? !)
+        (cons-r correct-functions x)
+      (. ! (correct-functions .x)))))
