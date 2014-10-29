7f2c0b94693087f0e4c367bde6f785cacb9ccf9d
Sven Michael Klose <pixel@copei.de>
2014-10-29 10:07:30 +0100

    Minor cleanup.

--- environment/transpiler/compile-environment.lisp
+++ environment/transpiler/compile-environment.lisp
@@ -1,6 +1,6 @@
-;;;;; tré – Copyright (c) 2008–2013 Sven Michael Klose <pixel@copei.de>
+;;;;; tré – Copyright (c) 2008–2014 Sven Michael Klose <pixel@copei.de>
 
-(defun %compile-environment-configure-transpiler (tr funs)
+(defun %environment-transpiler (tr funs)
   (aprog1 (copy-transpiler tr)
     (transpiler-add-wanted-functions ! (| (!? funs
                                              (ensure-list !))
@@ -8,11 +8,11 @@
 
 (defun compile-c-environment (&optional (funs nil))
   (put-file "interpreter/_compiled-env.c"
-            (compile-sections nil :transpiler (%compile-environment-configure-transpiler *c-transpiler* funs)))
+            (compile-sections nil :transpiler (%environment-transpiler *c-transpiler* funs)))
   nil)
 
 (defun compile-bytecode-environment (&optional (funs nil))
-  (alet (%compile-environment-configure-transpiler *bc-transpiler* funs)
+  (alet (%environment-transpiler *bc-transpiler* funs)
     (expr-to-code ! (compile-sections nil :transpiler !))))
 
 (defun compile-c-compiler ()
