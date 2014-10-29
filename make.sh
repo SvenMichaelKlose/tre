50aec46ec8edea8a43210fa9eafab91e7de8c1c9
Sven Michael Klose <pixel@copei.de>
2014-10-29 08:14:38 +0100

    Fixed releasetests.

--- make.sh
+++ make.sh
@@ -333,6 +333,7 @@ releasetests)
 	./make.sh build $ARGS || exit 1
 	./make.sh reload || exit 1
 	./make.sh distclean || exit 1
+	./make.sh interpreter $ARGS || exit 1
     echo | ./tre -n -e "(setq *targets* '(c))" || exit 1
 	./make.sh compiler $ARGS || exit 1
 	./make.sh crunsh $ARGS || exit 1
