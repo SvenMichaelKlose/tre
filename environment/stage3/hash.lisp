979495e81ed5af06212abecada745ebef17566e0
Sven Michael Klose <pixel@copei.de>
2014-10-30 09:58:21 +0100

    Oops.

--- environment/stage3/hash.lisp
+++ environment/stage3/hash.lisp
@@ -56,10 +56,11 @@
 
 (defun copy-hash-table (h)
   (when h
-    (with (n  (make-hash-table :test (hash-table-test h)
-                               :size (hash-table-size h))
-           nb (hash-table-buckets n)
-           hb (hash-table-buckets h))
+    (with (size  (hash-table-size h)
+           n     (make-hash-table :test (hash-table-test h)
+                                  :size size)
+           nb    (hash-table-buckets n)
+           hb    (hash-table-buckets h))
       (dotimes (i size n)
         (= (aref nb i) (copy-alist (aref hb i)))))))
 
