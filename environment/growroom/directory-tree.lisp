e19d0a1f478796a80155a95bca5c8a011b96e00d
Sven Michael Klose <pixel@copei.de>
2014-11-06 16:57:10 +0100

    Don't fail on broken symlinks.

--- environment/growroom/directory-tree.lisp
+++ environment/growroom/directory-tree.lisp
@@ -1,6 +1,7 @@
 ;;;;; tré – Copyright (c) 2014 Sven Michael Klose <pixel@hugbox.org>
 
 (defun directory-tree (pathname)
+  (print pathname)
   (let d (%directory pathname)
     (when (number? d)
       (? (== 13 d)
@@ -9,14 +10,17 @@
     (filter [? (| (string== "." _)
                   (string== ".." _))
                _
-               (with (p  (string-concat pathname "/" _)
-                      s  (stat p))
-                 (& (number? s)
-                    (error (%strerror s)))
-                 (alet (+ (list (. 'name _))
-                          s)
-                   (? (& (eq 'directory (assoc-value 'type ! :test #'eq))
-                         (not (readlink p)))
-                      (. (. 'list (directory-tree p)) !)
-                      !)))]
+               (with (p     (string-concat pathname "/" _)
+                      dest  (readlink p))
+                 (? (& dest (not (file-exists? dest)))
+                    (list (. 'name _))
+                    (let s (stat p)
+                      (& (number? s)
+                         (error (%strerror s)))
+                      (alet (+ (list (. 'name _))
+                               s)
+                        (? (& (eq 'directory (assoc-value 'type ! :test #'eq))
+                              (not dest))
+                           (. (. 'list (directory-tree p)) !)
+                           !)))))]
             d)))
