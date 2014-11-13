ed1335246a842ed3dec6099f5ee1f1a255ac8def
Sven Michael Klose <pixel@copei.de>
2014-11-13 14:49:55 +0100

    Don't return question mark if there're no parametres.

--- environment/platforms/shared/url/assoc-url.lisp
+++ environment/platforms/shared/url/assoc-url.lisp
@@ -1,8 +1,8 @@
-;;;;; tré – Copyright (c) 2009 Sven Michael Klose <pixel@copei.de>
+;;;;; tré – Copyright (c) 2009,2014 Sven Michael Klose <pixel@copei.de>
 
 (defun assoc-url (x)
   (concat-stringtree
-	"?"
+	(? x "?" "")
     (pad (mapcar (fn (list (encode-u-r-i-component _.)
 					  "="
 					  (encode-u-r-i-component ._)))
