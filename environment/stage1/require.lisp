f84aea685db49e053a909e9d4b79c304e167f561
Sven Michael Klose <pixel@copei.de>
2014-11-27 19:57:59 +0100

    Fix warning.

--- environment/stage1/require.lisp
+++ environment/stage1/require.lisp
@@ -1,5 +1,5 @@
-;:;;; tré – Copyright (c) 2012 Sven Michael Klose
+;:;;; tré – Copyright (c) 2012,2014 Sven Michael Klose <pixel@hugbox.org>
 
-(defun require (x))
-
-(require "fnord")
+(defun require (x)
+  x
+  nil)
