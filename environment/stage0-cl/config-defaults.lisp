96014f613aa60673b5699c81abf17ac2b12ad7fc
Sven Michael Klose <pixel@copei.de>
2014-11-28 13:52:04 +0100

    Only have C target.

--- environment/stage0-cl/config-defaults.lisp
+++ environment/stage0-cl/config-defaults.lisp
@@ -3,7 +3,7 @@
 (? (eq *assert* '*assert*)
    (setq *assert* t))
 (? (eq *targets* '*targets*)
-   (setq *targets* '(c bc js php)))
+   (setq *targets* '(c)))
 
 (defvar *tre-has-math*    t)
 (defvar *tre-has-alien*   t)
