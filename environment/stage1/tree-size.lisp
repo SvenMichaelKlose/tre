efcd3258c6c22a8853a959d8dd36234c77a5fc02
Sven Michael Klose <pixel@copei.de>
2014-11-28 21:09:51 +0100

    Not dot notation usable at this point.

--- environment/stage1/tree-size.lisp
+++ environment/stage1/tree-size.lisp
@@ -1,6 +1,6 @@
-;;;;; tré – Copyright (c) 2009,2011 Sven Michael Klose <pixel@copei.de>
+;;;;; tré – Copyright (c) 2009,2011,2014 Sven Michael Klose <pixel@hugbox.org>
 
 (defun tree-size (x &optional (n 0))
   (? (cons? x)
-     (integer+ 1 n (tree-size x.) (tree-size .x))
+     (integer+ 1 n (tree-size (car x)) (tree-size (cdr x)))
      n))
