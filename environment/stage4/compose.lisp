67175dfcbe1ae4e854928b5e08526f918e320d6f
Sven Michael Klose <pixel@copei.de>
2014-11-28 20:31:22 +0100

    Omit FUNCTION.

--- environment/stage4/compose.lisp
+++ environment/stage4/compose.lisp
@@ -1,8 +1,13 @@
-;;;;; tré – Copyright (c) 2008–2009,2012–2013 Sven Michael Klose <pixel@copei.de>
+;;;;; tré – Copyright (c) 2008–2009,2012–2014 Sven Michael Klose <pixel@copei.de>
 
 (defmacro compose (&rest function-list)
   (with (rec #'((l)
-				  `(,l.
+				  `(,(alet l.
+                       (? (& (cons? !)
+                             (eq 'function !.)
+                             (atom .!.))
+                          .!.
+                          !))
 					   ,(? .l
 				   		   (rec .l)
 				   		   'x))))
