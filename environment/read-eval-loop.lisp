3ff54dcc9ab25cee88373ee04eb5d234d5d14c21
Sven Michael Klose <pixel@copei.de>
2014-12-09 15:32:33 +0100

    Flush prompt.

--- environment/read-eval-loop.lisp
+++ environment/read-eval-loop.lisp
@@ -3,7 +3,7 @@
 (defun read-eval-loop (&key (in *standard-input*) (out *standard-output*))
   (while (peek-char in)
          nil
-    (princ "* " out)
+    (format out "* ~F")
     (late-print (eval (repeat-while-changes (compose #'dot-expand #'macroexpand #'quasiquote-expand)
                                             (read in)))
                 out)))
