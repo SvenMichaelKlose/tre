2524e3ab59399721b1af851e9022eea4de736eb5
Sven Michael Klose <pixel@copei.de>
2014-12-11 07:35:47 +0100

    Removed super-old interpreter workaround.

--- environment/stage0/backquote.lisp
+++ environment/stage0/backquote.lisp
@@ -1,61 +1,61 @@
 ;;;;; tré – Copyright (c) 2006–2014 Sven Michael Klose <pixel@copei.de>
 
 (%defun any-quasiquote? (x)
-       (? (cons? x)
-          (?
-            (eq (car x) 'quasiquote)         t
-            (eq (car x) 'quasiquote-splice)  t)))
+  (? (cons? x)
+     (?
+       (eq (car x) 'quasiquote)         t
+       (eq (car x) 'quasiquote-splice)  t)))
 
-(%defun %quasiquote-eval (%gsbq)
-  (eval (car (cdr (car %gsbq)))))
+(%defun %quasiquote-eval (x)
+  (eval (car (cdr (car x)))))
 
-(%defun %backquote-quasiquote (%gsbq)
-       (? (cpr %gsbq)
-          (setq *default-listprop* (cpr %gsbq)))
+(%defun %backquote-quasiquote (x)
+  (? (cpr x)
+     (setq *default-listprop* (cpr x)))
+     (#'((p c)
+           (rplacp c p))
+       *default-listprop*
+       (cons (? (any-quasiquote? (car (cdr (car x))))
+                (%backquote (car (cdr (car x))))
+                (%quasiquote-eval x))
+             (%backquote (cdr x)))))
+
+(%defun %backquote-quasiquote-splice (x)
+  (? (any-quasiquote? (car (cdr (car x))))
+     (progn
+       (? (cpr x)
+          (setq *default-listprop* (cpr x)))
        (#'((p c)
              (rplacp c p))
          *default-listprop*
-         (cons (? (any-quasiquote? (car (cdr (car %gsbq))))
-                  (%backquote (car (cdr (car %gsbq))))
-                  (%quasiquote-eval %gsbq))
-               (%backquote (cdr %gsbq)))))
-
-(%defun %backquote-quasiquote-splice (%gsbq)
-       (? (any-quasiquote? (car (cdr (car %gsbq))))
-          (progn
-            (? (cpr %gsbq)
-               (setq *default-listprop* (cpr %gsbq)))
-            (#'((p c)
-                  (rplacp c p))
-              *default-listprop*
-              (cons (car (cdr (car %gsbq)))
-                    (%backquote (cdr %gsbq)))))
-          (#'((%gstmp)
-                (?
-                  (not %gstmp)  (%backquote (cdr %gsbq))
-                  (atom %gstmp) (error "QUASIQUOTE-SPLICE expects a list instead of ~A." %gstmp)
-                  (%nconc (copy-list %gstmp)
-                          (%backquote (cdr %gsbq)))))
-            (%quasiquote-eval %gsbq))))
+         (cons (car (cdr (car x)))
+               (%backquote (cdr x)))))
+     (#'((evaluated)
+           (?
+             (not evaluated)  (%backquote (cdr x))
+             (atom evaluated) (error "QUASIQUOTE-SPLICE expects a list instead of ~A." evaluated)
+             (%nconc (copy-list evaluated)
+                     (%backquote (cdr x)))))
+       (%quasiquote-eval x))))
 
 ;; Expand BACKQUOTE arguments.
-(%defun %backquote (%gsbq)
-       (?
-         (atom %gsbq) %gsbq
-         (progn
-           (? (cpr %gsbq)
-              (setq *default-listprop* (cpr %gsbq)))
-           (#'((p c)
-                 (? (cons? c)
-                    (rplacp c (setq *default-listprop* p))))
-             *default-listprop*
-             (?
-               (atom (car %gsbq))                        (cons (car %gsbq)
-                                                               (%backquote (cdr %gsbq)))
-               (eq 'QUASIQUOTE (car (car %gsbq)))        (%backquote-quasiquote %gsbq)
-               (eq 'QUASIQUOTE-SPLICE (car (car %gsbq))) (%backquote-quasiquote-splice %gsbq)
-               (cons (%backquote (car %gsbq))
-                     (%backquote (cdr %gsbq))))))))
+(%defun %backquote (x)
+  (?
+    (atom x) x
+    (progn
+      (? (cpr x)
+         (setq *default-listprop* (cpr x)))
+      (#'((p c)
+            (? (cons? c)
+               (rplacp c (setq *default-listprop* p))))
+        *default-listprop*
+        (?
+          (atom (car x))                        (cons (car x)
+                                                      (%backquote (cdr x)))
+          (eq 'QUASIQUOTE (car (car x)))        (%backquote-quasiquote x)
+          (eq 'QUASIQUOTE-SPLICE (car (car x))) (%backquote-quasiquote-splice x)
+          (cons (%backquote (car x))
+                (%backquote (cdr x))))))))
 
 (%defun quasiquote (x)
   x
--- environment/stage0/macroexpand.lisp
+++ environment/stage0/macroexpand.lisp
@@ -6,86 +6,86 @@
 (%defvar *macroexpand-backquote-diversion* nil)
 (%defvar *macroexpand-print?* nil)
 
-(%defun %macroexpand-backquote (%g)
+(%defun %macroexpand-backquote (x)
   (?
-    (atom %g) %g
-    (atom (car %g))
-        (cons (car %g)
-              (%macroexpand-backquote (cdr %g)))
+    (atom x) x
+    (atom (car x))
+        (cons (car x)
+              (%macroexpand-backquote (cdr x)))
 
-    (eq (car (car %g)) 'QUASIQUOTE)
+    (eq (car (car x)) 'QUASIQUOTE)
         (cons (cons 'QUASIQUOTE
-                    (%macroexpand (cdr (car %g))))
-              (%macroexpand-backquote (cdr %g)))
+                    (%macroexpand (cdr (car x))))
+              (%macroexpand-backquote (cdr x)))
 
-    (eq (car (car %g)) 'QUASIQUOTE-SPLICE)
+    (eq (car (car x)) 'QUASIQUOTE-SPLICE)
         (cons (cons 'QUASIQUOTE-SPLICE
-                    (%macroexpand (cdr (car %g))))
-              (%macroexpand-backquote (cdr %g)))
+                    (%macroexpand (cdr (car x))))
+              (%macroexpand-backquote (cdr x)))
 
-    (cons (%macroexpand-backquote (car %g))
-          (%macroexpand-backquote (cdr %g)))))
+    (cons (%macroexpand-backquote (car x))
+          (%macroexpand-backquote (cdr x)))))
 
 (setq *macroexpand-backquote-diversion* #'%macroexpand-backquote)
 
-(%defun %macroexpand-rest (%g)
-  (? (atom %g)
-     %g
-     (cons (%macroexpand (car %g))
-           (%macroexpand-rest (cdr %g)))))
+(%defun %macroexpand-rest (x)
+  (? (atom x)
+     x
+     (cons (%macroexpand (car x))
+           (%macroexpand-rest (cdr x)))))
 
-(%defun %macroexpand-xlat (%g)
+(%defun %macroexpand-xlat (x)
   (? *macroexpand-print?*
      (progn
        (print '*macroexpand-print?*)
-       (print %g)))
-  (setq *current-macro* (car %g))
-  (#'((%g)
+       (print x)))
+  (setq *current-macro* (car x))
+  (#'((x)
         (? *macroexpand-print?*
-           (print %g))
+           (print x))
         (setq *current-macro* nil)
-        %g)
-    (apply *macrocall-diversion* (list %g))))
+        x)
+    (apply *macrocall-diversion* (list x))))
 
-(%defun %macroexpand-call (%g)
-  (? (? (atom (car %g))
-        (apply *macro?-diversion* (list %g)))
-     (%macroexpand-xlat %g)
-     %g))
+(%defun %macroexpand-call (x)
+  (? (? (atom (car x))
+        (apply *macro?-diversion* (list x)))
+     (%macroexpand-xlat x)
+     x))
 
-(%defun %macroexpand (%g)
+(%defun %macroexpand (x)
   (?
-    (atom %g) %g
-    (eq (car %g) 'QUOTE)             %g
-    (eq (car %g) 'BACKQUOTE)         (cons 'BACKQUOTE
-                                           (apply *macroexpand-backquote-diversion* (list (cdr %g))))
-    (eq (car %g) 'QUASIQUOTE)        (cons 'QUASIQUOTE
-                                           (%macroexpand (cdr %g)))
-    (eq (car %g) 'QUASIQUOTE-SPLICE) (cons 'QUASIQUOTE-SPLICE
-                                           (%macroexpand (cdr %g)))
-    (%macroexpand-call (%macroexpand-rest %g))))
+    (atom x) x
+    (eq (car x) 'QUOTE)             x
+    (eq (car x) 'BACKQUOTE)         (cons 'BACKQUOTE
+                                           (apply *macroexpand-backquote-diversion* (list (cdr x))))
+    (eq (car x) 'QUASIQUOTE)        (cons 'QUASIQUOTE
+                                           (%macroexpand (cdr x)))
+    (eq (car x) 'QUASIQUOTE-SPLICE) (cons 'QUASIQUOTE-SPLICE
+                                           (%macroexpand (cdr x)))
+    (%macroexpand-call (%macroexpand-rest x))))
 
-(%defun %%macro? (%g)
-  (? (symbol? (car %g))
-     (%%%macro? (car %g))))
+(%defun %%macro? (x)
+  (? (symbol? (car x))
+     (%%%macro? (car x))))
 
-(%defun %%env-macro? (%g)
-  (%%macro? %g))
+(%defun %%env-macro? (x)
+  (%%macro? x))
 
-(%defun %%env-macrocall (%g)
-  (%%macrocall %g))
+(%defun %%env-macrocall (x)
+  (%%macrocall x))
 
-(%defun native-macroexpand (%g)
-  (#'((%gp %gc %gcm)
+(%defun native-macroexpand (x)
+  (#'((predicate caller current-macro)
         (setq *macro?-diversion*    #'%%macro?
               *macrocall-diversion* #'%%macrocall
               *current-macro*       nil)
-        (#'((%g)
-              (setq *macro?-diversion*    %gp
-                    *macrocall-diversion* %gc
-                    *current-macro*       %gcm)
-              %g)
-          (%macroexpand %g)))
+        (#'((x)
+              (setq *macro?-diversion*    predicate
+                    *macrocall-diversion* caller
+                    *current-macro*       current-macro)
+              x)
+          (%macroexpand x)))
      *macro?-diversion* *macrocall-diversion* *current-macro*))
 
 (setq *macroexpand-hook* #'native-macroexpand)
