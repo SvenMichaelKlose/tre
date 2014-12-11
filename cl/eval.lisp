6a1820808553bf2e0be4d8e558e892fb48b31bf0
Sven Michael Klose <pixel@copei.de>
2014-12-11 07:57:17 +0100

    Cleaned up.

--- cl/eval.lisp
+++ cl/eval.lisp
@@ -2,6 +2,11 @@
 
 (in-package :tre-core)
 
+; Usually every function keeps its source code.
+; If we do this in SBCL, anonymous functions won't get garbage collected,
+; That's why it's disabled here.
+(defconstant +anonymous-function-sources?+ nil)
+
 (defvar *quasiquoteexpand-hook* nil)
 (defvar *dotexpand-hook* nil)
 
@@ -11,18 +16,28 @@
        (not (atom (cadr x)))
        (not (eq 'lambda (caadr x)))))
 
+(defun make-variable-function (x)
+  `(labels ((~local-var-fun ,@(make-lambdas (cadar x))))
+     (~local-var-fun ,@(make-lambdas (cdr x)))))
+
+(defun make-anonymous-function (x)
+  (? +anonymous-function-sources?+
+     `(let ((~anonymous-fun #'(lambda ,@(make-lambdas (cadr x)))))
+        (setf (gethash ~anonymous-fun *function-atom-sources*) ',(cadr x))
+        ~anonymous-fun)
+     `#'(lambda ,@(make-lambdas (cadr x)))))
+
+(defun &body-to-&rest (x)
+  (? (eq '&body x)
+     '&rest
+     x))
+
 (defun make-lambdas (x)
   (cond
-    ((atom x)                  (? (eq '&body x)
-                                  '&rest
-                                  x))
+    ((atom x)                  (&body-to-&rest x))
     ((eq 'quote (car x))       x)
-    ((function-expr? (car x))  `(labels ((~local-var-fun ,@(make-lambdas (cadar x))))
-                                  (~local-var-fun ,@(make-lambdas (cdr x)))))
-;    ((function-expr? x)        `(let ((~anonymous-fun #'(lambda ,@(make-lambdas (cadr x)))))
-;                                  (setf (gethash ~anonymous-fun *function-atom-sources*) ',(cadr x))
-;                                  ~anonymous-fun))
-    ((function-expr? x)        `#'(lambda ,@(make-lambdas (cadr x))))
+    ((function-expr? (car x))  (make-variable-function x))
+    ((function-expr? x)        (make-anonymous-function x))
     (t (mapcar #'make-lambdas x))))
 
 (defun tre2cl (x)
