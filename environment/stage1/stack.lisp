15ed1f9600aef4d2b77ac42b4311963e00f9d539
Sven Michael Klose <pixel@copei.de>
2014-12-17 13:22:00 +0100

    Don't print warnings.

--- environment/stage1/stack.lisp
+++ environment/stage1/stack.lisp
@@ -1,10 +1,10 @@
 ;;;;; tré – Copyright (c) 2005–2008,2011–2012 Sven Michael Klose <pixel@copei.de>
 
 (defmacro push (elm expr)
-  (& (cons? elm)
-     (eq 'cons (car elm))
-     (princ "; HINT: Macro PUSH: CONSed element: you may want to consider using ACONS! instead")
-     (terpri))
+;  (& (cons? elm)
+;     (eq 'cons (car elm))
+;     (princ "; HINT: Macro PUSH: CONSed element: you may want to consider using ACONS! instead")
+;     (terpri))
   `(= ,expr (cons ,elm ,expr)))
 
 (defmacro pop (expr)
