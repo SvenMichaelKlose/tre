91dc615e5b12ca54dcfc55c883fa2395cd38c32f
Sven Michael Klose <pixel@copei.de>
2014-12-20 11:23:40 +0100

    Set package.

--- cl/argument-expand.lisp
+++ cl/argument-expand.lisp
@@ -1,5 +1,7 @@
 ;;;;; tré – Copyright (c) 2008–2014 Sven Michael Klose <pixel@copei.de>
 
+(in-package :tre-core)
+
 (defun argument-rest-keyword? (x)     (in? x '&rest '&body))
 (defun argument-keyword? (x)          (in? x '&rest '&body '&optional '&key))
 (defun argument-name? (x)             (atom x))
