fed95b0a904b60ed92bc759a384d979fb4d43cb7
Sven Michael Klose <pixel@copei.de>
2014-12-20 12:07:06 +0100

    Be as verbose as the C core.

--- cl/main.lisp
+++ cl/main.lisp
@@ -1,7 +1,13 @@
 ;;;;; tré – Copyright (c) 2014 Sven Michael Klose <pixel@copei.de>
 
+; Use to debug...
 ;(proclaim '(optimize (speed 0) (space 0) (safety 3) (debug 3)))
-(proclaim '(optimize (speed 3) (space 0) (safety 0) (debug 0)))
+
+; Use if happy...
+;(proclaim '(optimize (speed 3) (space 3) (safety 0) (debug 0)))
+
+; Don't be noisier than the C core.
+(declaim #+sbcl(sb-ext:muffle-conditions compiler-note style-warning))
 
 (load "cl/init.lisp")
 (load "cl/core.lisp")
