52181c972665f144081fd57e43a543ab5480a071
Sven Michael Klose <pixel@copei.de>
2014-12-23 18:32:26 +0100

    Phew! That crazy SBCL macroexpansion workaround gone, too.

--- environment/transpiler/targets/common-lisp/core/function.lisp
+++ environment/transpiler/targets/common-lisp/core/function.lisp
@@ -8,32 +8,23 @@
 (defmacro %set-atom-fun (x v) `(setf (symbol-function ',x) ,v))
 
 (defmacro %defun-quiet (name args &body body)
-  (push (cons name (cons args body)) *functions*)
   `(progn
-     (defun ,name ,args ,@body)
-     (setf (gethash #',name *function-atom-sources*) ',(cons args body))))
+     (cl:push (. name ',(. args body)) *functions*)
+     (cl:defun ,name ,args ,@body)
+     (cl:setf (cl:gethash #',name *function-atom-sources*) ',(. args body))))
 
 (defmacro %defun (name args &body body)
-  (print `(%defun ,name ,args))
-  `(%defun-quiet ,name ,args ,@body))
-
-(%defmacro %defun-quiet (name args &body body)
-  (push (cons name (cons args body)) *functions*)
-  `(progn
-     (defun ,name ,args ,@body)
-     (setf (gethash #',name *function-atom-sources*) ',(cons args body))))
-
-(%defmacro %defun (name args &body body)
   (print `(%defun ,name ,args))
   `(%defun-quiet ,name ,args ,@body))
 
 (defvar *function-atom-sources* (make-hash-table :test #'eq))
 
 (defun function-source (x)
-  (or (functionp x)
-      (error "Not a function."))
-  (gethash x *function-atom-sources*))
+  (| (cl:functionp x)
+     (cl:error "Not a function."))
+  (cl:gethash x *function-atom-sources*))
 
-(defun =-function-source (v x) (setf (gethash x *function-atom-sources*) v))
+(defun =-function-source (v x)
+  (cl:setf (cl:gethash x *function-atom-sources*) v))
 
 (defun function-bytecode (x) x nil)
