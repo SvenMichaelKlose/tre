e384de38490da275061ff07dd13a68152d729651
Sven Michael Klose <pixel@copei.de>
2014-12-23 20:30:51 +0100

    Minor cleanup.

--- environment/stage3/argument-expand-tests.lisp
+++ environment/stage3/argument-expand-tests.lisp
@@ -20,12 +20,12 @@
 	      '(a b c d)))
   t)
 
-(define-test "argument expansion can handle &REST keyword"
+(define-test "argument expansion can handle &REST"
   ((equal (argument-expand 'test '(a b &rest c) '(23 5 42 65))
 		  '((a . 23) (b . 5) (c &rest 42 65))))
   t)
 
-(define-test "argument expansion can handle &REST keyword without :apply-values?"
+(define-test "argument expansion can handle &REST without :apply-values?"
   ((equal (argument-expand-names 'test '(a b c &rest d))
 		  '(a b c d)))
   t)
@@ -40,56 +40,56 @@
 		  '(a b c)))
   t)
 
-(define-test "argument expansion can handle &OPTIONAL keyword"
+(define-test "argument expansion can handle &OPTIONAL"
   ((equal (argument-expand 'test '(a b &optional c d)
                                  '(23 2 3 42))
 		  '((a . 23) (b . 2) (c . 3) (d . 42))))
   t)
 
-(define-test "argument expansion can handle &OPTIONAL keyword without :apply-values?"
+(define-test "argument expansion can handle &OPTIONAL without :apply-values?"
   ((equal (argument-expand-names 'test '(a b &optional c d))
 		  '(a b c d)))
   t)
 
-(define-test "argument expansion can handle &OPTIONAL keyword with init forms"
+(define-test "argument expansion can handle &OPTIONAL with init forms"
   ((equal (argument-expand 'test '(a b &optional (c 3) (d 42))
                                  '(23 2))
 		  '((a . 23) (b . 2) (c . 3) (d . 42))))
   t)
 
-(define-test "argument expansion can handle &OPTIONAL keyword with init forms without :apply-values?"
+(define-test "argument expansion can handle &OPTIONAL with init forms without :apply-values?"
   ((equal (argument-expand-names 'test '(a b &optional (c 3) (d 42)))
 		  '(a b c d)))
   t)
 
-(define-test "argument expansion can handle &KEY keyword"
+(define-test "argument expansion can handle &KEY"
   ((equal (argument-expand 'test '(a b &key c d)
                                  '(23 2 :c 3 :d 42))
 		  '((a . 23) (b . 2) (c . 3) (d . 42))))
   t)
 
-(define-test "argument expansion can handle &KEY keyword with overloaded init forms"
+(define-test "argument expansion can handle &KEY with overloaded init forms"
   ((equal (argument-expand 'test '(a b &key (c 3) (d 42))
                                  '(23 2 :c 5 :d 65))
 		  '((a . 23) (b . 2) (c . 5) (d . 65))))
   t)
 
-(define-test "argument expansion can handle &KEY keyword without :apply-values?"
+(define-test "argument expansion can handle &KEY without :apply-values?"
   ((equal (argument-expand-names 'test '(a b &key c d))
 		  '(a b c d)))
   t)
 
-(define-test "argument expansion can handle &OPTIONAL and &KEY keyword with init forms without :apply-values?"
+(define-test "argument expansion can handle &OPTIONAL and &KEY with init forms without :apply-values?"
   ((equal (argument-expand-names 'test '(a b &optional (c 3) &key (d 42)))
 		  '(a b c d)))
   t)
 
-(define-test "argument expansion can handle &OPTIONAL and &KEY keyword with init forms with :apply-values?"
+(define-test "argument expansion can handle &OPTIONAL and &KEY with init forms with :apply-values?"
   ((equal (argument-expand 'test '(a b &optional (c 3) &key (d 42))
 								 '(23 2 3 :d 65))
 		  '((a . 23) (b . 2) (c . 3) (d . 65))))
   t)
 
-;(define-test "argument expansion can handle &KEY keyword with init forms"
+;(define-test "argument expansion can handle &KEY with init forms"
 ;  ((equal (argument-expand '(a b &key (c 3) (d 42)) '(23 2) t)
 ;          '(values (a b c d) (23 2 3 42)))))
