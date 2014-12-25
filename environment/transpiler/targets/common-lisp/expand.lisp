13f4f0ab9194f0d888a30950cc3e464412acf5f9
Sven Michael Klose <pixel@copei.de>
2014-12-25 11:56:19 +0100

    First working version.

--- environment/transpiler/targets/common-lisp/expand.lisp
+++ environment/transpiler/targets/common-lisp/expand.lisp
@@ -8,13 +8,11 @@
 
 (define-cl-std-macro %defun (name args &body body)
   `(progn
-     (print `(%defun ,name ,args))
-     (cl:push (. name ',(. args body)) *functions*)
+     (push (. name ',(. args body)) *functions*)
      (cl:defun ,name ,args ,@body)
      (cl:setf (cl:gethash #',name *function-atom-sources*) ',(. args body))))
 
-(define-cl-std-macro %define-cl-st-macro (name args &body body)
-  (print `(%define-cl-std-macro ,name ,args))
+(define-cl-std-macro %defmacro (name args &body body)
   `(cl:push (. ',name
                (. ',args
                   #'(lambda ,(argument-expand-names 'define-cl-std-macro args)
@@ -22,7 +20,6 @@
             *macros*))
 
 (define-cl-std-macro %defvar (name &optional (init nil))
-  (print `(%defvar ,name))
   `(progn
-     (cl:push (. ',name ',init) *variables*)
+     (push (. ',name ',init) *variables*)
      (cl:defvar ,name ,init)))
