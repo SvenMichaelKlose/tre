3104dbcc5a8a58e81025b8a827cb180f8a5e5426
Sven Michael Klose <pixel@copei.de>
2014-12-25 09:20:40 +0100

    DEFINE-CL-STD-MACRO added.

--- environment/transpiler/targets/common-lisp/expand.lisp
+++ environment/transpiler/targets/common-lisp/expand.lisp
@@ -1,24 +1,27 @@
 ; tré – Copyright (c) 2014 Sven Michael Klose <pixel@copei.de>
 
-(define-cl-st-macro %set-atom-fun (x v)
+(defmacro define-cl-std-macro (name args &body body)
+  `(define-transpiler-std-macro *cl-transpiler* ,name ,args ,@body))
+
+(define-cl-std-macro %set-atom-fun (x v)
   `(cl:setf (cl:symbol-function ',x) ,v))
 
-(define-cl-st-macro %defun (name args &body body)
+(define-cl-std-macro %defun (name args &body body)
   `(progn
      (print `(%defun ,name ,args))
      (cl:push (. name ',(. args body)) *functions*)
      (cl:defun ,name ,args ,@body)
      (cl:setf (cl:gethash #',name *function-atom-sources*) ',(. args body))))
 
-(define-cl-st-macro %define-cl-st-macro (name args &body body)
-  (print `(%define-cl-st-macro ,name ,args))
+(define-cl-std-macro %define-cl-st-macro (name args &body body)
+  (print `(%define-cl-std-macro ,name ,args))
   `(cl:push (. ',name
                (. ',args
-                  #'(lambda ,(argument-expand-names '%define-cl-st-macro args)
+                  #'(lambda ,(argument-expand-names 'define-cl-std-macro args)
                       ,@body)))
             *macros*))
 
-(define-cl-st-macro %defvar (name &optional (init nil))
+(define-cl-std-macro %defvar (name &optional (init nil))
   (print `(%defvar ,name))
   `(progn
      (cl:push (. ',name ',init) *variables*)
