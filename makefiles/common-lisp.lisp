2f8cb6d17a3c276d5ff749cdfc931a4e09e8a744
Sven Michael Klose <pixel@copei.de>
2014-12-25 18:50:16 +0100

    Don't save sources.

--- makefiles/common-lisp.lisp
+++ makefiles/common-lisp.lisp
@@ -1,9 +1,9 @@
 ; tré – Copyright (c) 2014 Sven Michael Klose <pixel@copei.de>
 
 (alet (copy-transpiler *cl-transpiler*)
-  (= (transpiler-import-from-environment? !) nil)
+  (= (transpiler-save-sources? !) nil)
   (with-output-file o "tre.lisp"
     (filter [late-print _ o]
-            (cdar (compile-sections (list (. 'core *cl-base*))
-                                     :transpiler !)))))
+            (compile-sections (list (. 'core *cl-base*))
+                              :transpiler !))))
 (quit)
