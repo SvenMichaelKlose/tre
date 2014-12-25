15da4531c5827d21326aacbd2eed0690cf9b292b
Sven Michael Klose <pixel@copei.de>
2014-12-25 09:18:25 +0100

    Set EXPEX-INITIALIZER.

--- environment/transpiler/targets/common-lisp/config.lisp
+++ environment/transpiler/targets/common-lisp/config.lisp
@@ -1,4 +1,8 @@
-; tré – Copyright (c) 2008–2010,2012–2014 Sven Michael Klose <pixel@copei.de>
+; tré – Copyright (c) 2014 Sven Michael Klose <pixel@copei.de>
+
+(defun cl-expex-initializer (ex)
+  (= (expex-argument-filter ex) #'identity
+     (expex-setter-filter ex)   #'identity))
 
 (defun make-cl-transpiler ()
   (create-transpiler
@@ -7,6 +11,7 @@
       :import-variables?  t
       :lambda-export?     nil
       :stack-locals?      nil
+      :expex-initializer  #'cl-expex-initializer
       :postprocessor      #'tre2cl))
 
 (defvar *cl-transpiler* (copy-transpiler (make-cl-transpiler)))
