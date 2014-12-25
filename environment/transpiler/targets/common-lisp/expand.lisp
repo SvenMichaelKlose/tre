6845394a58f9dabd61a973e788b85c8fb432c0fd
Sven Michael Klose <pixel@copei.de>
2014-12-25 16:23:54 +0100

    Regard TRANSPILER-SAVE-SOURCES? and TRANSPILER-SAVE-ARGDEFS-ONLY?.

--- environment/transpiler/targets/common-lisp/expand.lisp
+++ environment/transpiler/targets/common-lisp/expand.lisp
@@ -7,13 +7,18 @@
   `(cl:setf (cl:symbol-function ',x) ,v))
 
 (define-cl-std-macro %defun (name args &body body)
+  (print-definition `(%defun ,name ,args))
   (transpiler-add-defined-function *transpiler* name args body)
   `(progn
      (push (. name ',(. args body)) *functions*)
      (cl:defun ,name ,args ,@body)
-     (cl:setf (cl:gethash #',name *function-atom-sources*) ',(. args body))))
+     ,@(& (transpiler-save-sources? *transpiler*)
+          `(cl:setf (cl:gethash #',name *function-atom-sources*)
+                    ',(. args (& (not (transpiler-save-argdefs-only? *transpiler*))
+                                 body))))))
 
 (define-cl-std-macro %defmacro (name args &body body)
+  (print-definition `(%defmacro ,name ,args))
   `(cl:push (. ',name
                (. ',args
                   #'(lambda ,(argument-expand-names 'define-cl-std-macro args)
@@ -21,6 +26,7 @@
             *macros*))
 
 (define-cl-std-macro %defvar (name &optional (init nil))
+  (print-definition `(%defvar ,name))
   (transpiler-add-defined-variable *transpiler* name)
   `(progn
      (push (. ',name ',init) *variables*)
