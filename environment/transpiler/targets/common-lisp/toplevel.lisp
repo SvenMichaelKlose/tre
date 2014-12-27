5f8e003f48b56dd204b25ed4eb35c708b75d5474
Sven Michael Klose <pixel@copei.de>
2014-12-27 13:41:48 +0100

    Make lambda exoression in (POSTPROCESSOR).

--- environment/transpiler/targets/common-lisp/toplevel.lisp
+++ environment/transpiler/targets/common-lisp/toplevel.lisp
@@ -1,19 +1,12 @@
 ; tré – Copyright (c) 2005–2014 Sven Michael Klose <pixel@copei.de>
 
-(defun tre-expansions (x)
-  (backquote-expand (quasiquote-expand (macroexpand (dot-expand x)))))
-(defun expr2cl (x)                 (make-lambdas (tre-expansions x)))
-(defun file2cl (pathname)          (expr2cl (read-file pathname)))
-(defun files2cl (&rest pathnames)  (. 'progn (mapcan #'file2cl pathnames)))
-
 (defun cl-expex-initializer (ex)
   (= (expex-argument-filter ex) #'identity
      (expex-setter-filter ex)   #'identity))
 
 (defun cl-frontend (x)
-  (alet (transpiler-macroexpand (quasiquote-expand (dot-expand x)))
-    (fake-expression-expand (fake-place-expand (lambda-expand (rename-arguments (backquote-expand (compiler-macroexpand !))))))
-    (make-lambdas !)))
+  (aprog1 (transpiler-macroexpand (quasiquote-expand (dot-expand x)))
+    (fake-expression-expand (fake-place-expand (lambda-expand (rename-arguments (backquote-expand (compiler-macroexpand !))))))))
 
 (defun cl-sections-before-deps ()
   (unless (exclude-base?)
@@ -29,6 +22,8 @@
       :sections-before-deps  #'cl-sections-before-deps
       :frontend-init         #'(() (= *cl-builtins* nil))
       :own-frontend          #'cl-frontend
-      :expex-initializer     #'cl-expex-initializer))
+      :expex-initializer     #'cl-expex-initializer
+      :postprocessor         #'((&rest x)
+                                  (make-lambdas (apply #'+ x)))))
 
 (defvar *cl-transpiler* (copy-transpiler (make-cl-transpiler)))
