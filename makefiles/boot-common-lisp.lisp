9a9ce8b4c6b40fc0306ae95f6ee4add8fe640754
Sven Michael Klose <pixel@copei.de>
2014-12-27 21:27:58 +0100

    Try to load the environment.

--- makefiles/boot-common-lisp.lisp
+++ makefiles/boot-common-lisp.lisp
@@ -25,7 +25,11 @@
 (alet (copy-transpiler *cl-transpiler*)
   (= (transpiler-save-sources? !) nil)
   (with-output-file o "boot-common.lisp"
-    (princ "(declaim #+sbcl(sb-ext:muffle-conditions compiler-note style-warning))" o)
+    (format o "(declaim #+sbcl(sb-ext:muffle-conditions compiler-note style-warning))~%")
+    ; Use to debug...
+    (format o "(proclaim '(optimize (speed 0) (space 0) (safety 3) (debug 3)))~%")
+    ; Use if happy...
+    ;(format o "(proclaim '(optimize (speed 3) (space 3) (safety 0) (debug 0)))~%")
     (let c (compile-sections (list (. 'core nil))
                              :transpiler !)
       (adolist ((cl-packages))
@@ -34,5 +38,6 @@
       (adolist ((cl-wrappers))
         (princ ! o))
       (filter [late-print _ o] c)
-      (late-print '(cl:in-package :tre) o))))
+      (late-print '(cl:in-package :tre) o)
+      (late-print '(env-load "main.lisp") o))))
 (quit)
