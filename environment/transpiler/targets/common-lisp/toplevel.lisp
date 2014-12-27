60da611dd65339dcae8e6675f82d61ca5bd9604f
Sven Michael Klose <pixel@copei.de>
2014-12-27 16:39:02 +0100

    Minor cleanup.

--- environment/transpiler/targets/common-lisp/toplevel.lisp
+++ environment/transpiler/targets/common-lisp/toplevel.lisp
@@ -1,12 +1,18 @@
 ; tré – Copyright (c) 2005–2014 Sven Michael Klose <pixel@copei.de>
 
-(defun cl-expex-initializer (ex)
-  (= (expex-argument-filter ex) #'identity
-     (expex-setter-filter ex)   #'identity))
+(defun cl-postprocessor (&rest x)
+  (make-lambdas (apply #'+ x)))
 
 (defun cl-frontend (x)
   (aprog1 (transpiler-macroexpand (quasiquote-expand (dot-expand x)))
     (fake-expression-expand (fake-place-expand (lambda-expand (rename-arguments (backquote-expand (compiler-macroexpand !))))))))
+
+(defun cl-frontend-init ()
+  (= *cl-builtins* nil))
+
+(defun cl-expex-initializer (ex)
+  (= (expex-argument-filter ex) #'identity
+     (expex-setter-filter ex)   #'identity))
 
 (defun cl-sections-before-import ()
   (unless (exclude-base?)
@@ -14,16 +20,15 @@
 
 (defun make-cl-transpiler ()
   (create-transpiler
-      :name                  :common-lisp
+      :name                    :common-lisp
       :frontend-only?          t
       :import-variables?       t
       :lambda-export?          nil
       :stack-locals?           nil
       :sections-before-import  #'cl-sections-before-import
-      :frontend-init           #'(() (= *cl-builtins* nil))
+      :frontend-init           #'cl-frontend-init
       :own-frontend            #'cl-frontend
       :expex-initializer       #'cl-expex-initializer
-      :postprocessor           #'((&rest x)
-                                    (make-lambdas (apply #'+ x)))))
+      :postprocessor           #'cl-postprocessor))
 
 (defvar *cl-transpiler* (copy-transpiler (make-cl-transpiler)))
