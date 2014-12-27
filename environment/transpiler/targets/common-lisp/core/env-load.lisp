a1a37eed4ab27311444fbbc6e0a173cbd135785b
Sven Michael Klose <pixel@copei.de>
2014-12-27 01:29:55 +0100

    No TRE-PARALLEL package anymore.

--- environment/transpiler/targets/common-lisp/core/env-load.lisp
+++ environment/transpiler/targets/common-lisp/core/env-load.lisp
@@ -1,10 +1,8 @@
 ; tré – Copyright (c) 2014 Sven Michael Klose <pixel@copei.de>
 
-(in-package :tre-core)
-
 (defvar *environment-path* ".")
 (defvar *environment-filenames* nil)
 
-(%defun env-load (pathname &optional (target nil))
+(defbuiltin env-load (pathname &optional (target nil))
   (setq *environment-filenames* (cons (cons pathname target) *environment-filenames*))
-  (tre-parallel:%load (string-concat *environment-path* "/environment/" pathname)))
+  (%load (string-concat *environment-path* "/environment/" pathname)))
