94a73d33d98e1ff5384ca68421fe355e44e3d818
Sven Michael Klose <pixel@copei.de>
2014-12-27 02:31:02 +0100

    Make wrappers for +CL-DIRECT-IMPORTS+. Fixed DEFPACKAGE.

--- makefiles/common-lisp.lisp
+++ makefiles/common-lisp.lisp
@@ -2,25 +2,34 @@
 
 (defun cl-packages ()
   `((defpackage :tre-core
-      ((:use :common-lisp)
       (:export :nil :t :setq :labels
-               ,@(make-keywords (+ (carlist +cl-renamed-imports+)
-                                   *cl-builtins*)))))
+                ,@(make-keywords (+ (carlist +cl-renamed-imports+)
+                                    *cl-builtins*))))
     (defpackage :tre
-      ((:use :tre-core)))))
+      (:use :tre-core))))
+
+(defun cl-symbol (x)
+  (make-symbol (symbol-name x) "COMMON-LISP"))
+
+(defun cl-wrapper (x)
+  (format nil "(cl:defun ~A (cl:&rest x) (cl:apply #'cl:~A x))~%"
+              (symbol-name x.) (symbol-name .x.)))
 
 (defun cl-wrappers ()
-  (filter [`(defun ,_. (&rest x)
-              (apply #',(make-symbol (symbol-name ._.) "COMMON-LISP") x))]
-          +cl-renamed-imports+))
+  (filter #'cl-wrapper
+          (+ (filter [list _ _] +cl-direct-imports+)
+             +cl-renamed-imports+)))
 
 (alet (copy-transpiler *cl-transpiler*)
   (= (transpiler-save-sources? !) nil)
   (with-output-file o "tre.lisp"
     (filter [late-print _ o]
-            (+ (cl-packages)
-               '((in-package :tre-core))
-               (compile-sections (list (. 'core (+ *cl-base* (cl-wrappers))))
-                                 :transpiler !)))))
+            (let c (compile-sections (list (. 'core *cl-base*))
+                                     :transpiler !)
+              (+ (cl-packages)
+                 '((in-package :tre-core))
+                 c)))
+    (adolist ((cl-wrappers))
+      (princ ! o))))
 
 (quit)
