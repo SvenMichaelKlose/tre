827258c5abbc45f64a223d4a29f7d3d67fec667c
Sven Michael Klose <pixel@copei.de>
2014-12-28 16:16:07 +0100

    Print definitions.

--- environment/transpiler/targets/common-lisp/core/defbuiltin.lisp
+++ environment/transpiler/targets/common-lisp/core/defbuiltin.lisp
@@ -1,5 +1,6 @@
 ; tré – Copyright (c) 2014 Sven Michael Klose <pixel@hugbox.org>
 
 (defmacro defbuiltin (name args &body body)
+  (print-definition `(defbuiltin ,name ,args))
   (push name *cl-builtins*)
   `(defun ,name ,args ,@body))
