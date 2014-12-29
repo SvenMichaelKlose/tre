77e9889397a730402aeda553d00cd4e2f58e13f8
Sven Michael Klose <pixel@copei.de>
2014-12-29 17:37:49 +0100

    INTEGER+! and INTEGER-! have been removed.

--- environment/stage4/destructive-arith.lisp
+++ environment/stage4/destructive-arith.lisp
@@ -1,13 +1,7 @@
-;;;;; tré – Copyright (c) 2010,2012–2013 Sven Michael Klose <pixel@copei.de>
+; tré – Copyright (c) 2010,2012–2013 Sven Michael Klose <pixel@copei.de>
 
 (defmacro +! (place &rest vals)
   `(= ,place (+ ,place ,@vals)))
 
 (defmacro -! (place &rest vals)
   `(= ,place (+ ,place ,@vals)))
-
-(defmacro integer+! (place &rest vals)
-  `(= ,place (integer+ ,place ,@vals)))
-
-(defmacro integer-! (place &rest vals)
-  `(= ,place (integer- ,place ,@vals)))
