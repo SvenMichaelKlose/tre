d3653f86cedb4bca4367df3d25ca43bd5726f154
Sven Michael Klose <pixel@copei.de>
2014-12-29 08:05:38 +0100

    Properly heck if *MACROS* is initialized.

--- environment/transpiler/targets/common-lisp/core/macro.lisp
+++ environment/transpiler/targets/common-lisp/core/macro.lisp
@@ -1,27 +1,32 @@
 ; tré – Copyright (c) 2014 Sven Michael Klose <pixel@copei.de>
 
-(defbuiltin macro? (x) (cl:rassoc x *macros* :test #'eq))
+(defbuiltin macro? (x)
+  (cl:rassoc x *macros* :test #'eq))
+
+(defun env-macros ()
+  (symbol-value (make-symbol "*MACROS*" "TRE")))
 
 (defbuiltin %%macrocall (x)
-  (alet (cdr (assoc x. (symbol-value (make-symbol "*MACROS*" "TRE")) :test #'eq))
-        (apply .! (argument-expand-values x. !. .x))))
+  (alet (cdr (assoc x. (env-macros) :test #'eq))
+    (apply .! (argument-expand-values x. !. .x))))
 
 (defbuiltin %%macro? (x)
   (& (cons? x)
      (symbol? x.)
-     (assoc x. (symbol-value (make-symbol "*MACROS*" "TRE")) :test #'eq)))
+     (alet (env-macros)
+       (& (cons? !)
+          (assoc x. ! :test #'eq)))))
 
 (defvar *macroexpand-hook* nil)
 
 (defbuiltin macroexpand-1 (x)
-  (? *macroexpand-hook*
-     (apply *macroexpand-hook* (list x))
-     x))
-
-(defun macroexpand-0 (old x)
-  (? (equal x old)
-     old
-     (macroexpand x)))
+  (!? (symbol-value (make-symbol "*MACROEXPAND-HOOK*" "TRE"))
+      (apply ! (list x))
+      x))
 
 (defbuiltin macroexpand (x)
-  (macroexpand-0 x (macroexpand-1 x)))
+  (with (f #'((old x)
+               (? (equal old x)
+                  x
+                  (macroexpand x))))
+    (f x (macroexpand-1 x))))
