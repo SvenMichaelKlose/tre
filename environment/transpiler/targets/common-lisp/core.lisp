88d5c5a724bcf4662611c5fa3a61c95753d0d844
Sven Michael Klose <pixel@copei.de>
2015-01-20 13:17:06 +0100

    No list of function atom sources anymore.

--- environment/transpiler/targets/common-lisp/core.lisp
+++ environment/transpiler/targets/common-lisp/core.lisp
@@ -1,4 +1,4 @@
-; tré – Copyright (c) 2014 Sven Michael Klose <pixel@copei.de>
+; tré – Copyright (c) 2014–2015 Sven Michael Klose <pixel@copei.de>
 
 ; Symbols directly imported from package CL-USER.
 (defconstant +cl-direct-imports+
@@ -31,7 +31,7 @@
 
 ; Global variables provided by all tré cores.
 (defconstant +cl-core-variables+
-    '(*universe* *variables* *functions* *function-atom-sources*
+    '(*universe* *variables* *functions*
       *environment-path* *environment-filenames*
       *macroexpand-hook* *quasiquoteexpand-hook* *dotexpand-hook*
       *default-listprop* *keyword-package*
