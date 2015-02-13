855981a6280165e8da0edd4984a661f45d215130
Sven Michael Klose <pixel@copei.de>
2015-02-13 11:13:22 +0100

    Minor cleanup.

--- environment/transpiler/targets/common-lisp/main.lisp
+++ environment/transpiler/targets/common-lisp/main.lisp
@@ -1,6 +1,6 @@
-; tré – Copyright (c) 2014 Sven Michael Klose <pixel@hugbox.org>
+; tré – Copyright (c) 2014–2015 Sven Michael Klose <pixel@hugbox.org>
 
-(env-load "transpiler/targets/common-lisp/core.lisp")
+(env-load "transpiler/targets/common-lisp/imports.lisp")
 (env-load "transpiler/targets/common-lisp/env-load.lisp")
 (env-load "transpiler/targets/common-lisp/toplevel.lisp")
 (env-load "transpiler/targets/common-lisp/expand.lisp")
