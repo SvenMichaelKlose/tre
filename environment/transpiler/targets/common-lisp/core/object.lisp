9bbc7ceafcaf57db5976ecd41683904e7f09c60e
Sven Michael Klose <pixel@copei.de>
2015-02-23 15:38:33 +0100

    EQL: Emulate C core version.

--- environment/transpiler/targets/common-lisp/core/object.lisp
+++ environment/transpiler/targets/common-lisp/core/object.lisp
@@ -1,4 +1,4 @@
-; tré – Copyright (c) 2014 Sven Michael Klose <pixel@copei.de>
+; tré – Copyright (c) 2014–2015 Sven Michael Klose <pixel@hugbox.org>
 
 (defbuiltin not (&rest x) (cl:every #'cl:not x))
 (defbuiltin builtin? (x)   (cl:gethash x *builtins*))
@@ -20,5 +20,16 @@
                  (symbol-name b))
      (cl:eq a b)))
 
+(defun tre-eql (a b)
+  (? (& (number? a)
+        (number? b))
+     (? (eq (character? a)
+            (character? b))
+        (== a b))
+     (& (cons? a)
+        (cons? b)) (& (tre-eql a. b.)
+                      (tre-eql .a .b))
+     (xpackeq a b)))
+
 (defbuiltin eq (&rest x) (variable-compare #'xpackeq x))
-(defbuiltin eql (&rest x) (variable-compare #'cl:eql x))
+(defbuiltin eql (&rest x) (variable-compare #'tre-eql x))
