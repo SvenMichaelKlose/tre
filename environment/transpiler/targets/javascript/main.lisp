ed7f7faf12f3f47caf93a91ef97c9e450226cdc3
Sven Michael Klose <pixel@copei.de>
2015-03-02 13:43:50 +0100

    Less compiler warnings.

--- environment/transpiler/targets/javascript/main.lisp
+++ environment/transpiler/targets/javascript/main.lisp
@@ -1,11 +1,11 @@
 ; tré – Copyright (c) 2005–2010,2012–2015 Sven Michael Klose <pixel@copei.de>
 
 (env-load "transpiler/targets/javascript/argument-filter.lisp")
+(env-load "transpiler/targets/javascript/core.lisp")
 (env-load "transpiler/targets/javascript/toplevel.lisp")
 (env-load "transpiler/targets/javascript/expand.lisp")
 (env-load "transpiler/targets/javascript/expand-inline.lisp")
 (env-load "transpiler/targets/javascript/class.lisp")
 (env-load "transpiler/targets/javascript/codegen.lisp")
 (env-load "transpiler/targets/javascript/codegen-inline-cons.lisp")
-(env-load "transpiler/targets/javascript/core.lisp")
-(env-load "transpiler/targets/javascript/bytecode-to-js.lisp")
+;(env-load "transpiler/targets/javascript/bytecode-to-js.lisp")
