a9556f8866ec1c15f1d88fdc84f61e322bd4d4b1
Sven Michael Klose <pixel@copei.de>
2015-03-04 21:19:25 +0100

    Minor cleanup.

--- environment/transpiler/targets/common-lisp/core/hash-table.lisp
+++ environment/transpiler/targets/common-lisp/core/hash-table.lisp
@@ -1,23 +1,23 @@
-; tré – Copyright (c) 2014 Sven Michael Klose <pixel@copei.de>
+; tré – Copyright (c) 2014–2015 Sven Michael Klose <pixel@copei.de>
 
 (defbuiltin make-hash-table (&key (test #'eql))
   (cl:make-hash-table :test (?
-                           (eq test #'eq)       #'cl:eq
-                           (| (eq test #'eql)
-                              (eq test #'==))  #'cl:eql
+                           (cl:eq test #'eq)       #'cl:eq
+                           (| (cl:eq test #'eql)
+                              (cl:eq test #'==))  #'cl:eql
                            test)))
 
-(defbuiltin hash-table? (x) (cl:hash-table-p x))
-(defbuiltin href (x i) (cl:gethash i x))
-(defbuiltin =-href (v x i) (cl:setf (cl:gethash i x) v))
-(defbuiltin hremove (x k) (cl:remhash k x))
+(defbuiltin hash-table? (x)  (cl:hash-table-p x))
+(defbuiltin href (x i)       (cl:gethash i x))
+(defbuiltin =-href (v x i)   (cl:setf (cl:gethash i x) v))
+(defbuiltin hremove (x k)    (cl:remhash k x))
 
 (defbuiltin copy-hash-table (x)
   (aprog1 (cl:make-hash-table :test (cl:hash-table-test x)
                               :size (cl:hash-table-size x))
-    (cl:maphash (lambda (k v)
+    (cl:maphash #'((k v)
                     (cl:setf (cl:gethash k !) v))
-             x)))
+                x)))
 
 (defbuiltin hashkeys (x)
   (aprog1 nil
