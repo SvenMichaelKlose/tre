adf6507d66bbc6e9e73d572a0f9f48e2543bacba
Sven Michael Klose <pixel@copei.de>
2015-03-05 12:28:14 +0100

    Minor cleanup.

--- environment/transpiler/targets/common-lisp/core/object.lisp
+++ environment/transpiler/targets/common-lisp/core/object.lisp
@@ -1,7 +1,7 @@
 ; tré – Copyright (c) 2014–2015 Sven Michael Klose <pixel@hugbox.org>
 
 (defbuiltin not (&rest x) (cl:every #'cl:not x))
-(defbuiltin builtin? (x)   (cl:gethash x *builtins*))
+(defbuiltin builtin? (x)  (cl:gethash x *builtins*))
 
 (defun variable-compare (predicate x)
   (? .x
@@ -11,25 +11,27 @@
             (return nil))))
      (cl:error "At least 2 arguments required.")))
 
+(defun non-keyword-symbol? (x)
+  (& (cl:symbolp x)
+     (cl:not (cl:keywordp x))))
+
 (defun xpackeq (a b)
-  (? (& (symbol? a)
-        (symbol? b)
-        (not (keyword? a)
-             (keyword? b)))
-     (cl:string= (symbol-name a)
-                 (symbol-name b))
+  (? (& (non-keyword-symbol? a)
+        (non-keyword-symbol? b))
+     (cl:string= (cl:symbol-name a)
+                 (cl:symbol-name b))
      (cl:eq a b)))
 
 (defun tre-eql (a b)
-  (? (& (number? a)
-        (number? b))
-     (? (eq (character? a)
-            (character? b))
-        (== a b))
-     (& (cons? a)
-        (cons? b)) (& (tre-eql a. b.)
-                      (tre-eql .a .b))
-     (xpackeq a b)))
+  (?
+    (& (number? a)
+       (number? b))  (? (cl:eq (character? a)
+                               (character? b))
+                        (== a b))
+    (& (cons? a)
+       (cons? b))    (& (tre-eql a. b.)
+                        (tre-eql .a .b))
+    (xpackeq a b)))
 
-(defbuiltin eq (&rest x) (variable-compare #'xpackeq x))
+(defbuiltin eq (&rest x)  (variable-compare #'xpackeq x))
 (defbuiltin eql (&rest x) (variable-compare #'tre-eql x))
