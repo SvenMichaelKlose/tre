6e420a9ddc307cf6bff7b674856d67f214e1b17b
Sven Michael Klose <pixel@copei.de>
2015-03-15 17:33:10 +0100

    Minor cleanup.

--- environment/transpiler/lib/pass.lisp
+++ environment/transpiler/lib/pass.lisp
@@ -6,35 +6,34 @@
   (print-definition `(transpiler-pass ,name))
   (with (cache-var ($ '*pass- name '*)
          init (gensym))
-        (print
     `(progn
        (defvar ,cache-var nil)
        (defun ,name (,init)
-         (when (dump-passes?)
+         (when (t? (dump-passes?))
            (fresh-line)
            (format t "; #### ~A ####~%" ',name))
          (= ,cache-var ,init)
          (@ (i (list ,@(@ #'cadr (group name-fun-pairs 2))) ,cache-var)
            (with-global-funinfo
-             (= ,cache-var (= (last-pass-result) (funcall i ,cache-var)))))))
-    )))
+             (= ,cache-var (= (last-pass-result) (funcall i ,cache-var)))))))))
+
+(defun dump-pass? (name)
+  (!? (dump-passes?)
+      (| (t? !)
+         (member name (ensure-list !)))))
 
 (defmacro def-pass-fun (name arg &body body)
   (print-definition `(def-pass-fun ,name ,arg))
   (with-gensym fun
     `(defun ,name (,arg)
        (with (,fun  #'((,arg) ,@body))
-         (? (& *transpiler*
-               (!? (dump-passes?)
-                (| (t? !)
-                   (member ',name (ensure-list !)))))
-            (progn
-              (fresh-line)
-              (format t ,(string-concat "; **** " (symbol-name name) "~%"))
-              (with-temporary *current-pass-input* ,arg
-                (prog1
-                  (with-temporary *always-print-package-names?* t
-                    (late-print (,fun ,arg)))
-                  (fresh-line)
-                  (format t ,(string-concat "; **** end of " (symbol-name name) "~%")))))
-            (,fun ,arg))))))
+         (unless (dump-pass? ',name)
+           (return (,fun ,arg)))
+         (fresh-line)
+         (format t ,(string-concat "; **** " (symbol-name name) "~%"))
+         (prog1
+           (with-temporaries (*current-pass-input* ,arg
+                              *always-print-package-names?* t)
+             (late-print (,fun ,arg)))
+           (fresh-line)
+           (format t ,(string-concat "; **** end of " (symbol-name name) "~%")))))))
