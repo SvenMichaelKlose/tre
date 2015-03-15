cc2a307c10113437f16b74c2206683999607c3ed
Sven Michael Klose <pixel@copei.de>
2015-03-15 17:31:30 +0100

    INTEGER?: Type checks first.

--- environment/stage1/predicates-c.lisp
+++ environment/stage1/predicates-c.lisp
@@ -1,6 +1,6 @@
-;;;;; tré – Copyright (c) 2005–2006,2008–2009,2011–2012 Sven Michael Klose <pixel@copei.de>
+; tré – Copyright (c) 2005–2006,2008–2009,2011–2012,2015 Sven Michael Klose <pixel@copei.de>
 
 (defun integer? (x)
   (& (number? x)
-     (== 0 (mod x 1))
-     (not (character? x))))
+     (not (character? x))
+     (== 0 (mod x 1))))
