4f90e8e67b22331950fdf70412b22f56b4462fdf
Sven Michael Klose <pixel@copei.de>
2015-03-15 23:26:40 +0100

    Streams are byte-oriented.

--- environment/transpiler/targets/common-lisp/core/file.lisp
+++ environment/transpiler/targets/common-lisp/core/file.lisp
@@ -1,19 +1,29 @@
-; tré – Copyright (c) 2014 Sven Michael Klose <pixel@copei.de>
+; tré – Copyright (c) 2014–2015 Sven Michael Klose <pixel@copei.de>
 
-(defbuiltin %princ (x stream) (cl:princ x stream))
+(defbuiltin %princ (x stream)
+  (alet (| stream cl:*standard-output*)
+    (?
+      (character? x)  (cl:write-byte (cl:char-code x) !)
+      (string? x)     (adotimes ((length x))
+                        (%princ (cl:elt x !) stream))
+      (cl:princ x !))))
+
 (defbuiltin %force-output (stream) (cl:force-output stream))
 
 (defbuiltin %fopen (pathname mode)
-  (cl:open pathname :direction (? (cl:find #\w mode :test #'cl:equal)
-                               :output
-                               :input)
-                 :if-exists :supersede))
+  (cl:open pathname
+           :direction (? (cl:find #\w mode :test #'cl:equal)
+                         :output
+                         :input)
+           :if-exists :supersede
+           :element-type '(cl:unsigned-byte 8)))
 
 (defbuiltin %fclose (stream) (cl:close stream))
 
 (defbuiltin %read-char (str)
-  (alet (cl:read-char str nil 'eof)
-    (unless (eq ! 'eof) !)))
+  (alet (cl:read-byte str nil 'eof)
+    (unless (eq ! 'eof)
+      (cl:code-char !))))
 
 (defbuiltin unix-sh-mkdir (pathname &key (parents nil))
   (cl:ensure-directories-exist pathname))
