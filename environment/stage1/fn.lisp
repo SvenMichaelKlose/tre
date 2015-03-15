7b3787b2e766ad7f20994aa7971ec700664de23b
Sven Michael Klose <pixel@copei.de>
2015-03-15 15:42:23 +0100

    FN or [expr]: Enclose body in a BLOCK NIL.

--- environment/stage1/fn.lisp
+++ environment/stage1/fn.lisp
@@ -1,11 +1,12 @@
-;;;;; tré – Copyright (c) 2008,2011–2012 Sven Michael Klose <pixel@copei.de>
+; tré – Copyright (c) 2008,2011–2012,2015 Sven Michael Klose <pixel@copei.de>
 
 (defmacro fn (&body body)
   `#'((_)
-	    ,@(? (& (cons? (car body))
-			    (not (eq '%slot-value (car (car body)))))
-			 body
-			 (list body))))
+        (block nil
+	      ,@(? (& (cons? (car body))
+			      (not (eq '%slot-value (car (car body)))))
+			   body
+			   (list body)))))
 
 (defmacro square (&body body)
   `(fn ,@body))
