04521ed289050c314e73d9b5fca524ab039d2875
Sven Michael Klose <pixel@copei.de>
2015-03-22 15:29:07 +0100

    DOTIMES issues error on negative number of iterations.

--- environment/stage1/dotimes.lisp
+++ environment/stage1/dotimes.lisp
@@ -1,8 +1,10 @@
-;;;;; tré – Copyright (c) 2005–2008,2010–2012 Sven Michael Klose <pixel@copei.de>
+; tré – Copyright (c) 2005–2008,2010–2012,2015 Sven Michael Klose <pixel@copei.de>
 
 (defmacro dotimes ((iter times &rest result) &body body)
   (let g (gensym)
     `(let ,g (integer ,times)
+       (? (integer< ,g 0)
+          (error "Negative number of iterations: ~A." ,g))
        (do ((,iter 0 (integer+ 1 ,iter)))
 	       ((integer== ,iter ,g) ,@result)
 	     ,@body))))
