72c08a244486ae652ba56a80ef31ccf053a2084f
Sven Michael Klose <pixel@copei.de>
2015-03-22 15:30:23 +0100

    FUNCTION-SOURCE: *FUNCTIONS* is an associative list.
    =-FUNCTION-SOURCE: Doesn't work in this target.

--- environment/transpiler/targets/common-lisp/core/function.lisp
+++ environment/transpiler/targets/common-lisp/core/function.lisp
@@ -5,11 +5,10 @@
 (defbuiltin function-native (x) x)
 
 (defbuiltin function-source (x)
-  (| (cl:functionp x)
-     (cl:error "Not a function."))
-  (cl:gethash x *functions*))
+  (cdr (cl:assoc x *functions* :test #'cl:eq)))
 
 (defbuiltin =-function-source (v x)
-  (cl:setf (cl:gethash x *functions*) v))
+  (error "Can't set function source in the Common Lisp core."))
+;(cl:setf (cdr (cl:assoc x *functions* :test #'cl:eq)) v))
 
 (defbuiltin function-bytecode (x) x nil)
