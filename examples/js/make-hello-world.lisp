5ac2f83256db1d107c727c03e6a8c3123aa09065
Sven Michael Klose <pixel@copei.de>
2015-03-24 22:39:50 +0100

    Minor cleanup.

--- examples/js/make-hello-world.lisp
+++ examples/js/make-hello-world.lisp
@@ -1,6 +1,5 @@
-(with-output-file out "examples/js/hello-world.js"
-  (make-project "Hello World"
-                '("examples/js/hello-world.lisp")
-                :transpiler  *js-transpiler*
-                :emitter     [princ _ out]))
+(make-project "Hello World"
+              '("examples/js/hello-world.lisp")
+              :transpiler  *js-transpiler*
+              :emitter     [make-html-script "examples/js/hello-world.html" _])
 (quit)
