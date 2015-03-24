6f4d84d03d69945f2991ef700a22269de1bdf48c
Sven Michael Klose <pixel@copei.de>
2015-03-24 20:32:54 +0100

    IDENTITY removed. It's already in 'environment/stage0/primitive-lib.lisp'.

--- environment/transpiler/environment/atom.lisp
+++ environment/transpiler/environment/atom.lisp
@@ -1,8 +1,6 @@
-;;;;; tré – Copyright (c) 2008–2009,2011,2013 Sven Michael Klose <pixel@copei.de>
+; tré – Copyright (c) 2008–2009,2011,2013,2015 Sven Michael Klose <pixel@copei.de>
 
 (declare-cps-exception atom identity)
 
 (defun atom (x)
   (not (cons? x)))
-
-(defun identity (x) x)
