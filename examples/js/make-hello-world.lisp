6b258debab9f23204ceb4ceddbb9cc4439814d54
Sven Michael Klose <pixel@copei.de>
2015-03-24 22:43:32 +0100

    Let's have a bunch of example makefiles first.

--- examples/js/make-hello-world.lisp
+++
@@ -1,5 +0,0 @@
-(make-project "Hello World"
-              '("examples/js/hello-world.lisp")
-              :transpiler  *js-transpiler*
-              :emitter     [make-html-script "examples/js/hello-world.html" _])
-(quit)
---
+++ examples/make-hello-world.lisp
@@ -0,0 +1,5 @@
+(make-project "Hello World"
+              '("examples/hello-world.lisp")
+              :transpiler  *js-transpiler*
+              :emitter     [make-html-script "compiled/hello-world.html" _])
+(quit)
