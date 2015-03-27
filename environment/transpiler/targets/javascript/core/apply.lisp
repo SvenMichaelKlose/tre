b0ce76af194d7f4cac398c7b99388405e47a392e
Sven Michael Klose <pixel@copei.de>
2015-03-27 15:22:56 +0100

    Use NCONC instead of %NCONC.
    Don't call LAST more than once.

--- environment/transpiler/targets/javascript/core/apply.lisp
+++ environment/transpiler/targets/javascript/core/apply.lisp
@@ -4,21 +4,24 @@
 
 ,(& (enabled-pass? :cps)
     '(progn
-       (declare-cps-exception apply methodapply %nconc last butlast)
+       (declare-cps-exception apply methodapply nconc last butlast)
        (declare-native-cps-function apply methodapply)))
 
 (defun apply (fun &rest lst)
-  (assert (function? fun) "First argument ~A is not a function." fun)
-  (assert (list? (car (last lst))) "Last argument ~A is not a list." (last lst))
-  (alet (%nconc (butlast lst) (car (last lst)))
-    ,(? (enabled-pass? :cps)
-        '(?
-           (defined? fun.tre-exp)            (fun.tre-exp.apply nil (%%native "[" ~%cont ", " ! "]"))
-           (defined? fun._cps-transformed?)  (fun.apply nil (list-array (. ~%cont !)))
-           (~%cont.call nil (fun.apply nil (list-array !))))
-        '(? (defined? fun.tre-exp)
-            (fun.tre-exp.apply nil (%%native "[" ! "]"))
-            (fun.apply nil (list-array !))))))
+  (let l (last lst)
+    (assert (function? fun) "First argument ~A is not a function." fun)
+    (assert (list? l.) "Last argument is not a list. Got ~A." l)
+    (alet (nconc (butlast lst) l.)
+      ,(? (enabled-pass? :cps)
+          '(?
+             (defined? fun.tre-exp)
+               (fun.tre-exp.apply nil (%%native "[" ~%cont ", " ! "]"))
+             (defined? fun._cps-transformed?)
+               (fun.apply nil (list-array (. ~%cont !)))
+             (~%cont.call nil (fun.apply nil (list-array !))))
+          '(? (defined? fun.tre-exp)
+              (fun.tre-exp.apply nil (%%native "[" ! "]"))
+              (fun.apply nil (list-array !)))))))
 
 (defun methodapply (obj fun &rest lst)
   ,(? (enabled-pass? :cps)
