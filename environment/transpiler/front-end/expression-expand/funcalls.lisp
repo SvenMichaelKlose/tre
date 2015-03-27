837e3c20117ecb6928bd2b9a3a9fa72f9646e7cc
Sven Michael Klose <pixel@copei.de>
2015-03-27 03:05:17 +0100

    Fixed call to FRONTEND.

--- environment/transpiler/front-end/expression-expand/funcalls.lisp
+++ environment/transpiler/front-end/expression-expand/funcalls.lisp
@@ -1,8 +1,8 @@
-;;;;; tré – Copyright (c) 2009–2013 Sven Michael Klose <pixel@copei.de>
+; tré – Copyright (c) 2009–2013,2015 Sven Michael Klose <pixel@copei.de>
 
 (defun %=-make-call-to-local-function (x)
   (with-%= place value x
-    (expex-body (frontend-1 `((%= ,place (apply ,value. ,(compiled-list .value))))))))
+    (expex-body (apply #'+ (frontend `((%= ,place (apply ,value. ,(compiled-list .value)))))))))
 
 (defun expex-compiled-funcall (x)
   (alet (%=-value x)
