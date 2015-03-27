ffc637ca8d82cc7e3ce0d4b43003fd75c9921869
Sven Michael Klose <pixel@copei.de>
2015-03-27 03:03:25 +0100

    Don't care about obfuscation for the duration.

--- environment/transpiler/targets/php/core/late-symbol.lisp
+++ environment/transpiler/targets/php/core/late-symbol.lisp
@@ -1,4 +1,4 @@
-;;;;; tré – Copyright (c) 2008–2013 Sven Michael Klose <pixel@copei.de>
+; tré – Copyright (c) 2008–2013,2015 Sven Michael Klose <pixel@copei.de>
 
 (defun make-symbol (x &optional (pkg nil))
   (symbol x pkg))
@@ -8,28 +8,28 @@
 
 (defun symbol-name (x)
   (?
-    (eq t x) ,*t-symbol-name*
-    x (? (symbol? x)
-         x.n
-         (progn
-           (print x)
-           (error "Symbol expected.")))
-    ,*nil-symbol-name*))
+    (eq t x)  "T"
+    x         (? (symbol? x)
+                 x.n
+                 (progn
+                   (print x)
+                   (error "Symbol expected.")))
+    "NIL"))
 
 (defun symbol-value (x)
   (?
-    (eq t x) t
+    (eq t x)  t
     (x.v)))
 
 (defun symbol-function (x)
   (?
-    (eq t x) nil
-    x (x.f)))
+    (eq t x)  nil
+    x         (x.f)))
 
 (defun symbol-package (x)
   (?
-    (not x) nil
-    (eq t x) nil
+    (not x)   nil
+    (eq t x)  nil
     x.p))
 
 (dont-obfuscate is_a)
