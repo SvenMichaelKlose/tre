31eb4817bf98aea3994ab24029824e9311b303e4
Sven Michael Klose <pixel@copei.de>
2015-03-30 02:48:21 +0200

    Minor cleanup.

--- environment/transpiler/front-end/define-compiled-literal.lisp
+++ environment/transpiler/front-end/define-compiled-literal.lisp
@@ -1,15 +1,13 @@
-; tré – Copyright (c) 2008–2014 Sven Michael Klose <pixel@copei.de>
+; tré – Copyright (c) 2008–2015 Sven Michael Klose <pixel@copei.de>
 
 (defmacro define-compiled-literal (name (x table) &key maker init-maker decl-maker)
-  "Define a collector of declarations and initializations for literals of a particular data type."
   (let slot `(,($ 'compiled- table 's))
     `(defun ,name (,x)
-       (| (href ,slot ,x)
-          (let n ,maker
-            (unless (funinfo-var? (global-funinfo) n)
-              (add-literal n)
-              (funinfo-var-add (global-funinfo) n))
-            ,@(& decl-maker
-                 `((push (funcall ,decl-maker n) (compiled-decls))))
-            (push `(= ,,n ,(list 'quasiquote init-maker)) (compiled-inits))
-            (= (href ,slot ,x) n))))))
+       (cache (aprog1 ,maker
+                (unless (funinfo-var? (global-funinfo) !)
+                  (add-literal !)
+                  (funinfo-var-add (global-funinfo) !))
+                ,@(& decl-maker
+                     `((push (funcall ,decl-maker !) (compiled-decls))))
+                (push `(= ,,! ,(list 'quasiquote init-maker)) (compiled-inits)))
+              (href ,slot ,x)))))
