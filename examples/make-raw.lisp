62cfbadf2e8c8937f5b76cf38a167718b901a8f4
Sven Michael Klose <pixel@copei.de>
2015-03-30 18:41:34 +0200

    An example for those who want to hack the compiler.

---
+++ examples/make-raw.lisp
@@ -0,0 +1,8 @@
+(make-project "Hello World compiler dumps with no core or imports"
+              '("examples/hello-world.lisp")
+              :transpiler  (aprog1 (copy-transpiler *js-transpiler*)
+                             (= (transpiler-dump-passes? !) t)
+                             (= (transpiler-import-from-host? !) nil)
+                             (= (transpiler-configuration ! :exclude-core?) t))
+              :emitter     [put-file "compiled/hello-world-coreless.js" _])
+(quit)
