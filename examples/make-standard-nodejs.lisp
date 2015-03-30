ff52e48afa9a208b0b5c573642198fe92200a85a
Sven Michael Klose <pixel@copei.de>
2015-03-30 14:16:25 +0200

    Example on how to add modules.

--- examples/make-standard-nodejs.lisp
+++ examples/make-standard-nodejs.lisp
@@ -1,6 +1,9 @@
 (make-project "Hello World for node.js"
               '("examples/hello-world.lisp")
               :transpiler  (aprog1 *js-transpiler*
-                             (= (transpiler-configuration ! :platform) :nodejs))
+                             (= (transpiler-configuration ! :platform) :nodejs)
+                             ; Gives 'var fs = require ("fs");':
+                             (= (transpiler-configuration ! :nodejs-requirements) '("fs")))
+                                                               '("fs" 
               :emitter     [put-file "compiled/hello-world.js" _])
 (quit)
