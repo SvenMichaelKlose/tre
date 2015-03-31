3ddd5b49631fa33b4dc560594f242582687fa5a2
Sven Michael Klose <pixel@copei.de>
2015-03-31 19:11:45 +0200

    QUARTER added.

--- environment/math/half.lisp
+++
@@ -1,4 +0,0 @@
-;;;;; tré - Copyright (c) 2011 Sven Michael Klose <pixel@copei.de>
-
-(defun half (x)
-  (/ x 2))
--- environment/math/main.lisp
+++ environment/math/main.lisp
@@ -1,8 +1,8 @@
-;;;; tré – Copyright (c) 2008,2011,2013–2014 Sven Michael Klose <pixel@copei.de>
+; tré – Copyright (c) 2008,2011,2013–2015 Sven Michael Klose <pixel@copei.de>
 
 (env-load "math/pi.lisp" :c)
 (env-load "math/precision.lisp")
-(env-load "math/half.lisp")
+(env-load "math/fractions.lisp")
 (env-load "math/min.lisp")
 (env-load "math/percent.lisp")
 (env-load "math/saturate.lisp")
