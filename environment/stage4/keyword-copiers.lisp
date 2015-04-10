cfea8d883d31ab3eb53796036f9d8ed23add2c97
Sven Michael Klose <pixel@copei.de>
2015-04-10 04:17:50 +0200

    Now wants keywords as arguments.

--- environment/stage4/keyword-copiers.lisp
+++ environment/stage4/keyword-copiers.lisp
@@ -1,4 +1,4 @@
-;;;;; tré – Copyright (c) 2013 Sven Michael Klose <pixel@copei.de>
+; tré – Copyright (c) 2013,2015 Sven Michael Klose <pixel@copei.de>
 
 (defun keyword-copiers (&rest x)
-  (mapcan [list (make-keyword _) _] x))
+  (mapcan [list (make-keyword _) (make-symbol (symbol-name _))] x))
