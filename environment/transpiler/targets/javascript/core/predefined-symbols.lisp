39fcbaae79159f2d89597a9fe355dde88cf55825
Sven Michael Klose <pixel@copei.de>
2015-09-13 18:14:47 +0200

    *VARIABLES* and *TESTS* added.

--- environment/transpiler/targets/javascript/core/predefined-symbols.lisp
+++ environment/transpiler/targets/javascript/core/predefined-symbols.lisp
@@ -1,4 +1,6 @@
-;;;;; tré – Copyright (c) 2013–2014 Sven Michael Klose <pixel@copei.de>
+; tré – Copyright (c) 2013–2015 Sven Michael Klose <pixel@copei.de>
 
 (defvar-native true false window document)
 (defvar *application-funinfos* nil)
+(defvar *variables* nil)
+(defvar *tests* nil)
