4986482ab6a2bc034878b48c6a576d70656f7405
Sven Michael Klose <pixel@copei.de>
2015-09-13 15:47:48 +0200

    Don't include *DEFUNCT-TARGETS*.

--- environment/stage0/config-defaults.lisp
+++ environment/stage0/config-defaults.lisp
@@ -1,6 +1,6 @@
-; tré – Copyright (c) 2005–2014 Sven Michael Klose <pixel@copei.de>
+; tré – Copyright (c) 2005–2015 Sven Michael Klose <pixel@copei.de>
 
-(%defvar *development?*       nil)
+(%defvar *development?*       t)
 (%defvar *assert*             t)
 (%defvar *print-definitions?* t)
 
@@ -10,9 +10,17 @@
 (%defvar *tre-has-alien* t) ; C function interface. (C core only)
 (%defvar *tre-has-class* t) ; STRUCT CLASS.
 
-;; transpiler
+;; Transpiler
 
-(%defvar *targets*                 '(:cl :js :php :bc :c))
+;;; Targets to include in environment:
+;;; :cl     Common Lisp (sbcl)
+;;; :js     JavaScript/ECMAScript (browser + node.js)
+;;; :php    PHP
+;;; :bc     bytecode (defunct)
+;;; :c      C (defunct)
+(%defvar *targets*                 '(:cl :js :php))
+(%defvar *defunct-targets*         '(:c :bc))
+
 (%defvar *print-notes?*            t)
 (%defvar *print-status?*           t)
 (%defvar *have-environment-tests*  nil)
