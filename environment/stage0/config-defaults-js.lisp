5d308f1b918e5e1b30b6956693fa2de5c89e9dfa
Sven Michael Klose <pixel@copei.de>
2015-09-14 16:05:26 +0200

    Have different configurations for CL and JS environment.

---
+++ environment/stage0/config-defaults-js.lisp
@@ -0,0 +1,28 @@
+; tré – Copyright (c) 2005–2015 Sven Michael Klose <pixel@copei.de>
+
+(%defvar *development?*       t)
+(%defvar *assert*             t)
+(%defvar *print-definitions?* t)
+
+;; Optional environment sections.
+
+(%defvar *tre-has-math*  t) ; Mathematical functions.
+(%defvar *tre-has-alien* t) ; C function interface. (C core only)
+(%defvar *tre-has-class* t) ; STRUCT CLASS.
+
+;; Transpiler
+
+;;; Targets to include in environment:
+;;; :cl     Common Lisp (sbcl)
+;;; :js     JavaScript/ECMAScript (browser + node.js)
+;;; :php    PHP
+;;; :bc     bytecode (defunct)
+;;; :c      C (defunct)
+(%defvar *targets*                 '(:js :php))
+(%defvar *defunct-targets*         '(:c :bc))
+
+(%defvar *print-notes?*            t)
+(%defvar *print-status?*           t)
+(%defvar *have-environment-tests*  nil)
+(%defvar *have-compiler?*          nil)
+(%defvar *have-c-compiler?*        t)
