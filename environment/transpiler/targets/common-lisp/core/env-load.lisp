7f733a5ef98d62513339b07cbb4d539806c28d62
Sven Michael Klose <pixel@copei.de>
2015-09-25 18:08:44 +0200

    PRINT-DEFINITION.

--- environment/transpiler/targets/common-lisp/core/env-load.lisp
+++ environment/transpiler/targets/common-lisp/core/env-load.lisp
@@ -4,5 +4,6 @@
 (defvar *environment-filenames* nil)
 
 (defbuiltin env-load (pathname &optional (target nil))
+  (print-definition `(env-load ,pathname ,target))
   (setq *environment-filenames* (. (. pathname target) *environment-filenames*))
   (load (+ *environment-path* "/environment/" pathname)))
