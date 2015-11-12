e76f2a49e4753e250b72cdbc09a71083211f3d3a
Sven Michael Klose <pixel@copei.de>
2015-11-12 10:17:45 +0100

    Avoid overflows.

--- environment/stage5/byte.lisp
+++ environment/stage5/byte.lisp
@@ -1,6 +1,7 @@
 ; tré – Copyright (c) 2015 Sven Michael Klose <pixel@copei.de>
 
 (defun byte (x)
-  (? (< x 0)
-     (+ 256 x)
-     x))
+  (bit-and (? (< x 0)
+              (+ 256 x)
+              x)
+           #xff))
