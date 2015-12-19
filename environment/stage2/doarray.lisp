eef850fb437db8f9cb3b5c0c66b2bf22c91908f1
Sven Michael Klose <pixel@copei.de>
2015-12-19 13:03:44 +0100

    Minor cleanup.

--- environment/stage2/doarray.lisp
+++ environment/stage2/doarray.lisp
@@ -1,9 +1,9 @@
-;;;;; tré – Copyright (c) 2011–2012,2014 Sven Michael Klose <pixel@copei.de>
+; tré – Copyright (c) 2011–2012,2014 Sven Michael Klose <pixel@copei.de>
 
 (defmacro doarray ((var seq &rest result) &body body)
   (with-gensym (evald-seq idx)
     `(let ,evald-seq ,seq
        (when ,evald-seq
-         (dotimes (,idx (length ,evald-seq) ,@result)                                                                               
+         (dotimes (,idx (length ,evald-seq) ,@result)
            (let ,var (aref ,evald-seq ,idx)
              ,@body))))))
