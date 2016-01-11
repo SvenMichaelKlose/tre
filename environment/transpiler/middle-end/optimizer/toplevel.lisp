e10cb8fef9051cd1ad0526a57b9e0d2a2998dff6
Sven Michael Klose <pixel@copei.de>
2016-01-11 03:56:29 +0100

    Dump each optimizer pass.

--- environment/transpiler/middle-end/optimizer/toplevel.lisp
+++ environment/transpiler/middle-end/optimizer/toplevel.lisp
@@ -1,10 +1,13 @@
 ; tré – Copyright (c) 2008–2014 Sven Michael Klose <pixel@copei.de>
 
+(defmacro optimizer-pass (x)
+  `[dump-pass 'middleend ',x (,x _)])
+
 (defun optimizer-passes ()
-  (compose #'optimize-jumps
-           #'optimize-places
-           #'opt-peephole
-           #'optimize-tags))
+  (compose (optimizer-pass optimize-jumps)
+           (optimizer-pass optimize-places)
+           (optimizer-pass opt-peephole)
+           (optimizer-pass optimize-tags)))
 
 (defun optimize (statements)
   (? *opt-peephole?*
