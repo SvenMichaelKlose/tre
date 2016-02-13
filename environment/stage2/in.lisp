b5d38e404d12f206131de4cfc58bca74179ceb91
Sven Michael Klose <pixel@copei.de>
2016-02-13 13:19:13 +0100

    IN-CHARS?: New function.

--- environment/stage2/in.lisp
+++ environment/stage2/in.lisp
@@ -1,7 +1,10 @@
-; tré – Copyright (c) 2006–2008,2012,2015 Sven Michael Klose <pixel@copei.de>
+; tré – Copyright (c) 2006–2008,2012,2015–2016 Sven Michael Klose <pixel@hugbox.org>
 
 (defmacro in? (obj &rest lst)
   `(| ,@(@ [`(eq ,obj ,_)] lst)))
 
 (defmacro in=? (obj &rest lst)
   `(| ,@(@ [`(== ,obj ,_)] lst)))
+
+(defmacro in-chars? (obj &rest lst)
+  `(| ,@(@ [`(character== ,obj ,_)] lst)))
