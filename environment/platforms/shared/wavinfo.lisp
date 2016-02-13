828c345c677eef4646a8721f960001fb907edb13
Sven Michael Klose <pixel@copei.de>
2016-02-13 09:47:21 +0100

    WRITE-WAVINFO: New function.

--- environment/platforms/shared/wavinfo.lisp
+++ environment/platforms/shared/wavinfo.lisp
@@ -4,11 +4,29 @@
   file-size
   format-tag
   channels
-  sample-rate
+  rate
   bytes-second
-  block-align
+  (block-align 1)
   bits
   data-size)
+
+(def-wavinfo write-wavinfo (wavinfo size o)
+  (princ "RIFF" o)
+  (alet (with-string-stream o
+          (alet (with-string-stream o
+                  (write-word format-tag o)
+                  (write-word channels o)
+                  (write-dword rate o)
+                  (write-dword (/ (* rate bits) 8) o)
+                  (write-word block-align o)
+                  (write-word bits o))
+            (princ "WAVEfmt " o)
+            (write-dword (length !) o)
+            (princ ! o)))
+    (write-dword (+ 16 (length !) size) o)
+    (princ ! o))
+  (princ "data" o)
+  (write-dword size o))
 
 (defun read-wavinfo (i)
   (aprog1 (make-wavinfo)
@@ -20,7 +38,7 @@
     (read-dword i) ; Rest of header size.
     (= (wavinfo-format-tag !) (read-word i))
     (= (wavinfo-channels !) (read-word i))
-    (= (wavinfo-sample-rate !) (read-dword i))
+    (= (wavinfo-rate !) (read-dword i))
     (= (wavinfo-bytes-second !) (read-dword i))
     (= (wavinfo-block-align !) (read-word i))
     (= (wavinfo-bits !) (read-word i))
