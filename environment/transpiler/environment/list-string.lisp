bc824170f8e75e5fab07ca45e43bb26f6f119f87
Sven Michael Klose <pixel@copei.de>
2016-02-13 08:05:32 +0100

    LIST-STRING: Process in groups to work around argument list length limits.

--- environment/transpiler/environment/list-string.lisp
+++ environment/transpiler/environment/list-string.lisp
@@ -1,4 +1,7 @@
-; tré – Copyright (c) 2005–2009,2011,2015 Sven Michael Klose <pixel@copei.de>
+; tré – Copyright (c) 2005–2009,2011,2015–2016 Sven Michael Klose <pixel@hugbox.org>
 
-(defun list-string (lst)
-  (apply #'string-concat (@ #'string lst)))
+(defun list-string-0 (x)
+  (apply #'string-concat (@ #'string x)))
+
+(defun list-string (x)
+  (apply #'string-concat (@ #'list-string-0 (group x 16384))))
