6e3fa717d2b325597dcf62cd57636a8ab9ff4b85
Sven Michael Klose <pixel@copei.de>
2016-07-13 01:03:50 +0200

    Try to import all symbols as both functions or variables.

--- environment/transpiler/front-end/gather-imports.lisp
+++ environment/transpiler/front-end/gather-imports.lisp
@@ -1,12 +1,8 @@
-; tré – Copyright (c) 2015 Sven Michael Klose <pixel@hugbox.org>
+; tré – Copyright (c) 2015–2016 Sven Michael Klose <pixel@hugbox.org>
 
 (metacode-walker gather-imports (x)
   :if-setq (with-%= place value x.
-             (? (atom value)
-                (add-wanted-variable value))
-             (when (cons? value)
-               (add-wanted-function value.)
-               (adolist (.value)
+             (adolist ((ensure-list value))
                  (add-wanted-function !)
-                 (add-wanted-variable !)))
+                 (add-wanted-variable !))
              (list x.)))
