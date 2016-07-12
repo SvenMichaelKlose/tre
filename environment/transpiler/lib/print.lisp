0397ea08083d321c0eaa37fd8af2efb1c3be4d03
Sven Michael Klose <pixel@copei.de>
2016-07-13 01:03:04 +0200

    Don't use FRESH-LINE temporarily.

--- environment/transpiler/lib/print.lisp
+++ environment/transpiler/lib/print.lisp
@@ -1,7 +1,7 @@
 ; tré – Copyright (c) 2014 Sven Michael Klose <pixel@hugbox.org>
 
 (defun %print-note (fmt &rest args)
-  (fresh-line)
+;  (fresh-line) ; No tracking?
   (princ "; ")
   (apply #'format t fmt args))
 
