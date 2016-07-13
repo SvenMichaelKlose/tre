d0bd9928665d5e592c8fef868c4c72e2a7b41c4a
Sven Michael Klose <pixel@copei.de>
2016-07-13 04:47:26 +0200

    EQL: Fixed for numbers and characters. Faster through by trying EQ first.

--- environment/transpiler/targets/common-lisp/core/object.lisp
+++ environment/transpiler/targets/common-lisp/core/object.lisp
@@ -1,4 +1,4 @@
-; tré – Copyright (c) 2014–2015 Sven Michael Klose <pixel@hugbox.org>
+; tré – Copyright (c) 2014–2016 Sven Michael Klose <pixel@hugbox.org>
 
 (defbuiltin not (&rest x) (cl:every #'cl:not x))
 
@@ -10,17 +10,20 @@
      (cl:error "At least 2 arguments required.")))
 
 (defun tre-eql (a b)
-  (?
-    (& (number? a)
-       (number? b))     (& (cl:eq (cl:characterp a)
-                                  (cl:characterp b))
-                           (== a b))
-    (& (cl:consp a)
-       (cl:consp b))    (& (tre-eql a. b.)
-                           (tre-eql .a .b))
-    (& (cl:stringp a)
-       (cl:stringp b))  (cl:string= a b)
-    (cl:eq a b)))
+  (| (cl:eq a b)
+     (?
+       (& (cl:characterp a)
+          (cl:characterp b))   (cl:= (cl:char-code a)
+                                     (cl:char-code b))
+       (& (not (cl:characterp a)
+               (cl:characterp b))
+          (number? a)
+          (number? b))         (cl:= a b)
+       (& (cl:consp a)
+          (cl:consp b))        (& (tre-eql a. b.)
+                                  (tre-eql .a .b))
+       (& (cl:stringp a)
+          (cl:stringp b))      (cl:string= a b))))
 
 (defbuiltin eq (&rest x)  (variable-compare #'cl:eq x))
 (defbuiltin eql (&rest x) (variable-compare #'tre-eql x))
