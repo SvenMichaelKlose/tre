95f508b5a1b1d605ecbef63fe1360cbf2db03d3a
Sven Michael Klose <pixel@copei.de>
2016-07-15 12:21:36 +0200

    Some hopefully temporary macro generation utils added. Needs a new identifier scheme.

--- environment/stage4/keyword-copiers.lisp
+++ environment/stage4/keyword-copiers.lisp
@@ -1,4 +1,10 @@
-; tré – Copyright (c) 2013,2015 Sven Michael Klose <pixel@copei.de>
+; tré – Copyright (c) 2013,2015–2016 Sven Michael Klose <pixel@copei.de>
 
 (defun keyword-copiers (&rest x)
   (mapcan [list (make-keyword _) (make-symbol (symbol-name _))] x))
+
+(defun keyword-argument-declarations (x)
+  (& x `(&key ,@(@ [`(,_ nil)] x))))
+
+(defun gen-vars-to-alist (x)
+  (@ [`(. ,(make-keyword _), _)] x))
