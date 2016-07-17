f1125de164de43ea684388a74febabbe5420fefb
Sven Michael Klose <pixel@copei.de>
2016-07-17 15:09:02 +0200

    Removed debug code.

--- environment/platforms/js/dom/iframe/make-with-url.lisp
+++ environment/platforms/js/dom/iframe/make-with-url.lisp
@@ -3,10 +3,8 @@
 (dont-obfuscate onload)
 
 (defun make-iframe-with-url (continuer url html-document &key (ns nil))
-  (when-debug (format t "MAKE-IFRAME-WITH-URL ~A.~%" url))
   (let iframe (make-iframe html-document :ns ns)
     (= iframe.onload #'(()
-                         (when-debug (format t "MAKE-IFRAME-WITH-URL iframe loaded.~%"))
                          (clr iframe.onload)
                          (iframe-extend iframe)
                          (funcall continuer iframe)))
