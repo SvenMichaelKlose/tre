461439d24c0ef2973dfc7eff25899b45847cdf1c
Sven Michael Klose <pixel@copei.de>
2016-08-12 13:46:06 +0200

    New CAROSHI-HTML-DOCUMENT.

---
+++ environment/platforms/js/dom/objects/document.lisp
@@ -0,0 +1,23 @@
+; tré – Copyright (c) 2016 Sven Michael Klose <pixel@copei.de>
+
+(defclass caroshi-html-document ()
+  this)
+
+(defmember caroshi-html-document
+    document-element
+    query-selector
+    query-selector-all)
+
+(defmethod caroshi-html-document get (css-selector)
+  (query-selector css-selector))
+
+(defmethod caroshi-html-document get-list (css-selector)
+  (array-list (query-selector-all css-selector)))
+
+(defmethod caroshi-html-document get-last (css-selector)
+  (last (get-list css-selector)))
+
+(defmethod caroshi-html-document get-nodes (css-selector)
+  (new nodelist (get-list css-selector)))
+
+(finalize-class caroshi-html-document)
