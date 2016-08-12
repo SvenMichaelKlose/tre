180723a1fa90919d2d0a4eb700863777852d47d2
Sven Michael Klose <pixel@copei.de>
2016-08-12 13:44:54 +0200

    CAROSHI-HTML-DOCUMENT: New class extends regular HTML documents.

--- environment/platforms/js/dom/objects/extend.lisp
+++ environment/platforms/js/dom/objects/extend.lisp
@@ -1,18 +1,19 @@
-;;;;; tré – Copyright (c) 2008,2009,2011–2013 Sven Michael Klose <pixel@copei.de>
+; tré – Copyright (c) 2008,2009,2011–2013,2016 Sven Michael Klose <pixel@copei.de>
 
 (defun dom-extend (x)
-  (?
-    (not x)       nil
-    (element? x)  (hash-merge x caroshi-element.prototype)
-    (text? x)     (& *extended-textnodes?*
-                     (hash-merge x *text-node.prototype))))
+  (pcase x
+    document? (hash-merge x caroshi-html-document.prototype)
+    element?  (hash-merge x caroshi-element.prototype)
+    text?     (& *extended-textnodes?*  ; TODO Check if this makes sense. (was text events in safari).
+                 (hash-merge x *text-node.prototype))))
 
 (defun dom-tree-extend (root)
-  (when root
-    ((dom-extend root).walk #'dom-extend)))
+  (!? root
+      ((dom-extend !).walk #'dom-extend)))
 
 (dont-obfuscate document document-element)
 
 (defun document-extend (&optional (doc document))
+  (dom-extend doc)
   (dom-tree-extend doc.document-element)
   doc)
--- environment/platforms/js/dom/viewport.lisp
+++ environment/platforms/js/dom/viewport.lisp
@@ -1,4 +1,4 @@
-;;;;; tré – Copyright (c) 2009–2010,2012 Sven Michael Klose <pixel@copei.de>
+; tré – Copyright (c) 2009–2010,2012,2016 Sven Michael Klose <pixel@copei.de>
 
 (dont-obfuscate
   	document
@@ -14,49 +14,52 @@
 	client-width
 	client-height)
 
+; TODO: Make HTML5 style.
 (defun get-viewport (&optional (win window))
-  (with (x 0
-		 y 0
-		 w (| win.inner-width
-			  win.document.document-element.client-width
-			  win.document.body.client-width)
-		 h (| win.inner-height
-			  win.document.document-element.client-height
-			  win.document.body.client-height))
+  (with (x  0
+		 y  0
+         wd win.document
+		 w  (| win.inner-width
+			   wd.document-element.client-width
+			   wd.body.client-width)
+		 h  (| win.inner-height
+			   wd.document-element.client-height
+			   wd.body.client-height))
     (?
-  	  win.document.document-element.scroll-left
-        (= x win.document.document-element.scroll-left
-	       y win.document.document-element.scroll-top)
-	  win.document.body.scroll-left
+  	  wd.document-element.scroll-left
+        (= x wd.document-element.scroll-left
+	       y wd.document-element.scroll-top)
+	  wd.body.scroll-left
         (= x document.body.scroll-left
 	       y document.body.scroll-top)
 	  win.page-x-offset
-        (= win.page-x-offset
-		   win.page-y-offset))
+        (= x win.page-x-offset
+		   y win.page-y-offset))
 	(values x y w h)))
 
 (defun set-viewport (x y &optional (win window))
-  (?
-	win.document.document-element.scroll-left
-      (= win.document.document-element.scroll-left x
-	     win.document.document-element.scroll-top y)
-    win.document.body.scroll-left
-      (= document.body.scroll-left x
-	     document.body.scroll-top y)
-	win.page-x-offset
-      (= win.page-x-offset x
-		 win.page-y-offset y)))
+  (let wd win.document
+    (?
+	  wd.document-element.scroll-left
+        (= wd.document-element.scroll-left x
+	       wd.document-element.scroll-top y)
+      wd.body.scroll-left
+        (= document.body.scroll-left x
+	       document.body.scroll-top y)
+	    win.page-x-offset
+        (= win.page-x-offset x
+		   win.page-y-offset y))))
 
 (dont-obfuscate scroll-into-view)
 
 (defun adjust-viewport (elm)
   (with ((x y w h) (get-viewport)
-         xr (+ x w)
-         yb (+ y h)
-         cx (elm.get-position-x)
-         cy (elm.get-position-y)
-         cxr (+ cx (elm.get-width))
-         cyb (+ cy (elm.get-height)))
+         xr        (+ x w)
+         yb        (+ y h)
+         cx        (elm.get-position-x)
+         cy        (elm.get-position-y)
+         cxr       (+ cx (elm.get-width))
+         cyb       (+ cy (elm.get-height)))
     (when (| (< (- cx 8) x)
              (< (- cy 8) y)
              (< xr (+ cxr 8))
