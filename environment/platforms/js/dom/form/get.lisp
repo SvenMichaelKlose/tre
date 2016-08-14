0b3ff48316fbf200d733f21a2e23fd800c2dd86d
Sven Michael Klose <pixel@copei.de>
2016-08-14 23:41:29 +0200

    fixed element lookup.

--- environment/platforms/js/dom/form/get.lisp
+++ environment/platforms/js/dom/form/get.lisp
@@ -1,4 +1,4 @@
-; tré – Copyright (c) 2009–2010,2012–2015 Sven Michael Klose <pixel@copei.de>
+; tré – Copyright (c) 2009–2010,2012–2016 Sven Michael Klose <pixel@copei.de>
 
 (defun form-action-get (x)
   ((x.ancestor-or-self "form").read-attribute "action"))
@@ -8,9 +8,9 @@
      (x.has-tag-name? '("input" "textarea" "select"))))
 
 (defun form-get-input-elements (x)
-  (+ (remove-if-not #'form-input-element? (form-get-input-elements x))
-     (get-textarea-elements x)
-     (get-select-elements x)))
+  (+ (remove-if-not #'form-input-element? (x.get-list "input"))
+     (x.get-list "textarea")
+     (x.get-list "select")))
 
 (defun form-get-submit-buttons (x)
   (remove-if-not #'submit-button? (form-get-input-elements x)))
