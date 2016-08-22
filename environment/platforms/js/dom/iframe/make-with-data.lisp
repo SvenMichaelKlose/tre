0224eb461756b88b987c13f14c55d9354bba8506
Sven Michael Klose <pixel@copei.de>
2016-08-22 14:08:43 +0200

    DATA-URL takes more keyword arguments.

--- environment/platforms/js/dom/iframe/make-with-data.lisp
+++ environment/platforms/js/dom/iframe/make-with-data.lisp
@@ -1,4 +1,7 @@
-;;;;; tré – Copyright (c) 2010–2011,2013 Sven Michael Klose <pixel@copei.de>
+; tré – Copyright (c) 2010–2011,2013,2016 Sven Michael Klose <pixel@copei.de>
 
 (defun make-iframe-with-data (continuer data html-document &key (ns nil))
-  (make-iframe-with-url continuer (data-url data "text" "html") html-document :ns ns))
+  (make-iframe-with-url continuer (data-url data 
+                                            :typ "text"
+                                            :fmt "html")
+                        html-document :ns ns))
--- environment/platforms/shared/url/data-url.lisp
+++ environment/platforms/shared/url/data-url.lisp
@@ -1,6 +1,6 @@
-;;;;; tré – Copyright (c) 2011–2012 Sven Michael Klose <pixel@copei.de>
+; tré – Copyright (c) 2011–2012,2016 Sven Michael Klose <pixel@copei.de>
 
-(defun data-url (x typ fmt &key (encoding nil))
+(defun data-url (x &key typ fmt (encoding nil))
   (+ "data:" typ "/" fmt
      (!? encoding
          (+ ";" encoding)
