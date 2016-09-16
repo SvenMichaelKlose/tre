087b21f1faa7a3f354696aac9481f775d9c1e29c
Sven Michael Klose <pixel@copei.de>
2016-09-16 09:51:56 +0200

    Don't import READ when no sources have been saved.

--- environment/transpiler/targets/javascript/core/function-source.lisp
+++ environment/transpiler/targets/javascript/core/function-source.lisp
@@ -1,23 +1,29 @@
-; tré - Copyright (c) 2011–2013,2015 Sven Michael Klose <pixel@copei.de>
+; tré - Copyright (c) 2011–2013,2016 Sven Michael Klose <pixel@copei.de>
 
 (defun function|symbol-function (x)
   (? (symbol? x)
      (symbol-function x)
      x))
 
-(defun function-arguments (x)
-  (!? (function|symbol-function x)
-      (!? !.__source
-          (with-stream-string s !.
-            (read s))
-          '(&rest unknown-args))
-      '(&rest unknown-args)))
+,(? (| (configuration :save-sources?)
+       (configuration :save-argument-defs-only?))
+    '(defun function-arguments (x)
+       (!? (function|symbol-function x)
+           (!? !.__source
+               (with-stream-string s !.
+                 (read s))
+               '(&rest unknown-args))
+           '(&rest unknown-args)))
+    '(defun function-arguments (x)
+       '(&rest unknown-args)))
 
-(defun function-body (x)
-  (alet (function|symbol-function x)
-    (!? !.__source
-        (with-stream-string s .!
-          (read s)))))
+,(? (configuration :save-sources?)
+    '(defun function-body (x)
+       (alet (function|symbol-function x)
+         (!? !.__source
+             (with-stream-string s .!
+               (read s)))))
+    '(defun function-body (x)))
 
 (defun function-source (x)
   (alet (function|symbol-function x)
