9571734ce9c30fa6a7402ea60318100e597d7d42
Sven Michael Klose <pixel@copei.de>
2016-10-03 15:11:07 +0200

    Output less code.

--- environment/stage1/dotimes.lisp
+++ environment/stage1/dotimes.lisp
@@ -1,10 +1,13 @@
 ; tré – Copyright (c) 2005–2008,2010–2012,2015 Sven Michael Klose <pixel@copei.de>
 
+(defun dotimes-error-negative (x)
+  (error "DOTIMES: Number of iterations is negative. It's ~A." x))
+
 (defmacro dotimes ((iter times &rest result) &body body)
   (let g (gensym)
     `(let ,g (integer ,times)
        (? (integer< ,g 0)
-          (error "DOTIMES: Number of iterations is negative. It's ~A." ,g))
+          (dotimes-error-negative ,g))
        (do ((,iter 0 (integer+ 1 ,iter)))
 	       ((integer== ,iter ,g) ,@result)
 	     ,@body))))
