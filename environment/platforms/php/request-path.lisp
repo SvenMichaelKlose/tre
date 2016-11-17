2424bfb360ea304889edeef2b8dc0841ddfc740b
Sven Michael Klose <pixel@copei.de>
2016-11-17 10:58:41 +0100

    Weed out empty string components.

--- environment/platforms/php/request-path.lisp
+++ environment/platforms/php/request-path.lisp
@@ -1,11 +1,11 @@
-;;;;; tré – Copyright (c) 2012–2014 Sven Michael Klose <pixel@copei.de>
+; tré – Copyright (c) 2012–2014,2016 Sven Michael Klose <pixel@copei.de>
 
 (defvar *request-path-offset* nil)
 (defvar *base-url* nil)
 
 (defun parse-request-path ()
   (with (path  (%%%href *_SERVER* "SCRIPT_NAME")
-         comp  (path-pathlist path)
+         comp  (remove-if #'empty-string? (path-pathlist path))
          ofs   (? comp
                   (-- (length comp))
                   0))
@@ -14,10 +14,10 @@
 
 (parse-request-path)
 
-(dont-obfuscate parse_url)
-
 (defun request-uri () 
   (aref *_SERVER* "REQUEST_URI"))
+
+(dont-obfuscate parse_url)
 
 (defun parse-url ()
   (parse_url (request-uri)))
@@ -26,4 +26,4 @@
   (aref (parse-url) "path"))
 
 (defun request-path-components ()
-  (subseq (path-pathlist (request-uri)) *request-path-offset*))
+  (subseq (remove-if #'empty-string? (path-pathlist (request-uri))) *request-path-offset*))
