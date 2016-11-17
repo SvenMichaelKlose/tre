3fcb0726bf93bad477e1d1f7e0a9e360997fc717
Sven Michael Klose <pixel@copei.de>
2016-11-17 11:08:05 +0100

    Quick makefile for testing code snippets with the JavaScript back end.

---
+++ makefiles/snippet-js.lisp
@@ -0,0 +1,13 @@
+; tré – Copyright (c) 2008–2016 Sven Michael Klose <pixel@copei.de>
+
+(= (transpiler-dump-passes? *js-transpiler*) nil)
+
+(unix-sh-mkdir "compiled" :parents t)
+(make-project "tré JavaScript back end snippet for testing"
+              `((toplevel . ((print 'add-your-test-here))))
+              :transpiler  (aprog1 (copy-transpiler *js-transpiler*)
+                             (= (transpiler-configuration ! :save-sources?) nil))
+              :emitter     [(format t "Writing to 'compiled/snippet.html'…~F")
+                            (make-html-script "compiled/snippet.html" _)
+                            (terpri)])
+(quit)
