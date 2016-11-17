bfc4aeb9be30fd61137016d1419429e048908699
Sven Michael Klose <pixel@copei.de>
2016-11-17 10:58:04 +0100

    Also have *DEFAULT-LISTPROP* here.

--- environment/transpiler/targets/php/core/cons.lisp
+++ environment/transpiler/targets/php/core/cons.lisp
@@ -1,6 +1,8 @@
-; tré – Copyright (c) 2008–2011,2013–2014 Sven Michael Klose <pixel@copei.de>
+; tré – Copyright (c) 2008–2011,2013–2014,2016 Sven Michael Klose <pixel@copei.de>
 
 (dont-obfuscate *cars* *cdrs* *cprd* *cons-id* a d sa sd)
+
+(defvar *default-listprop* nil)
 
 (defnative cons (x y)
   (new __cons x y))
