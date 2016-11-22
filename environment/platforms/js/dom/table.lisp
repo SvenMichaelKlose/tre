4c132214b3ca64d2e357db13e52ba8aabecec5cc
Sven Michael Klose <pixel@copei.de>
2016-11-22 16:21:20 +0100

    Fixed getters and cleaned up.

--- environment/platforms/js/dom/table.lisp
+++ environment/platforms/js/dom/table.lisp
@@ -1,19 +1,16 @@
 ; tré – Copyright (c) 2009–2013,2015–2016 Sven Michael Klose <pixel@copei.de>
 
 (defun table-get-rows (x)
-  ((x.get "tbody").child-list))
+  (((x.get "<table").get "tbody").child-list))
 
 (defun table-num-columns (x)
-  (x.get "tr").children.length)
+  ((x.get "<table").get "tr").children.length)
 
 (defun table-num-rows (x)
-  (x.get "table").children.length)
+  (x.get "<table").children.length)
 
 (defun table-get-first-row (x)
-  (x.get "tbody").first-child)
-
-(defun table-get-column-index (cell)
-  (cell.get-index))
+  ((x.get "<table").get "tbody").first-child)
 
 (defun table-get-column-0 (x idx)
   (& x
@@ -21,15 +18,14 @@
         (table-get-column-0 x.next-sibling idx))))
 
 (defun table-get-column (x)
-  (table-get-column-0 (table-get-first-row x) (table-get-column-index x)))
-
-(defun table-insert-column-0 (index rows column after?)
-  (when rows
-    (((car rows).get-child-at index).insert-next-to column. after?)
-    (table-insert-column-0 index .rows .column after?)))
+  (table-get-column-0 (table-get-first-row x) (x.get-index)))
 
 (defun table-insert-column (drop-cell column after?)
   (assert (== (length (table-get-rows drop-cell))
 		      (length column))
 	  	  (error "column not the same"))
-  (table-insert-column-0 (table-get-column-index drop-cell) (table-get-rows drop-cell) column after?))
+  (let index (drop-cell.get-index)
+    (mapcar #'((row column-cell)
+                ((row.get-child-at index).insert-next-to column-cell after?))
+            (table-get-rows drop-cell)
+            column)))
