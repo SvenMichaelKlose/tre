3579c75a222b0bcfb598f719bf53800edd6d723a
Sven Michael Klose <pixel@copei.de>
2016-11-25 13:38:58 +0100

    No special deals with empty strings.

--- environment/platforms/shared/url/path-append.lisp
+++ environment/platforms/shared/url/path-append.lisp
@@ -1,7 +1,5 @@
 ; tré – Copyright (c) 2010–2014,2016 Sven Michael Klose <pixel@copei.de>
 
 (defun path-append (dir &rest path-components)
-  (? (empty-string? dir)
-     (apply #'path-append .path-components)
-     (@ (x path-components dir)
-       (= dir (+ (trim-tail dir "/") "/" (trim x "/"))))))
+  (@ (x path-components dir)
+    (= dir (+ (trim-tail dir "/") "/" (trim x "/")))))
