cb69105bf60b1115083cf9e48f56d412d08e7299
Sven Michael Klose <pixel@copei.de>
2016-12-07 04:49:21 +0100

    Only make assertion if *ASSERT?* is not NIL.

--- environment/stage3/assert.lisp
+++ environment/stage3/assert.lisp
@@ -1,5 +1,6 @@
-;;;; tré – Copyright (c) 2008 Sven Michael Klose <pixel@copei.de?
+; tré – Copyright (c) 2008,2016 Sven Michael Klose <pixel@copei.de?
 
 (defun make-assertion (x &optional (txt "") (args nil))
-  `(unless ,x
-	 (error (+ "Assertion failed: " ,txt) ,@args)))
+  (& *assert?*
+     `(unless ,x
+	    (error (+ "Assertion failed: " ,txt) ,@args))))
