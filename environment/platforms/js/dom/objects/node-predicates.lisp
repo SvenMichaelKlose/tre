a0f0573674407560e1fa27bccbfc5b033a329366
Sven Michael Klose <pixel@copei.de>
2016-12-07 18:59:18 +0100

    == does not compare strings anymore.

--- environment/platforms/js/dom/objects/node-predicates.lisp
+++ environment/platforms/js/dom/objects/node-predicates.lisp
@@ -1,11 +1,11 @@
-;;;;; tré – Copyright (c) 2008–2012 Sven Michael Klose <pixel@copei.de>
+; tré – Copyright (c) 2008–2012,2016 Sven Michael Klose <pixel@copei.de>
 
 (dont-obfuscate node-type)
 
 (defmacro define-dom-node-predicate (which type)
   `(defun ,($ which '?) (x)
      (& (object? x)
-	    (== ,type x.node-type))))
+	    (string== ,(string type) x.node-type))))
 
 (mapcar-macro x '((element 1) (text 3) (comment 8) (document 9))
   `(define-dom-node-predicate ,x. ,.x.))
