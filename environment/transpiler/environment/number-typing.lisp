19fbfac6bf7c62a75d9e40951cdf9993f97bc539
Sven Michael Klose <pixel@copei.de>
2016-12-17 18:28:40 +0100

    Now that characters aren't numbers anymore, translate ops to native ones.

--- environment/transpiler/environment/number-typing.lisp
+++ environment/transpiler/environment/number-typing.lisp
@@ -1,18 +1,16 @@
 ; tré – Copyright (c) 2008–2016 Sven Michael Klose <pixel@hugbox.org>
 
-(defmacro + (&rest x)
-  (opt-string-concat x '+))
+(defmacro + (&rest x)       (opt-string-concat x '+))
+(defmacro - (&rest x)       `(%%%- ,@x))
+(defmacro * (&rest x)       `(%%%* ,@x))
+(defmacro / (&rest x)       `(%%%/ ,@x))
+(defmacro mod (&rest x)     `(%%%mod ,@x))
+(defmacro number+ (&rest x) `(%%%+ ,@x))
 
 (defmacro integer- (&rest x)
   (? .x
      `(%%%- ,@x)
      `(%%native "(-" ,x. ")")))
-
-(defmacro integer1+ (x)
-  `(%%%+ ,x 1))
-
-(defmacro integer1- (x)
-  `(%%%- ,x 1))
 
 (defmacro def-integer-op (op)
   `(defmacro ,($ 'integer op) (&rest x)
