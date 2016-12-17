bf66698878767171abe500fc1a857ac084709ead
Sven Michael Klose <pixel@copei.de>
2016-12-17 18:26:17 +0100

    Deal with NIL to add.

--- environment/platforms/shared/url/path-append.lisp
+++ environment/platforms/shared/url/path-append.lisp
@@ -1,5 +1,5 @@
 ; tré – Copyright (c) 2010–2014,2016 Sven Michael Klose <pixel@copei.de>
 
 (defun path-append (dir &rest path-components)
-  (@ (x path-components dir)
+  (@ (x (remove-if #'not path-components) dir)
     (= dir (+ (trim-tail dir "/") "/" (trim x "/")))))
