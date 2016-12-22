2f74a4e950ca5ad877c458cc54d82bb0a018b061
Sven Michael Klose <pixel@copei.de>
2016-12-22 04:06:51 +0100

    SUBSEQ test added,

--- environment/stage2/subseq-tests.lisp
+++ environment/stage2/subseq-tests.lisp
@@ -12,6 +12,10 @@
   ((list-subseq '(1 2 3 4) 2))
   '(3 4))
 
+(define-test "SUBSEQ returns head"
+  ((subseq "lisp" 1))
+  "isp")
+
 (define-test "SUBSEQ returns NIL when totally out of range"
   ((subseq "lisp" 10))
   nil)
