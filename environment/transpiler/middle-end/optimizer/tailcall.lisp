e09f6a73a1b0bd939b46461e0b85c3ce51b452ad
Sven Michael Klose <pixel@copei.de>
2016-12-22 11:38:42 +0100

    Code redux.

--- environment/transpiler/middle-end/optimizer/tailcall.lisp
+++ environment/transpiler/middle-end/optimizer/tailcall.lisp
@@ -1,15 +1,10 @@
 ; tré – Copyright (c) 2010–2014,2016 Sven Michael Klose <pixel@copei.de>
 
-(defun atom&integer== (a b)
-  (& (number? a)
-     (number? b)
-     (integer== a b)))
-
 (defun function-exits? (x)
   (alet x.
     (?
 	  (not x)          t
-	  (%%go? !)        (!? (member .!. .x :test #'atom&integer==)
+	  (%%go? !)        (!? (member .!. .x)
 		                   (function-exits? .!))
 	  (| (vm-jump? !)
 	     (%=? !))      nil
@@ -27,7 +22,6 @@
 
 (defun opt-tailcall-fun (l body front-tag)
   (with-lambda name args dummy-body l 
-    args dummy-body
     (& body
        (? (& (%=-funcall-of? body. name)
              (function-exits? .body))
@@ -38,7 +32,6 @@
              (opt-tailcall-fun l .body front-tag))))))
 
 (metacode-walker opt-tailcall (x)
-  :if-named-function
-    (with-compiler-tag front-tag
-      `(,front-tag
-        ,@(opt-tailcall-fun x. (lambda-body x.) front-tag))))
+  :if-named-function (with-compiler-tag front-tag
+                       `(,front-tag
+                         ,@(opt-tailcall-fun x. (lambda-body x.) front-tag))))
