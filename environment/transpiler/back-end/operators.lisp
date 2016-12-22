24b5146ea82d96238cfe45b22c762f8c4d12e3d8
Sven Michael Klose <pixel@copei.de>
2016-12-22 12:18:05 +0100

    Removed debug code.

--- environment/transpiler/back-end/operators.lisp
+++ environment/transpiler/back-end/operators.lisp
@@ -1,11 +1,9 @@
-; tré – Copyright (c) 2008–2009,2011–2015 Sven Michael Klose <pixel@copei.de>
+; tré – Copyright (c) 2008–2009,2011–2016 Sven Michael Klose <pixel@copei.de>
 
 (defmacro define-transpiler-infix (tr name)
   (print-definition `(define-transpiler-infix ,tr ,name))
-  (print
   `(define-expander-macro (transpiler-codegen-expander ,tr) ,name (x y)
      `(%%native ,,x " " ,(downcase (string name)) " " ,,y)))
-  )
 
 (defun transpiler-binary-expand (op x)
   (? .x
