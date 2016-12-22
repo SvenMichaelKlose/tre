a2f2a93bb6ed0a5076b771a65668997d630f5568
Sven Michael Klose <pixel@copei.de>
2016-12-22 12:18:19 +0100

    Fixed definition order.

--- environment/transpiler/targets/php/main.lisp
+++ environment/transpiler/targets/php/main.lisp
@@ -1,5 +1,6 @@
 ; tré – Copyright (c) 2005–2011,2013–2015 Sven Michael Klose <pixel@copei.de>
 
+(env-load "transpiler/targets/php/core.lisp")
 (env-load "transpiler/targets/php/literals.lisp")
 (env-load "transpiler/targets/php/argument-filter.lisp")
 (env-load "transpiler/targets/php/setter-filter.lisp")
@@ -8,4 +9,3 @@
 (env-load "transpiler/targets/php/class.lisp")
 (env-load "transpiler/targets/php/codegen.lisp")
 ;(env-load "transpiler/targets/php/codegen-inline.lisp")
-(env-load "transpiler/targets/php/core.lisp")
