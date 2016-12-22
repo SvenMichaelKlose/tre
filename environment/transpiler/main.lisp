e90c51773e83c0b2452e6e6ea167063007f07e50
Sven Michael Klose <pixel@copei.de>
2016-12-22 12:17:37 +0100

    Don't have COMPILE*ENVIRONMENT in CL back end.

--- environment/transpiler/main.lisp
+++ environment/transpiler/main.lisp
@@ -10,6 +10,6 @@
 (env-load "transpiler/import.lisp")
 (env-load "transpiler/compile.lisp")
 (env-load "transpiler/targets/main.lisp")
-(env-load "transpiler/compile-environment.lisp" :c :cl)
+(env-load "transpiler/compile-environment.lisp" :c)
 ;(env-load "transpiler/eval.lisp" :c)
 (env-load "transpiler/make-project.lisp" :c :cl)
