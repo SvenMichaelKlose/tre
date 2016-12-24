f7698603430034d66c7b1add457107dd90c09e32
Sven Michael Klose <pixel@copei.de>
2016-12-24 10:34:59 +0100

    Removed unused code.

--- environment/transpiler/front-end/main.lisp
+++ environment/transpiler/front-end/main.lisp
@@ -1,8 +1,6 @@
-; tré transpiler – Copyright (c) 2005–2015 Sven Michael Klose <pixel@hugbox.org>
+; tré transpiler – Copyright (c) 2005–2016 Sven Michael Klose <pixel@hugbox.org>
 
 (env-load "transpiler/front-end/arith-wrappers.lisp" :c)
-
-(env-load "transpiler/front-end/cpr-count.lisp")
 (env-load "transpiler/front-end/wrap-atoms.lisp")
 (env-load "transpiler/front-end/gather-imports.lisp")
 (env-load "transpiler/front-end/unassign-lambdas.lisp")
