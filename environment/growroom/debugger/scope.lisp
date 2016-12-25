7e29e60cdda591473618512958f0dfd2ea4da93f
Sven Michael Klose <pixel@copei.de>
2016-12-25 08:45:37 +0100

    Removed outdated code.

--- environment/growroom/debugger/scope.lisp
+++
@@ -1,8 +0,0 @@
-; tré – Copyright (c) 2014–2015 Sven Michael Klose <pixel@copei.de>
-
-(def-funinfo make-scope (funinfo)
-  `(div :class "scope"
-     ,@(@ [`(div :class "variable"
-              (div ,(symbol-name _))
-              (div "not available"))]
-          (+ args vars scoped-vars))))
