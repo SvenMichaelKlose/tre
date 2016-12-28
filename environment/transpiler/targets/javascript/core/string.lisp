b42d2bbf3cfdf6154b81fd7b2bcfbb822b974ca3
Sven Michael Klose <pixel@copei.de>
2016-12-28 06:23:43 +0100

    UPCASE, DOWNCASE: No need to check if argument is not NIL.

--- environment/transpiler/targets/javascript/core/string.lisp
+++ environment/transpiler/targets/javascript/core/string.lisp
@@ -1,19 +1,19 @@
 (js-type-predicate %string? "string")
 
-(defun string? (x)
+(fn string? (x)
   (| (%string? x)
      (instanceof x (%%native "String"))))
 
-(defun string-concat (&rest x)
+(fn string-concat (&rest x)
   (alet (make-array)
     (@ (i x (!.join ""))
       (& i (!.push i)))))
 
-(defun %elt-string (seq idx)
+(fn %elt-string (seq idx)
   (& (%%%< idx seq.length)
      (code-char (seq.char-code-at idx))))
 
-(defun string== (x &rest y)
+(fn string== (x &rest y)
   (@ (i y t)
     (| (%%%== x i)
        (return))))
@@ -21,18 +21,18 @@
 (defmacro string== (x &rest y)
   `(%%%== ,x ,@y))
 
-(defun upcase (x)
-  (& x (x.to-upper-case)))  ; TODO: No need to check if not NIL.
+(fn upcase (x)
+  (x.to-upper-case))
 
-(defun downcase (x)
-  (& x (x.to-lower-case)))
+(fn downcase (x)
+  (x.to-lower-case))
 
-(defun string-subseq (seq start &optional (end 99999))
+(fn string-subseq (seq start &optional (end 99999))
   (unless (& (< (- (length seq) 1) start)
              (< start end))
-    (? (== start end)
+    (? (== start end)   ; TODO: Should return NIL.
 	   ""
        (seq.substr start (- end start)))))
 
-(defun number-string (x)
+(fn number-string (x)
   (*String x))
