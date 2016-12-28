2adbe6d537a8dc253731686dc6f9cefcca25f3c8
Sven Michael Klose <pixel@copei.de>
2016-12-28 06:20:25 +0100

    Removed RPLAC* breakpoints. Target debuggers do the right job nowadays.

--- environment/transpiler/targets/javascript/core/late-cons.lisp
+++ environment/transpiler/targets/javascript/core/late-cons.lisp
@@ -1,51 +1,34 @@
-(defun car (x)
+(fn car (x)
   (? x
      x._
      (assert (not x) "Cons or NIL expected instead of ~A." x)))
 
-(defun cdr (x)
+(fn cdr (x)
   (? x
      x.__
      (assert (not x) "Cons or NIL expected instead of ~A." x)))
 
-(defun cpr (x)
+(fn cpr (x)
   (? x
      x._p
      (assert (not x) "Cons or NIL expected instead of ~A." x)))
 
-(defvar *rplaca-breakpoints* nil)
-(defvar *rplacd-breakpoints* nil)
-(defvar *rplacp-breakpoints* nil)
-
-(defmacro when-rplac-breakpoints (&body body)
-  (when (configuration :rplac-breakpoints)
-    body))
-
-(defun rplaca (x val)
-  (declare type cons x)     ; TODO: Declaring the type isn't of much help anymore with modern debuggers.
-  (when-rplac-breakpoints
-    (& (member x *rplaca-breakpoints* :test #'eq)
-       (invoke-debugger)))
+(fn rplaca (x val)
+  (declare type cons x)
   (= x._ val)
   x)
 
-(defun rplacd (x val)
+(fn rplacd (x val)
   (declare type cons x)
-  (when-rplac-breakpoints
-    (& (member x *rplacd-breakpoints* :test #'eq)
-       (invoke-debugger)))
   (= x.__ val)
   x)
 
-(defun rplacp (x val)
+(fn rplacp (x val)
   (declare type cons x)
-  (when-rplac-breakpoints
-    (& (member x *rplacp-breakpoints* :test #'eq)
-       (invoke-debugger)))
   (= x._p val)
   x)
 
-(defun cons? (x)
+(fn cons? (x)
   (& (object? x)
      x.__class
      (%%%== x.__class ,(obfuscated-identifier 'cons))))
