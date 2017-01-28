72697ff078a67babb2909ba999347ebb385c2a64
Sven Michael Klose <pixel@copei.de>
2017-01-28 18:52:19 +0100

    THREAD, FORCE-THREAD-SWITCH: Removed.

--- environment/platforms/shared/continued.lisp
+++ environment/platforms/shared/continued.lisp
@@ -4,14 +4,3 @@
 			 (ensure-list return-values))
            ,@body)
 	   ,@args))
-
-(defmacro thread (return-values (fun &rest args) &body body)
-  `(,fun
-	   #'(,(when return-values
-			 (ensure-list return-values))
-           (wait #'(() ,@body) 0))
-	   ,@args))
-
-(defmacro force-thread-switch (&body body)
-  `(continued nil (wait 0)
-     ,@body))
