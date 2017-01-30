32fa6588829df4655bef963b96ea67cdddad30f3
Sven Michael Klose <pixel@copei.de>
2017-01-30 09:59:54 +0100

    Cleaned up.

--- environment/stage2/search-sequence.lisp
+++ environment/stage2/search-sequence.lisp
@@ -1,24 +1,23 @@
 (functional find position)
 
-(defun %find-if-list (pred seq from-end with-index)
+(fn %find-if-list (pred seq from-end with-index)
   (alet (? from-end
            (reverse seq)
            seq)
     (? with-index
        (let idx 0
-         (adolist !
-           (& (funcall pred ! idx)
-              (return !))
+         (@ (i !)
+           (& (funcall pred i idx)
+              (return i))
            (++! idx)))
-       (adolist !
-         (& (funcall pred !)
-            (return !))))))
+       (@ (i !)
+         (& (funcall pred i)
+            (return i))))))
 
-(defun %find-if-sequence (pred seq start end from-end with-index)
+(fn %find-if-sequence (pred seq start end from-end with-index)
   (& seq (< 0 (length seq))
      (let* ((e (| end (-- (length seq))))
 	 	    (s (| start 0)))
-       ; Make sure the start and end indexes are sane.
        (& (| (& (> s e) (not from-end))
              (& (< s e) from-end))
           (xchg s e))
@@ -32,41 +31,41 @@
            (& (apply pred (. ! (& with-index (list i))))
 		      (return !)))))))
  
-(defun find-if (pred seq &key (start nil) (end nil) (from-end nil) (with-index nil))
+(fn find-if (pred seq &key (start nil) (end nil) (from-end nil) (with-index nil))
   (? (not (atom seq) start end)
      (%find-if-list pred seq from-end with-index)
      (%find-if-sequence pred seq start end from-end with-index)))
 
-(defun find-if-not (pred seq &key (start nil) (end nil) (from-end nil) (with-index nil))
+(fn find-if-not (pred seq &key (start nil) (end nil) (from-end nil) (with-index nil))
   (find-if [not (funcall pred _)] seq :start start :end end :from-end from-end :with-index with-index))
 
-(defun find (obj seq &key (start nil) (end nil) (from-end nil) (test #'eql))
+(fn find (obj seq &key (start nil) (end nil) (from-end nil) (test #'eql))
   (find-if [funcall test _ obj] seq :start start :end end :from-end from-end))
 
-(defun position (obj seq &key (start nil) (end nil) (from-end nil) (test #'eql))
+(fn position (obj seq &key (start nil) (end nil) (from-end nil) (test #'eql))
   (aprog1 nil
     (find-if #'((x i)
                  (& (funcall test x obj)
                     (= ! i)))
              seq :start start :end end :from-end from-end :with-index t)))
 
-(defun position-if (pred seq &key (start nil) (end nil) (from-end nil))
+(fn position-if (pred seq &key (start nil) (end nil) (from-end nil))
   (aprog1 nil
     (find-if #'((x i)
 				  (& (funcall pred x)
 					 (= ! i)))
 			 seq :start start :end end :from-end from-end :with-index t)))
 
-(defun some (pred &rest seqs)
+(fn some (pred &rest seqs)
   (find-if pred (apply #'append seqs)))
 
-(defun every (pred &rest seqs)
+(fn every (pred &rest seqs)
   (@ (seq seqs t)
     (adotimes ((length seq))
       (| (funcall pred (elt seq !))
          (return-from every nil)))))
 
-(defun none? (pred &rest args)
+(fn none? (pred &rest args)
   (apply #'every [not (funcall pred _)] args))
 
 (define-test "FIND finds elements"
