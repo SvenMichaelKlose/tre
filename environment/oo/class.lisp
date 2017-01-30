87a3dbaf7b9319b49eb642a6a2b7a84d05f2f3fc
Sven Michael Klose <pixel@copei.de>
2017-01-30 20:46:19 +0100

    Minor cleanup.

--- environment/oo/class.lisp
+++ environment/oo/class.lisp
@@ -1,17 +1,17 @@
 (defstruct class
-  (name nil)
+  (name    nil)
   (members nil)
   (methods nil)
-  (parent nil))
+  (parent  nil))
 
-(defun class-add-method (cls name code)
+(fn class-add-method (cls name code)
   (acons! name code (class-methods cls)))
 
-(defun class-change-method (cls name code)
+(fn class-change-method (cls name code)
   (= (assoc-value name (class-methods cls)) code))
 
-(defun class-method (cls name)
+(fn class-method (cls name)
   (assoc-value name cls))
 
-(defun class-add-member (cls name)
+(fn class-add-member (cls name)
   (push (list name t) (class-members cls)))
