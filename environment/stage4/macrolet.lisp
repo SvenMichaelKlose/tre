31d586b60acc7b553f5fa6572fb1609358d4de60
Sven Michael Klose <pixel@copei.de>
2017-02-02 00:07:32 +0100

    Removed debug code.

--- environment/stage4/macrolet.lisp
+++ environment/stage4/macrolet.lisp
@@ -1,6 +1,4 @@
 (defmacro macrolet ((&body macros) &body body)
-  (print
   (let e (define-expander 'macrolet)
     (@ (i macros (expander-expand e `(progn ,@body)))
       (set-expander-macro e i. .i. (eval `#'(,.i. ,@..i))))))
-  )
