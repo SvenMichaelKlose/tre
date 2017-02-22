c572ba306e38cd08dec4cfec0040ae90c3373ea2
Sven Michael Klose <pixel@copei.de>
2017-02-22 15:02:12 +0100

    Minor cleanup.

--- environment/transpiler/middle-end/optimizer/funinfos.lisp
+++ environment/transpiler/middle-end/optimizer/funinfos.lisp
@@ -1,41 +1,40 @@
 (fn used-vars ()
-  (alet *funinfo*
+  (!= *funinfo*
     (+ (funinfo-scoped-vars !)
        (intersect (funinfo-vars !) (funinfo-used-vars !) :test #'eq)
        (& (copy-arguments-to-stack?)
           (funinfo-args !)))))
 
 (fn remove-unused-scope-arg (fi)
-  (when (& (not (funinfo-fast-scope? fi))
-           (funinfo-closure-without-free-vars? fi))
-     (= (funinfo-scope-arg fi) nil)
-     (pop (funinfo-args fi))
-     (pop (funinfo-argdef fi))
-     (optimizer-message "; Made ~A a regular function.~%"
-                        (human-readable-funinfo-names fi))))
+  (& (not (funinfo-fast-scope? fi))
+     (funinfo-closure-without-free-vars? fi)
+     {(= (funinfo-scope-arg fi) nil)
+      (pop (funinfo-args fi))
+      (pop (funinfo-argdef fi))
+      (optimizer-message "; Made ~A a regular function.~%" (human-readable-funinfo-names fi))}))
 
 (fn remove-scoped-vars (fi)
-  (when (& (sole? (funinfo-scoped-vars fi))
-           (not (funinfo-place? fi (car (funinfo-scoped-vars fi)))))
-    (optimizer-message "; Unscoping ~A in ~A.~%"
-                       (alet (funinfo-scoped-vars fi) (? .! ! !.))
-                       (human-readable-funinfo-names fi))
-    (= (funinfo-scoped-vars fi) nil)
-    (= (funinfo-scope fi) nil)))
+  (& (sole? (funinfo-scoped-vars fi))
+     (not (funinfo-place? fi (car (funinfo-scoped-vars fi))))
+     {(optimizer-message "; Unscoping ~A in ~A.~%"
+                         (alet (funinfo-scoped-vars fi) (? .! ! !.))
+                         (human-readable-funinfo-names fi))
+      (= (funinfo-scoped-vars fi) nil)
+      (= (funinfo-scope fi) nil)}))
 
 (fn replace-scope-arg (fi)
-  (when (& (funinfo-scope-arg fi)
-           (not (funinfo-fast-scope? fi))
-           (sole? (funinfo-free-vars fi))
-           (not (funinfo-scoped-vars (funinfo-parent fi))))
-    (alet (car (funinfo-free-vars fi))
-      (optimizer-message "; Removing array allocation for sole scoped ~A in ~A.~%"
-                         ! (human-readable-funinfo-names fi))
-      (= (funinfo-free-vars fi) nil)
-      (= (funinfo-scope-arg fi) !)
-      (= (funinfo-argdef fi) (. ! (cdr (funinfo-argdef fi))))
-      (= (funinfo-args fi) (. ! (cdr (funinfo-args fi))))
-      (= (funinfo-fast-scope? fi) t))))
+  (& (funinfo-scope-arg fi)
+     (not (funinfo-fast-scope? fi))
+     (sole? (funinfo-free-vars fi))
+     (not (funinfo-scoped-vars (funinfo-parent fi)))
+     (!= (car (funinfo-free-vars fi))
+       (optimizer-message "; Removing array allocation for sole scoped ~A in ~A.~%"
+                          ! (human-readable-funinfo-names fi))
+       (= (funinfo-free-vars fi) nil)
+       (= (funinfo-scope-arg fi) !)
+       (= (funinfo-argdef fi) (. ! (cdr (funinfo-argdef fi))))
+       (= (funinfo-args fi) (. ! (cdr (funinfo-args fi))))
+       (= (funinfo-fast-scope? fi) t))))
 
 (fn remove-argument-stackplaces (fi)
   (funinfo-vars-set fi (remove-if [& (funinfo-arg? fi _)
@@ -44,20 +43,19 @@
                                   (funinfo-vars fi))))
 
 (fn warn-unused-arguments (fi)
-  (@ (i(funinfo-args fi))
+  (@ (i (funinfo-args fi))
     (| (funinfo-used-var? fi i)
-       (warn "Unused argument ~A of function ~A."
-             i (human-readable-funinfo-names fi)))))
+       (warn "Unused argument ~A of function ~A." i (human-readable-funinfo-names fi)))))
 
 (fn correct-funinfo ()
-  (alet *funinfo*
-;    (when (lambda-export?)
+  (!= *funinfo*
+;    (when (lambda-export?) ; TODO: What? Why is this gone? Probably buggy…
 ;      (remove-unused-scope-arg !)
 ;      ;(remove-scoped-vars !)
 ;      (replace-scope-arg !))
     (funinfo-vars-set ! (intersect (funinfo-vars !) (funinfo-used-vars !) :test #'eq))
-    (when (stack-locals?)
-      (remove-argument-stackplaces !))))
+    (& (stack-locals?)
+       (remove-argument-stackplaces !))))
 
 (fn remove-unused-vars (x)
   (when x
