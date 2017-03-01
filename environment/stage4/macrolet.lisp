96a42ac95d2b53204b41580dd96d08ad0de0b3ac
Sven Michael Klose <pixel@copei.de>
2017-03-01 10:37:46 +0100

    Macro functions need their argument names expanded.

--- environment/stage4/macrolet.lisp
+++ environment/stage4/macrolet.lisp
@@ -1,4 +1,4 @@
 (defmacro macrolet ((&body macros) &body body)
   (let e (define-expander 'macrolet)
     (@ (i macros (expander-expand e `(progn ,@body)))
-      (set-expander-macro e i. .i. (eval `#'(,.i. ,@..i))))))
+      (set-expander-macro e i. .i. (eval `#'(,(argument-expand-names 'macrolet .i.) ,@..i))))))
