7c45767e0b177232cb162e68b3a5fc0d2ab7e4b1
Sven Michael Klose <pixel@copei.de>
2017-03-17 19:00:18 +0100

    LML attributes must be keywords.
    Removed unused LML functions.

--- environment/platforms/shared/lml.lisp
+++ environment/platforms/shared/lml.lisp
@@ -1,30 +1,7 @@
 (fn string-or-cons? (expr)
   (| (string? expr) (cons? expr)))
 
-(fn lml-get-children (x)
-  (& (cons? x)
-     (? (cons? x.)
-        x
-        (lml-get-children .x))))
-
-(fn lml-get-attribute (x name)
-  (& x
-     (unless (cons? x.)
-       (? (eq name x.)
-          (cadr x)
-          (lml-get-attribute .x name)))))
-
-(fn lml-child? (expr)
-  (string-or-cons? expr))
-
-(fn string-or-empty-string (x)
-  (? x (string x) ""))
-
 (fn lml-attr-string (x)
-  (& (cons? x) (error "Cannot take cons as a LML attribute."))
-  (downcase (string-or-empty-string x)))
-
-(fn lml-attr-value-string (x)
-  (? (string? x)
-	 x
-     (lml-attr-string x)))
+  (| (keyword? x)
+     (error "LML attribute keyword expected instead of ~A." x))
+  (downcase (symbol-name x)))
