795acb4a71921958881d16c6ebb5dd00dcaab817
Sven Michael Klose <pixel@copei.de>
2017-03-20 14:00:01 +0100

    MERGE-PROPERTIES: New function to merge associative lists.

--- environment/transpiler/environment/property.lisp
+++ environment/transpiler/environment/property.lisp
@@ -5,3 +5,10 @@
   (& x
      (aprog1 (new)
        (@ [= (aref ! _.) ._] x))))
+
+(fn merge-properties (a b)
+  (aprog1 (new)
+    (@ (i (property-names a))
+      (= (aref ! i) (aref a i)))
+    (@ (i (property-names b))
+      (= (aref ! i) (aref b i)))))
