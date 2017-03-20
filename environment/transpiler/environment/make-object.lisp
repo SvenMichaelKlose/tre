29968636abe87b8fa46ba2572b4a93e02793392a
Sven Michael Klose <pixel@copei.de>
2017-03-20 13:59:32 +0100

    MAKE-OBJECT, NEW, {}: Also takes keywords whose symbol names are converted to downcase as property names.

--- environment/transpiler/environment/make-object.lisp
+++ environment/transpiler/environment/make-object.lisp
@@ -1,4 +1,6 @@
 (fn make-object (&rest x)
   (!= (%%%make-object)
     (@ (i (group x 2) !)
-      (%%%=-aref .i. ! i.))))
+      (%%%=-aref .i. ! (? (symbol? i.)
+                          (downcase (symbol-name i.))
+                          i.)))))
