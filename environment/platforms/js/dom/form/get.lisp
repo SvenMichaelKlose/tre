879217a1b4d25a721fbe04e6d5d0a5728ab81db6
Sven Michael Klose <pixel@copei.de>
2018-12-02 12:44:24 +0100

    FORM-GET-ELEMENTS, FORM-GET-TEXT-ELEMENTS: New functions.

--- environment/platforms/js/dom/form/get.lisp
+++ environment/platforms/js/dom/form/get.lisp
@@ -1,11 +1,11 @@
 (fn form-action-get (x)
   ((x.get "<form").read-attribute "action"))
 
-(fn form-input-element? (x)     ; TODO: Move to predicates.lisp.
-  (& (not (submit-button? x))
-     (x.is? "input, textarea, select")))
+(fn form-get-elements (x)
+  (x.get-list "input, textarea, select"))
 
 (fn form-get-input-elements (x)
-  (+ (remove-if-not #'form-input-element? (x.get-list "input"))
-     (x.get-list "textarea")
-     (x.get-list "select")))
+  (remove-if #'submit-button? (form-get-elements x)))
+
+(fn form-get-text-elements (x)
+  (remove-if-not #'form-text-element? (form-get-elements x)))
--- environment/platforms/js/dom/form/predicates.lisp
+++ environment/platforms/js/dom/form/predicates.lisp
@@ -5,3 +5,7 @@
 (fn submit-button? (x)
   (& (element? x)
      (x.is? "[type=submit]")))
+
+(fn form-text-element? (x)
+  (& (not (submit-button? x))
+     (x.is? "input, textarea")))
