8a6f1a9147ed6b3cf37e1e3ac6c715895afdf897
Sven Michael Klose <pixel@copei.de>
2019-01-04 10:01:35 +0100

    MILLISECONDS-SINCE-1970: Simplified.

--- environment/platforms/php/milliseconds-since-1970.lisp
+++ environment/platforms/php/milliseconds-since-1970.lisp
@@ -1,4 +1,2 @@
 (fn milliseconds-since-1970 ()
-  (!= (explode " " (microtime))
-    (+ (integer (* 1000 (aref ! 1)))
-       (integer (round (* 1000 (aref ! 0)))))))
+  (floor (* (microtime t) 1000)))
