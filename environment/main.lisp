17a6f498e8416954d5d89721d9e2b64a0025aff6
Sven Michael Klose <pixel@copei.de>
2019-01-27 10:43:17 +0100

    Don't include removed doctype string functions.

--- environment/main.lisp
+++ environment/main.lisp
@@ -6,14 +6,13 @@
 (env-load "stage4/main.lisp")
 (env-load "stage5/main.lisp")
 
-(& *tre-has-math*       (env-load "math/main.lisp"))
-(when *tre-has-class*   (env-load "oo/class.lisp"))
+(& *tre-has-math*   (env-load "math/main.lisp"))
+(& *tre-has-class*  (env-load "oo/class.lisp"))
 
 (env-load "transpiler/main.lisp")
 
 (env-load "read-eval-loop.lisp")
 
-(env-load "platforms/shared/html/doctypes.lisp")    ; TODO: Remove these. Think up some tpm or something...
 (env-load "platforms/shared/html/script.lisp")
 (env-load "platforms/shared/lml.lisp")
 (env-load "platforms/shared/lml2xml.lisp")
