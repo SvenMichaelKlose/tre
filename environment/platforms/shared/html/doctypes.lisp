115595f79fc1d010576321e3f570339aa5710180
Sven Michael Klose <pixel@copei.de>
2019-01-27 10:42:23 +0100

    Doctype string returning functions removed.  Can finally stick to HTML5.

--- environment/platforms/shared/html/doctypes.lisp
+++
@@ -1,7 +0,0 @@
-(fn doctype-html-401 () ; TODO: Remove.
-  nil
-  "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\">")
-
-(fn doctype-html-5 () ; TODO: Remove.
-  nil
-  "<!DOCTYPE HTML>")
