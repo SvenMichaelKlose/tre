c450ecb1ad31324d2df6355a0a69c0a769e727c4
Sven Michael Klose <pixel@hugbox.org>
2019-03-11 16:24:57 +0100

    PATH-APPEND: Ignore NIL.

--- environment/stage5/path-append.lisp
+++ environment/stage5/path-append.lisp
@@ -1,3 +1,3 @@
 (fn path-append (dir &rest path-components)
   (@ (x (remove-if #'not path-components) dir)
-    (= dir (string-concat (trim-tail dir "/") "/" (trim x "/")))))
+    (= dir (string-concat (| (trim-tail dir "/") "") "/" (trim x "/")))))
