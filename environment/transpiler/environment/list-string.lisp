06869570b989859ade9efcb98504cefcfd637bd7
Sven Michael Klose <pixel@hugbox.org>
2019-03-25 17:20:14 +0100

    Use MAPCAR instead of @.

--- environment/transpiler/environment/list-string.lisp
+++ environment/transpiler/environment/list-string.lisp
@@ -1,5 +1,5 @@
 (fn list-string-0 (x)
-  (apply #'string-concat (@ #'string x)))
+  (apply #'string-concat (mapcar #'string x)))
 
-(fn list-string (x) ; TODO: Cleanup.
-  (apply #'string-concat (@ #'list-string-0 (group x 16384))))
+(fn list-string (x)
+  (apply #'string-concat (mapcar #'list-string-0 (group x 16384))))
