2ca557403d0092d6e52c4f5685abdc0234fa9208
Sven Michael Klose <pixel@hugbox.org>
2019-03-25 18:56:45 +0100

    Minor cleanup.

--- environment/transpiler/environment/string.lisp
+++ environment/transpiler/environment/string.lisp
@@ -1,10 +1,11 @@
 (fn %=-elt-string (val seq idx)
   (error "Cannot modify strings."))
 
-(fn string (x)      ; TODO: PCASE…
-  (?
-    (string? x)     x
-    (character? x)  (char-string x)
-    (symbol? x)     (symbol-name x)
-    (number? x)     (number-string x)
-    (not x)         "NIL"))
+(fn string (x)
+  (pcase x
+    string?     x
+    character?  (char-string x)
+    symbol?     (symbol-name x)
+    number?     (number-string x)
+    not         "NIL"
+    (error "Don't know how to convert ~A to string." x)))
