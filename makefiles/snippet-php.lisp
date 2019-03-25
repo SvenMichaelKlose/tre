999e48bbe1a08ff6f05ef47848be72696f10121b
Sven Michael Klose <pixel@hugbox.org>
2019-03-25 18:44:33 +0100

    Removed outdated workaround.

--- makefiles/snippet-php.lisp
+++ makefiles/snippet-php.lisp
@@ -2,8 +2,7 @@
 
 (unix-sh-mkdir "compiled" :parents t)
 (make-project "tré PHP  back end snippet for testing"
-              `("environment/stage3/print.lisp" ; TODO: Remove this workaround.
-                (toplevel . ((print 'add-your-test-here))))
+              `((toplevel . ((print 'add-your-test-here))))
               :transpiler  (aprog1 (copy-transpiler *php-transpiler*)
                              (= (transpiler-configuration ! :save-sources?) nil))
               :emitter     [(format t "Writing to 'compiled/snippet.php'…~F")
