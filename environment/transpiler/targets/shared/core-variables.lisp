b4b6c9e054405c279b5d89eee7627eabf8a811be
Sven Michael Klose <pixel@hugbox.org>
2019-04-03 19:30:03 +0200

    Moved +CORE-VARIABLES+ to the only place where it is being used.

--- environment/transpiler/targets/shared/core-variables.lisp
+++
@@ -1,8 +0,0 @@
-(const +core-variables+ '(*universe* *variables* *functions*
-                          *environment-path* *environment-filenames*
-                          *macroexpand* *quasiquote-expand* *dot-expand*
-                          *package* *keyword-package*
-                          *pointer-size* *launchfile*
-                          *assert?* *targets*
-                          *endianess* *cpu-type* *libc-path* *rand-max*
-                          *eval*))
--- makefiles/boot-common-lisp.lisp
+++ makefiles/boot-common-lisp.lisp
@@ -1,5 +1,14 @@
 (load "environment/stage0/config-defaults-cl.lisp")
 
+(const +core-variables+ '(*universe* *variables* *functions*
+                          *environment-path* *environment-filenames*
+                          *macroexpand* *quasiquote-expand* *dot-expand*
+                          *package* *keyword-package*
+                          *pointer-size* *launchfile*
+                          *assert?* *targets*
+                          *endianess* *cpu-type* *libc-path* *rand-max*
+                          *eval*))
+
 (fn cl-packages ()
   `((defpackage "TRE-CORE"
       (:export       ,@(@ #'symbol-name
