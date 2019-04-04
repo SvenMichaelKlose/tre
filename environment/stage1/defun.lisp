6f552b3076268cff6be37d0b1f630a675a388df0
Sven Michael Klose <pixel@hugbox.org>
2019-04-05 00:13:56 +0200

    Removed some kind of argument checking.
    Forgot about it ages ago.  Leave argument checks to the (host)
    compiler.

--- environment/stage1/defun.lisp
+++ environment/stage1/defun.lisp
@@ -1,33 +1,19 @@
 (var *function-sources* nil)
 
-(%defun %defun-arg-keyword (args)
-  (let a args.
-    (let d (& .args .args.)
-      (? (%arg-keyword? a)
-         (? d
-            (? (%arg-keyword? d)
-               (error "Keyword ~A follows keyword ~A." d a))
-            (error "Unexpected end of argument list after keyword ~A." a))))))
-
-(%defun %defun-checked-args (args)
-  (& args
-     (| (%defun-arg-keyword args)
-        (. args. (%defun-checked-args .args)))))
-
 (%defun %defun-name (name)
   (? (symbol? name)
      name
      (? (& (cons? name)
            (eq name. '=))
         (make-symbol (string-concat "=-" (string .name.)))
-;                     (symbol-package (cadr name)))
+;                     (symbol-package (cadr name)))     ; TODO review
         (error "Illegal function name ~A. It must be a symbol or of the form (= symbol)." name))))
 
 (defmacro defun (name args &body body)
   (let name (%defun-name name)
     `(block nil
        (print-definition `(defun ,name ,args))
-       (%defun-quiet ,name ,(%defun-checked-args args)
+       (%defun-quiet ,name ,args
          (block ,name
            (block nil
              ,@(%add-documentation name body))))
