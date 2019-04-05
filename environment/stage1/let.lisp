e8a70f986655a8835f577680db9c6636616ce0d8
Sven Michael Klose <pixel@hugbox.org>
2019-04-05 15:43:38 +0200

    Moved checks from LET* to LET.

--- environment/stage1/let.lisp
+++ environment/stage1/let.lisp
@@ -1,45 +1,26 @@
 (defmacro let (place expr &body body)
   (?
-    (not body)    (error "Body expected.")
-    (cons? place) (error "Place ~A is not an atom." place)
+    (not body)
+      (error "Body expected.")
+    (not (symbol? place))
+      (error "Place ~A is not a symbol." place)
+    (keyword? place)
+      (error "Place ~A: symbol expected instead of a keyword." place)
+    (%arg-keyword? place)
+      (error "Place ~A: symbol expected instead of an argument keyword." place)
     `(#'((,place)
-          ,@body) ,expr)))
-
-(%defun %chk-place (x)
-  (? (%arg-keyword? x)
-     (error "Place ~A is an argument keyword." x)))
-
-(%defun %error-if-not-unique (x)
-  (%simple-map #'((i)
-                    (? (< 1 (count i x))
-                       (error "Place ~A is not unique." i)))
-               x))
-
-(%defun %let-places (x)
-  (%simple-mapcar #'car x))
-
-(%defun %let-chk-places (x)
-  (? (atom x)
-     (error "Assignment list expected instead of atom ~A." x))
-  (%simple-map #'%chk-place x)
-  (%simple-map #'((p)
-                    p
-                    (%error-if-not-unique (%let-places x)))
-               x))
+           ,@body)
+       ,expr)))
 
 (defmacro let* (alst &body body)
   (?
     (not alst)        `(progn
                          ,@body)
-    (not (cdr alst))  (progn
-                        (%let-chk-places alst)
-                        `(let ,(caar alst) ,(cadar alst)
-                            ,@body))
-    (progn
-      (%let-chk-places alst)
-      `(let ,(caar alst) ,(cadar alst)
-         (let* ,(cdr alst)
-           ,@body)))))
+    (not (cdr alst))  `(let ,(caar alst) ,(cadar alst)
+                         ,@body)
+    `(let ,(caar alst) ,(cadar alst)
+       (let* ,(cdr alst)
+         ,@body))))
 
 (defmacro let-if (x expr &body body)
   `(let ,x ,expr
