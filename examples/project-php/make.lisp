4d8b56dc3fc9d5be757c9b0cbfe191199a97fb69
Sven Michael Klose <pixel@hugbox.org>
2019-04-14 10:50:39 +0200

    Have compile-time code for module "l10n".

--- examples/project-php/make.lisp
+++ examples/project-php/make.lisp
@@ -1,6 +1,8 @@
 (const *fallback-language* 'en)
 (const *available-languages* '(en))
 
+(load "tre_modules/l10n/compile-time.lisp")
+
 (make-project "tré PHP only project"
               `("tre_modules/php/json.lisp"
                 "tre_modules/php/log-message.lisp"
