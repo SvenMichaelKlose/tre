06244ae44122a9ec5892395d64940e6f3ec0a43c
Sven Michael Klose <pixel@hugbox.org>
2019-04-16 00:27:29 +0200

    Removed unused +ANONYMOUS-FUNCTION-SOURCES?+.
    Sources are saved by DEFUN only.

--- environment/transpiler/targets/common-lisp/make-lambdas.lisp
+++ environment/transpiler/targets/common-lisp/make-lambdas.lisp
@@ -1,11 +1,6 @@
-; Usually every function keeps its source code.
-; If we do this in SBCL, anonymous functions won't get garbage collected,
-; That's why it's disabled here.
-(const +anonymous-function-sources?+ nil)
-
 (fn make-scoping-function (x)
   (with-gensym g
-    `(labels ((,g ,@(make-lambdas (cadar x))))
+    `(labels ((,g ,@(make-lambdas (cadr x.))))
        (,g ,@(make-lambdas .x)))))
 
 (fn make-anonymous-function (x)
@@ -14,17 +9,17 @@
        `(lambda nil nil)
        `(lambda ,@!))))
 
-(fn lambda-expr-without-lambda-keyword? (x)
+(fn lambda-expr-w/o-lambda-keyword? (x)
   (& (cons? x)
      (eq 'function x.)
      (not (atom .x.))
-     (not (eq 'lambda (car .x.)))))
+     (not (eq 'lambda .x..))))
 
 (fn make-lambdas (x)
   (?
     (eq '&body x)   '&rest
     (atom x)        x
     (eq 'quote x.)  x
-    (lambda-expr-without-lambda-keyword? x.) (make-scoping-function x)
-    (lambda-expr-without-lambda-keyword? x)  (make-anonymous-function x)
+    (lambda-expr-w/o-lambda-keyword? x.) (make-scoping-function x)
+    (lambda-expr-w/o-lambda-keyword? x)  (make-anonymous-function x)
     (@ #'make-lambdas x)))
