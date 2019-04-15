f3e3701c5bd3649c4910a52c9dc424e2a2a67fdc
Sven Michael Klose <pixel@hugbox.org>
2019-04-16 00:09:17 +0200

    Do not test of symbol package is NIL.

--- environment/transpiler/import.lisp
+++ environment/transpiler/import.lisp
@@ -6,9 +6,7 @@
      (codegen-macro? x)))
 
 (fn alien-package? (x)
-  (| (not (symbol-package x))
-     (string== "COMMON-LISP" (package-name (symbol-package x)))
-     (string== "SB-EXT" (package-name (symbol-package x)))))
+  (in? (package-name (symbol-package x)) "COMMON-LISP" "SB-EXT"))
 
 (fn can-import-function? (x)
   (& (symbol? x)
