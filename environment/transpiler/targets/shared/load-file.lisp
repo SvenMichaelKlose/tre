9a32124929b164d5c2a6210ed899bbbc9cab2910
Sven Michael Klose <pixel@hugbox.org>
2019-04-17 14:20:24 +0200

    Removed stale code.
    It was supposed to do something for packaging.

--- environment/transpiler/targets/shared/load-file.lisp
+++ environment/transpiler/targets/shared/load-file.lisp
@@ -2,13 +2,7 @@
   (with-queue q
     (while (seek-char s)
            (queue-list q)
-      (!= (read s)
-        (? (cons? !)
-           (?
-             (eq 'defpackage !.)  (eval `(cl:defpackage ,@.!))
-             (eq 'in-package !.)  (= *package* (make-symbol (symbol-name .!.)))
-             (enqueue q !))
-           (enqueue q !))))))
+      (enqueue q (read s)))))
 
 (fn load-file (name)
   (with-open-file s (open name :direction 'input)
