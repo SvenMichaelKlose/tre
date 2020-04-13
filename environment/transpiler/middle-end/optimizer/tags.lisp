b0dd2d4294fe72a05b4837844bf096939adf1421
Sven Michael Klose <pixel@hugbox.org>
2020-04-14 00:44:41 +0200

    Do not wildly translate all numbers as if they were jump tags.

--- environment/transpiler/middle-end/optimizer/tags.lisp
+++ environment/transpiler/middle-end/optimizer/tags.lisp
@@ -27,33 +27,39 @@
   (with (removed-tags nil
          replace-tag
            #'((old-dest new-dest)
-                (= removed-tags (@ [? (eq ._ old-dest)
-                                      (. _. new-dest)
-                                      _]
-                                   removed-tags)))
+               (= removed-tags (@ [? (eq ._ old-dest)
+                                     (. _. new-dest)
+                                     _]
+                                  removed-tags)))
 
          add-removed-tag
            #'((old-tag new-tag)
-                (replace-tag old-tag new-tag)
-                (acons! old-tag new-tag removed-tags))
+               (replace-tag old-tag new-tag)
+               (acons! old-tag new-tag removed-tags))
 
          reduce-tags
            #'((x)
-                (optimizer reduce-tags
-                  (two-subsequent-tags? a d)
-                    (progn
-                      (add-removed-tag a d.)
-                      (reduce-tags d))
-                  (& (number? a)
-                     (%%go? d.))
-                    (progn
-                      (add-removed-tag a (%%go-tag d.))
-                      (reduce-tags d))))
+               (optimizer reduce-tags
+                 (two-subsequent-tags? a d)
+                   (progn
+                     (add-removed-tag a d.)
+                     (reduce-tags d))
+                 (& (number? a)
+                    (%%go? d.))
+                   (progn
+                     (add-removed-tag a (%%go-tag d.))
+                     (reduce-tags d))))
+
+         translate-tag
+           [| (assoc-value _ removed-tags) _]
 
          translate-tags
-                    ; XXX [| (assoc-value _ removed-tags) _]
-           [maptree [!? (assoc _ removed-tags)
-                        .!
-                        _]
-                    _])
+           #'((x)
+               (optimizer translate-tags
+                 (vm-jump? a)
+                   (. `(,a. ,(translate-tag .a.) ,@..a)
+                      (translate-tags d))
+                 (number? a)
+                   (. (translate-tag a)
+                      (translate-tags d)))))
     (remove-spare-tags (translate-tags (reduce-tags statements)))))
