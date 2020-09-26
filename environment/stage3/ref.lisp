0bbb0c6181cfa31010e8bd1fe42684787bcf08fd
Sven Michael Klose <pixel@hugbox.org>
2020-09-26 11:08:37 +0200

    REF: New function.

---
+++ environment/stage3/ref.lisp
@@ -0,0 +1,9 @@
+; TODO: Argument expander does not support keyword
+; arguments after &REST.
+(fn ref (o &rest indexes)
+  (dolist (i indexes o)
+    (= o (?
+           (cons? o)        (assoc-value i o)
+           (array? o)       (aref o i)
+           (hash-table? o)  (href o i)
+           (object? o)      (oref o i)))))
