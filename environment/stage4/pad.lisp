1bde2510d2522817b64e2ffc2ef53a8e6aeaff6b
Sven Michael Klose <pixel@hugbox.org>
2020-09-26 07:38:43 +0200

    PAD accepts arrays.

--- environment/stage4/pad.lisp
+++ environment/stage4/pad.lisp
@@ -1,4 +1,11 @@
-(fn pad (seq p)
+(fn pad-0 (seq p)
   (!? seq
       (. !.  (& .!
                 (. p (pad .! p))))))
+
+(fn pad (seq p)
+  (pad-0 (?
+           (cons? seq)  seq
+           (array? seq) (array-list seq)
+           seq)
+         p))
