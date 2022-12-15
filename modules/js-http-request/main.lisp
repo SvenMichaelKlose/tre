5b4ca73957e85cfb11327a0883de388d2e77481b
Sven Michael Klose <s.klose@classictic.com>
2022-12-15 15:36:51 +0100

    Add function AJAX.

--- modules/js-http-request/main.lisp
+++ modules/js-http-request/main.lisp
@@ -31,6 +31,23 @@
          (funcall onerror req url data))
       req.response-text)))
 
+(fn ajax (url data &key (onerror nil) (onresult nil) (post? t))
+  (with (req       (new *x-m-l-http-request)
+         listener  [0 & (== 4 req.ready-state)
+                        (? (== 200 req.status)
+                           (funcall onresult req.response)
+                           (funcall onerror req url data))])
+    (& onresult
+       (= req.onreadystatechange listener))
+    (req.open (? post? "POST" "GET") url (& onresult t))
+    (req.set-request-header "Content-type" (+ "application/json; charset=UTF-8; boundary="
+                                              *multipart-boundary*))
+    (req.send (json-encode data))
+    (unless onresult
+      (| (== 200 req.status)
+         (funcall onerror req url data))
+      req.response)))
+
 (fn http-request-error (req url data)
   (error (+ "Connection error: " req.status
             ", URL: " url
