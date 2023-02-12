fcbf2bbe76399c510f11fa1a61d57b1ed50799fd
Sven Michael Klose <pixel@hugbox.org>
2023-02-12 18:03:25 +0100

    Use native bind() instead of BIND.

--- modules/js/event/listener-methods.lisp
+++ modules/js/event/listener-methods.lisp
@@ -1,13 +1,13 @@
 (defmacro add-listener-methods (elm &rest event-types)
+  "For use in constructors or methods. Can only be used once per instance."
   `(progn
      ,@(@ [`((slot-value ,elm 'add-event-listener) ,(downcase (symbol-name _))
-                                                   (= (slot-value this ',($ '_ _)) (bind (slot-value this ',($ '_ _))))
-                                                   ,elm)]
+                                                   ((slot-value (slot-value this ',($ '_ _)) 'bind) this))]
           event-types)))
 
 (defmacro remove-listener-methods (elm &rest event-types)
+  "For use in constructors or methods. Can only be used once per instance."
   `(progn
      ,@(@ [`((slot-value ,elm 'remove-event-listener) ,(downcase (symbol-name _))
-                                                      (slot-value this ',($ '_ _))
-                                                      ,elm)]
+                                                      ((slot-value (slot-value this ',($ '_ _)) 'bind) this))]
           event-types)))
