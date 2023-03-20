6a96030cf57e5f131c44d113ea5cbe8e67913e34
Sven Michael Klose <pixel@hugbox.org>
2023-03-20 09:04:42 +0100

    Bind/unbind event listener methods.

--- modules/js/event/listener-methods.lisp
+++ modules/js/event/listener-methods.lisp
@@ -3,13 +3,16 @@
   `(progn
      ,@(@ [`((slot-value ,elm 'add-event-listener)
                ,(downcase (symbol-name _))
-               ((slot-value (slot-value this ',($ '_ _)) 'bind) this))]
+               (= (slot-value this ',($ '_ _))
+                    ((slot-value (slot-value this ',($ '_ _)) 'bind) this)))]
           event-types)))
 
 (defmacro remove-listener-methods (elm &rest event-types)
   "For use in constructors or methods. Can only be used once per instance."
   `(progn
-     ,@(@ [`((slot-value ,elm 'remove-event-listener)
-               ,(downcase (symbol-name _))
-               ((slot-value (slot-value this ',($ '_ _)) 'bind) this))]
+     ,@(@ [`(progn
+              ((slot-value ,elm 'remove-event-listener)
+                 ,(downcase (symbol-name _))
+                 ((slot-value (slot-value this ',($ '_ _)) 'bind) this))
+              (= (slot-value this ',($ '_ _)) nil))]
           event-types)))
