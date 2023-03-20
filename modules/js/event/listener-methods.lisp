e7f18958e25b4a64a76c6a7f4531fad3349a8d7a
Sven Michael Klose <pixel@hugbox.org>
2023-03-20 09:00:48 +0100

    Narrower formatting.

--- modules/js/event/listener-methods.lisp
+++ modules/js/event/listener-methods.lisp
@@ -1,13 +1,15 @@
 (defmacro add-listener-methods (elm &rest event-types)
   "For use in constructors or methods. Can only be used once per instance."
   `(progn
-     ,@(@ [`((slot-value ,elm 'add-event-listener) ,(downcase (symbol-name _))
-                                                   ((slot-value (slot-value this ',($ '_ _)) 'bind) this))]
+     ,@(@ [`((slot-value ,elm 'add-event-listener)
+               ,(downcase (symbol-name _))
+               ((slot-value (slot-value this ',($ '_ _)) 'bind) this))]
           event-types)))
 
 (defmacro remove-listener-methods (elm &rest event-types)
   "For use in constructors or methods. Can only be used once per instance."
   `(progn
-     ,@(@ [`((slot-value ,elm 'remove-event-listener) ,(downcase (symbol-name _))
-                                                      ((slot-value (slot-value this ',($ '_ _)) 'bind) this))]
+     ,@(@ [`((slot-value ,elm 'remove-event-listener)
+               ,(downcase (symbol-name _))
+               ((slot-value (slot-value this ',($ '_ _)) 'bind) this))]
           event-types)))
