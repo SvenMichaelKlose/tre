7a14ef0c903f4473002cdecd2bf29d3f76481620
Sven Michael Klose <pixel@hugbox.org>
2023-10-30 21:40:11 +0100

    Replace BASE64-ENCODE and BASE64-DECODE.  Add test.

--- modules/shared/base64.lisp
+++ modules/shared/base64.lisp
@@ -1,38 +1,26 @@
 (defvar *base64-key*
         "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=")
 
-(fn base64-encode-char (x)
-  (string (elt *base64-key* x)))
-
 (fn base64-encode (x)
-  (let out ""
-    (let len (length x)
-      (while x
-             out
-        (with (c1 x.
-               c2 (when .x .x.)
-               c3 (when ..x ..x.)
-               e1 (>> c1 2)
-               e2 (bit-or (<< (bit-and c1 3)
-                              4)
-                          (>> (| c2 0) 4))
-               e3 (? c2
-                     (bit-or (<< (bit-and c2 15)
-                                 2)
-                             (>> (| c3 0) 6))
-                     64)
-               e4 (? c3
-                     (bit-and c3 63)
-                     64))
-          (= x ...x)
-          (+! out (base64-encode-char e1)
-                  (base64-encode-char e2)
-                  (base64-encode-char e3)
-                  (base64-encode-char e4)))))))
+  (apply #'string-concat
+         (@ #'list-string
+            (with (enc [elt *base64-key* _])
+              (@ [list (enc
+                         (>> _. 2))
+                       (enc
+                         (bit-or (<< (bit-and _. 3) 4)
+                                 (>> (| ._. 0) 4)))
+                       (enc
+                         (? ._.
+                            (bit-or (<< (bit-and ._. 15) 2)
+                                    (>> (| .._. 0) 6))
+                            64))
+                       (enc
+                         (? .._.
+                            (bit-and .._. 63)
+                            64))]
+                 (group (@ #'char-code (array-list x)) 3))))))
  
-(fn base64-decode-char (x)
-  (position x *base64-key* :test #'==))
-
 (fn base64-compress (x)
   (when x
     (? (| (alphanumeric? x.)
@@ -41,27 +29,20 @@
        (base64-compress .x))))
 
 (fn base64-decode (x)
-  (let out ""
-    (= x (base64-compress x))
-    (let len (length x)
-      (while x out
-        (with (e1 (base64-decode-char x.)
-               e2 (base64-decode-char .x.)
-               e3 (base64-decode-char ..x.)
-               e4 (base64-decode-char ...x.)
-               c1 (bit-or (<< e1 2)
-                          (>> e2 4))
-               c2 (bit-or (<< (bit-and e2 15)
-                              4)
-                          (>> e3 2))
-               c3 (bit-or (<< (bit-and e3 3)
-                              6)
-                          e4))
-              (= x ....x)
-              (+! out (string (code-char c1))
-                      (? (not (== 64 e3))
-                         (string (code-char c2))
-                         "")
-                      (? (not (== 64 e4))
-                         (string (code-char c3))
-                         "")))))))
+  (with (dec [position _ *base64-key* :test #'character==])
+    (apply #'string-concat
+           (@ [list-string (@ [code-char _] _)]
+              (@ [list (bit-or (<< (dec _.) 2)
+                               (>> (dec ._.) 4))
+                       (unless (== 64 (dec .._.))
+                          (bit-or (<< (bit-and (dec ._.) 15) 4)
+                                  (>> (dec .._.) 2)))
+                       (unless (== 64 (dec ..._.))
+                          (bit-or (<< (bit-and (dec .._.) 3) 6)
+                                  (dec ..._.)))]
+                 (group (base64-compress (string-list x)) 4))))))
+
+(!= "This is a test of the base64 functions."
+  (unless (equal ! (base64-decode (base64-encode !)))
+    (error "BASE64-DECODE does not return a string equal to the input of BASE64-ENCODE. ~A instead of ~A."
+           (base64-decode (base64-encode !)) !)))
