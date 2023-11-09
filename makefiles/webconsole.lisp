280665e8ff05111d0992c49ff78af60d4666b9a9
Sven Michael Klose <pixel@hugbox.org>
2023-11-09 15:42:18 +0100

    Compile with MAKE-JS-PROJECT.

--- makefiles/webconsole.lisp
+++ makefiles/webconsole.lisp
@@ -1,45 +1,18 @@
-(load "tre_modules/js/event/names.lisp")
+(load (+ *modules-path* "/js/make-js-project.lisp"))
 
 (= *allow-redefinitions?* t)
 (= *have-compiler?* t)
 
 (unix-sh-mkdir "compiled" :parents t)
-(make-project "tré web console"
-              `(,@(list+ "tre_modules/js/"
-                         '("wait.lisp"))
-
-                ,@(list+ "tre_modules/js/dom/"
-                         '("def-aos.lisp"
-                           "do.lisp"
-                           "objects/node-predicates.lisp"
-                           "objects/visible-node.lisp"
-                           "objects/text-node.lisp"
-                           "objects/element.lisp"
-                           "objects/document.lisp"
-                           "objects/extend.lisp"
-                           "get.lisp"
-                           "form/predicates.lisp"
-                           "form/get.lisp"
-                           "form/element-value.lisp"
-                           "table.lisp"
-                           "viewport.lisp"))
-
-                ,@(list+ "tre_modules/js/event/"
-                         '("native.lisp"
-                           "names.lisp"
-                           "utils.lisp"
-                           "listener-methods.lisp"
-                           "keycodes.lisp"))
-                ,@(list+ "tre_modules/js/"
-                         '("dump.lisp"))
-
-                (toplevel . ((document-extend)
-                             (*event-manager*.init-document document)
-                             (*event-manager*.set-send-natively-by-default? document t)
-                             (format t "Welcome to tré, revision ~A.~%" *tre-revision*))))
-              :transpiler  (aprog1 (copy-transpiler *js-transpiler*)
-                             (= (transpiler-configuration ! :save-sources?) t))
-              :emitter     [(format t "Writing to 'compiled/webconsole.html'…~F")
-                            (make-html-script "compiled/webconsole.html" _)
-                            (terpri)])
+(make-js-project
+  :title
+    "tré web console"
+  :outfile
+    "compiled/webconsole.html"
+  :files
+    `((toplevel . ((document-extend)
+                   (format t "Welcome to tré, revision ~A.~%" *tre-revision*))))
+  :transpiler
+    (aprog1 (copy-transpiler *js-transpiler*)
+      (= (transpiler-configuration ! :save-sources?) t)))
 (quit)
