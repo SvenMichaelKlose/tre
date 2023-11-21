22c4fb90dc60ace3a94a65fd6cd6547917b3d2b4
Sven Michael Klose <pixel@hugbox.org>
2023-11-21 21:44:38 +0100

    Fix number of arguments to FLATTEN.

--- modules/sql-clause/insert.lisp
+++ modules/sql-clause/insert.lisp
@@ -1,15 +1,9 @@
 (fn sql-clause-insert (&key table (fields nil) (default-values-if-empty? nil))
   (flatten
-      "INSERT INTO " table
-      (? (not fields)
-         (? default-values-if-empty?
-            " DEFAULT VALUES"
-            " VALUES()")
-         (flatten
-             " ("
-             (pad (keys fields) ",")
-             ") VALUES ("
-             (pad (@ [+ "\"" (escape-string _) "\""]
-                     (property-values fields))
-                  ",")
-             ")"))))
+      (list "INSERT INTO " table
+            (wben fields
+              (list " (" (pad (keys fields) ",") ") VALUES ("
+                    (pad (@ [+ "\"" (escape-string _) "\""]
+                            (property-values fields))
+                         ",")
+                    ")")))))
