62fe8bb8c0e1f755cf9e46ef509453ea471b93c1
Sven Michael Klose <pixel@hugbox.org>
2023-11-22 21:26:57 +0100

    Rewrote ESCAPE-STRING.

--- environment/stage5/escape-string.lisp
+++ environment/stage5/escape-string.lisp
@@ -1,16 +1,14 @@
-(fn escape-charlist (x &optional (quote-char #\") (chars-to-escape #\"))
-  (when x
-    (= chars-to-escape (ensure-list chars-to-escape))
-    (?
-      (eql #\\ x.)
-        (. #\\ (? (& .x (digit-char? .x.))
-                  (escape-charlist .x quote-char chars-to-escape)
-                  (. #\\ (escape-charlist .x quote-char chars-to-escape))))
-      (eql quote-char x.)
-        (. #\\ (. x. (escape-charlist .x quote-char chars-to-escape)))
-      (member x. chars-to-escape :test #'character==)
-        (. #\\ (. x. (escape-charlist .x quote-char chars-to-escape)))
-      (. x. (escape-charlist .x quote-char chars-to-escape)))))
-
 (fn escape-string (x &optional (quote-char #\") (chars-to-escape #\"))
-  (list-string (escape-charlist (string-list x) quote-char chars-to-escape)))
+  (ensure-list! chars-to-escape)
+  (with (f [when _
+             (?
+               (eql #\\ _.)
+                 (. #\\ (? (& ._ (digit-char? ._.))
+                           (f ._)
+                           (. #\\ (f ._))))
+               (eql quote-char _.)
+                 (. #\\ (. _. (f ._)))
+               (member _. chars-to-escape :test #'character==)
+                 (. #\\ (. _. (f ._)))
+               (. _. (f ._)))])
+    (list-string (f (string-list x)))))
