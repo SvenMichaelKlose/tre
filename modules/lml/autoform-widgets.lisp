02774933cacb02898ba68445d10483560147ad15
Sven Michael Klose <pixel@hugbox.org>
2023-12-18 07:25:29 +0100

    Remove features not compatible to JSON Schema.

--- modules/lml/autoform-widgets.lisp
+++ modules/lml/autoform-widgets.lisp
@@ -1,25 +1,22 @@
 (var *autoform-widgets* nil)
 
 (defmacro def-autoform-widget (args predicate &body body)
-  `(= *autoform-widgets* (+ *autoform-widgets*
-                            (list {:predicate  ,predicate
-                                   :maker      #'(,args ,@body)}))))
+  `(+! *autoform-widgets* (list {:predicate  ,predicate
+                                 :maker      #'(,args ,@body)})))
 
 (defmacro def-editable-autoform-widget (args predicate &body body)
   `(def-autoform-widget ,args [& _.is_editable (funcall ,predicate _)] ,@body))
 
 (fn autoform-value (schema v)
-  (| v
-     (& (defined? schema.default)
-        schema.default)
-     ""))
+  (| v schema.default ""))
 
 
 ;;;;;;;;;;;;;;;;;
 ;;; Editables ;;;
 ;;;;;;;;;;;;;;;;;
 
-(def-editable-autoform-widget (store name schema v) [eql (schema-type _) "enum"]
+(def-editable-autoform-widget (store name schema v)
+                              [eql (schema-type _) "enum"]
   (with (has-default?  (defined? schema.default)
          av            (autoform-value schema v))
     `(select :name       ,name
@@ -30,22 +27,24 @@
                 ,(ref schema.enum _))]
             schema.enum))))
 
-(fn autoform-pattern-required (schema)
-  `(,@(!? schema.pattern      `(:pattern ,!))))
+(fn autoform-pattern (schema)
+  `(,@(!? schema.format `(:pattern ,!))))
 
 (fn make-autoform-input-element (typ store name schema v)
   `(input :type       ,typ
           :name       ,name
-          ,@(autoform-pattern-required schema)
+          ,@(autoform-pattern schema)
           :on-change  ,[store.write {name _.target.value}]
           :value      ,(autoform-value schema v)))
 
-(def-editable-autoform-widget (store name schema v) [in? (schema-type _) "string" "password" "email"]
+(def-editable-autoform-widget (store name schema v)
+                              [in? (schema-type _) "string" "password" "email"]
   (make-autoform-input-element (!= (schema-type schema)
                                  (? (eql ! "string") "text" !))
                                store name schema v))
 
-(def-editable-autoform-widget (store name schema v) [eql (schema-type _) "boolean"]
+(def-editable-autoform-widget (store name schema v)
+                              [eql (schema-type _) "boolean"]
   (let av (? (defined? (slot-value store.data name))
              v
              (store.write {name (autoform-value schema v)}))
@@ -54,10 +53,11 @@
             :on-click  ,[store.write {name _.target.checked}]
             ,@(& av '(:checked "1")))))
 
-(def-editable-autoform-widget (store name schema v) [eql (schema-type _) "string"]
+(def-editable-autoform-widget (store name schema v)
+                              [eql (schema-type _) "string"]
   `(textarea :name       ,name
              :on-change  ,[store.write {name _.target.value}]
-             ,@(autoform-pattern-required schema)
+             ,@(autoform-pattern schema)
      ,(autoform-value schema v)))
 
 
@@ -65,14 +65,16 @@
 ;;; Non-editables ;;;
 ;;;;;;;;;;;;;;;;;;;;;
 
-(def-autoform-widget (store name schema v) [eql (schema-type _) "string"]
+(def-autoform-widget (store name schema v)
+                     [eql (schema-type _) "string"]
   `(pre ,(autoform-value schema v)))
 
-(def-autoform-widget (store name schema v) [identity t]
+(def-autoform-widget (store name schema v)
+                     [identity t]
   (autoform-value schema v))
 
 (fn set-schema-items (value what schema &rest fields)
-  (@ (i (| fields (keys schema)) schema)
+  (@ (i (| fields (keys schema.properties)) schema)
     (= (ref (ref schema i) what) value)))
 
 (fn make-schema-editable (schema &rest fields)
--- modules/lml/autoform.lisp
+++ modules/lml/autoform.lisp
@@ -1,11 +1,6 @@
-;;; TODO: Rename 'field' to 'key'. (pixel)
-
 (defclass (autoform lml-component) (init-props)
   (super (merge {:widgets *autoform-widgets*} init-props))
   this)
-
-(defmethod autoform render ()
-  (error "Class AUTOFORM cannot be used alone. Use derived classes like AUTOFORM-RECORD or AUTOFORM-LIST."))
 
 (finalize-class autoform)
 (declare-lml-component autoform)
@@ -15,54 +10,52 @@
   (super init-props)
   this)
 
-(defmethod autoform-field _value ()
-  (let v (props.store.value props.field)
-    (!? props.schema-item.printer
-        (funcall ! v)
-        v)))
-
-(defmethod autoform-field _render-typed-field ()
-  (@ (widget props.widgets)
-    (& (funcall widget.predicate props.schema-item)
-       (return (funcall widget.maker props.store props.field props.schema-item (_value))))))
+(defmethod autoform-field _render-widget ()
+  (!= props
+    (@ (widget !.widgets)
+      (& (funcall widget.predicate !.schema-item)
+         (return (funcall widget.maker
+                          !.store !.key !.schema-item
+                          (!.store.value !.key)))))))
 
 (defmethod autoform-field render ()
-  ($$ (!= props.field
-        (?
-          (function? !)  (funcall ! props.store.data)
-          (string? !)    (_render-typed-field)
-          !))))
+  (!= props.key
+    ($$
+      (?
+        (function? !)
+          (funcall ! props.store.data)
+        (string? !)
+          (_render-widget)
+        !))))
 
 (finalize-class autoform-field)
 (declare-lml-component autoform-field)
 
 
-(fn field-class-name (x)
-  (+ "field-" x))
-
 (fn autoform-list (props)
-  ($$ `(tr ,@(@ [`(td ,@(& (string? _) `(:class ,(field-class-name _)))
-                    (autoform-field :schema-item  ,(ref props.schema _)
-                                    :field        ,_
-                                    :store        ,props.store))]
-                props.fields))))
+  ($$
+    `(tr
+       ,@(@ [`(td
+                (autoform-field
+                    :schema-item  ,props.schema.items
+                    :key          ,_
+                    :store        ,props.store))]
+            props.fields))))
 
 (declare-lml-component autoform-list)
 
 
 (fn autoform-record (props)
-  (let render-label [!? (ref (ref props.schema _) "title")
-                        (? (object? !)
-                           (ref ! (downcase (symbol-name *language*)))
-                           !)]
-    ($$ `(div :class "autoform autoform-record"
-           ,@(@ [`(label ,@(& (string? _) `(:class ,(field-class-name _)))
-                    (span
-                      ,(? (string? _)
-                          (render-label _)))
-                    (autoform-field :schema-item  ,(ref props.schema _)
-                                    :field        ,_
-                                    :store        ,props.store))]
-                props.fields)))))
+  ($$
+    `(div
+       ,@(@ [!= (ref props.schema.properties _)
+              `(label
+                 (span
+                   ,(| !.title _))
+                 (autoform-field
+                     :schema-item  ,!
+                     :key          ,_
+                     :store        ,props.store))]
+            props.fields))))
 
 (declare-lml-component autoform-record)
