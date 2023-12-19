19e4de1a8313f378df5805bb6c9b70356d1d0f7a
Sven Michael Klose <pixel@hugbox.org>
2023-12-19 15:24:52 +0100

    COPY: Support JSON objects.

--- environment/stage4/copy.lisp
+++ environment/stage4/copy.lisp
@@ -1,6 +1,9 @@
 (functional copy)
 (fn copy (x)
   (?
-    (cons? x)       (copy-list x)
-    (array? x)      (copy-array x)
-    (hash-table? x) (copy-hash-table x)))
+    (cons? x)         (copy-list x)
+    (array? x)        (copy-array x)
+    (json-object? x)  (copy-props x)
+    (hash-table? x)   (copy-hash-table x))
+    (error (+ "COPY can only handle lists, arrays, JSON objects"
+              " and hash tables.")))
