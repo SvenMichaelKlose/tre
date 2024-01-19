30db6c022cb36ff64704ea2e3279a54d954a401f
Sven Michael Klose <pixel@hugbox.org>
2024-01-19 02:43:59 +0100

    Check out new release settings.

--- environment/stage0/config-defaults-cl.lisp
+++ environment/stage0/config-defaults-cl.lisp
@@ -1,22 +1,25 @@
-(%defvar *development?*       t)
-(%defvar *assert?*            t)
+;; Targets to include in environment:
+;; :cl     Common Lisp (sbcl)
+;; :js     JavaScript/ECMAScript (browser + node.js)
+;; :php    PHP
+(%defvar *targets* '(:cl :js :php))
+
+;; Enable run-time type checks,
+(%defvar *assert?* t)
+
+;; Developer mode with various effects.
+(%defvar *development?* nil)
+
+;; Verbosity
 (%defvar *print-definitions?* t)
+(%defvar *print-notes?*       *development?*)
+(%defvar *print-status?*      *development?*)
 
-;; Optional environment sections.
-
+;; Optional environment sections. TODO: Remove.
 (%defvar *tre-has-math*  t) ; Mathematical functions.
 (%defvar *tre-has-class* t) ; STRUCT CLASS.
 
+; TODO: Override by environment variable TRE_MODULES.
 (%defvar *modules-path* "/usr/local/lib/tre/modules/")
 
-;; Transpiler
-
-;;; Targets to include in environment:
-;;; :cl     Common Lisp (sbcl)
-;;; :js     JavaScript/ECMAScript (browser + node.js)
-;;; :php    PHP
-(%defvar *targets*                 '(:cl :js :php))
-
-(%defvar *print-notes?*            t)
-(%defvar *print-status?*           t)
-(%defvar *have-compiler?*          nil)
+(%defvar *have-compiler?* nil) ; TODO: Remove.
