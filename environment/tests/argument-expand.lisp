7cedcc0c5378e91ca3b0810f68c042d268e2c490
Sven Michael Klose <pixel@hugbox.org>
2024-01-20 18:13:31 +0100

    Add test for type-checked argument via DEFTYPE type specifier.

--- environment/tests/argument-expand.lisp
+++ environment/tests/argument-expand.lisp
@@ -8,16 +8,21 @@
           '(a b)))
   t)
 
-(deftest "ARGUMENT-EXPAND works with type"
+(deftest "ARGUMENT-EXPAND works with constant type"
   ((equal (argument-expand nil '((a "foo") b) '("foo" "bar"))
           '((a . "foo") (b . "bar"))))
   t)
 
-(deftest "ARGUMENT-EXPAND works with wrong type"
+(deftest "ARGUMENT-EXPAND works with wrong constant type"
   ((equal (argument-expand nil '((a "foo") b) '("fnord" "bar")
                            :break-on-errors? nil)
           '((a . "foo") (b . "bar"))))
   nil)
+
+(deftest "ARGUMENT-EXPAND works with type"
+  ((equal (argument-expand nil '((a string) b) '("foo" "bar"))
+          '((a . "foo") (b . "bar"))))
+  t)
 
 (deftest "ARGUMENT-EXPAND can handle nested lists"
   ((equal (argument-expand 'test-3 '(a (b c) d) '(23 (2 3) 42))
