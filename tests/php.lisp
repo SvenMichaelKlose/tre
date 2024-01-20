e3f05e63243c0c3e9c8370acaf8b5d1d05cc0f2b
Sven Michael Klose <pixel@hugbox.org>
2024-01-20 18:03:50 +0100

    Fix loading DEFTYPE.

--- tests/php.lisp
+++ tests/php.lisp
@@ -1,15 +1,15 @@
 (unix-sh-mkdir "compiled" :parents t)
 
 (make-project "PHP target test"
-  (list (. 'types '("environment/stage3/type.lisp"))
-        (. 'tests (make-environment-tests))
-        (. 'toplevel '((environment-tests))))
-  :transpiler  (copy-transpiler *php-transpiler*)
-  :emitter     [put-file "compiled/test.php" _])
+    (list "environment/stage3/type.lisp"
+          (. 'tests (make-environment-tests))
+          (. 'toplevel '((environment-tests))))
+    :transpiler  (copy-transpiler *php-transpiler*)
+    :emitter     [put-file "compiled/test.php" _])
 
 (make-project "PHP test unit 1"
-  `("tests/unit-1.lisp")
-  :transpiler  (copy-transpiler *php-transpiler*)
-  :emitter     [put-file "compiled/unit-1.php" _])
+    `("tests/unit-1.lisp")
+    :transpiler  (copy-transpiler *php-transpiler*)
+    :emitter     [put-file "compiled/unit-1.php" _])
 
 (quit)
