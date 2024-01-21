24e0c152d8c602b98130505caef2438210e2a3b8
Sven Michael Klose <pixel@hugbox.org>
2024-01-21 14:22:09 +0100

    Remove nonsensical test.  A character is not a number.

--- environment/stage3/princ.lisp
+++ environment/stage3/princ.lisp
@@ -1,11 +1,9 @@
-(fn number-not-character? (x)
-  (& (not (character? x))
-     (number? x)))
-
 (fn princ (x &optional (str *standard-output*))
   (with-default-stream s str
     (?
-      (number-not-character? x)  (princ-number x s)
-      (symbol? x)                (stream-princ (symbol-name x) s)
+      (number? x)
+        (princ-number x s)
+      (symbol? x)
+        (stream-princ (symbol-name x) s)
       (stream-princ x s))
     x))
