7ee44ba1abf5b8f8c1c767cf868539c7ce6b147b
Sven Michael Klose <pixel@hugbox.org>
2024-01-22 09:58:27 +0100

    DOTIMES: Fix printing faulty index error message.

--- environment/stage1/dotimes.lisp
+++ environment/stage1/dotimes.lisp
@@ -1,12 +1,9 @@
-(fn dotimes-error-negative (x)
-  (error "DOTIMES: Number of iterations is negative. It's ~A." x))
-
 (defmacro dotimes ((iter times &rest result) &body body)
   (let g (gensym)
     `(let ,g (integer ,times)
        ,@(? *assert?*
             `((? (< ,g 0)
-                 (dotimes-error-negative ,g))))
+                 (error "DOTIMES: Number of iterations is negative: ~A." ,g))))
        (do ((,iter 0 (number+ 1 ,iter)))
            ((== ,iter ,g) ,@result)
          ,@body))))
