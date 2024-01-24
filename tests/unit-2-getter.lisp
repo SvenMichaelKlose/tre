dac11f6c681b7fffdd81abfc7d41932ac96a4759
Sven Michael Klose <pixel@hugbox.org>
2024-01-24 09:56:17 +0100

    PHP array class accessors may be overloaded.
    By members AREF, =-AREF and DELETE-AREF.

--- tests/unit-2-getter.lisp
+++ tests/unit-2-getter.lisp
@@ -6,6 +6,13 @@
 (defmethod record aref (name)
   (aref _data name))
 
+(defmethod record =-aref (v name)
+  (= (aref _data name) v)
+  v)
+
+(defmethod record delete-aref (name)
+  (aref _data name))
+
 (finalize-class record)
 
 (| (eq t (%aref (new record) "fnord"))
