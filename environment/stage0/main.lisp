6ef85582d8b3e634aa0ad82ddf8fc1de4c08c600
Sven Michael Klose <pixel@hugbox.org>
2024-01-24 13:20:40 +0100

    Rename file.

--- environment/stage0/main.lisp
+++ environment/stage0/main.lisp
@@ -1,6 +1,6 @@
 (env-load "stage0/config-defaults-cl.lisp" :cl)
 (env-load "stage0/config-defaults-js.lisp" :js)
-(env-load "stage0/primitive-lib.lisp" :cl)
+(env-load "stage0/lib.lisp" :cl)
 (env-load "stage0/macroexpand.lisp")
 (env-load "stage0/quasiquote.lisp")
 (env-load "stage0/print-definition.lisp")
