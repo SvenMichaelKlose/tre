84cb8b84cec13992c7250b584a8e9b6d8862c30e
Sven Michael Klose <pixel@hugbox.org>
2024-01-29 00:39:16 +0100

    Do not thisify symbols in package GLOBAL.

--- environment/transpiler/front-end/thisify.lisp
+++ environment/transpiler/front-end/thisify.lisp
@@ -5,23 +5,30 @@
          (thisify-collect-methods-and-members !))))
 
 (fn thisify-symbol (classdef x exclusions)
-  (? (eq 'this x)
-     '~%this
-     (!? (& classdef
-            (not (member x exclusions :test #'eq))
-            (assoc x classdef))
-         `(%slot-value ~%this ,x)
-         x)))
+  (?
+    (eq 'this x)
+      '~%this
+    (string== "GLOBAL" (package-name (symbol-package x)))
+      (make-symbol (symbol-name x) "TRE")
+    (!? (& classdef
+           (not (member x exclusions :test #'eq))
+           (assoc x classdef))
+        `(%slot-value ~%this ,x)
+        x)))
 
 (fn thisify-list-0 (classdef x exclusions)
   (?
-    (symbol? x)       (thisify-symbol classdef x exclusions)
+    (& x (symbol? x))
+      (thisify-symbol classdef x exclusions)
     (| (atom x)
-       (quote? x))    x
-    (%slot-value? x)  `(%slot-value ,(thisify-symbol classdef .x. exclusions) ,..x.)
-    (lambda? x)       (copy-lambda x :body (thisify-list-0 classdef
-                                                           (lambda-body x)
-                                                           (+ exclusions (lambda-args x))))
+       (quote? x))
+      x
+    (%slot-value? x)
+      `(%slot-value ,(thisify-symbol classdef .x. exclusions) ,..x.)
+    (lambda? x)
+      (copy-lambda x :body (thisify-list-0 classdef
+                                           (lambda-body x)
+                                           (+ exclusions (lambda-args x))))
     (. (? (%slot-value? x.)
           `(%slot-value ,(thisify-list-0 classdef (cadr x.) exclusions)
                         ,(caddr x.))
@@ -29,22 +36,27 @@
        (thisify-list-0 classdef .x exclusions))))
 
 (fn thisify-list (classes x cls exclusions)
-  (thisify-list-0 (thisify-collect-methods-and-members (href classes cls)) x exclusions))
+  (thisify-list-0
+      (thisify-collect-methods-and-members
+          (href classes cls)) x exclusions))
 
 (def-head-predicate %thisify)
 
 (fn thisify (x &optional (classes (defined-classes)) (exclusions nil))
   (?
-    (atom x)        x
-    (%thisify? x.)  (compiler-macroexpand
-                      (transpiler-macroexpand
-                        `((let ~%this this
-                            ,@(| (+ (thisify-list classes (cddr x.) (cadr x.) exclusions)
-                                    (thisify .x classes exclusions))
-                                 '(nil))))))
-    (lambda? x.)    (. (copy-lambda x. :body (thisify (lambda-body x.)
-                                                      classes
-                                                      (+ exclusions (lambda-args x.))))
-                       (thisify .x classes exclusions))
+    (atom x)
+      x
+    (%thisify? x.)
+      (compiler-macroexpand
+          (transpiler-macroexpand
+              `((let ~%this this
+                  ,@(| (+ (thisify-list classes (cddr x.) (cadr x.) exclusions)
+                          (thisify .x classes exclusions))
+                       '(nil))))))
+    (lambda? x.)
+      (. (copy-lambda x. :body (thisify (lambda-body x.)
+                                        classes
+                                        (+ exclusions (lambda-args x.))))
+         (thisify .x classes exclusions))
     (. (thisify x. classes exclusions)
        (thisify .x classes exclusions))))
