084692c27b23a60d1f685b0a2283403b75fa498c
Sven Michael Klose <pixel@hugbox.org>
2024-02-14 20:50:22 +0100

    METACODE-WALKER: Add IF-COND.

--- environment/transpiler/lib/metacode/walker.lisp
+++ environment/transpiler/lib/metacode/walker.lisp
@@ -3,7 +3,7 @@
 (defmacro metacode-walker (name args
                            &key (if-atom nil) (if-cons nil) (if-setq nil)
                                 (if-go nil) (if-go-nil nil) (if-go-not-nil nil)
-                                (if-named-function nil))
+                                (if-go-cond nil) (if-named-function nil))
   (with-cons x r args
     (with-gensym v
       `(fn ,name ,args
@@ -12,12 +12,13 @@
              (+ (?
                   (%native? ,v)
                     (error "%%NATIVE in metacode.")
-                  (atom ,v)             ,(| if-atom `(… ,v))
-                  ,@(!? if-setq         `((%=? ,v) ,!))
-                  ,@(!? if-go           `((%go? ,v) ,!))
-                  ,@(!? if-go-nil       `((%go-nil? ,v) ,!))
-                  ,@(!? if-go-not-nil   `((%go-not-nil? ,v) ,!))
-                  (%comment? ,v)        (… ,v)
+                  (atom ,v)            ,(| if-atom `(… ,v))
+                  ,@(!? if-setq        `((%=? ,v) ,!))
+                  ,@(!? if-go          `((%go? ,v) ,!))
+                  ,@(!? if-go-nil      `((%go-nil? ,v) ,!))
+                  ,@(!? if-go-not-nil  `((%go-not-nil? ,v) ,!))
+                  ,@(!? if-go-cond     `((%go-cond? ,v) ,!))
+                  (%comment? ,v)       (… ,v)
                   (named-lambda? ,v)
                     (with-lambda-funinfo ,v
                       (with-temporary *body* (lambda-body ,v)
