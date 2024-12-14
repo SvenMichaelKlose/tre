bd5ca7be3f91c05e7eececadd92b0d64405a3e65
Sven Michael Klose <pixel@hugbox.org>
2024-12-14 09:41:42 +0100

    Update docker-compose configurations to version 3.

--- examples/project-js-php/make.sh
+++ examples/project-js-php/make.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-mkdir -p compiled/data
+mkdir -p compiled/data/mysql
 cp -r docker docker-compose.yml db-config.php compiled/
 tre make-server.lisp
 tre make-client.lisp
--- examples/project-php/make.sh
+++ examples/project-php/make.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-mkdir -p compiled/data
+mkdir -p compiled/data/mysql
 cp -r docker docker-compose.yml db-config.php compiled/
 tre make.lisp
