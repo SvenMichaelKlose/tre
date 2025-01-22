a4f205d621b17e3e58f142bdc033a0a9e4711277
Sven Michael Klose <pixel@hugbox.org>
2025-01-22 13:47:46 +0100

    Clean up.  Add documentation.

--- modules/lml/event.lisp
+++ modules/lml/event.lisp
@@ -1,15 +1,22 @@
+; Holy Moly!
+
+; All event names prefixed by "ON-".
 (var *lml-hook-attrs* (@ [. (make-keyword (upcase (+ "on-" _))) _] *all-events*))
 
+(fn lml-hook-attr? (x)
+  (assoc-value x *lml-hook-attrs*))
+
 (define-lml-macro %hook (event-names handler child)
+  "Wrapper around single child elements to attach an event listener to it."
   `(%exec ,#'((parent child)
                (@ (i (ensure-list event-names))
                  (child.add-event-listener i handler)))
      ,child))
 
-(fn lml-hook-attr? (x)
-  (assoc-value x *lml-hook-attrs*))
-
 (fn lml-hook (x)
+  "Called by $$, LML-HOOK wraps all elements with \"ON-<event name>\" attributes in %HOOK expressions
+to have event listeners attached to them when generating the DOM.
+I don't doubt the slightest that this could be easier to read. (pixel)"
   (? (atom x)
      x
      (with-queue q
