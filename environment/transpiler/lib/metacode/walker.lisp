0da6a56f64cc359f30200692c96994f204a0664a
Sven Michael Klose <pixel@hugbox.org>
2025-01-25 06:34:42 +0100

    METACODE-WALKER: Use sub-level argument instead of WITH-CONS.

--- environment/transpiler/lib/metacode/walker.lisp
+++ environment/transpiler/lib/metacode/walker.lisp
@@ -1,6 +1,7 @@
 (var *body*)
 
-(defmacro metacode-walker (name args
+(defmacro metacode-walker (name
+                           (x &rest r)
                            &key (if-atom nil)
                                 (if-cons nil)
                                 (if-setq nil)
@@ -9,32 +10,31 @@
                                 (if-go-not-nil nil)
                                 (if-go-cond nil)
                                 (if-named-function nil))
-  (with-cons x r args
-    (with-gensym v
-      `(fn ,name ,args
-         (when ,x
-           (let ,v (car ,x)
-             (+ (?
-                  (%native? ,v)
-                    (error "%%NATIVE in metacode.")
-                  (atom ,v)            ,(| if-atom `(… ,v))
-                  ,@(!? if-setq        `((%=? ,v) ,!))
-                  ,@(!? if-go          `((%go? ,v) ,!))
-                  ,@(!? if-go-nil      `((%go-nil? ,v) ,!))
-                  ,@(!? if-go-not-nil  `((%go-not-nil? ,v) ,!))
-                  ,@(!? if-go-cond     `((%go-cond? ,v) ,!))
-                  (%comment? ,v)       (list ,v)
-                  (named-lambda? ,v)
-                    (with-lambda-funinfo ,v
-                      (with-temporary *body* (lambda-body ,v)
-                        (list (copy-lambda ,v
-                                  :body ,(| if-named-function
-                                            `(,name (lambda-body ,v) ,@r))))))
-                  (%collection? ,v)
-                    (list (append (list '%collection (cadr ,v))
-                                  (@ [. _. (,name ._)]
-                                     (cddr ,v))))
-                  (not (metacode-statement? ,v))
-                    (funinfo-error "METACODE-STATEMENT? is NIL for ~A." ,v)
-                  ,(| if-cons `(list ,v)))
-                (,name (cdr ,x) ,@r))))))))
+  (with-gensym v
+    `(fn ,name ,(. x r)
+       (when ,x
+         (let ,v (car ,x)
+           (+ (?
+                (%native? ,v)
+                  (error "%%NATIVE in metacode.")
+                (atom ,v)            ,(| if-atom `(… ,v))
+                ,@(!? if-setq        `((%=? ,v) ,!))
+                ,@(!? if-go          `((%go? ,v) ,!))
+                ,@(!? if-go-nil      `((%go-nil? ,v) ,!))
+                ,@(!? if-go-not-nil  `((%go-not-nil? ,v) ,!))
+                ,@(!? if-go-cond     `((%go-cond? ,v) ,!))
+                (%comment? ,v)       (list ,v)
+                (named-lambda? ,v)
+                  (with-lambda-funinfo ,v
+                    (with-temporary *body* (lambda-body ,v)
+                      (list (copy-lambda ,v
+                                :body ,(| if-named-function
+                                          `(,name (lambda-body ,v) ,@r))))))
+                (%collection? ,v)
+                  (list (append (list '%collection (cadr ,v))
+                                (@ [. _. (,name ._)]
+                                   (cddr ,v))))
+                (not (metacode-statement? ,v))
+                  (funinfo-error "METACODE-STATEMENT? is NIL for ~A." ,v)
+                ,(| if-cons `(list ,v)))
+              (,name (cdr ,x) ,@r)))))))
