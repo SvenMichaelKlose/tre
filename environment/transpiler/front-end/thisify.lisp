ebf6fa6d17c24bcf92c960f8459300b3b4d596d1
Sven Michael Klose <pixel@hugbox.org>
2025-01-30 20:52:18 +0100

    CLASSDEF -> SLOTS

--- environment/transpiler/front-end/thisify.lisp
+++ environment/transpiler/front-end/thisify.lisp
@@ -1,59 +1,59 @@
-(fn thisify-symbol (classdef x exclusions)
+(fn thisify-symbol (slots x exclusions)
   (?
     (eq 'this x)
       '~%this
     (string== "GLOBAL" (package-name (symbol-package x)))
       (make-symbol (symbol-name x) "TRE")
-    (!? (& classdef
+    (!? (& slots
            (not (member x exclusions :test #'eq))
-           (member x classdef))
+           (member x slots))
         `(%slot-value ~%this ,x)
         x)))
 
-(fn thisify-list-0 (classdef x exclusions)
+(fn thisify-list-0 (slots x exclusions)
   (?
     (& x (symbol? x))
-      (thisify-symbol classdef x exclusions)
+      (thisify-symbol slots x exclusions)
     (| (atom x)
        (quote? x))
       x
     (%slot-value? x)
-      `(%slot-value ,(thisify-symbol classdef .x. exclusions) ,..x.)
+      `(%slot-value ,(thisify-symbol slots .x. exclusions) ,..x.)
     (unnamed-lambda? x)
-      (copy-lambda x :body (thisify-list-0 classdef
+      (copy-lambda x :body (thisify-list-0 slots
                                            (lambda-body x)
                                            (+ exclusions (lambda-args x))))
     (. (? (%slot-value? x.)
-          `(%slot-value ,(thisify-list-0 classdef (cadr x.) exclusions)
+          `(%slot-value ,(thisify-list-0 slots (cadr x.) exclusions)
                         ,(caddr x.))
-          (thisify-list-0 classdef x. exclusions))
-       (thisify-list-0 classdef .x exclusions))))
+          (thisify-list-0 slots x. exclusions))
+       (thisify-list-0 slots .x exclusions))))
 
-(fn thisify-list (classes x class-name exclusions)
-  (thisify-list-0 (@ #'%slot-name
-                     (class-and-parent-slot-names (href classes class-name)))
-                  x exclusions))
+(fn thisify-list (x class-name exclusions)
+  (thisify-list-0
+      (@ #'%slot-name
+         (class-and-parent-slot-names (href (defined-classes) class-name)))
+      x exclusions))
 
-(fn thisify-expr (x classes exclusions)
+(fn thisify-expr (x exclusions)
   (compiler-macroexpand
       (transpiler-macroexpand
          `((let ~%this this
-             ,@(| (+ (thisify-list classes (cddr x.) (cadr x.) exclusions)
-                     (thisify .x classes exclusions))
+             ,@(| (+ (thisify-list (cddr x.) (cadr x.) exclusions)
+                     (thisify .x exclusions))
                   '(nil)))))))
 
 (def-head-predicate %thisify)
 
-(fn thisify (x &optional (classes (defined-classes)) (exclusions nil))
+(fn thisify (x &optional (exclusions nil))
   (?
     (atom x)
       x
     (%thisify? x.)
-      (thisify-expr x classes exclusions)
+      (thisify-expr x exclusions)
     (unnamed-lambda? x.)
       (. (copy-lambda x. :body (thisify (lambda-body x.)
-                                        classes
                                         (+ exclusions (lambda-args x.))))
-         (thisify .x classes exclusions))
-    (. (thisify x. classes exclusions)
-       (thisify .x classes exclusions))))
+         (thisify .x exclusions))
+    (. (thisify x. exclusions)
+       (thisify .x exclusions))))
