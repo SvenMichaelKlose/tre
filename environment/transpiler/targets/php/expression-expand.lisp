96b7e2039cb56238843562d9a8d83e1857bae10a
Sven Michael Klose <pixel@hugbox.org>
2025-02-01 11:54:43 +0100

    JS target: Expression expansion with proper file name.

---
+++ environment/transpiler/targets/php/expression-expand.lisp
@@ -0,0 +1,18 @@
+(fn php-expex-add-global (x)
+  (funinfo-var-add (global-funinfo) x)
+  (adjoin! x (funinfo-globals *funinfo*))
+  x)
+
+(fn php-argument-filter (x)
+  (pcase x
+    quote?   (php-expex-add-global (php-compiled-symbol .x.))
+    keyword? (php-expex-add-global (php-compiled-symbol x))
+    x))
+
+(fn php-assignment-filter (x)
+  `(%= ,(php-argument-filter .x.)
+       ,(!= ..x.
+          (? (& (cons? !)
+                (defined-function !.))
+             `(,(compiled-function-name !.) ,@.!))
+             !)))
