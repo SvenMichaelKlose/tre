ca5d5320772d71693815f4e2a94578fdd6134a76
Sven Michael Klose <pixel@hugbox.org>
2025-02-02 00:06:02 +0100

    MACROLET instead of DEFMACRO.

--- environment/transpiler/environment/number.lisp
+++ environment/transpiler/environment/number.lisp
@@ -1,62 +1,64 @@
-(fn number== (x &rest y)
-  (every [%== x _] y))
-
-(defmacro def-simple-op (op)
-  `(fn ,op (&rest x)
-     (let n x.
-       (@ (i .x n)
-         (= n (,($ '% op) n i))))))
-
-(progn
-  ,@(@ [`(def-simple-op ,_)] '(* / mod)))
-
-(fn number+ (&rest x)
-  (let n x.
-    (@ (i .x n)
-      (= n (%+ n i)))))
-
-(defmacro define-generic-transpiler-minus ()
-  (let gen-body `(? .x
-                    (let n x.
-                      (@ (i .x n)
-                        (= n (%- n i))))
-                    (%- x.))
-    `(progn
-       (fn - (&rest x)
-         ,gen-body)
-       (fn number- (&rest x)
-         ,gen-body))))
-
-(define-generic-transpiler-minus)
-
-(defmacro def-generic-transpiler-comparison (name)
-  (let op ($ '% name)
-    `(progn
-       (fn ,name (n &rest x)
-         (@ (i x t)
-           (| (,op n i)
-              (return))
-           (= n i)))
-       (fn ,($ 'character name) (n &rest x)
-         (let n (char-code n)
-           (@ (i x t)
-             (| (,op n (char-code i))
-                (return))
-             (= n i)))))))
-
-(def-generic-transpiler-comparison ==)
-(def-generic-transpiler-comparison <)
-(def-generic-transpiler-comparison >)
-(def-generic-transpiler-comparison <=)
-(def-generic-transpiler-comparison >=)
-
 (fn integer (x)
   (?
     (character? x)  (char-code x)
     (string? x)     (string-integer x)
     (number-integer x)))
 
-(fn << (a b)      (%<< a b))
-(fn >> (a b)      (%>> a b))
-(fn bit-or (a b)  (%bit-or a b))
-(fn bit-and (a b) (%bit-and a b))
+(macrolet
+    ((define-operator (name)
+       `(fn ,name (&rest x)
+          (let n x.
+            (@ (i .x n)
+              (= n (,($ '% name) n i)))))))
+  ,@(@ [`(define-operator ,_)]
+       '(* / mod)))
+
+(macrolet
+    ((define-binary-operator (name)
+       `(fn ,name (a b)
+          (,($ '% name) a b))))
+  ,@(@ [`(define-binary-operator ,_)]
+       '(<< >> bit-or bit-and)))
+
+(fn number+ (&rest x)
+  (let n x.
+    (@ (i .x n)
+      (= n (%+ n i)))))
+
+(macrolet
+    ((define-minus ()
+       (let gen-body `(? .x
+                         (let n x.
+                           (@ (i .x n)
+                             (= n (%- n i))))
+                         (%- x.))
+         `(progn
+            (fn - (&rest x)
+              ,gen-body)
+            (fn number- (&rest x)
+              ,gen-body)))))
+  (define-minus))
+
+(fn number== (x &rest y)
+  (every [%== x _] y))
+
+(macrolet
+    ((define-char-and-number-comparator (name)
+       (let op ($ '% name)
+         `(progn
+            (fn ,name (n &rest x)
+              (@ (i x t)
+                (| (,op n i)
+                   (return))
+                (= n i)))
+            (fn ,($ 'character name) (n &rest x)
+              (let n (char-code n)
+                (@ (i x t)
+                  (| (,op n (char-code i))
+                     (return))
+                  (= n i))))))))
+  (define-char-and-number-comparator ==)
+  (define-char-and-number-comparator <)
+  (define-char-and-number-comparator >)
+  (define-char-and-number-comparator <=)
+  (define-char-and-number-comparator >=))
