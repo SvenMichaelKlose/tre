5ce7cda63b465adc2e4014f5b1272b2726d637f8
Sven Michael Klose <pixel@hugbox.org>
2025-02-02 01:02:51 +0100

    Clean up.

--- environment/transpiler/targets/common-lisp/make-lambdas.lisp
+++ environment/transpiler/targets/common-lisp/make-lambdas.lisp
@@ -1,3 +1,9 @@
+(fn sharp-quote-w/o-lambda? (x)
+  (& (cons? x)
+     (eq 'function x.)
+     (not (atom .x.))
+     (not (eq 'lambda .x..))))
+
 (fn make-scoping-function (x)
   (with-gensym g
     `(labels ((,g ,@(make-lambdas (cadr x.))))
@@ -9,17 +15,13 @@
        `(lambda nil nil)
        `(lambda ,@!))))
 
-(fn lambda-expr-w/o-lambda-keyword? (x)
-  (& (cons? x)
-     (eq 'function x.)
-     (not (atom .x.))
-     (not (eq 'lambda .x..))))
-
 (fn make-lambdas (x)
   (?
-    (eq '&body x)   '&rest
-    (atom x)        x
-    (eq 'quote x.)  x
-    (lambda-expr-w/o-lambda-keyword? x.) (make-scoping-function x)
-    (lambda-expr-w/o-lambda-keyword? x)  (make-anonymous-function x)
+    (eq x '&body)  '&rest
+    (atom x)       x
+    (eq x. 'quote) x
+    (sharp-quote-w/o-lambda? x.)
+      (make-scoping-function x)
+    (sharp-quote-w/o-lambda? x)
+      (make-anonymous-function x)
     (@ #'make-lambdas x)))
