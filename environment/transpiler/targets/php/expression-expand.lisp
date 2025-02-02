51efc9e0eb2ef5129fb93802767bc31c8b98bb14
Sven Michael Klose <pixel@hugbox.org>
2025-02-02 23:16:47 +0100

    PHP-ARGUMENT-FILTER: Pass argument through.

--- environment/transpiler/targets/php/expression-expand.lisp
+++ environment/transpiler/targets/php/expression-expand.lisp
@@ -1,8 +1,8 @@
 (fn php-argument-filter (x)
   (pcase x
     quote?   (funinfo-add-global *funinfo* (php-compiled-symbol .x.))
-    keyword? (funinfo-add-global *funinfo* (php-compiled-symbol x))
-    x))
+    keyword? (funinfo-add-global *funinfo* (php-compiled-symbol x)))
+  x)
 
 (fn php-assignment-filter (x)
   `(%= ,(php-argument-filter .x.)
