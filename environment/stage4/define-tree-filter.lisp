689a0b526d692379355a2dfee480872281bcf86d
Sven Michael Klose <pixel@hugbox.org>
2025-02-03 01:49:36 +0100

    Minor clean-up.

--- environment/stage4/define-tree-filter.lisp
+++ environment/stage4/define-tree-filter.lisp
@@ -1,8 +1,8 @@
 (defmacro define-tree-filter (name args &body body)
-  (let expargs (argument-expand-names name .args)
+  (!= (argument-expand-names name .args)
     `(fn ,name ,args
        (?
          ,@body
          (atom ,args.) ,args.
-         (. (,name (car ,args.) ,@expargs)
-            (,name (cdr ,args.) ,@expargs))))))
+         (. (,name (car ,args.) ,@!)
+            (,name (cdr ,args.) ,@!))))))
