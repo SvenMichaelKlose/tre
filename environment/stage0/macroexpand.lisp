9e9d9430a2e972c5723c9ab8c5dfd2403fbc6f8e
Sven Michael Klose <pixel@hugbox.org>
2025-02-04 13:27:24 +0100

    Remove *CURRENT-MACRO*.
    It's in backtraces.

--- environment/stage0/macroexpand.lisp
+++ environment/stage0/macroexpand.lisp
@@ -1,21 +1,26 @@
-(%defvar *macros* nil)          ; All macros defined in the host environment.
+; All macros defined in the host environment.
+(%defvar *macros* nil)
 
-(%defvar *macro?* nil)          ; Predicate if symbol belongs to a named macro.
-(%defvar *macrocall* nil)       ; Function expanding a macro.
-(%defvar *macroexpand* nil)     ; The function called by DEFMACRO.
+; Predicate if symbol belongs to a named macro.
+(%defvar *macro?* nil)
 
-(%defvar *current-macro* nil)   ; For debugging.
+; Function currently expanding a macro.
+(%defvar *macrocall* nil)
+
+; Current version of MACROEXPAND.
+(%defvar *macroexpand* nil)
 
 (%fn %macroexpand-backquote (x)
   (?
-    (atom x)                     x
-    (atom x.)                    (. x. (%macroexpand-backquote .x))
-    (eq x.. 'quasiquote)         (. (. 'quasiquote
-                                       (%macroexpand (cdr x.)))
-                                    (%macroexpand-backquote .x))
-    (eq x.. 'quasiquote-splice)  (. (. 'quasiquote-splice
-                                       (%macroexpand (cdr x.)))
-                                    (%macroexpand-backquote .x))
+    (atom x) x
+    (atom x.)
+      (. x. (%macroexpand-backquote .x))
+    (eq x.. 'quasiquote)
+      (. (. 'quasiquote (%macroexpand (cdr x.)))
+         (%macroexpand-backquote .x))
+    (eq x.. 'quasiquote-splice)
+      (. (. 'quasiquote-splice (%macroexpand (cdr x.)))
+            (%macroexpand-backquote .x))
     (. (%macroexpand-backquote x.)
        (%macroexpand-backquote .x))))
 
@@ -29,30 +34,32 @@
 
 (%fn %macroexpand (x)
   (?
-    (atom x)                    x
-    (apply *macro?* (list x))   (#'((x)
-                                     (? (cons? x)
-                                        (. x. (%macroexpand-rest .x))
-                                        x))
-                                   (apply *macrocall* (list x)))
-    (eq x. 'quote)              x
-    (eq x. 'backquote)          (. 'backquote (apply *macroexpand-backquote* (list .x)))
-    (eq x. 'quasiquote)         (. 'quasiquote (%macroexpand .x))
-    (eq x. 'quasiquote-splice)  (. 'quasiquote-splice (%macroexpand .x))
+    (atom x) x
+    (apply *macro?* (list x))
+      (#'((x)
+           (? (cons? x)
+              (. x. (%macroexpand-rest .x))
+              x))
+        (apply *macrocall* (list x)))
+    (eq x. 'quote) x
+    (eq x. 'backquote)
+      (. 'backquote (apply *macroexpand-backquote* (list .x)))
+    (eq x. 'quasiquote)
+      (. 'quasiquote (%macroexpand .x))
+    (eq x. 'quasiquote-splice)
+      (. 'quasiquote-splice (%macroexpand .x))
     (. (%macroexpand x.)
        (%macroexpand-rest .x))))
 
 (%fn native-macroexpand (x)
-  (#'((predicate caller current-macro)
+  (#'((predicate caller)
         (setq *macro?*         #'%%macro?
-              *macrocall*      #'%%macrocall
-              *current-macro*  nil)
+              *macrocall*      #'%%macrocall)
         (#'((x)
               (setq *macro?*         predicate
-                    *macrocall*      caller
-                    *current-macro*  current-macro)
+                    *macrocall*      caller)
               x)
           (%macroexpand x)))
-     *macro?* *macrocall* *current-macro*))
+     *macro?* *macrocall*))
 
 (setq *macroexpand* #'native-macroexpand)
