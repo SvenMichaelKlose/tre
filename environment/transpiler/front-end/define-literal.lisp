e935f503d88934d3da81c32958369dce5ed91741
Sven Michael Klose <pixel@hugbox.org>
2025-02-09 00:58:03 +0100

    Clean up.

--- environment/transpiler/front-end/define-literal.lisp
+++ environment/transpiler/front-end/define-literal.lisp
@@ -5,17 +5,16 @@
      x))
 
 (defmacro define-literal (name (x table)
-                                   &key maker
-                                        initializer
-                                        (declaration nil))
+                          &key maker
+                               initializer
+                               (declaration nil))
   (let transpiler-slot `(,($ 'compiled- table 's))
     `(fn ,name (,x)
        (cache (href ,transpiler-slot ,x)
               (aprog1 ,maker
-                (unless (funinfo-var? (global-funinfo) !)
-                  (funinfo-add-var (global-funinfo) !))
+                (funinfo-add-var (global-funinfo) !)
                 ,@(when declaration
-                     `((push (~> ,declaration !)
-                             (global-decls))))
+                    `((push (~> ,declaration !)
+                            (global-decls))))
                 (push `(= ,,! ,(… 'quasiquote initializer))
                       (global-inits)))))))
