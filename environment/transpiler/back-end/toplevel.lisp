1d153665cacce1bb4beebfbc7b5e95f5f692ac48
Sven Michael Klose <pixel@hugbox.org>
2025-02-12 16:46:56 +0100

    Always run PLACE-EXPAND and PLACE-ASSIGN.

--- environment/transpiler/back-end/toplevel.lisp
+++ environment/transpiler/back-end/toplevel.lisp
@@ -1,5 +1,9 @@
-(define-transpiler-end :backend-generate-code
+(define-transpiler-end :backend
     :backend-input             #'identity
+    :make-framed-functions     #'make-framed-functions
+    :place-expand              #'place-expand
+    :place-assign              #'place-assign
+    :warn-unused               #'warn-unused
     :collect-used-functions    [(collect-used-functions _) _]
     :translate-function-names  #'translate-function-names
     :encapsulate-strings       #'encapsulate-strings
@@ -8,19 +12,3 @@
     :codegen-expand            #'codegen-expand
     :convert-identifiers       #'convert-identifiers
     :output-filter             #'flatten)
-
-(define-transpiler-end :backend-make-places
-    :place-expand  #'place-expand
-    :place-assign  #'place-assign
-    :warn-unused   #'warn-unused)
-
-(fn backend-prepare (x)
-  (!= (make-framed-functions x)
-    (? (lambda-export?)
-       (backend-make-places !)
-       !)))
-
-(fn backend (x)
-  (? (enabled-end? :backend)
-     (@ [backend-generate-code (backend-prepare (list _))] x)
-     x))
