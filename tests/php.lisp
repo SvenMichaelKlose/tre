6b76e77e27f36d105afbb47a5f17c351657f1411
Sven Michael Klose <pixel@hugbox.org>
2026-06-20 16:59:30 +0200

    Re-enable PHP target tests

--- tests/php.lisp
+++ tests/php.lisp
@@ -1,12 +1,12 @@
 (unix-sh-mkdir "compiled" :parents t)
 
-;(make-project "PHP target test"
-;    (list "environment/stage3/type.lisp"
-;          (. 'tests (make-environment-tests))
-;          (. 'toplevel '((environment-tests))))
-;    :transpiler  (copy-transpiler *php-transpiler*)
-;    :emitter     [put-file "compiled/test.php" _])
-;
+(make-project "PHP target test"
+    (list "environment/stage3/type.lisp"
+          (. 'tests (make-environment-tests))
+          (. 'toplevel '((environment-tests))))
+    :transpiler  (copy-transpiler *php-transpiler*)
+    :emitter     [put-file "compiled/test.php" _])
+
 (make-project "PHP test unit 1"
     `("tests/unit-1.lisp")
     :transpiler  (aprog1 (copy-transpiler *php-transpiler*)
